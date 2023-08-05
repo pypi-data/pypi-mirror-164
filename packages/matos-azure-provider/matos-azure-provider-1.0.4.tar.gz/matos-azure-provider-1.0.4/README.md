0b3d64e3e9d9ac81553aca64724103ea8d71da70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:50 +0000

    Add package matos-azure-provider/1.0.4/matos-azure-provider-1.0.4.tar.gz

---
+++ packages/matos-azure-provider/matos-azure-provider-1.0.4.tar.gz/matos-azure-provider-1.0.4/README.md
@@ -0,0 +1,7 @@
+[![Test](https://github.com/cloudmatos/matos-azure-provider/actions/workflows/test.yaml/badge.svg?branch=main)](https://github.com/cloudmatos/matos-azure-provider/actions/workflows/test.yaml)
+[![Pylint](https://github.com/cloudmatos/matos-azure-provider/actions/workflows/pylint.yaml/badge.svg?branch=main)](https://github.com/cloudmatos/matos-azure-provider/actions/workflows/pylint.yaml)
+
+# Matos Python Azure provider
+
+This is a python package for azure provider service. 
+It interacts with azure native cloud services api's or third party tools to deduce current status and metadata of underlying resources.
---
+++ packages/matos-azure-provider/matos-azure-provider-1.0.4.tar.gz/matos-azure-provider-1.0.4/src/matos_azure_provider/lib/factory.py
@@ -0,0 +1,28 @@
+from typing import Any, Dict
+
+_PLUGINS: Dict[str, Any] = {}
+
+
+def register(resource_type: str, creator_fn: Any) -> None:
+    """Register a new resource type."""
+    _PLUGINS[resource_type] = creator_fn
+
+
+def fetch_plugins() -> Dict[str, Any]:
+    """fetch all plugins
+
+    Returns:
+        Dict[str, Any]: plugin type dict
+    """
+    return _PLUGINS
+
+
+def create(arguments: Dict[str, Any]) -> Any:
+    """Create a resource of a specific type, given JSON data."""
+    args_copy = arguments.copy()
+    resource_type = args_copy.get("type")
+    try:
+        creator_func = _PLUGINS[resource_type]
+    except KeyError:
+        raise ValueError(f"unknown resource type {resource_type!r}") from None
+    return creator_func(args_copy)
