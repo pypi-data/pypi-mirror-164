89d777108b659a8ff1f721c556c5f94a64b93cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:42 +0000

    Add package veevatools/0.0.27/veevatools-0.0.27.tar.gz

---
+++ packages/veevatools/veevatools-0.0.27.tar.gz/veevatools-0.0.27/salesforce/utilities/__init__.py
@@ -0,0 +1,6 @@
+try:
+    from salesforce.utilities.df_utils import *
+    from salesforce.utilities.async_utils import *
+except:
+    from utilities.df_utils import *
+    from utilities.async_utils import *
