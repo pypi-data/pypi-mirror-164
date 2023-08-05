47a35b8832e61fb649078a3752bdb5e115d85481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:17 +0000

    Add package torch-composer/0.0.1/torch_composer-0.0.1.tar.gz

---
+++ packages/torch-composer/torch_composer-0.0.1.tar.gz/torch_composer-0.0.1/README.md
@@ -0,0 +1,12 @@
+# torch-composer
+Compose pytorch neural networks
+
+```python
+import torch_composer as tc
+
+net = tc.nn.compose()
+```
+
+#### Potential future plans
+
+- Composition of `torch.optim` funcs.
---
+++ packages/torch-composer/torch_composer-0.0.1.tar.gz/torch_composer-0.0.1/torch_composer/_nn_composer/__init__.py
@@ -0,0 +1,6 @@
+
+__module_name__ = "__init__.py"
+__author__ = ", ".join(["Michael E. Vinyard"])
+__email__ = ", ".join(["vinyard@g.harvard.edu",])
+
+from ._compose_nn_sequential import _compose_nn_sequential as compose
