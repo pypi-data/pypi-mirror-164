25532f9fe9ae1bbf50a93dbb76bd01e9e6f928ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:35 +0000

    Add package bingus/0.2.1/bingus-0.2.1.tar.gz

---
+++ packages/bingus/bingus-0.2.1.tar.gz/bingus-0.2.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bingus', 'bingus.environ', 'bingus.file']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'bingus',
+    'version': '0.2.1',
+    'description': 'Many python utilities in one package.',
+    'long_description': '# Bingus: the package\n\nBingus is a library of utility scripts and functions.\n',
+    'author': 'Bruno Robert',
+    'author_email': 'bruno.jeanluke@icloud.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://gitlab.com/brunorobert/bingus',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
