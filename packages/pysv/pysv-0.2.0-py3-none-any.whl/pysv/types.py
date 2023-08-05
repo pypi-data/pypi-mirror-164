d6ad272413834d5505155257c8cde0cc68c262e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:50 +0000

    Add package pysv/0.2.0/pysv-0.2.0-py3-none-any.whl

---
+++ packages/pysv/pysv-0.2.0-py3-none-any.whl/pysv/types.py
@@ -0,0 +1,24 @@
+import enum
+
+
+class DataType(enum.Enum):
+    Bit = "bit"
+    Byte = "byte"
+    ShortInt = "shortint"
+    Int = "int"
+    LongInt = "longint"
+    UByte = "byte unsigned"
+    UShortInt = "shortint unsigned"
+    UInt = "int unsigned"
+    ULongInt = "longint unsigned"
+    Object = "chandle"
+    String = "string"
+    Float = "shortreal"
+    Double = "real"
+    # only for return type
+    Void = "void"
+
+
+class Reference:
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
