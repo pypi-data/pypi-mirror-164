ae417232754edbaa0fe6a9607029c977b26e968e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:19 +0000

    Add package wechaty-plugin-contrib/0.0.18/wechaty-plugin-contrib-0.0.18.tar.gz

---
+++ packages/wechaty-plugin-contrib/wechaty-plugin-contrib-0.0.18.tar.gz/wechaty-plugin-contrib-0.0.18/src/wechaty_plugin_contrib/__init__.py
@@ -0,0 +1,71 @@
+"""import all contrib"""
+
+# Finder & Matcher
+from wechaty_plugin_contrib.finders.finder import FinderOption
+from wechaty_plugin_contrib.finders.contact_finder import ContactFinder
+from wechaty_plugin_contrib.finders.room_finder import RoomFinder
+
+# Plugin List
+from wechaty_plugin_contrib.contrib.ding_dong_plugin import DingDongPlugin
+from wechaty_plugin_contrib.contrib.daily_plugin import (
+    DailyPluginOptions,
+    DailyPlugin
+)
+from wechaty_plugin_contrib.contrib.auto_reply_plugin import (
+    AutoReplyOptions, AutoReplyPlugin,
+    AutoReplyRule
+)
+
+from wechaty_plugin_contrib.contrib.github_webhook_plugin import (
+    GithubHookItem,
+    GithubContentType,
+    GithubWebhookOptions
+)
+
+from wechaty_plugin_contrib.contrib.rasa_rest_plugin import (
+    RasaRestPlugin,
+    RasaRestPluginOptions
+)
+
+from wechaty_plugin_contrib.contrib.room_inviter import (
+    RoomInviterOptions,
+    RoomInviterPlugin
+)
+
+from wechaty_plugin_contrib.contrib.chat_history_plugin import (
+    ChatHistoryPluginOptions,
+    ChatHistoryPlugin
+)
+
+__all__ = [
+    'FinderOption',
+
+    'ContactFinder',
+    'RoomFinder',
+
+    'DingDongPlugin',
+
+    'DailyPluginOptions',
+    'DailyPlugin',
+
+    'RasaRestPluginOptions',
+    'RasaRestPlugin',
+
+    'AutoReplyRule',
+    'AutoReplyOptions',
+    'AutoReplyPlugin',
+
+    'GithubHookItem',
+    'GithubContentType',
+    'GithubWebhookOptions',
+
+    'RasaRestPlugin',
+    'RasaRestPluginOptions',
+
+    'RoomInviterOptions',
+    'RoomInviterPlugin',
+
+    'ChatHistoryPluginOptions',
+    'ChatHistoryPlugin'
+
+]
