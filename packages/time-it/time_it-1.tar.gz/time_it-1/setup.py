5f18c943c3c29e296105bea3874dd0e09093cd9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:41 +0000

    Add package time-it/1/time_it-1.tar.gz

---
+++ packages/time-it/time_it-1.tar.gz/time_it-1/setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='time_it',
+    version='1',
+    license='MIT',
+    author="Code Blanco",
+    author_email='blancotree1@gmail.com',
+    description="Package for timing python",
+    url='https://github.com/CodeBlanco/python-module-time_it',
+    keywords='time it',
+    install_requires=[
+      ],
+
+)
