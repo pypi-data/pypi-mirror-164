b134afaf42fb0c18ac77d3aa406b5339fe1c38bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:06 +0000

    Add package easy-choose/1.1.1/easy_choose-1.1.1-py3-none-any.whl

---
+++ packages/easy-choose/easy_choose-1.1.1-py3-none-any.whl/easy_choose/__init__.py
@@ -0,0 +1,14 @@
+# Import functions from chooser.py
+
+from .chooser import chooser
+from .chooser import sortchooser
+from .chooser import revchooser
+
+# Import functions from tests.py
+
+from .tests import testnumbers
+from .tests import testalphabet_lower
+from .tests import testalphabet_upper
+from .tests import testascii
+
+__version__ = '1.1.1'
