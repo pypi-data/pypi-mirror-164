bd284674b16534a01330bf8921857f66cc8090de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:04 +0000

    Add package cambridge/3.1.0/cambridge-3.1.0.tar.gz

---
+++ packages/cambridge/cambridge-3.1.0.tar.gz/cambridge-3.1.0/cambridge/main.py
@@ -0,0 +1,39 @@
+"""
+Cambridge is a terminal version of Cambridge Dictionary.
+The dictionary data comes from https://dictionary.cambridge.org
+If you're not satisfied with the result, you can try with "-w" flag to look up the word in Merriam-Webster Dictionary.
+"""
+
+import sqlite3
+
+# import sys
+# import os
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from .args import parse_args
+from .cache import DB
+# from .utils import timer
+
+
+# Test fzf preview under development env
+# python main.py l | fzf --preview 'python main.py  -w {}'
+
+# @timer
+def main():
+    try:
+        con = sqlite3.connect(
+            DB, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
+        )
+        cur = con.cursor()
+
+        args = parse_args()
+        args.func(args, con, cur)
+
+        cur.close()
+        con.close()
+
+    except KeyboardInterrupt:
+        print("\nStopped by user")
+
+
+# main()
