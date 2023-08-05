13e6eb5b7d19ba688933d3bc44a0c080932d4abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:05 +0000

    Add package cronaion/1.0.2/cronaion-1.0.2.tar.gz

---
+++ packages/cronaion/cronaion-1.0.2.tar.gz/cronaion-1.0.2/setup.py
@@ -0,0 +1,13 @@
+"""Setup file for package."""
+import setuptools
+
+setuptools.setup(
+    name="cronaion",
+    version="1.0.2",
+    author="Chris Reusch",
+    description="Scheduler for python tasks.",
+    packages=["cronaion"],
+    install_requires=[
+        "cron_converter",
+    ],
+)
