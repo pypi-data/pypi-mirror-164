1a400ffbfb872a5d89d97ae9fc711602878e5df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:53 +0000

    Add package novalabs/1.0.2/novalabs-1.0.2.tar.gz

---
+++ packages/novalabs/novalabs-1.0.2.tar.gz/novalabs-1.0.2/setup.py
@@ -0,0 +1,33 @@
+import re
+import ast
+from setuptools import setup, find_packages
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('nova/__init__.py', 'rb') as f:
+    _parsed = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+    
+VERSION=_parsed[:-1] + str(int(_parsed[-1])+1)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="novalabs",
+    version=VERSION,
+    author="Nova Labs",
+    author_email="devteam@novalabs.ai",
+    description="Wrappers around Nova Labs utilities focused on safety and testability",
+    long_description=long_description,
+    url="https://github.com/Nova-DevTeam/nova-python",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    setup_requires=['setuptools_scm']
+)
