6f80a8a616ea5afe044e7d457141a00703de4cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:41 +0000

    Add package django-migration-docs/1.0.4/django_migration_docs-1.0.4-py3-none-any.whl

---
+++ packages/django-migration-docs/django_migration_docs-1.0.4-py3-none-any.whl/migration_docs/__init__.py
@@ -0,0 +1,27 @@
+import django
+
+from migration_docs.core import bootstrap
+from migration_docs.core import check
+from migration_docs.core import Migration
+from migration_docs.core import Migrations
+from migration_docs.core import show
+from migration_docs.core import sync
+from migration_docs.core import update
+from migration_docs.version import __version__
+
+
+__all__ = [
+    "bootstrap",
+    "check",
+    "show",
+    "sync",
+    "update",
+    "Migration",
+    "Migrations",
+    "__version__",
+]
+
+if django.VERSION < (3, 2):
+    default_app_config = "migration_docs.apps.MigrationDocsConfig"
+
+del django
