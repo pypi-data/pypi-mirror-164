361296c8d212d52502f67f5b95aae7cac2e42500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:25 +0000

    Add package formgram/0.0.5/formgram-0.0.5.tar.gz

---
+++ packages/formgram/formgram-0.0.5.tar.gz/formgram-0.0.5/src/formgram/grammars/str_interface/tables/__init__.py
@@ -0,0 +1,10 @@
+
+
+
+"""This package stores precomputed parse tables from the ply package
+
+DO NOT EDIT.
+
+If changes are made in ../backus_naur_parser.py these tables probably need to be
+replaced.
+"""
