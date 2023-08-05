c82823a596ac642824283f0d64b80d3e2c2e3191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:08 +0000

    Add package quantpiler/0.1.3/quantpiler-0.1.3.tar.gz

---
+++ packages/quantpiler/quantpiler-0.1.3.tar.gz/quantpiler-0.1.3/quantpiler/qram.py
@@ -0,0 +1,40 @@
+from qiskit import QuantumRegister, ClassicalRegister
+from qiskit.circuit import QuantumCircuit
+
+
+def new_qram(address_count: int, data_count: int, values):
+    address = QuantumRegister(address_count)
+    data = QuantumRegister(data_count)
+    qc = QuantumCircuit(address, data)
+
+    key_i = 0
+    for key in values:
+        key_i = key_i + 1
+
+        if key > 2**address_count - 1:
+            raise ValueError(
+                f"Key {key} larger than maximum ({2**address_count - 1})")
+
+        if key > 2**address_count - 1:
+            raise ValueError(
+                f"Value {values[key]} larger than maximum ({2**data_count - 1})")
+
+        k = [(key >> bit) & 1 for bit in range(address_count - 1, -1, -1)]
+        v = [(values[key] >> bit) & 1 for bit in range(data_count - 1, -1, -1)]
+
+        for i in range(address_count):
+            if not k[i]:
+                qc.x(address[i])
+
+        for i in range(data_count):
+            if v[i]:
+                qc.mct(address, data[i])
+
+        for i in range(address_count):
+            if not k[i]:
+                qc.x(address[i])
+
+        if key_i != len(values):
+            qc.barrier()
+
+    return qc
