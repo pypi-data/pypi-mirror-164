e9c7dca85a0ac3e51d04e6de35f160c7be548d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:22 +0000

    Add package sec-api/1.0.13/sec-api-1.0.13.tar.gz

---
+++ packages/sec-api/sec-api-1.0.13.tar.gz/sec-api-1.0.13/sec_api/__init__.py
@@ -0,0 +1,9 @@
+name = "sec_api"
+from sec_api.index import QueryApi
+from sec_api.index import FullTextSearchApi
+from sec_api.index import RenderApi
+from sec_api.index import XbrlApi
+from sec_api.index import ExtractorApi
+from sec_api.index import MappingApi
+from sec_api.index import ExecCompApi
+from sec_api.index import InsiderTradingApi
