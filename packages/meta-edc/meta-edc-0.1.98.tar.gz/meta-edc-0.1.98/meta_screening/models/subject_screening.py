62ab7f49388e674b24b8ceff8a0fe030e5a9cb64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:00 +0000

    Add package meta-edc/0.1.98/meta-edc-0.1.98.tar.gz

---
+++ packages/meta-edc/meta-edc-0.1.98.tar.gz/meta-edc-0.1.98/meta_screening/models/subject_screening.py
@@ -0,0 +1,52 @@
+from django.core.validators import RegexValidator
+from django_crypto_fields.fields import EncryptedCharField
+from edc_constants.constants import QUESTION_RETIRED
+from edc_model.models import BaseUuidModel
+from edc_screening.model_mixins import ScreeningModelMixin
+from edc_screening.screening_identifier import (
+    ScreeningIdentifier as BaseScreeningIdentifier,
+)
+
+from .calculated_model_mixin import CalculatedModelMixin
+from .eligibility_model_mixin import EligibilityModelMixin
+from .part_one_fields_model_mixin import PartOneFieldsModelMixin
+from .part_three_fields_model_mixin import PartThreeFieldsModelMixin
+from .part_two_fields_model_mixin import PartTwoFieldsModelMixin
+
+
+class SubjectScreeningModelError(Exception):
+    pass
+
+
+class ScreeningIdentifier(BaseScreeningIdentifier):
+
+    template = "S{random_string}"
+
+
+class SubjectScreening(
+    PartOneFieldsModelMixin,
+    PartTwoFieldsModelMixin,
+    PartThreeFieldsModelMixin,
+    EligibilityModelMixin,
+    CalculatedModelMixin,
+    ScreeningModelMixin,
+    BaseUuidModel,
+):
+
+    identifier_cls = ScreeningIdentifier
+
+    contact_number = EncryptedCharField(
+        validators=[RegexValidator(r"^[0-9\-\(\)\ ]+$", message="Enter a valid number")],
+        null=True,
+        help_text="Provide a contact number if repeating glucose measures (Encryption: RSA)",
+    )
+
+    def save(self, *args, **kwargs):
+        if self._meta.label_lower == "meta_screening.subjectscreening":
+            raise SubjectScreeningModelError("Unable to save. Save via P1-3 proxy models.")
+        self.consent_ability = QUESTION_RETIRED
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Subject Screening"
+        verbose_name_plural = "Subject Screening"
