4430459b6bbd6af979fae688af2e9c16810fc85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:12 +0000

    Add package korrektor-uz-async/0.0.92/korrektor_uz_async-0.0.92.tar.gz

---
+++ packages/korrektor-uz-async/korrektor_uz_async-0.0.92.tar.gz/korrektor_uz_async-0.0.92/README.md
@@ -0,0 +1,6 @@
+API wrapper for https://korrektor.uz
+
+API documentation: https://korrektor.uz/api/
+
+Foydalanish: https://korrektor.uz/api/
+
---
+++ packages/korrektor-uz-async/korrektor_uz_async-0.0.92.tar.gz/korrektor_uz_async-0.0.92/setup.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    from setuptools import setup
+
+    setup(version="0.0.92",
+          install_requires=[
+              'aiohttp',
+              'aiofiles',
+              'pydantic',
+          ],
+          )
