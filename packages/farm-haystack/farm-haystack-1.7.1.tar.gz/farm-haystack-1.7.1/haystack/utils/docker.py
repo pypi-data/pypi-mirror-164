03d1aae28e87512868db20dc12ae1a1a55f30ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:38 +0000

    Add package farm-haystack/1.7.1/farm-haystack-1.7.1.tar.gz

---
+++ packages/farm-haystack/farm-haystack-1.7.1.tar.gz/farm-haystack-1.7.1/haystack/utils/docker.py
@@ -0,0 +1,25 @@
+import logging
+
+
+def cache_models(models=None):
+    """
+    Small function that caches models and other data.
+    Used only in the Dockerfile to include these caches in the images.
+    """
+    # Backward compat after adding the `model` param
+    if models is None:
+        models = ["deepset/roberta-base-squad2"]
+
+    # download punkt tokenizer
+    logging.info("Caching punkt data")
+    import nltk
+
+    nltk.download("punkt")
+
+    # Cache models
+    import transformers
+
+    for model_to_cache in models:
+        logging.info(f"Caching {model_to_cache}")
+        transformers.AutoTokenizer.from_pretrained(model_to_cache)
+        transformers.AutoModel.from_pretrained(model_to_cache)
