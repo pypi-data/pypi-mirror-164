77f595e42fbb56c9bae406752dbbfd91e59de9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:32 +0000

    Add package mcap-protobuf-support/0.0.7/mcap_protobuf_support-0.0.7-py3-none-any.whl

---
+++ packages/mcap-protobuf-support/mcap_protobuf_support-0.0.7-py3-none-any.whl/tests/test_decoder.py
@@ -0,0 +1,27 @@
+from io import BytesIO
+
+from mcap.mcap0.reader import make_reader
+from mcap_protobuf.decoder import Decoder
+
+from .generate import generate_sample_data
+
+
+def test_protobuf_decoder():
+    output = BytesIO()
+    generate_sample_data(output)
+
+    decoder = Decoder()
+    reader = make_reader(output)
+    count = 0
+    for schema, channel, message in reader.iter_messages():
+        proto_msg = decoder.decode(schema, message)
+        count += 1
+        if channel.topic == "/complex_message":
+            assert proto_msg.fieldA.startswith("Field A")
+            assert proto_msg.fieldB.startswith("Field B")
+        elif channel.topic == "/simple_message":
+            assert proto_msg.data.startswith("Hello MCAP protobuf world")
+        else:
+            raise AssertionError(f"unrecognized topic {channel.topic}")
+
+    assert count == 20
