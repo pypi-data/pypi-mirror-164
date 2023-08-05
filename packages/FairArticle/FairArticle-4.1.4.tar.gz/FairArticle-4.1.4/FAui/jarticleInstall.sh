49123df1ae1a1069ebe4d8151a0d518ccd46e0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:39 +0000

    Add package FairArticle/4.1.4/FairArticle-4.1.4.tar.gz

---
+++ packages/FairArticle/FairArticle-4.1.4.tar.gz/FairArticle-4.1.4/FAui/jarticleInstall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+python3.9 -m pip  install fairarticle>=4.1.3
+python3.9 -m pip  install fairmongo>=4.1.4
+python3.9 -m pip  install fairqt>=1.0.1
+python3.9 -m pip  install faircore>=4.1.1
+python3.9 -m pip  install fairresources>=4.0.3
+python3.9 -m pip install flask-socketio
+python3.9 -m pip install pyngrok
+python3.9 -m pip install pytube
+
+python3.9 jarticleMain.py
