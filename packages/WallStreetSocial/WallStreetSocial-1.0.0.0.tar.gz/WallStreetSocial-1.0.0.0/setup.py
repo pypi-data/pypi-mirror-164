be0e6b7e9472547379c099b49f9eaf9767be75c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:21 +0000

    Add package WallStreetSocial/1.0.0.0/WallStreetSocial-1.0.0.0.tar.gz

---
+++ packages/WallStreetSocial/WallStreetSocial-1.0.0.0.tar.gz/WallStreetSocial-1.0.0.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="WallStreetSocial",
+    version="1.0.0.0",
+    author=["Joshua David Golafshan", "John Hutton"],
+    description="""Is an open source piece of software that is designed to allow anyone to quickly get
+                    familiar with the basics of textual analysis.""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JGolafshan/WallStreetSocial",
+    keywords=["Database", "WallStreetBets", "Reddit"],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['setuptools',
+                      'dash',
+                      'yfinance',
+                      'plotly',
+                      'matplotlib',
+                      'pandas',
+                      'gensim',
+                      'datetime',
+                      'pmaw',
+                      'spacy',
+                      'vaderSentiment',
+                      'numpy',
+                      'nltk',
+                      'requests',
+                      'dash-bootstrap-components',
+                      ],
+    classifiers=["Programming Language :: Python :: 3",
+                 "Operating System :: OS Independent",
+                 ]
+)
+
+"""
+py setup.py sdist
+py setup.py bdist_wheel
+py -m twine upload --skip-existing dist/*
+"""
