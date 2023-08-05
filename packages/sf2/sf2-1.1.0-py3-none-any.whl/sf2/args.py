f9aafa3fa3aa9aab728fb2692f982160b15e59d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:50 +0000

    Add package sf2/1.1.0/sf2-1.1.0-py3-none-any.whl

---
+++ packages/sf2/sf2-1.1.0-py3-none-any.whl/sf2/args.py
@@ -0,0 +1,33 @@
+import argparse
+from argparse import RawTextHelpFormatter
+
+DESCRIPTION = """
+
+   _____ _________ 
+  / ___// ____/__ \\
+  \__ \/ /_   __/ /
+ ___/ / __/  / __/ 
+/____/_/    /____/ 
+                   
+Simple Fernet File
+
+This tool manages an encrypted file with the Fernet algorithm.
+
+By Laulin
+Supported by Spartan Conseil
+"""
+
+def get_args():
+    parser = argparse.ArgumentParser(description=DESCRIPTION, formatter_class=RawTextHelpFormatter)
+    action = parser.add_mutually_exclusive_group(required=True)
+    action.add_argument('-e', action='store_true', default=False, dest='encrypt', help='Encrypt a file')
+    action.add_argument('-d', action='store_true', default=False, dest='decrypt', help='Decrypt a file. If -o is not provided, stdout is used')
+    action.add_argument('-v', action='store_true', default=False, dest='verify', help='Check if the key is valid on the file')
+    action.add_argument('-n', action='store_true', default=False, dest='new', help='Create an empty encrypted file')
+    action.add_argument('--edit', action='store_true', default=False, dest='edit', help='Run the internal editor')
+    action.add_argument('--external', action='store_true', default=False, dest='external', help='Run an external editor')
+
+    parser.add_argument('-i', default=None, dest='infilename', help='Select the encrypt file pass', required=True)
+    parser.add_argument('-o', default=None, dest='outfilename', help='Select the encrypt file pass')
+    parser.add_argument('--editor', default="mousepad", dest='editor', help='Select the external editor')
+    return parser.parse_args()
---
+++ packages/sf2/sf2-1.1.0-py3-none-any.whl/sf2/sf2.py
@@ -0,0 +1,118 @@
+from getpass import getpass
+import sys
+
+from sf2.args import get_args
+from sf2.cipher import Cipher
+from sf2.extern import Extern
+
+# prevent trouble on cli only env
+try:
+    from sf2.editor import Editor
+except Exception as e:
+    def Editor(*args, **kwargs):
+        print(f"No editor support ! ({e})")
+        sys.exit(-1)
+
+
+
+def main():
+    args = get_args()
+
+    if args.encrypt:
+        print("We recommand min 12 chars with a-z, A-Z, 0-9 and special symbol")
+        password1 = getpass("Password : ")
+        password2 = getpass("Confirm password : ")
+        if password1 != password2:
+            print("Password are not the same, abord")
+            sys.exit(-1)
+        password = password1
+        cipher = Cipher()
+        try:
+            cipher.encrypt_file(password, args.infilename, args.outfilename)
+        except Exception as e:
+            print(f"Failed to encrypt file {args.infilename} : {e}")
+            sys.exit(-1)
+
+    elif args.decrypt:
+        password = getpass()
+        cipher = Cipher()
+        try:
+            cipher.decrypt_file(password, args.infilename, args.outfilename)
+        except Exception as e:
+            print(f"Failed to decrypt file {args.infilename} : {e}")
+            sys.exit(-1)
+
+    elif args.verify:
+        password = getpass()
+        cipher = Cipher()
+        
+        try:
+            is_ok = cipher.verify_file(password, args.infilename)
+        except Exception as e:
+            print(f"Failed to verify file {args.infilename} : {e}")
+            sys.exit(-1)
+
+        if is_ok :
+            print("OK")
+        else:
+            print("FAILED !")
+            sys.exit(-1)
+
+    if args.new:
+        print("We recommand min 12 chars with a-z, A-Z, 0-9 and special symbol")
+        password1 = getpass("Password : ")
+        password2 = getpass("Confirm password : ")
+        if password1 != password2:
+            print("Password are not the same, abord")
+            sys.exit(-1)
+        password = password1
+        cipher = Cipher()
+        try:
+            encrypted = cipher.encrypt(password, "") 
+
+            with open(args.infilename, "w") as f:
+                f.write(encrypted)
+
+        except Exception as e:
+            print(f"Failed to encrypt file {args.infilename} : {e}")
+            sys.exit(-1)
+
+    elif args.edit:
+        password = getpass()
+        cipher = Cipher()
+
+        try:
+            is_ok = cipher.verify_file(password, args.infilename)
+        except Exception as e:
+            print(f"Failed to open file {args.infilename} : {e}")
+            sys.exit(-1)
+
+        if not is_ok :
+            print(f"Failed to open file {args.infilename} : bad key ?")
+            sys.exit(-1)
+
+        editor = Editor(password, args.infilename)
+        editor.load()
+        editor.create()
+
+    elif args.external:
+        password = getpass()
+        cipher = Cipher()
+
+        try:
+            is_ok = cipher.verify_file(password, args.infilename)
+        except Exception as e:
+            print(f"Failed to open file {args.infilename} : {e}")
+            sys.exit(-1)
+
+        if not is_ok :
+            print(f"Failed to open file {args.infilename} : bad key ?")
+            sys.exit(-1)
+
+        editor = Extern(password, args.infilename, args.editor)
+        editor.run()
+
+    sys.exit(0)        
+
+if __name__ == "__main__":
+    main()
