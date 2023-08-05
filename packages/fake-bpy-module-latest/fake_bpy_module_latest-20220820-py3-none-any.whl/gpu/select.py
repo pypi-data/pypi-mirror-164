9ba57588905474e2653b561e33c232a52b748147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:13 +0000

    Add package fake-bpy-module-latest/20220820/fake_bpy_module_latest-20220820-py3-none-any.whl

---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20220820-py3-none-any.whl/gpu/select.py
@@ -0,0 +1,13 @@
+import sys
+import typing
+
+GenericType = typing.TypeVar("GenericType")
+
+
+def load_id(id):
+    ''' Set the selection ID.
+
+    :type select: int
+    '''
+
+    pass
---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20220820-py3-none-any.whl/mathutils/interpolate.py
@@ -0,0 +1,12 @@
+import sys
+import typing
+
+GenericType = typing.TypeVar("GenericType")
+
+
+def poly_3d_calc(veclist, pt):
+    ''' Calculate barycentric weights for a point on a polygon.
+
+    '''
+
+    pass
