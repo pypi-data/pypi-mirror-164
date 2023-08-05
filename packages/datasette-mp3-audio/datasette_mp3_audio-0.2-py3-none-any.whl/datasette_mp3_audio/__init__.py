10f391f30a82afcfe474c275f9fb457e1e931efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:13 +0000

    Add package datasette-mp3-audio/0.2/datasette_mp3_audio-0.2-py3-none-any.whl

---
+++ packages/datasette-mp3-audio/datasette_mp3_audio-0.2-py3-none-any.whl/datasette_mp3_audio/__init__.py
@@ -0,0 +1,25 @@
+from datasette import hookimpl
+from markupsafe import Markup, escape
+
+
+@hookimpl
+def extra_js_urls(columns, datasette):
+    if not columns:
+        return None
+    return [
+        datasette.urls.static_plugins("datasette-mp3-audio", "datasette-mp3-audio.js")
+    ]
+
+
+@hookimpl
+def render_cell(value):
+    if not isinstance(value, str):
+        return
+    if value.endswith(".mp3") and (
+        value.startswith("http://")
+        or value.startswith("https://")
+        or value.startswith("/")
+    ):
+        return Markup(
+            '<audio controls src="{}">Audio not supported</audio>'.format(escape(value))
+        )
