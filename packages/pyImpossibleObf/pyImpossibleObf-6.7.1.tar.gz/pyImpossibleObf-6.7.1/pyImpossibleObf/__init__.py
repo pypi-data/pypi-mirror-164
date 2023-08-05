2f4a3bd9f19cd7d20e2c86e35a4f4f0096853748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:12 +0000

    Add package pyImpossibleObf/6.7.1/pyImpossibleObf-6.7.1.tar.gz

---
+++ packages/pyImpossibleObf/pyImpossibleObf-6.7.1.tar.gz/pyImpossibleObf-6.7.1/pyImpossibleObf/__init__.py
@@ -0,0 +1,40 @@
+import os
+import base64
+def transcrireCle(cle):
+	return "".join([str(ord(elt)) for elt in cle])
+
+def deobfuscate(arg):
+	message = ""
+	cle = transcrireCle("loooood")
+	chiffre = arg
+	for i in range(len(chiffre)):
+		chiffre[i] -= int(cle[i % len(cle)])
+		message += chr(chiffre[i])
+	return message
+
+def obfuscate(msg):
+	if(os.path.isfile(msg)):
+		print("[.] Obfuscating : " + msg)
+		cle = transcrireCle("loooood")
+		chiffre = []
+		message = open(msg).read()
+
+		for elt in message:
+			chiffre.append(ord(elt))
+
+
+		for i in range(len(chiffre)):
+			pos_cle = i % len(cle)
+			chiffre[i] += int(cle[pos_cle])
+		crypt = ""
+		for elt in chiffre:
+			try:
+				crypt += chr(elt)
+			except UnicodeEncodeError:
+				crypt += "x"
+		open(msg + ".hashed.py", "x").write("import base64\nexec(base64.b64decode('" + str(base64.b64encode("import pyImpossibleObf\nexec(pyImpossibleObf.deobfuscate(".encode()+ str(chiffre).encode() + "))".encode()).decode() + "').decode())"))
+		print("[.] Successfully obfuscated : " + msg)
+		return chiffre
+	else:
+		print("[.] Please input a valid path.")
+obfuscate("t..txt")
---
+++ packages/pyImpossibleObf/pyImpossibleObf-6.7.1.tar.gz/pyImpossibleObf-6.7.1/pyImpossibleObf/t..txt.hashed.py
@@ -0,0 +1,2 @@
+import pyImpossibleObf
+exec(pyImpossibleObf.deobfuscate([113, 114, 113, 111, 117, 41, 35, 105, 35, 42]))
