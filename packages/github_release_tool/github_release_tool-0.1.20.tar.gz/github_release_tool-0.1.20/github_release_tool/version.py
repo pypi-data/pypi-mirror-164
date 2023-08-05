7ee22f3fcfbde409f645834dca1f982914b3c692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:11 +0000

    Add package github_release_tool/0.1.20/github_release_tool-0.1.20.tar.gz

---
+++ packages/github_release_tool/github_release_tool-0.1.20.tar.gz/github_release_tool-0.1.20/github_release_tool/version.py
@@ -0,0 +1,4 @@
+__version__ = "0.1.20"
+__author__ = "Matt Krueger"
+__email__ = "mkrueger@rstms.net"
+__timestamp__ = "2022-08-19 08:00:22-05:00"
---
+++ packages/github_release_tool/github_release_tool-0.1.20.tar.gz/github_release_tool-0.1.20/make.include/release.mk
@@ -0,0 +1,37 @@
+# publish - build package and publish
+
+# create distributable files if sources have changed
+
+.PHONY: dist 
+dist: .dist 
+
+.dist: gitclean tox
+	@echo Building $(project)
+	flit build
+	@touch $@
+
+RELEASE = release\
+  --organization $(organization)\
+  --repository $(repo)\
+  --token $(GITHUB_DEPLOY_TOKEN)\
+  --module-dir $(MODULE_DIR)\
+  --wheel-dir ./dist\
+  --version $(version) 
+
+## create a github release from the current version
+.PHONY: release
+release: .release
+
+.release: dist
+	@echo pushing Release $(project) v$(version) to github...
+	$(RELEASE) create --force | tee dist/$(project)-$(version)-release.json
+	$(RELEASE) upload --force | tee dist/$(project)-$(version)-asset.json
+	@touch $@
+
+
+# clean up the publish temp files
+release-clean:
+	rm -f .dist
+	rm -f .release
+	rm -rf .tox
+	rm -f requirements*.txt
