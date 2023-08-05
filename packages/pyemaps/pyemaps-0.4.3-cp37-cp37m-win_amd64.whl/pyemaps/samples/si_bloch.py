c2983212756923992a2b806f50cb68e216b02e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:37 +0000

    Add package pyemaps/0.4.3/pyemaps-0.4.3-cp37-cp37m-win_amd64.whl

---
+++ packages/pyemaps/pyemaps-0.4.3-cp37-cp37m-win_amd64.whl/pyemaps/samples/si_bloch.py
@@ -0,0 +1,82 @@
+"""
+This file is part of pyemaps
+___________________________
+
+pyemaps is free software for non-comercial use: you can 
+redistribute it and/or modify it under the terms of the GNU General 
+Public License as published by the Free Software Foundation, either 
+version 3 of the License, or (at your option) any later version.
+
+pyemaps is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyemaps.  If not, see <https://www.gnu.org/licenses/>.
+
+Contact supprort@emlabsoftware.com for any questions and comments.
+___________________________
+
+This sample code is to demostrate using pyemaps to generate and render
+kinematic diffraction patterns while changing with sample tilt in 
+x direction 
+
+Author:     EMLab Solutions, Inc.
+Date:       July 07, 2022    
+
+"""
+
+from pyemaps import EMC, EMCError, BlochError
+
+MAX_PROCWORKERS = 4
+
+def generate_bloch_images(name = 'Silicon', dsize = 0.16, ckey = 'tilt'):
+    import concurrent.futures
+    from pyemaps import Crystal as cryst
+    from pyemaps import BImgList
+
+    cr = cryst.from_builtin(name)
+    
+    fs=[]
+   
+    emclist =[] 
+
+    for i in range(-3,3): 
+
+        if ckey == 'tilt':
+            emclist.append(EMC(tilt=(i*0.5, 0.0), cl = 200))
+        
+        if ckey == 'zone':
+            emclist.append(EMC(zone=(0, i, 1), cl = 200))
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=MAX_PROCWORKERS) as e:
+
+        for ec in emclist:
+            fs.append(e.submit(cr.generateBloch, disk_size=dsize, sampling = 20, em_controls = ec))
+        
+        bimgs = BImgList(name) 
+        for f in concurrent.futures.as_completed(fs):
+            try:
+               emc, img = f.result()
+
+            except (BlochError, EMCError) as e:
+                print(f'{f} generated an exception: {e.message}')
+            except:
+                print('failed to generate diffraction patterns')  
+            bimgs.add(emc, img) 
+            
+    return bimgs
+
+from pyemaps import showBloch
+
+def run_bloch_tests():
+    # from sample_base import generate_bimages
+    em_keys = ['tilt', 'zone']
+    for k in em_keys:
+        imgs = generate_bloch_images(ckey=k)
+        showBloch(imgs)
+
+if __name__ == '__main__':
+    
+    run_bloch_tests()
---
+++ packages/pyemaps/pyemaps-0.4.3-cp37-cp37m-win_amd64.whl/pyemaps/setup.py
@@ -0,0 +1,131 @@
+from ast import keyword
+from ensurepip import version
+from multiprocessing import AuthenticationError
+from nturl2path import url2pathname
+from random import sample
+from ssl import Options
+
+from numpy.distutils.command.build_ext import build_ext as numpy_build_ext
+import os
+
+emaps_fsource = 'diffract'
+scatteringdir = 'scattering'
+spgdir = 'spg'
+
+dpgen_cobj = 'write_dpbin.o'
+
+def get_comp():
+    '''
+    Get pyemaps component to be built from comp.json file
+    '''
+    
+    import json, os
+    from pathlib import Path
+
+    json_cfg = os.getenv('PYEMAPS_JSON')
+
+    if not json_cfg:
+        json_cfg = "comp.json" # default
+
+    currdir = Path(os.path.dirname(os.path.realpath(__file__)))/emaps_fsource
+    comp = 'dif'
+
+    comp_cfg = os.path.join(currdir,json_cfg)
+    try:
+        with open(comp_cfg, 'r') as jf:
+            comp = json.load(jf)['component']
+
+    except IOError as e:
+        raise ValueError(f"Error reading component configure file: {e}")
+   
+    # delete temp config file comp.json
+    # if os.path.exists(comp_cfg):
+    #     os.remove(comp_cfg)
+    # else:
+    #     print(f"The comfiguration file {comp_cfg)} does not exits")
+
+    return comp
+
+class build_dp_ext(numpy_build_ext):
+    def finalize_options(self):
+        numpy_build_ext.finalize_options(self)
+        cobj = os.path.join(emaps_fsource, dpgen_cobj)
+        self.link_objects = [cobj]
+
+def get_samples(sdn = 'samples'):
+    '''
+    input: sdn = sample directory name under pyemaps
+    '''
+
+    import os, glob
+    base_dir = os.path.realpath(__file__)
+    samples_base_dir = os.path.join(os.path.dirname(base_dir), sdn)
+    sbase_files = os.path.join(samples_base_dir, '*.py')
+    sfile_list = glob.glob(sbase_files)
+
+    return [os.path.join(sdn, os.path.basename(name)) for name in sfile_list]
+    
+def get_intel_redist():
+    intel_redistdir = os.path.join(os.getenv('IFORTROOT'), 'redist', 'intel64_win', 'compiler')
+    return intel_redistdir
+
+def configuration(parent_package='',top_path=None):
+    from codecs import open
+    from os import path
+    
+    here = path.abspath(path.dirname(__file__))
+    
+    # Get the long description from the README file
+    with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+    from numpy.distutils.misc_util import Configuration
+    
+    version = {}
+    with open("__version__.py") as fp:
+        exec(fp.read(), version)
+
+    config = Configuration(
+        'pyemaps',
+        parent_package,
+        top_path,
+        version = version['__version__'],
+        description = 'Python Modules for Transmission Electron Diffraction Simulations',
+        long_description_content_type='text/markdown',
+        long_description = long_description
+    )
+
+    config.add_subpackage('diffract',emaps_fsource)
+    config.add_subpackage('spg',spgdir)
+    config.add_subpackage('scattering',scatteringdir)
+    # config.add_subpackage('CifReader','CifReader')
+    
+    samplelist = get_samples()
+    print(f'samples files found: {samplelist}')
+    config.add_data_files('license.txt', 'README.md', 'COPYING', 
+                          ('samples', samplelist),
+                          ('test', []))
+
+
+    config.add_data_dir('cdata')
+                          
+# add intel redist folder to .lib   
+    config.add_data_dir(('.libs', get_intel_redist()))
+    
+    config.make_config_py() #generated automatically by distutil based on supplied __config__.py
+    return config
+
+if __name__ == '__main__':
+    import setuptools 
+    from numpy.distutils.core import setup
+
+    config = configuration(top_path='')
+    
+    comp = get_comp()
+    if comp == 'dpgen':
+        cmd ={}
+        cmd['build_ext'] = build_dp_ext
+
+        setup(**config.todict(), cmdclass = cmd)
+    else:
+        setup(**config.todict())
