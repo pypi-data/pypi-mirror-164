942795249fa0f7d9f9b753bac7c58d7305ebf824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:59 +0000

    Add package streamlit-charty/0.0.1/streamlit_charty-0.0.1-py3-none-any.whl

---
+++ packages/streamlit-charty/streamlit_charty-0.0.1-py3-none-any.whl/charty/frontend/build/precache-manifest.ca3d0590e910e3b6ccfb1e0626f847a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5988b1678eae721ea4bab7670a308411",
+    "url": "./index.html"
+  },
+  {
+    "revision": "57549d7f7cdfa1b4319e",
+    "url": "./static/js/2.27bd71cc.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.27bd71cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a2e789bf6de98100e4c",
+    "url": "./static/js/main.804ec37d.chunk.js"
+  },
+  {
+    "revision": "b6fdd73ea308285c3bb7",
+    "url": "./static/js/runtime-main.b42ca973.js"
+  }
+]);
