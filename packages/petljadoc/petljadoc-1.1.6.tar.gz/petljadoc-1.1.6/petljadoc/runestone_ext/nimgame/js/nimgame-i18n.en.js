f713c2e3f51973cbcb7114ccf5856d6b408a350a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:05 +0000

    Add package petljadoc/1.1.6/petljadoc-1.1.6.tar.gz

---
+++ packages/petljadoc/petljadoc-1.1.6.tar.gz/petljadoc-1.1.6/petljadoc/runestone_ext/nimgame/js/nimgame-i18n.en.js
@@ -0,0 +1,10 @@
+$.i18n().load({
+    "en": {
+        "nimgame_restart_buttnon": "Restart",
+        "nimgame_take": "Take",
+        "nimgame_single_player": "Single player",
+        "nimgame_error": "You can take up to $1",
+        "nimgame_winner" : "Player $1 won.",
+        "nimgame_alg_won" : "Computer won."
+    }
+})
