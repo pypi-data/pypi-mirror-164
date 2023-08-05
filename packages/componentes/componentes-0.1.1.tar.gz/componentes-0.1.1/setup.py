ca14a33a75df9e15e0c525778d5c6ad99b1913d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:24 +0000

    Add package componentes/0.1.1/componentes-0.1.1.tar.gz

---
+++ packages/componentes/componentes-0.1.1.tar.gz/componentes-0.1.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['componentes']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.22.2,<2.0.0',
+ 'pandas>=1.4.1,<2.0.0',
+ 'pycarol>=2.54.0,<3.0.0',
+ 'pymongo>=4.1.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'componentes',
+    'version': '0.1.1',
+    'description': 'Componetes de uso de teste',
+    'long_description': None,
+    'author': 'Bruno',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
