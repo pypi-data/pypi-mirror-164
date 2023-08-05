94622642d4f6f76929bbdbca8282a6eb441909b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:07 +0000

    Add package statistica-distributions/0.1/statistica_distributions-0.1.tar.gz

---
+++ packages/statistica-distributions/statistica_distributions-0.1.tar.gz/statistica_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='statistica_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['statistica_distributions'],
+      author = 'Jonathan Koefman',
+      author_email = 'jkoefman@outlook.com',
+      zip_safe=False)
