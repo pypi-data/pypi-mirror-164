1b32e1458ca6a60ef3c61e9996e37d8d7a448bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:21 +0000

    Add package quarchQCS/1.1.0.dev13/quarchQCS-1.1.0.dev13-py3-none-any.whl

---
+++ packages/quarchQCS/quarchQCS-1.1.0.dev13-py3-none-any.whl/QuarchpyQCS/dtsGlobals.py
@@ -0,0 +1,17 @@
+class dtsGlobals:
+    GUI_TCP_IP = "127.0.0.1"
+    choiceResponse = None
+    quarchpy_installed = None
+    continueTest = True
+    validVersion = True
+    minQCSVersion = "1.10"
+    QCSVersionValid = False
+    runUntilFirstFailure = False
+    send_to_gui = True
+    qcs_dir = None
+
+    guiAddress = "localhost"
+    guiPort = 9921
+
+
+
