e3de8a11a4b7f083095a919b3d1db4c9b31234a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:21 +0000

    Add package cambridge/3.0.0/cambridge-3.0.0-py2.py3-none-any.whl

---
+++ packages/cambridge/cambridge-3.0.0-py2.py3-none-any.whl/cambridge/main.py
@@ -0,0 +1,35 @@
+"""
+Cambridge is a terminal version of Cambridge Dictionary.
+The dictionary data comes from https://dictionary.cambridge.org.
+"""
+
+import sqlite3
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from cambridge.args import parse_args
+from cambridge.cache import DB
+from cambridge.utils import timer
+
+
+# @timer
+def main():
+    try:
+        con = sqlite3.connect(
+            DB, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
+        )
+        cur = con.cursor()
+
+        args = parse_args()
+        args.func(args, con, cur)
+
+        cur.close()
+        con.close()
+
+    except KeyboardInterrupt:
+        print("\nStopped by user")
+
+
+main()
