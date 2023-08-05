004b824da1f0c5d941b426ed8ec039eeea30bcb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:05 +0000

    Add package jptest/0.8.4/jptest-0.8.4-py3-none-any.whl

---
+++ packages/jptest/jptest-0.8.4-py3-none-any.whl/jptest/__init__.py
@@ -0,0 +1,5 @@
+from .JPFixture import JPPreRun, JPPreTest, JPPostTest, JPPostRun
+from .JPTestParams import JPTestParams
+from .JPTestBook import JPTestBook
+from .JPTest import JPTest
+from .JPTestCell import JPTestCell
