d98a19401ff96348557a8de2f4175b58912332cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:12 +0000

    Add package kss/3.5.4/kss-3.5.4.tar.gz

---
+++ packages/kss/kss-3.5.4.tar.gz/kss-3.5.4/setup.py
@@ -0,0 +1,69 @@
+import codecs
+import shutil
+import subprocess
+from contextlib import suppress
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+required = [
+    "emoji==1.2.0",
+    "regex",
+    "more_itertools",
+]
+
+
+class InstallCommand(install):
+    def run(self):
+        try:
+            import mecab
+        except:
+            with suppress():
+                pip_executable = "pip3"
+                if not shutil.which(pip_executable):
+                    pip_executable = "pip"
+                subprocess.call(
+                    [pip_executable, "install", "python-mecab-kor"],
+                    stderr=subprocess.DEVNULL,
+                )
+
+        super().run()
+
+
+def read_file(filename, cb):
+    with codecs.open(filename, "r", "utf8") as f:
+        return cb(f)
+
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="kss",
+    version="3.5.4",
+    author="Hyunwoong Ko",
+    author_email="kevin.ko@tunib.ai",
+    url="https://github.com/hyunwoongko/kss",
+    license='BSD 3-Clause "New" or "Revised" License',
+    description="A Toolkit for Korean sentence segmentation",
+    long_description_content_type="text/markdown",
+    platforms=["any"],
+    install_requires=required,
+    long_description=long_description,
+    packages=find_packages(exclude=["tests"]),
+    python_requires=">=3",
+    zip_safe=False,
+    package_data={"": ["kss/pynori/resources/*"]},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    cmdclass={"install": InstallCommand},
+)
