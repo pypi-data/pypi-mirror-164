76eda3bdb983f7e0345dcf26ff70f25486b3e38f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:16:09 +0000

    Add package docker-composition/0.0.20/docker_composition-0.0.20.tar.gz

---
+++ packages/docker-composition/docker_composition-0.0.20.tar.gz/docker_composition-0.0.20/composition/uninstall.py
@@ -0,0 +1,16 @@
+import logging
+import sys
+
+from composition import directory, storage
+
+
+def uninstall_application(application_id_list, force):
+    if not application_id_list:
+        logging.error("Please include an id to delete.")
+        logging.error("e.g. composer uninstall -i moon_baboon")
+        sys.exit(1)
+    for application_id in application_id_list:
+        logging.info(f"Uninstalling {application_id}")
+        directory.handle_delete(application_id, force=force)
+        storage.remove(application_id)
+
