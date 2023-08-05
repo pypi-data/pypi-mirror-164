5436a8366d7fb26794c5ee96bf78c944aaa8e6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:15:32 +0000

    Add package AthenaTwitchBot/0.8.1/AthenaTwitchBot-0.8.1.tar.gz

---
+++ packages/AthenaTwitchBot/AthenaTwitchBot-0.8.1.tar.gz/AthenaTwitchBot-0.8.1/setup.py
@@ -0,0 +1,45 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# - Package Imports -
+# ----------------------------------------------------------------------------------------------------------------------
+# General Packages
+from __future__ import annotations
+import setuptools
+
+# Custom Library
+
+# Custom Packages
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - Code -
+# ----------------------------------------------------------------------------------------------------------------------
+def readme_handler() -> str:
+    with open("README.md", "r") as readme_file:
+        return readme_file.read()
+
+def version_handler() -> str:
+    # ------------------------------------------------------------------------------------------------------------------
+    version = 0,8,1 # <-- DEFINE THE VERSION IN A TUPLE FORMAT HERE
+    # ------------------------------------------------------------------------------------------------------------------
+    return ".".join(str(i) for i in version)
+
+setuptools.setup(
+    name="AthenaTwitchBot",
+    version=version_handler(),
+    author="Andreas Sas",
+    author_email="",
+    description="A zero 3rd party dependency Twitch bot Connector",
+    long_description=readme_handler(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/DirectiveAthena/AthenaTwitchBot",
+    project_urls={
+        "Bug Tracker": "https://github.com/DirectiveAthena/AthenaTwitchBot/issues",
+    },
+    license="GPLv3",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.10",
+    install_requires=[
+        "AthenaLib>=1.1.0",
+        "AthenaColor>=6.0.1"
+    ]
+)
