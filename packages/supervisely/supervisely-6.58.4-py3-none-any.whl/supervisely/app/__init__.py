18e806ef3627f02c424fadc98329bafac375ad8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:39 +0000

    Add package supervisely/6.58.4/supervisely-6.58.4-py3-none-any.whl

---
+++ packages/supervisely/supervisely-6.58.4-py3-none-any.whl/supervisely/app/__init__.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI
+from supervisely.app.content import StateJson, DataJson
+from supervisely.app.content import get_data_dir
+import supervisely.app.fastapi as fastapi
+import supervisely.app.widgets as widgets
