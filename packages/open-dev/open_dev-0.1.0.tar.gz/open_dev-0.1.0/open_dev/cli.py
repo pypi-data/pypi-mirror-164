58a7af39bd14a7519404b80d82de1655407b8ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:46 +0000

    Add package open-dev/0.1.0/open_dev-0.1.0.tar.gz

---
+++ packages/open-dev/open_dev-0.1.0.tar.gz/open_dev-0.1.0/open_dev/cli.py
@@ -0,0 +1,18 @@
+"""Console script for open_dev."""
+
+import click
+
+description = "A collection of tooling to enable open source development."
+
+
+@click.command()
+def main():
+    """Main entrypoint."""
+    click.echo("open_dev")
+    click.echo("=" * len("open_dev"))
+    click.echo(description)
+    click.echo("*" * len(description))
+
+
+if __name__ == "__main__":
+    main()  # pragma: no cover
