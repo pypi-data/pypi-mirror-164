3d9d84f7fe89970b40825b70042152b0862f544d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:09 +0000

    Add package gptj/3.0.9/gptj-3.0.9-py3-none-any.whl

---
+++ packages/gptj/gptj-3.0.9-py3-none-any.whl/gpt_j/__init__.py
@@ -0,0 +1,7 @@
+from .Gptj import generate
+from .gptj_api import Completion
+from .ImaginaryFriend import GPTJ_API
+from .Text2TextGen import GPTJ
+from .gptj_api import Completion
+from .Basic_api import simple_completion
+from .Text2TextGen import GPTJ
