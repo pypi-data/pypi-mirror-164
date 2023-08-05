a3231c512b22a98cd8658cbe2347853e8f622057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:21 +0000

    Add package gdsfactory/5.19.0/gdsfactory-5.19.0.tar.gz

---
+++ packages/gdsfactory/gdsfactory-5.19.0.tar.gz/gdsfactory-5.19.0/gdsfactory/geometry/boolean.py
@@ -0,0 +1,86 @@
+from typing import Tuple, Union
+
+import phidl.geometry as pg
+
+import gdsfactory as gf
+from gdsfactory.component import Component
+from gdsfactory.types import ComponentOrReference, Int2, Layer
+
+
+@gf.cell
+def boolean(
+    A: Union[ComponentOrReference, Tuple[ComponentOrReference, ...]],
+    B: Union[ComponentOrReference, Tuple[ComponentOrReference, ...]],
+    operation: str,
+    precision: float = 1e-4,
+    num_divisions: Union[int, Int2] = (1, 1),
+    max_points: int = 4000,
+    layer: Layer = (1, 0),
+) -> Component:
+    """Performs boolean operations between 2 Component/Reference objects, or \
+    lists of Devices/DeviceReferences.
+
+    ``operation`` should be one of {'not', 'and', 'or', 'xor', 'A-B', 'B-A', 'A+B'}.
+    Note that 'A+B' is equivalent to 'or', 'A-B' is equivalent to 'not', and
+    'B-A' is equivalent to 'not' with the operands switched
+
+    gdsfactory wrapper for phidl.geometry.boolean
+
+    You can also use gdsfactory.drc.boolean_klayout
+
+    Args:
+        A: Component(/Reference) or list of Component(/References).
+        B: Component(/Reference) or list of Component(/References).
+        operation: {'not', 'and', 'or', 'xor', 'A-B', 'B-A', 'A+B'}.
+        precision: float Desired precision for rounding vertex coordinates..
+        num_divisions: number of divisions with which the geometry is divided into
+          multiple rectangular regions. This allows for each region to be
+          processed sequentially, which is more computationally efficient.
+        max_points: The maximum number of vertices within the resulting polygon.
+        layer: Specific layer to put polygon geometry on.
+
+    Returns: Component with polygon(s) of the boolean operations between
+      the 2 input Devices performed.
+
+    Notes
+    -----
+    'A+B' is equivalent to 'or'.
+    'A-B' is equivalent to 'not'.
+    'B-A' is equivalent to 'not' with the operands switched.
+
+    """
+    A = list(A) if isinstance(A, tuple) else A
+    B = list(B) if isinstance(B, tuple) else B
+
+    c = pg.boolean(
+        A=A,
+        B=B,
+        operation=operation,
+        precision=precision,
+        num_divisions=num_divisions,
+        max_points=max_points,
+        layer=layer,
+    )
+    return gf.read.from_phidl(component=c)
+
+
+def test_boolean() -> None:
+    c = gf.Component()
+    e1 = c << gf.components.ellipse()
+    e2 = c << gf.components.ellipse(radii=(10, 6))
+    e3 = c << gf.components.ellipse(radii=(10, 4))
+    e3.movex(5)
+    e2.movex(2)
+    c = boolean(A=[e1, e3], B=e2, operation="A-B")
+    assert len(c.polygons) == 2, len(c.polygons)
+
+
+if __name__ == "__main__":
+    c = gf.Component()
+    e1 = c << gf.components.ellipse()
+    e2 = c << gf.components.ellipse(radii=(10, 6))
+    e3 = c << gf.components.ellipse(radii=(10, 4))
+    e3.movex(5)
+    e2.movex(2)
+    c = boolean(A=[e1, e3], B=e2, operation="A-B")
+    c.show(show_ports=True)
---
+++ packages/gdsfactory/gdsfactory-5.19.0.tar.gz/gdsfactory-5.19.0/gdsfactory/simulation/simphony/model_from_gdsfactory.py
@@ -0,0 +1,55 @@
+import numpy as np
+from scipy.constants import speed_of_light
+from simphony import Model
+from simphony.pins import Pin, PinList
+from simphony.tools import interpolate
+
+import gdsfactory as gf
+import gdsfactory.simulation.lumerical as sim
+from gdsfactory.component import Component
+
+
+def model_from_gdsfactory(
+    component: Component, dirpath=gf.CONFIG["sparameters"], **kwargs
+) -> Model:
+    """Return simphony model from gdsfactory Component Sparameters.
+
+    Args:
+        component: component factory or instance.
+        dirpath: sparameters directory.
+        kwargs: settings.
+
+    """
+    kwargs.pop("function_name", "")
+    kwargs.pop("module", "")
+    component = gf.call_if_func(component, **kwargs)
+    pins, f, s = sim.read_sparameters_lumerical(component=component, dirpath=dirpath)
+
+    def interpolate_sp(freq):
+        return interpolate(freq, f, s)
+
+    Model.pin_count = len(pins)
+    m = Model()
+    m.pins = PinList([Pin(component=m, name=pins[i]) for i, _ in enumerate(pins)])
+    m.__setattr__("sparams", (f, s))
+    m.s_parameters = interpolate_sp
+    m.freq_range = (m.sparams[0][0], m.sparams[0][-1])
+    m.wavelengths = speed_of_light / np.array(f)
+    m.s = s
+    return m
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    c = model_from_gdsfactory(gf.c.mmi2x2())
+    # c = model_from_gdsfactory(gf.c.mmi2x2())
+    # c = model_from_gdsfactory(gf.c.bend_euler())
+    # wav = np.linspace(1520, 1570, 1024) * 1e-9
+    # f = speed_of_light / wav
+    # s = c.s_parameters(freq=f)
+
+    wav = c.wavelengths
+    s = c.s
+    plt.plot(wav * 1e9, np.abs(s[:, 1] ** 2))
+    plt.show()
