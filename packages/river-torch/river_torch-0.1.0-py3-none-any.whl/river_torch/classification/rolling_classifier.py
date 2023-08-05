904f228528e0228518059e23199fed28f03a0c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:57 +0000

    Add package river-torch/0.1.0/river_torch-0.1.0-py3-none-any.whl

---
+++ packages/river-torch/river_torch-0.1.0-py3-none-any.whl/river_torch/classification/rolling_classifier.py
@@ -0,0 +1,256 @@
+from typing import Callable, Dict, List, Type, Union
+
+import pandas as pd
+import torch
+from river import base
+from river.base.typing import ClfTarget
+
+from river_torch.base import RollingDeepEstimator
+from river_torch.utils.tensor_conversion import (df2rolling_tensor,
+                                                 dict2rolling_tensor,
+                                                 labels2onehot, output2proba)
+
+
+class RollingClassifier(RollingDeepEstimator, base.Classifier):
+    """
+    Wrapper that feeds a sliding window of the most recent examples to the wrapped PyTorch classification model. The class also automatically handles increases in the number of classes by adding output neurons in case the number of observed classes exceeds the current number of output neurons.
+
+    Parameters
+    ----------
+    module
+        Torch Module that builds the autoencoder to be wrapped. The Module should accept parameter `n_features` so that the returned model's input shape can be determined based on the number of features in the initial training example.
+    loss_fn
+        Loss function to be used for training the wrapped model. Can be a loss function provided by `torch.nn.functional` or one of the following: 'mse', 'l1', 'cross_entropy', 'binary_crossentropy', 'smooth_l1', 'kl_div'.
+    optimizer_fn
+        Optimizer to be used for training the wrapped model. Can be an optimizer class provided by `torch.optim` or one of the following: "adam", "adam_w", "sgd", "rmsprop", "lbfgs".
+    lr
+        Learning rate of the optimizer.
+    device
+        Device to run the wrapped model on. Can be "cpu" or "cuda".
+    seed
+        Random seed to be used for training the wrapped model.
+    window_size
+        Number of recent examples to be fed to the wrapped model at each step.
+    append_predict
+        Whether to append inputs passed for prediction to the rolling window.
+    **kwargs
+        Parameters to be passed to the `build_fn` function aside from `n_features`.
+    """
+
+    def __init__(
+        self,
+        module: Union[torch.nn.Module, Type[torch.nn.Module]],
+        loss_fn: Union[str, Callable] = "binary_cross_entropy",
+        optimizer_fn: Union[str, Callable] = "sgd",
+        lr: float = 1e-3,
+        device: str = "cpu",
+        seed: int = 42,
+        window_size: int = 10,
+        append_predict: bool = False,
+        **kwargs,
+    ):
+        self.observed_classes = []
+        self.output_layer = None
+        super().__init__(
+            module=module,
+            loss_fn=loss_fn,
+            optimizer_fn=optimizer_fn,
+            window_size=window_size,
+            append_predict=append_predict,
+            device=device,
+            lr=lr,
+            seed=seed,
+            **kwargs,
+        )
+
+    @classmethod
+    def _unit_test_params(cls) -> dict:
+        """
+        Returns a dictionary of parameters to be used for unit testing the respective class.
+
+        Yields
+        -------
+        dict
+            Dictionary of parameters to be used for unit testing the respective class.
+        """
+
+        class MyModule(torch.nn.Module):
+            def __init__(self, n_features):
+                super().__init__()
+                self.hidden_size = 2
+                self.n_features = n_features
+                self.lstm = torch.nn.LSTM(
+                    input_size=n_features, hidden_size=self.hidden_size, num_layers=1
+                )
+                self.softmax = torch.nn.Softmax(dim=-1)
+
+            def forward(self, X, **kwargs):
+                output, (hn, cn) = self.lstm(
+                    X
+                )  # lstm with input, hidden, and internal state
+                hn = hn.view(-1, self.hidden_size)
+                return self.softmax(hn)
+
+        yield {
+            "module": MyModule,
+            "optimizer_fn": "sgd",
+            "lr": 1e-3,
+        }
+
+    @classmethod
+    def _unit_test_skips(self) -> set:
+        """
+        Indicates which checks to skip during unit testing.
+        Most estimators pass the full test suite. However, in some cases, some estimators might not
+        be able to pass certain checks.
+        Returns
+        -------
+        set
+            Set of checks to skip during unit testing.
+        """
+        return {
+            "check_pickling",
+            "check_shuffle_features_no_impact",
+            "check_emerging_features",
+            "check_disappearing_features",
+            "check_predict_proba_one",
+            "check_predict_proba_one_binary",
+        }
+
+    def learn_one(self, x: dict, y: ClfTarget, **kwargs) -> "RollingClassifier":
+        """
+        Performs one step of training with the most recent training examples stored in the sliding window.
+
+        Parameters
+        ----------
+        x
+            Input example.
+        y
+            Target value.
+
+        Returns
+        -------
+        Classifier
+            The classifier itself.
+        """
+
+        if not self.module_initialized:
+            self.kwargs["n_features"] = len(x)
+            self.initialize_module(**self.kwargs)
+
+        self._x_window.append(list(x.values()))
+
+        # check last layer
+        if y not in self.observed_classes:
+            self.observed_classes.append(y)
+
+        # training process
+        x = dict2rolling_tensor(x, self._x_window, device=self.device)
+        if x is not None:
+            return self._learn(x=x, y=y)
+        return self
+
+    def _learn(self, x: torch.Tensor, y: Union[ClfTarget, List[ClfTarget]]):
+        self.module.train()
+        self.optimizer.zero_grad()
+        y_pred = self.module(x)
+        n_classes = y_pred.shape[-1]
+        y = labels2onehot(
+            y=y, classes=self.observed_classes, n_classes=n_classes, device=self.device
+        )
+        loss = self.loss_fn(y_pred, y)
+        loss.backward()
+        self.optimizer.step()
+        return self
+
+    def predict_proba_one(self, x: dict) -> Dict[ClfTarget, float]:
+        """
+        Predict the probability of each label given the most recent examples stored in the sliding window.
+
+        Parameters
+        ----------
+        x
+            Input example.
+
+        Returns
+        -------
+        Dict[ClfTarget, float]
+            Dictionary of probabilities for each label.
+        """
+        if not self.module_initialized:
+            self.kwargs["n_features"] = len(x)
+            self.initialize_module(**self.kwargs)
+
+        x = dict2rolling_tensor(x, self._x_window, device=self.device)
+        if x is not None:
+            self.module.eval()
+            y_pred = self.module(x)
+            proba = output2proba(y_pred, self.observed_classes)
+        else:
+            proba = self._get_default_proba()
+        return proba
+
+    def learn_many(self, X: pd.DataFrame, y: list) -> "RollingClassifier":
+        """
+        Performs one step of training with the most recent training examples stored in the sliding window.
+
+        Parameters
+        ----------
+        x
+            Input examples.
+        y
+            Target values.
+
+        Returns
+        -------
+        Classifier
+            The classifier itself.
+        """
+        # check if model is initialized
+        if not self.module_initialized:
+            self.kwargs["n_features"] = len(X.columns)
+            self.initialize_module(**self.kwargs)
+
+        x = df2rolling_tensor(X, self._x_window, device=self.device)
+        y = labels2onehot(
+            y,
+            self.observed_classes,
+            self.output_layer.out_features,
+            device=self.device,
+        )
+        if X is not None:
+            self._learn(x=x, y=y)
+        return self
+
+    def predict_proba_many(self, X: pd.DataFrame) -> List:
+        if not self.module_initialized:
+            self.kwargs["n_features"] = len(X.columns)
+            self.initialize_module(**self.kwargs)
+
+        batch = df2rolling_tensor(
+            X, self._x_window, device=self.device, update_window=self.append_predict
+        )
+
+        if batch is not None:
+            self.module.eval()
+            y_preds = self.module(batch)
+            probas = output2proba(y_preds, self.observed_classes)
+            if len(probas) < len(X):
+                default_proba = self._get_default_proba()
+                probas = [default_proba] * (len(X) - len(probas)) + probas
+        else:
+            default_proba = self._get_default_proba()
+            probas = [default_proba] * len(X)
+        return probas
+
+    def _get_default_proba(self):
+        if len(self.observed_classes) > 0:
+            mean_proba = (
+                1 / len(self.observed_classes)
+                if len(self.observed_classes) != 0
+                else 0.0
+            )
+            proba = {c: mean_proba for c in self.observed_classes}
+        else:
+            proba = {c: 1.0 for c in self.observed_classes}
+        return proba
---
+++ packages/river-torch/river_torch-0.1.0-py3-none-any.whl/river_torch/utils/tensor_conversion.py
@@ -0,0 +1,96 @@
+from typing import Deque, Dict, List, Union
+
+import numpy as np
+import pandas as pd
+import torch
+from river import base
+from river.base.typing import RegTarget
+
+
+def dict2tensor(x: dict, device="cpu", dtype=torch.float32) -> torch.Tensor:
+    x = torch.tensor([list(x.values())], device=device, dtype=dtype)
+    return x
+
+
+def float2tensor(
+    y: Union[float, int, RegTarget], device="cpu", dtype=torch.float32
+) -> torch.Tensor:
+    y = torch.tensor([[y]], device=device, dtype=dtype)
+    return y
+
+
+def dict2rolling_tensor(
+    x: Dict, window: Deque, device="cpu", dtype=torch.float32, update_window=True
+) -> torch.Tensor:
+    output = None
+    excess_len = len(window) + 1 - window.maxlen
+    if update_window:
+        window.append(list(x.values()))
+        if excess_len >= 0:
+            output = torch.tensor(window, device=device, dtype=dtype)
+    else:
+        if excess_len >= 0:
+            window_copy = list(window)[excess_len:] + [list(x.values())]
+            output = torch.tensor(window_copy, device=device, dtype=dtype)
+
+    return output
+
+
+def df2tensor(x: pd.DataFrame, device="cpu", dtype=torch.float32) -> torch.Tensor:
+    x = torch.tensor(x.values, device=device, dtype=dtype)
+    return x
+
+
+def df2rolling_tensor(
+    x: pd.DataFrame,
+    window: Deque,
+    device="cpu",
+    dtype=torch.float32,
+    update_window=True,
+) -> torch.Tensor:
+    x_old = list(window)
+    if len(window) >= window.maxlen:
+        x_old = x_old[1:]
+    x_new = x.values.tolist()
+    x = x_old + x_new
+    if len(x) >= window.maxlen:
+        x = [x[i : i + window.maxlen] for i in range(len(x) - window.maxlen + 1)]
+        x = torch.tensor(x, device=device, dtype=dtype)
+    else:
+        x = None
+    if update_window:
+        window.extend(x_new)
+    return x
+
+
+def labels2onehot(
+    y: Union[base.typing.ClfTarget, List],
+    classes: list,
+    n_classes: int = None,
+    device="cpu",
+) -> torch.Tensor:
+    if n_classes is None:
+        n_classes = len(classes)
+    if not isinstance(y, list):
+        y = [y]
+    onehot = torch.zeros(len(y), n_classes, device=device)
+    pos_idcs = [classes.index(y_i) for y_i in y]
+    for i, pos_idx in enumerate(pos_idcs):
+        if pos_idx < n_classes:
+            onehot[i, pos_idx] = 1
+    return onehot
+
+
+def output2proba(preds: torch.Tensor, classes: List) -> List:
+    preds = preds.detach().numpy()
+    if preds.shape[1] == 1:
+        preds = np.hstack((preds, 1 - preds))
+    n_unobserved_classes = preds.shape[1] - len(classes)
+    if n_unobserved_classes > 0:
+        classes = classes + [f"unobserved {i}" for i in range(n_unobserved_classes)]
+    probas = (
+        dict(zip(classes, preds[0]))
+        if preds.shape[0] == 1
+        else [dict(zip(classes, pred)) for pred in preds]
+    )
+    return probas
