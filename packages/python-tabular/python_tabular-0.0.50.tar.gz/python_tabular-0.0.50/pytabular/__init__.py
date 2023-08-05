e16f748b89e4f131c7322519b5c13656fd4229cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:54 +0000

    Add package python-tabular/0.0.50/python_tabular-0.0.50.tar.gz

---
+++ packages/python-tabular/python_tabular-0.0.50.tar.gz/python_tabular-0.0.50/pytabular/__init__.py
@@ -0,0 +1,27 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s :: %(module)s :: %(levelname)s :: %(message)s')
+logging.debug('Logging configured...')
+
+logging.debug(f'Setting up file paths for {__file__}')
+import os
+import sys
+dll = os.path.join(os.path.dirname(__file__),"dll")
+sys.path.append(dll)
+sys.path.append(os.path.dirname(__file__))
+
+logging.debug(f'Beginning CLR references...')
+import clr
+logging.debug('Adding Reference Microsoft.AnalysisServices.AdomdClient')
+clr.AddReference('Microsoft.AnalysisServices.AdomdClient')
+logging.debug('Adding Reference Microsoft.AnalysisServices.Tabular')
+clr.AddReference('Microsoft.AnalysisServices.Tabular')
+logging.debug('Adding Reference Microsoft.AnalysisServices')
+clr.AddReference('Microsoft.AnalysisServices')
+
+logging.debug(f"Importing from the rest...")
+from . pytabular import Tabular
+from . basic_checks import Return_Zero_Row_Tables, Table_Last_Refresh_Times, BPA_Violations_To_DF
+from . logic_utils import pd_dataframe_to_m_expression, pandas_datatype_to_tabular_datatype
+from . tabular_tracing import Base_Trace, Refresh_Trace
+from . tabular_editor import Tabular_Editor
+from . best_practice_analyzer import BPA
