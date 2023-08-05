8e55d723f8cad0d1c765374f556591a895114980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:42 +0000

    Add package merkle-patricia-trie/0.3.1/merkle_patricia_trie-0.3.1-py3-none-any.whl

---
+++ packages/merkle-patricia-trie/merkle_patricia_trie-0.3.1-py3-none-any.whl/mpt/__init__.py
@@ -0,0 +1,13 @@
+"""Python implementation of Merkle Patricia Trie.
+
+:copyright: © 2019 by Igor Aleksanov.
+
+:license: MIT, see LICENSE for more details.
+"""
+from .mpt import MerklePatriciaTrie
+
+__version__ = '0.3.1'
+
+name = 'mpt'
+
+__all__ = ['MerklePatriciaTrie']
