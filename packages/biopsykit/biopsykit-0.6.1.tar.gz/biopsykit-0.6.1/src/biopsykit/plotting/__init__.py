028504863260ae3fb1f397045d59e67abc52d74e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:16:05 +0000

    Add package biopsykit/0.6.1/biopsykit-0.6.1.tar.gz

---
+++ packages/biopsykit/biopsykit-0.6.1.tar.gz/biopsykit-0.6.1/src/biopsykit/plotting/__init__.py
@@ -0,0 +1,9 @@
+"""Module providing various customized plotting functions."""
+from biopsykit.plotting.plotting import feature_boxplot, lineplot, multi_feature_boxplot, stacked_barchart
+
+__all__ = [
+    "lineplot",
+    "stacked_barchart",
+    "feature_boxplot",
+    "multi_feature_boxplot",
+]
