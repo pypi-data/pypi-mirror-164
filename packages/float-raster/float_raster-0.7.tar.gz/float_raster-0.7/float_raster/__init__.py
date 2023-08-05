f7e7c9915a4b3ce4c43a91d89adefff9aa61c1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:28 +0000

    Add package float-raster/0.7/float_raster-0.7.tar.gz

---
+++ packages/float-raster/float_raster-0.7.tar.gz/float_raster-0.7/float_raster/__init__.py
@@ -0,0 +1,11 @@
+"""
+Module for rasterizing polygons, with float-precision anti-aliasing on
+ a non-uniform rectangular grid.
+
+See the documentation for float_raster.raster(...) for details.
+"""
+
+from .float_raster import *
+
+__author__ = 'Jan Petykiewicz'
+__version__ = '0.7'
