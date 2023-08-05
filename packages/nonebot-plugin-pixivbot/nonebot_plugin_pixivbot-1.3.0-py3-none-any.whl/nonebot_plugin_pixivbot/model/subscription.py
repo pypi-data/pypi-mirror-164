7f4198e46c7e44625b16522e4d38313a71d97c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:37 +0000

    Add package nonebot-plugin-pixivbot/1.3.0/nonebot_plugin_pixivbot-1.3.0-py3-none-any.whl

---
+++ packages/nonebot-plugin-pixivbot/nonebot_plugin_pixivbot-1.3.0-py3-none-any.whl/nonebot_plugin_pixivbot/model/subscription.py
@@ -0,0 +1,27 @@
+from enum import Enum
+from typing import TypeVar, Generic, Sequence, Dict, Any
+
+from pydantic.generics import GenericModel
+
+from nonebot_plugin_pixivbot.model import PostIdentifier
+
+UID = TypeVar("UID")
+GID = TypeVar("GID")
+
+
+class ScheduleType(Enum):
+    random_bookmark = "random_bookmark"
+    random_recommended_illust = "random_recommended_illust"
+    random_illust = "random_illust"
+    random_user_illust = "random_user_illust"
+    ranking = "ranking"
+
+
+class Subscription(GenericModel, Generic[UID, GID]):
+    type: ScheduleType
+    kwargs: Dict[str, Any]
+    subscriber: PostIdentifier[UID, GID]
+    schedule: Sequence[int]
+
+
+__all__ = ("Subscription", "ScheduleType")
