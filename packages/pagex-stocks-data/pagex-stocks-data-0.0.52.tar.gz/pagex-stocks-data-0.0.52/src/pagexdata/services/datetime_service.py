8bae277695368cb6c7189be0d522ac27ce10cf8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:43 +0000

    Add package pagex-stocks-data/0.0.52/pagex-stocks-data-0.0.52.tar.gz

---
+++ packages/pagex-stocks-data/pagex-stocks-data-0.0.52.tar.gz/pagex-stocks-data-0.0.52/src/pagexdata/services/datetime_service.py
@@ -0,0 +1,12 @@
+import datetime
+
+date_format = "%Y-%m-%d"
+
+
+def create_date(custom_date):
+    date_to_search = custom_date if custom_date else datetime.date.today() - datetime.timedelta(days=1)
+    return datetime.datetime.strptime(str(date_to_search), date_format).replace(tzinfo=datetime.timezone.utc)
+
+
+def get_str_for_date(custom_date):
+    return custom_date.strftime(date_format)
