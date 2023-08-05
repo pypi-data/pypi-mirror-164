6ec3c76b8c878f97d7a90e515fdedc4416347bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:04 +0000

    Add package lnschema_wetlab/0.2.0/lnschema_wetlab-0.2.0-py2.py3-none-any.whl

---
+++ packages/lnschema_wetlab/lnschema_wetlab-0.2.0-py2.py3-none-any.whl/lnschema_wetlab/_core.py
@@ -0,0 +1,56 @@
+from datetime import datetime as datetime
+from typing import Optional  # noqa
+
+from lnschema_bionty import featureset  # noqa
+from sqlmodel import Field, SQLModel
+
+
+def utcnow():
+    return datetime.utcnow().replace(microsecond=0)
+
+
+class version_vvhc(SQLModel, table=True):  # type: ignore
+    """Schema module version."""
+
+    v: Optional[str] = Field(primary_key=True)
+    user_id: str = Field(foreign_key="user.id")
+    time_created: datetime = Field(default_factory=utcnow, nullable=False)
+
+
+class dobject_biometa(SQLModel, table=True):  # type: ignore
+    """Link between dobject and meta."""
+
+    dobject_id: Optional[str] = Field(
+        default=None, foreign_key="dobject.id", primary_key=True
+    )
+    biometa_id: Optional[int] = Field(
+        default=None, foreign_key="biometa.id", primary_key=True
+    )
+
+
+class biometa(SQLModel, table=True):  # type: ignore
+    """Metadata is a combination of biosample and experiment."""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    biosample_id: int = Field(default=None, foreign_key="biosample.id")
+    readout_type_id: int = Field(default=None, foreign_key="readout_type.id")
+    featureset_id: int = Field(default=None, foreign_key="featureset.id")
+
+
+class biosample(SQLModel, table=True):  # type: ignore
+    """Biological samples that are registered in experiments."""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    species_id: str = Field(default=None, foreign_key="species.id")
+
+
+class readout_type(SQLModel, table=True):  # type: ignore
+    """Readouts of experiments."""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    efo_id: str = Field(default=None, index=True)
+    name: str = Field(default=None, index=True)
+    molecule: str = Field(default=None)
+    instrument: str = Field(default=None)
+    measurement: str = Field(default=None)
