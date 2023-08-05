a36b9dfe9bc92d1bac24e4946dbe43a67de840d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:07 +0000

    Add package board-automate/0.1.0/board-automate-0.1.0.tar.gz

---
+++ packages/board-automate/board-automate-0.1.0.tar.gz/board-automate-0.1.0/automate/config.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+from typing import Any, Dict, Optional
+
+from invoke.config import Config, merge_dicts
+
+
+class AutomateConfig(Config):
+    """Encapsulates ~/.automate.yml"""
+
+    prefix = "automate"
+    env_prefix = "AUTOMATE"
+
+    @staticmethod
+    def global_defaults() -> Any:
+        their_defaults = Config.global_defaults()
+
+        my_defaults = {
+            "automate": {
+                "metadata": os.path.expanduser("~/.automate/metadata"),
+                "identity": os.path.expanduser("~/.ssh/id_rsa"),
+                "toolroot": os.path.expanduser("~/.automate/tools"),
+                "boardroot": os.path.expanduser("~/.automate/boards"),
+            },
+            "debug": {"level": "INFO"},
+            "tasks": {"collection_name": "autofile"},
+        }
+
+        return merge_dicts(their_defaults, my_defaults)
