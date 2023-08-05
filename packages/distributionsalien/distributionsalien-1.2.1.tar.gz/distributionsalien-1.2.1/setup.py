2bb8c21e8ae7ba6e510a278a1ea4ed1525b663ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:21 +0000

    Add package distributionsalien/1.2.1/distributionsalien-1.2.1.tar.gz

---
+++ packages/distributionsalien/distributionsalien-1.2.1.tar.gz/distributionsalien-1.2.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsalien',
+      version='1.2.1',
+      description='Gaussian distributions',
+      packages=['distributionsalien'],
+      zip_safe=False)
