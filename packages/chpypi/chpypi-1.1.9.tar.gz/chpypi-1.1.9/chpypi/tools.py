20271d31dc309ca6cd987ae4dc7c6cb8c7db614e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:57 +0000

    Add package chpypi/1.1.9/chpypi-1.1.9.tar.gz

---
+++ packages/chpypi/chpypi-1.1.9.tar.gz/chpypi-1.1.9/chpypi/tools.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# @Created : maker
+# @Time    : 1992
+# @Project : chpypi
+# @Software: PyCharm
+import getproxies
+import login
+
+
+
+
+
+
+
+
+
+
+
+
+class HTTPTOOLS():
+    def __init__(self):
+        ...
+
+    def login_chen_hai(self):
+        return login.LOGIN
+
+    def get_proxies(self):
+        return getproxies.PROXIES
+
+
+
+
+
+
