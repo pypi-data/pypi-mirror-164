66b31a176e6d1824cbd0deff6d2e66d216dd947e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:03 +0000

    Add package bilix/0.8.2a5/bilix-0.8.2a5.tar.gz

---
+++ packages/bilix/bilix-0.8.2a5.tar.gz/bilix-0.8.2a5/bilix/download/__init__.py
@@ -0,0 +1,16 @@
+# base
+from .base_downloader_m3u8 import BaseDownLoaderM3u8
+from .base_downloader_part import BaseDownloaderPart
+# site
+from .downloader_bilibili import DownloaderBilibili
+from .downloader_jable import DownloaderJable
+from .downloader_douyin import DownLoaderDouyin
+from .downloader_yinghuacd import DownloaderYinghuacd
+
+# js runtime require
+try:
+    from .downloader_yhdmp import DownloaderYhdmp
+except Exception as e:
+    from bilix.log import logger as _logger
+
+    _logger.debug(f"Due to {e} Yhdmp is not available")
