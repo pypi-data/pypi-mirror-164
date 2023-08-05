b24e588e4c4d28541e006bf4e06df55aeee9ad38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:16:19 +0000

    Add package pv-sizing/0.6.2/pv_sizing-0.6.2.tar.gz

---
+++ packages/pv-sizing/pv_sizing-0.6.2.tar.gz/pv_sizing-0.6.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages, setuptools
+
+setup(
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    name='pv_sizing',
+    version='0.6.2',
+    license='MIT',
+    author="Kiril Ivanov Kurtev",
+    author_email='brakisto2015@gmail.com',
+    packages=['dimension', 'utils', 'example_data'],
+    package_dir={'': 'src'},
+    url='https://github.com/brakisto/PV-sizing',
+    keywords='photovoltaic sizing batteries financial analysis',
+    install_requires=[
+          'numpy',
+          'numpy-financial',
+          'pandas',
+          'pvlib'
+      ])
