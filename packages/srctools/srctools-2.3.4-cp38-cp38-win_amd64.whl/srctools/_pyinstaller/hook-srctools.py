6c50a801aa339c4c6f66bd1f19912178f6fbb6ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:20 +0000

    Add package srctools/2.3.4/srctools-2.3.4-cp38-cp38-win_amd64.whl

---
+++ packages/srctools/srctools-2.3.4-cp38-cp38-win_amd64.whl/srctools/_pyinstaller/hook-srctools.py
@@ -0,0 +1,16 @@
+"""Pyinstaller hooks for the main module."""
+from PyInstaller.utils.hooks import get_module_file_attribute  # type: ignore
+import os
+
+srctools_loc = os.path.dirname(get_module_file_attribute('srctools'))
+
+datas = [
+    # Add our FGD database.
+    (os.path.join(srctools_loc, 'fgd.lzma'), 'srctools'),
+]
+
+excludedimports = [
+    'PIL',  # Pillow is optional for VTF, the user will import if required.
+    'tkinter',  # Same for Tkinter.
+    'wx',  # And wxWidgets.
+]
