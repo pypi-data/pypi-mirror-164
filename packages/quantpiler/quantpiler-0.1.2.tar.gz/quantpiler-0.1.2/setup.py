1f6e1882ee967d35ce8fdf811e7df63541302ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:28 +0000

    Add package quantpiler/0.1.2/quantpiler-0.1.2.tar.gz

---
+++ packages/quantpiler/quantpiler-0.1.2.tar.gz/quantpiler-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['quantpiler']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['qiskit>=0.37.1,<0.38.0']
+
+setup_kwargs = {
+    'name': 'quantpiler',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'AveryanAlex',
+    'author_email': 'alex@averyan.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
