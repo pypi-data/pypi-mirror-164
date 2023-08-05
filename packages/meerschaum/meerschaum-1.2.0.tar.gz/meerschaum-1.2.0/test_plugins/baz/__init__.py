51177da39ace554184c0f9e1817899641c4839d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:11 +0000

    Add package meerschaum/1.2.0/meerschaum-1.2.0.tar.gz

---
+++ packages/meerschaum/meerschaum-1.2.0.tar.gz/meerschaum-1.2.0/test_plugins/baz/__init__.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2022 bmeares <bmeares@fedora>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+print('init baz')
