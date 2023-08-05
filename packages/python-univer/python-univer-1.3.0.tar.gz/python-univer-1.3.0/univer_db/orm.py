472fb2ffe0145c9e494d933b753c41c741757573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:17 +0000

    Add package python-univer/1.3.0/python-univer-1.3.0.tar.gz

---
+++ packages/python-univer/python-univer-1.3.0.tar.gz/python-univer-1.3.0/univer_db/orm.py
@@ -0,0 +1,23 @@
+import urllib
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+def get_session(config):
+    quoted = urllib.parse.quote_plus('DRIVER={driver};Server={host};Database={db};UID={user};PWD={password};TDS_Version=7.3;Port=1433;'.format(
+        driver=config.driver, host=config.host, db=config.db, user=config.user, password=config.password))
+    engine = create_engine('mssql+pyodbc:///?odbc_connect={}'.format(quoted))
+
+    engine.connect()
+
+    session_factory = sessionmaker(bind=engine)
+
+    return session_factory
+
+
+def get_base():
+    return Base
