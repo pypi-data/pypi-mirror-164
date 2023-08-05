370209a48ed7e38393166bfae3c710deff9e562c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:46 +0000

    Add package immosheets/1.0.10/immosheets-1.0.10-py3-none-any.whl

---
+++ packages/immosheets/immosheets-1.0.10-py3-none-any.whl/immosheets/seloger/seloger_service.py
@@ -0,0 +1,31 @@
+import requests
+from typing import Generator
+from .seloger_search_query import SelogerSearchQuery
+from ..real_estate import RealEstate
+from ..real_estate_service import RealEstateService
+from ..settings import settings
+from ..target import Target
+
+class SelogerService(RealEstateService):
+    def __init__(self, api_key: str) -> None:
+        self.url = settings.seloger_url
+        self.session = requests.Session
+        self.headers = {
+            "X-RapidAPI-Key": api_key,
+            "X-RapidAPI-Host": settings.seloger_host
+        }
+    
+    def search(self, query: SelogerSearchQuery) -> Generator[list[RealEstate], None, None]:
+        retrieve_page = lambda: self.session.get(
+            url=self.url,
+            headers=self.headers,
+            params=query.dict()
+        )
+
+        total: int = retrieve_page().json()['totalCount']     
+        num_pages: int = total // settings.page_size
+
+        for page in range(2, num_pages + 1):
+            query.pageIndex = page
+
+            yield RealEstate.from_response(retrieve_page(), Target.SELOGER)
