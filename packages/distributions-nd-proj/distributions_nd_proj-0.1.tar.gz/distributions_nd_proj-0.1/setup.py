fafd99df9178732b0fdac5107effb2700be79ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:45 +0000

    Add package distributions-nd-proj/0.1/distributions_nd_proj-0.1.tar.gz

---
+++ packages/distributions-nd-proj/distributions_nd_proj-0.1.tar.gz/distributions_nd_proj-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_nd_proj',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_nd_proj'],
+      zip_safe=False)
