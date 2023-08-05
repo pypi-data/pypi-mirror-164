4a3ac82146532df09bd98ff8179ba92a9dd5c8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:09 +0000

    Add package cronaion/1.0.3/cronaion-1.0.3.tar.gz

---
+++ packages/cronaion/cronaion-1.0.3.tar.gz/cronaion-1.0.3/cronaion/examples.py
@@ -0,0 +1,19 @@
+"""Examples for Aion."""
+
+from cronaion import Aion
+
+watcher = Aion()
+
+
+@watcher.watch("1 3 * * 0")
+def check_the_mail():
+    print("I will run at 03:01 every Sunday.")
+
+
+@watcher.watch("*/1 * * * *")
+def blink_my_eyes():
+    print("I will run every 30 seconds.")
+
+
+if __name__ == "__main__":
+    watcher.start()
