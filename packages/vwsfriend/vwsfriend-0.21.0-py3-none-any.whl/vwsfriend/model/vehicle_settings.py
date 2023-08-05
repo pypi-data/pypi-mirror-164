52f1b90fffbf665cb284ef74056fa5e15e5910a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:50 +0000

    Add package vwsfriend/0.21.0/vwsfriend-0.21.0-py3-none-any.whl

---
+++ packages/vwsfriend/vwsfriend-0.21.0-py3-none-any.whl/vwsfriend/model/vehicle_settings.py
@@ -0,0 +1,22 @@
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from vwsfriend.model.base import Base
+
+
+class VehicleSettings(Base):
+    __tablename__ = 'vehicle_settings'
+    vehicle_vin = Column(String, ForeignKey('vehicles.vin'), primary_key=True)
+    vehicle = relationship("Vehicle", back_populates="settings")
+
+    primary_capacity = Column(Integer)
+    primary_capacity_total = Column(Integer)
+    primary_wltp_range = Column(Integer)
+    secondary_capacity = Column(Integer)
+    secondary_capacity_total = Column(Integer)
+    secondary_wltp_range = Column(Integer)
+    timezone = Column(String(256))
+
+    def __init__(self, vehicle):
+        self.vehicle = vehicle
