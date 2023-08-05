d49f505f83d7babd2fcc00d5d83630707d595fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:57 +0000

    Add package yappa/0.4.28/yappa-0.4.28-py3-none-any.whl

---
+++ packages/yappa/yappa-0.4.28-py3-none-any.whl/yappa/packaging/common.py
@@ -0,0 +1,27 @@
+import platform
+
+from click import ClickException
+
+REQUIRED_PACKAGES = ("httpx", "PyYAML")
+ENCODING = "utf-8"
+IS_WINDOWS = any(platform.win32_ver())
+
+
+def validate_requirements_file(requirements_filename):
+    if IS_WINDOWS:
+        return
+    try:
+        with open(requirements_filename, encoding=ENCODING) as f:
+            requirements = f.read()
+    except FileNotFoundError:
+        raise ClickException(
+            "Couldn't find requirements file. Please specify "
+            "it in yappa.yaml"
+        )
+    for package in REQUIRED_PACKAGES:
+        if package not in requirements:
+            raise ClickException(
+                f"{package} package should be in requirements."
+                "Please update requirements running "
+                "'$pip freeze > requirements.txt'"
+            )
