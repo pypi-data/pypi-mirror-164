892c65b28c8e99a36fb7399c03b990c37ac4bda7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:44 +0000

    Add package pysupplier/1.0.0/pysupplier-1.0.0-py3-none-any.whl

---
+++ packages/pysupplier/pysupplier-1.0.0-py3-none-any.whl/gys/Api_Delete.py
@@ -0,0 +1,12 @@
+#删除
+from k3cloud_webapi_sdk.main import K3CloudApiSdk
+
+api_sdk = K3CloudApiSdk()
+
+api_sdk.InitConfig('62f49d037697ee', '于洋', '232258_100AXajE2ooX7WUP5/Qq58Tr4L3WWsoI', 'ed4939dc463142698c8459afd45e0379','http://cellprobio.gnway.cc/k3cloud')
+#api_sdk.Init(config_path='conf.ini', config_node='config')
+
+print(api_sdk.Delete("BD_Supplier", { "CreateOrgId": 0,
+    "Numbers": ['V001192'],
+    "Ids": "",
+    "NetworkCtrl": ""}))
