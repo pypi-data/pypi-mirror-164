833646e42623f7bf34bec0bd9b1966c72df4ff01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:16 +0000

    Add package HolaMundoLibreria/0.2/HolaMundoLibreria-0.2.tar.gz

---
+++ packages/HolaMundoLibreria/HolaMundoLibreria-0.2.tar.gz/HolaMundoLibreria-0.2/setup.py
@@ -0,0 +1,10 @@
+from importlib.resources import Package
+from setuptools import find_packages, setup
+
+setup(
+    name="HolaMundoLibreria",
+    version ="0.2",
+    packages = find_packages(),
+    py_modules=["HolaMundoLibreria"]
+
+)
