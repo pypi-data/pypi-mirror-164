09769e0db8003fe4905cd4be0c4e4746c60fa733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:53 +0000

    Add package whoisit/2.6.3/whoisit-2.6.3.tar.gz

---
+++ packages/whoisit/whoisit-2.6.3.tar.gz/whoisit-2.6.3/whoisit/overrides.py
@@ -0,0 +1,19 @@
+"""
+    Various RDAP servers are either incorrect or not listed in the IANA
+    bootstrap data. This file contains a list of overrides that are overlayed
+    onto the IANA data by default.
+"""
+
+iana_overrides = {
+
+    'domain': {
+
+        # 2021-11-30 - .de has an RDAP endpoint it's just not listed
+        'de': ['https://rdap.denic.de/'],
+
+        # 2022-01-15 - .ch has an RDAP endpoint it's just not listed
+        'ch': ['https://rdap.nic.ch/'],
+
+    }
+
+}
