7683ff17eb321f145250141804827bfc98eef35a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:59 +0000

    Add package evalml/0.56.1/evalml-0.56.1-py3-none-any.whl

---
+++ packages/evalml/evalml-0.56.1-py3-none-any.whl/evalml/pipelines/components/transformers/scalers/standard_scaler.py
@@ -0,0 +1,66 @@
+"""A transformer that standardizes input features by removing the mean and scaling to unit variance."""
+import pandas as pd
+from sklearn.preprocessing import StandardScaler as SkScaler
+from woodwork.logical_types import Boolean, Categorical, Integer
+
+from evalml.pipelines.components.transformers import Transformer
+from evalml.utils import infer_feature_types
+
+
+class StandardScaler(Transformer):
+    """A transformer that standardizes input features by removing the mean and scaling to unit variance.
+
+    Args:
+        random_seed (int): Seed for the random number generator. Defaults to 0.
+    """
+
+    name = "Standard Scaler"
+    hyperparameter_ranges = {}
+    """{}"""
+
+    def __init__(self, random_seed=0, **kwargs):
+        parameters = {}
+        parameters.update(kwargs)
+
+        scaler = SkScaler(**parameters)
+        super().__init__(
+            parameters=parameters,
+            component_obj=scaler,
+            random_seed=random_seed,
+        )
+
+    def transform(self, X, y=None):
+        """Transform data using the fitted standard scaler.
+
+        Args:
+            X (pd.DataFrame): The input training data of shape [n_samples, n_features].
+            y (pd.Series, optional): The target training data of length [n_samples].
+
+        Returns:
+            pd.DataFrame: Transformed data.
+        """
+        X = infer_feature_types(X)
+        X = X.ww.select(exclude=["datetime"])
+        X_t = self._component_obj.transform(X)
+        X_t_df = pd.DataFrame(X_t, columns=X.columns, index=X.index)
+
+        schema = X.ww.select(
+            exclude=[Integer, Categorical, Boolean],
+            return_schema=True,
+        )
+        X_t_df.ww.init(schema=schema)
+        return X_t_df
+
+    def fit_transform(self, X, y=None):
+        """Fit and transform data using the standard scaler component.
+
+        Args:
+            X (pd.DataFrame): The input training data of shape [n_samples, n_features].
+            y (pd.Series, optional): The target training data of length [n_samples].
+
+        Returns:
+            pd.DataFrame: Transformed data.
+        """
+        X = infer_feature_types(X)
+        X = X.select_dtypes(exclude=["datetime"])
+        return self.fit(X, y).transform(X, y)
---
+++ packages/evalml/evalml-0.56.1-py3-none-any.whl/evalml/tests/component_tests/test_standard_scaler.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import pytest
+import woodwork as ww
+from woodwork.logical_types import Boolean, Categorical, Double, Integer
+
+from evalml.pipelines.components import StandardScaler
+
+
+@pytest.mark.parametrize(
+    "X_df",
+    [
+        pd.DataFrame(pd.Series([1, 2, 3], dtype="Int64")),
+        pd.DataFrame(pd.Series([1.0, 2.0, 3.0], dtype="float")),
+        pd.DataFrame(pd.Series([True, False, True], dtype="boolean")),
+    ],
+)
+def test_standard_scaler_woodwork_custom_overrides_returned_by_components(X_df):
+    y = pd.Series([1, 2, 1])
+    override_types = [Integer, Double, Categorical, Boolean]
+    for logical_type in override_types:
+        try:
+            X_df.ww.init(logical_types={0: logical_type})
+        except ww.exceptions.TypeConversionError:
+            continue
+
+        std_scaler = StandardScaler()
+        std_scaler.fit(X_df, y)
+        transformed = std_scaler.transform(X_df, y)
+        assert isinstance(transformed, pd.DataFrame)
+        assert {k: type(v) for k, v in transformed.ww.logical_types.items()} == {
+            0: Double,
+        }
