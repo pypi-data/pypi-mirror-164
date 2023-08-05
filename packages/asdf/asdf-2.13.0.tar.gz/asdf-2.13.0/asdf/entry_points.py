439e37f7846c3445a18f348dad830296049c8ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:09 +0000

    Add package asdf/2.13.0/asdf-2.13.0.tar.gz

---
+++ packages/asdf/asdf-2.13.0.tar.gz/asdf-2.13.0/asdf/entry_points.py
@@ -0,0 +1,71 @@
+import warnings
+
+from pkg_resources import iter_entry_points
+
+from .exceptions import AsdfWarning
+from .extension import ExtensionProxy
+from .resource import ResourceMappingProxy
+
+RESOURCE_MAPPINGS_GROUP = "asdf.resource_mappings"
+EXTENSIONS_GROUP = "asdf.extensions"
+LEGACY_EXTENSIONS_GROUP = "asdf_extensions"
+
+
+def get_resource_mappings():
+    return _list_entry_points(RESOURCE_MAPPINGS_GROUP, ResourceMappingProxy)
+
+
+def get_extensions():
+    extensions = _list_entry_points(EXTENSIONS_GROUP, ExtensionProxy)
+    legacy_extensions = _list_entry_points(LEGACY_EXTENSIONS_GROUP, ExtensionProxy)
+
+    return extensions + legacy_extensions
+
+
+def _list_entry_points(group, proxy_class):
+    results = []
+
+    entry_points = list(iter_entry_points(group=group))
+
+    # The order of plugins may be significant, since in the case of
+    # duplicate functionality the first plugin in the list takes
+    # precedence.  It's not clear if entry points are ordered
+    # in a consistent way across systems so we explicitly sort
+    # by package name.  Plugins from this package are placed
+    # at the end so that other packages can override them.
+    asdf_entry_points = [e for e in entry_points if e.dist.project_name == "asdf"]
+    other_entry_points = sorted(
+        (e for e in entry_points if e.dist.project_name != "asdf"), key=lambda e: e.dist.project_name
+    )
+
+    for entry_point in other_entry_points + asdf_entry_points:
+        package_name = entry_point.dist.project_name
+        package_version = entry_point.dist.version
+
+        def _handle_error(e):
+            warnings.warn(
+                "{} plugin from package {}=={} failed to load:\n\n"
+                "{}: {}".format(
+                    group,
+                    package_name,
+                    package_version,
+                    e.__class__.__name__,
+                    e,
+                ),
+                AsdfWarning,
+            )
+
+        try:
+            elements = entry_point.load()()
+
+            if not isinstance(elements, list):
+                elements = [elements]
+
+            for element in elements:
+                try:
+                    results.append(proxy_class(element, package_name=package_name, package_version=package_version))
+                except Exception as e:
+                    _handle_error(e)
+        except Exception as e:
+            _handle_error(e)
+    return results
---
+++ packages/asdf/asdf-2.13.0.tar.gz/asdf-2.13.0/asdf/tests/test_entry_points.py
@@ -0,0 +1,148 @@
+import pkg_resources
+import pytest
+from pkg_resources import EntryPoint
+
+from asdf import entry_points
+from asdf._version import version as asdf_package_version
+from asdf.exceptions import AsdfWarning
+from asdf.extension import ExtensionProxy
+from asdf.resource import ResourceMappingProxy
+
+
+@pytest.fixture
+def mock_entry_points():
+    return []
+
+
+@pytest.fixture(autouse=True)
+def monkeypatch_entry_points(monkeypatch, mock_entry_points):
+    def _iter_entry_points(*, group):
+        for candidate_group, name, func_name in mock_entry_points:
+            if candidate_group == group:
+                yield EntryPoint(
+                    name,
+                    "asdf.tests.test_entry_points",
+                    attrs=(func_name,),
+                    dist=pkg_resources.get_distribution("asdf"),
+                )
+
+    monkeypatch.setattr(entry_points, "iter_entry_points", _iter_entry_points)
+
+
+def resource_mappings_entry_point_successful():
+    return [
+        {"http://somewhere.org/schemas/foo-1.0.0": b"foo"},
+        {"http://somewhere.org/schemas/bar-1.0.0": b"bar"},
+    ]
+
+
+def resource_mappings_entry_point_failing():
+    raise Exception("NOPE")
+
+
+def resource_mappings_entry_point_bad_element():
+    return [
+        {"http://somewhere.org/schemas/baz-1.0.0": b"baz"},
+        object(),
+        {"http://somewhere.org/schemas/foz-1.0.0": b"foz"},
+    ]
+
+
+def test_get_resource_mappings(mock_entry_points):
+    mock_entry_points.append(("asdf.resource_mappings", "successful", "resource_mappings_entry_point_successful"))
+    mappings = entry_points.get_resource_mappings()
+    assert len(mappings) == 2
+    for m in mappings:
+        assert isinstance(m, ResourceMappingProxy)
+        assert m.package_name == "asdf"
+        assert m.package_version == asdf_package_version
+
+    mock_entry_points.clear()
+    mock_entry_points.append(("asdf.resource_mappings", "failing", "resource_mappings_entry_point_failing"))
+    with pytest.warns(AsdfWarning, match="Exception: NOPE"):
+        mappings = entry_points.get_resource_mappings()
+    assert len(mappings) == 0
+
+    mock_entry_points.clear()
+    mock_entry_points.append(("asdf.resource_mappings", "bad_element", "resource_mappings_entry_point_bad_element"))
+    with pytest.warns(AsdfWarning, match="TypeError: Resource mapping must implement the Mapping interface"):
+        mappings = entry_points.get_resource_mappings()
+    assert len(mappings) == 2
+
+
+class MinimumExtension:
+    def __init__(self, extension_uri):
+        self._extension_uri = extension_uri
+
+    @property
+    def extension_uri(self):
+        return self._extension_uri
+
+
+def extensions_entry_point_successful():
+    return [
+        MinimumExtension("http://somewhere.org/extensions/foo-1.0"),
+        MinimumExtension("http://somewhere.org/extensions/bar-1.0"),
+    ]
+
+
+def extensions_entry_point_failing():
+    raise Exception("NOPE")
+
+
+def extensions_entry_point_bad_element():
+    return [
+        MinimumExtension("http://somewhere.org/extensions/baz-1.0"),
+        object(),
+        MinimumExtension("http://somewhere.org/extensions/foz-1.0"),
+    ]
+
+
+class LegacyExtension:
+    types = []
+    tag_mapping = []
+    url_mapping = []
+
+
+class FauxLegacyExtension:
+    pass
+
+
+def test_get_extensions(mock_entry_points):
+    mock_entry_points.append(("asdf.extensions", "successful", "extensions_entry_point_successful"))
+    extensions = entry_points.get_extensions()
+    assert len(extensions) == 2
+    for e in extensions:
+        assert isinstance(e, ExtensionProxy)
+        assert e.package_name == "asdf"
+        assert e.package_version == asdf_package_version
+
+    mock_entry_points.clear()
+    mock_entry_points.append(("asdf.extensions", "failing", "extensions_entry_point_failing"))
+    with pytest.warns(AsdfWarning, match="Exception: NOPE"):
+        extensions = entry_points.get_extensions()
+    assert len(extensions) == 0
+
+    mock_entry_points.clear()
+    mock_entry_points.append(("asdf.extensions", "bad_element", "extensions_entry_point_bad_element"))
+    with pytest.warns(
+        AsdfWarning, match="TypeError: Extension must implement the Extension or AsdfExtension interface"
+    ):
+        extensions = entry_points.get_extensions()
+    assert len(extensions) == 2
+
+    mock_entry_points.clear()
+    mock_entry_points.append(("asdf_extensions", "legacy", "LegacyExtension"))
+    extensions = entry_points.get_extensions()
+    assert len(extensions) == 1
+    for e in extensions:
+        assert isinstance(e, ExtensionProxy)
+        assert e.package_name == "asdf"
+        assert e.package_version == asdf_package_version
+        assert e.legacy is True
+
+    mock_entry_points.clear()
+    mock_entry_points.append(("asdf_extensions", "failing", "FauxLegacyExtension"))
+    with pytest.warns(AsdfWarning, match="TypeError"):
+        extensions = entry_points.get_extensions()
+    assert len(extensions) == 0
