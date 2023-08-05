b16335e8dac3257fd0d839cc80f0fb579774df5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:35 +0000

    Add package codex/0.12.1/codex-0.12.1-py3-none-any.whl

---
+++ packages/codex/codex-0.12.1-py3-none-any.whl/codex/serializers/browser.py
@@ -0,0 +1,249 @@
+"""Serializers for the browser view."""
+from abc import ABC
+
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from rest_framework.serializers import (
+    BooleanField,
+    CharField,
+    ChoiceField,
+    DecimalField,
+    Field,
+    IntegerField,
+    ListField,
+    Serializer,
+)
+
+from codex.serializers.choices import CHOICES, VUETIFY_NULL_CODE
+from codex.serializers.mixins import UNIONFIX_PREFIX, BrowserCardOPDSBaseSerializer
+
+
+VUETIFY_NULL_CODE_STR = str(VUETIFY_NULL_CODE)
+
+
+def validate_decades(decades):
+    """Validate decades."""
+    if not decades:
+        return
+    # * We don't need a whole db call just to be perfectly accurate
+    # * -1s are decoded back into None before validation
+    exists = False
+    for decade in decades:
+        if decade is None or decade % 10 == 0:
+            exists = True
+            break
+    if not exists:
+        raise ValidationError(_(f"Invalid decade in: {decades}"))
+
+
+def validate_int_null(values):
+    """
+    Use a special code for null.
+
+    Because if a vuetify component has a null key it changes it to the
+    array index.
+    """
+    for index, value in enumerate(values):
+        if value == VUETIFY_NULL_CODE:
+            values[index] = None
+    return values
+
+
+def validate_str_null(values):
+    """
+    Use a special code for null.
+
+    Because if a vuetify component has a null key it changes it to the
+    array index. This is the version for CharFields.
+    """
+    for index, value in enumerate(values):
+        if value == VUETIFY_NULL_CODE_STR:
+            values[index] = None
+    return values
+
+
+class BrowserSettingsShowGroupFlagsSerializer(Serializer):
+    """Show Group Flags."""
+
+    p = BooleanField()
+    i = BooleanField()
+    s = BooleanField()
+    v = BooleanField()
+
+
+class FilterListField(ListField, ABC):
+    """Filter List field with custom arguments."""
+
+    CHILD_CLASS = Field
+    VALIDATORS = tuple()
+
+    def __init__(self, *args, **kwargs):
+        """Apply the subclass's arguments."""
+        super().__init__(
+            *args,
+            child=self.CHILD_CLASS(allow_null=True, **kwargs),
+            required=False,
+            validators=self.VALIDATORS,
+        )
+
+
+class DecimalListField(FilterListField):
+    """Decimal List Field with validation."""
+
+    CHILD_CLASS = DecimalField
+    VALIDATORS = (validate_int_null,)
+
+
+class IntListField(FilterListField):
+    """Integer List Field with validation."""
+
+    CHILD_CLASS = IntegerField
+    VALIDATORS = (validate_int_null,)
+
+
+class DecadeListField(IntListField):
+    """Integer List Field with validation for decades."""
+
+    VALIDATORS = (validate_int_null, validate_decades)
+
+
+class CharListField(FilterListField):
+    """Char List Field with validation."""
+
+    CHILD_CLASS = CharField
+    VALIDATORS = (validate_str_null,)
+
+
+class BrowserSettingsFilterSerializer(Serializer):
+    """Filter values for settings."""
+
+    bookmark = ChoiceField(
+        choices=tuple(CHOICES["bookmarkFilter"].keys()), required=False
+    )
+    # Dynamic filters
+    community_rating = DecimalListField(
+        max_digits=5,
+        decimal_places=2,
+        coerce_to_string=False,
+    )
+    characters = IntListField()
+    country = CharListField()
+    creators = IntListField()
+    critical_rating = DecimalListField(
+        max_digits=5,
+        decimal_places=2,
+        coerce_to_string=False,
+    )
+    decade = DecadeListField()
+    format = CharListField()
+    genres = IntListField()
+    language = CharListField()
+    locations = IntListField()
+    age_rating = CharListField()
+    read_ltr = ListField(child=BooleanField(), required=False)
+    series_groups = IntListField()
+    story_arcs = IntListField()
+    tags = IntListField()
+    teams = IntListField()
+    year = IntListField()
+
+
+class BrowserSettingsSerializer(Serializer):
+    """
+    Browser Settings that the user can change.
+
+    This is the only browse serializer that's submitted.
+    It is also sent to the browser as part of BrowserOpenedSerializer.
+    """
+
+    filters = BrowserSettingsFilterSerializer(required=False)
+    autoquery = CharField(allow_blank=True, required=False)
+    top_group = ChoiceField(choices=tuple(CHOICES["topGroup"].keys()), required=False)
+    order_by = ChoiceField(choices=tuple(CHOICES["orderBy"].keys()), required=False)
+    order_reverse = BooleanField(required=False)
+    show = BrowserSettingsShowGroupFlagsSerializer(required=False)
+
+
+class BrowserCardSerializer(BrowserCardOPDSBaseSerializer):
+    """Browse card displayed in the browser."""
+
+    publisher_name = CharField(
+        read_only=True, source=UNIONFIX_PREFIX + "publisher_name"
+    )
+    series_name = CharField(read_only=True, source=UNIONFIX_PREFIX + "series_name")
+    volume_name = CharField(read_only=True, source=UNIONFIX_PREFIX + "volume_name")
+    name = CharField(read_only=True, source=UNIONFIX_PREFIX + "name")
+    issue = DecimalField(
+        max_digits=16,
+        decimal_places=3,
+        read_only=True,
+        coerce_to_string=False,
+        source=UNIONFIX_PREFIX + "issue",
+    )
+    issue_suffix = CharField(read_only=True, source=UNIONFIX_PREFIX + "issue_suffix")
+    order_value = CharField(read_only=True, source=UNIONFIX_PREFIX + "order_value")
+    page_count = IntegerField(read_only=True, source=UNIONFIX_PREFIX + "page_count")
+    read_ltr = BooleanField(read_only=True, source=UNIONFIX_PREFIX + "read_ltr")
+
+
+class BrowserRouteSerializer(Serializer):
+    """A vue route for the browser."""
+
+    group = CharField(read_only=True)
+    pk = IntegerField(read_only=True)
+    page = IntegerField(read_only=True)
+
+
+class BrowserAdminFlagsSerializer(Serializer):
+    """These choices change with browse context."""
+
+    enable_folder_view = BooleanField(read_only=True)
+
+
+class BrowserTitleSerializer(Serializer):
+    """Elements for constructing the browse title."""
+
+    parent_name = CharField(read_only=True, allow_null=True)
+    group_name = CharField(read_only=True)
+    group_count = IntegerField(read_only=True, allow_null=True)
+
+
+class BrowserPageSerializer(Serializer):
+    """The main browse list."""
+
+    NUM_AUTOCOMPLETE_QUERIES = 10
+
+    browser_title = BrowserTitleSerializer(read_only=True)
+    model_group = CharField(read_only=True)
+    up_route = BrowserRouteSerializer(allow_null=True, read_only=True)
+    obj_list = ListField(
+        child=BrowserCardSerializer(read_only=True), allow_empty=True, read_only=True
+    )
+    issue_max = DecimalField(
+        max_digits=16,
+        decimal_places=3,
+        read_only=True,
+        coerce_to_string=False,
+    )
+    num_pages = IntegerField(read_only=True)
+    admin_flags = BrowserAdminFlagsSerializer(read_only=True)
+    libraries_exist = BooleanField(read_only=True)
+    queries = ListField(
+        child=CharField(read_only=True), allow_empty=True, read_only=True
+    )
+    covers_timestamp = IntegerField(read_only=True)
+
+
+class VersionsSerializer(Serializer):
+    """Codex version information."""
+
+    installed = CharField(read_only=True)
+    latest = CharField(read_only=True)
+
+
+class BrowserOpenedSerializer(Serializer):
+    """Component open settings."""
+
+    settings = BrowserSettingsSerializer(read_only=True)
+    browser_page = BrowserPageSerializer(read_only=True)
+    versions = VersionsSerializer(read_only=True)
---
+++ packages/codex/codex-0.12.1-py3-none-any.whl/codex/views/session.py
@@ -0,0 +1,137 @@
+"""Manage user sessions with appropriate defaults."""
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+from rest_framework.views import APIView
+
+from codex.serializers.bookmark import ComicReaderSettingsSerializer
+from codex.serializers.browser import BrowserSettingsSerializer
+from codex.serializers.choices import DEFAULTS
+from codex.settings.logging import get_logger
+from codex.views.auth import IsAuthenticatedOrEnabledNonUsers
+
+
+LOG = get_logger(__name__)
+
+
+class SessionViewBase(APIView):
+    """Generic Session View."""
+
+    CREDIT_PERSON_UI_FIELD = "creators"
+    _DYNAMIC_FILTER_DEFAULTS = {
+        "age_rating": [],
+        "autoquery": "",
+        "characters": [],
+        "country": [],
+        CREDIT_PERSON_UI_FIELD: [],
+        "community_rating": [],
+        "critical_rating": [],
+        "decade": [],
+        "format": [],
+        "genres": [],
+        "language": [],
+        "locations": [],
+        "read_ltr": [],
+        "series_groups": [],
+        "story_arcs": [],
+        "tags": [],
+        "teams": [],
+        "year": [],
+    }
+    FILTER_ATTRIBUTES = frozenset(_DYNAMIC_FILTER_DEFAULTS.keys())
+    SESSION_KEY = "UNDEFINED"  # Must override
+    SERIALIZER = Serializer
+    BROWSER_KEY = "browser"
+    READER_KEY = "reader"
+    SESSION_DEFAULTS = {
+        BROWSER_KEY: {
+            "filters": {
+                "bookmark": DEFAULTS["bookmarkFilter"],
+                **_DYNAMIC_FILTER_DEFAULTS,
+            },
+            "autoquery": DEFAULTS["autoquery"],
+            "top_group": DEFAULTS["topGroup"],
+            "order_by": DEFAULTS["orderBy"],
+            "order_reverse": False,
+            "show": DEFAULTS["show"],
+            "route": DEFAULTS["route"],
+        },
+        READER_KEY: {"display": {"fit_to": DEFAULTS["fitTo"], "two_pages": False}},
+    }
+
+    def _get_defaults_and_session(self, session_key: str):
+        """Get the session defaults and the session."""
+        defaults = self.SESSION_DEFAULTS[session_key]
+        session = self.request.session.get(session_key, defaults)
+        return defaults, session
+
+    @classmethod
+    def _get_source_values_or_set_defaults(cls, defaults_dict, source_dict, data):
+        """Recursively copy source_dict values into data or use defaults."""
+        for key, default_value in defaults_dict.items():
+            data[key] = source_dict.get(key, default_value)
+            if data[key] is None:
+                # extra check for migrated or corrupt data
+                data[key] = default_value
+            if isinstance(default_value, dict):
+                cls._get_source_values_or_set_defaults(
+                    default_value, source_dict.get(key, {}), data[key]
+                )
+
+    def load_params_from_session(self):
+        """Set the params from view session, creating missing values from defaults."""
+        defaults, session = self._get_defaults_and_session(self.SESSION_KEY)
+        data = {}
+
+        self._get_source_values_or_set_defaults(defaults, session, data)
+        self.params = data
+
+    def get_from_session(self, key, session_key=None):
+        """Get one key from the session or its default."""
+        if session_key is None:
+            session_key = self.SESSION_KEY
+        defaults, session = self._get_defaults_and_session(session_key)
+        value = session.get(key, defaults[key])
+        return value
+
+    def save_params_to_session(self):
+        """Save the session from params with defaults for missing values."""
+        try:
+            defaults = self.SESSION_DEFAULTS[self.SESSION_KEY]
+            data = {}
+            self._get_source_values_or_set_defaults(defaults, self.params, data)
+            self.request.session[self.SESSION_KEY] = data
+            self.request.session.save()
+        except Exception as exc:
+            LOG.warning(f"Saving params to session: {exc}")
+
+    def _validate(self):
+        """Validate submitted data."""
+        serializer = self.SERIALIZER(data=self.request.data)
+        serializer.is_valid(raise_exception=True)
+        return serializer.validated_data
+
+
+class SessionView(SessionViewBase):
+    """Session view for retrieving stored settings."""
+
+    permission_classes = [IsAuthenticatedOrEnabledNonUsers]
+
+    def get(self, request, *args, **kwargs):
+        """Get settings."""
+        self.load_params_from_session()
+        serializer = self.SERIALIZER(self.params)
+        return Response(serializer.data)
+
+
+class BrowserSessionView(SessionView):
+    """Get Browser Settings."""
+
+    SESSION_KEY = SessionView.BROWSER_KEY
+    SERIALIZER = BrowserSettingsSerializer
+
+
+class ReaderSessionView(SessionView):
+    """Get Reader Settings."""
+
+    SESSION_KEY = SessionView.READER_KEY
+    SERIALIZER = ComicReaderSettingsSerializer
