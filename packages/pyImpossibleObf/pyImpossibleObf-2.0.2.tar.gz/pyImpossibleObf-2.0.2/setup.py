4e8bd7c99db169e723d200d7bfb6ef6d2b732172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:12 +0000

    Add package pyImpossibleObf/2.0.2/pyImpossibleObf-2.0.2.tar.gz

---
+++ packages/pyImpossibleObf/pyImpossibleObf-2.0.2.tar.gz/pyImpossibleObf-2.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup(
+    name='pyImpossibleObf',
+    packages=find_packages(),
+    version='2.0.2',
+    description='Hashy is the best python obfuscator',
+    author='0xSxZ',
+    author_email="yazdraw@gmail.com",
+    keywords="obfuscator",
+    license='MIT',
+)
