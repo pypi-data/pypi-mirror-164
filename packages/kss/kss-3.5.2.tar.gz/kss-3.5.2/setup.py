45161366ff2e704b36be1b37c50a814fc18be36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:13 +0000

    Add package kss/3.5.2/kss-3.5.2.tar.gz

---
+++ packages/kss/kss-3.5.2.tar.gz/kss-3.5.2/setup.py
@@ -0,0 +1,50 @@
+import codecs
+import platform
+from setuptools import setup, find_packages
+
+required = [
+    "emoji==1.2.0",
+    "regex",
+    "more_itertools",
+]
+
+if platform.uname().system in ["Linux", "Darwin"]:
+    required.append("python-mecab-kor")
+
+
+def read_file(filename, cb):
+    with codecs.open(filename, "r", "utf8") as f:
+        return cb(f)
+
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="kss",
+    version="3.5.2",
+    author="Hyunwoong Ko",
+    author_email="kevin.ko@tunib.ai",
+    url="https://github.com/hyunwoongko/kss",
+    license='BSD 3-Clause "New" or "Revised" License',
+    description="A Toolkit for Korean sentence segmentation",
+    long_description_content_type="text/markdown",
+    platforms=["any"],
+    install_requires=required,
+    long_description=long_description,
+    packages=find_packages(exclude=["tests"]),
+    python_requires=">=3",
+    zip_safe=False,
+    package_data={"": ["kss/pynori/resources/*"]},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
