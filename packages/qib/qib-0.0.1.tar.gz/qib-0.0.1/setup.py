11ea8e7bf8bf77bed20c191e5864682261895466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:06 +0000

    Add package qib/0.0.1/qib-0.0.1.tar.gz

---
+++ packages/qib/qib-0.0.1.tar.gz/qib-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="qib",
+    version="0.0.1",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "importlib-metadata; python_version == '3.8'",
+    ],
+)
---
+++ packages/qib/qib-0.0.1.tar.gz/qib-0.0.1/src/qib/__init__.py
@@ -0,0 +1,5 @@
+from qib import (
+    lattice,
+    operator,
+    util
+)
