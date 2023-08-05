a70daf9a2a39e2a9214a8b870a277cbaab5c4401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:08 +0000

    Add package petljadoc/1.1.7/petljadoc-1.1.7-py3-none-any.whl

---
+++ packages/petljadoc/petljadoc-1.1.7-py3-none-any.whl/petljadoc/runestone_ext/nimgame/js/nimgame-i18n.sr-Latn.js
@@ -0,0 +1,10 @@
+$.i18n().load({
+    "sr-Latn": {
+        "nimgame_restart_buttnon": "Ponovi igru",
+        "nimgame_take": "Označi",
+        "nimgame_single_player": "Igraj protiv računara",
+        "nimgame_error": "Možes označiti najviše $1 kružić",
+        "nimgame_winner" : "Igrač $1 je pobedio.",
+        "nimgame_alg_won" : "Računar je pobedio."
+    }
+})
