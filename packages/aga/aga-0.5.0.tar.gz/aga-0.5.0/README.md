c87e06fd57001bc2d9c637e5067b0821b449ae9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:39 +0000

    Add package aga/0.5.0/aga-0.5.0.tar.gz

---
+++ packages/aga/aga-0.5.0.tar.gz/aga-0.5.0/README.md
@@ -0,0 +1,61 @@
+# aga
+
+[![tests](https://github.com/nihilistkitten/aga/workflows/tests/badge.svg)](https://github.com/nihilistkitten/aga/actions?workflow=tests)
+[![lints](https://github.com/nihilistkitten/aga/workflows/lints/badge.svg)](https://github.com/nihilistkitten/aga/actions?workflow=lints)
+[![Codecov](https://codecov.io/gh/nihilistkitten/aga/branch/main/graph/badge.svg)](https://codecov.io/gh/nihilistkitten/aga)
+[![PyPI](https://img.shields.io/pypi/v/aga.svg)](https://pypi.org/project/aga/)
+[![Read the Docs](https://readthedocs.org/projects/aga/badge/)](https://aga.readthedocs.io/)
+[![License](https://img.shields.io/github/license/nihilistkitten/aga)](https://choosealicense.com/licenses/mit/)
+
+**aga** (aga grades assignments) is a tool for easily producing autograders for python programming assignments, originally developed for Reed College's CS1 course.
+
+## Installation
+
+Install from pip:
+
+```bash
+pip install aga
+```
+
+## Quickstart
+
+In `square.py` (or any python file), write:
+
+```python
+from aga import problem, test_case
+
+
+@test_case(-3)
+@test_case(100)
+@test_case(2, aga_output=4)
+@test_case(-2, aga_output=4)
+@problem()
+def square(x: int) -> int:
+    """Square x."""
+    return x * x
+```
+
+Then run `aga gen square` from the directory with `square.py`. This will generate a ZIP file suitable for upload to gradescope.
+
+## Usage
+
+Aga relies on the notion of a _golden solution_ to a given problem which is known to be correct. The main work of the library is to compare the output of this golden solution on some family of test inputs against the output of a student submission. To that end, aga integrates with frontends: existing classroom software which allow submission of student code. Currently, only gradescope is supported.
+
+To use aga:
+
+1. Write a golden solution to some programming problem.
+2. Decorate this solution with the `problem` decorator.
+3. Decorate this problem with any number of `test_case` decorators, which take arbitrary positional or keyword arguments and pass them verbatim to the golden and submitted functions.
+4. Generate the autograder using the CLI: `aga gen <function_name>`.
+
+The `test_case` decorator may optionally take a special keyword argument called `aga_output`. This allows easy testing of the golden solution: aga will not successfully produce an autograder unless the golden solution's output matches the `aga_output`. You should use these as sanity checks to ensure your golden solution is implemented correctly.
+
+For complete documentation, including configuring problem and test case metadata, see the [API reference](https://aga.readthedocs.io/en/stable/reference.html).
+
+For CLI documentation, run `aga --help`, or access the docs [online](https://aga.readthedocs.io/en/stable/cli.html).
+
+## Contributing
+
+Bug reports, feature requests, and pull requests are all welcome. For details on our test suite, development environment, and more, see the [developer documentation](https://aga.readthedocs.io/en/stable/development.html).
+
+<!-- vim:set tw=0: -->
---
+++ packages/aga/aga-0.5.0.tar.gz/aga-0.5.0/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['aga', 'aga.gradescope', 'aga.gradescope.resources']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dataclasses-json>=0.5.6,<0.6.0',
+ 'dill>=0.3.4,<0.4.0',
+ 'gradescope-utils>=0.4.0,<0.5.0',
+ 'typer>=0.4.0,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['aga = aga.cli:app']}
+
+setup_kwargs = {
+    'name': 'aga',
+    'version': '0.5.0',
+    'description': 'aga grades assignments',
+    'long_description': '# aga\n\n[![tests](https://github.com/nihilistkitten/aga/workflows/tests/badge.svg)](https://github.com/nihilistkitten/aga/actions?workflow=tests)\n[![lints](https://github.com/nihilistkitten/aga/workflows/lints/badge.svg)](https://github.com/nihilistkitten/aga/actions?workflow=lints)\n[![Codecov](https://codecov.io/gh/nihilistkitten/aga/branch/main/graph/badge.svg)](https://codecov.io/gh/nihilistkitten/aga)\n[![PyPI](https://img.shields.io/pypi/v/aga.svg)](https://pypi.org/project/aga/)\n[![Read the Docs](https://readthedocs.org/projects/aga/badge/)](https://aga.readthedocs.io/)\n[![License](https://img.shields.io/github/license/nihilistkitten/aga)](https://choosealicense.com/licenses/mit/)\n\n**aga** (aga grades assignments) is a tool for easily producing autograders for python programming assignments, originally developed for Reed College\'s CS1 course.\n\n## Installation\n\nInstall from pip:\n\n```bash\npip install aga\n```\n\n## Quickstart\n\nIn `square.py` (or any python file), write:\n\n```python\nfrom aga import problem, test_case\n\n\n@test_case(-3)\n@test_case(100)\n@test_case(2, aga_output=4)\n@test_case(-2, aga_output=4)\n@problem()\ndef square(x: int) -> int:\n    """Square x."""\n    return x * x\n```\n\nThen run `aga gen square` from the directory with `square.py`. This will generate a ZIP file suitable for upload to gradescope.\n\n## Usage\n\nAga relies on the notion of a _golden solution_ to a given problem which is known to be correct. The main work of the library is to compare the output of this golden solution on some family of test inputs against the output of a student submission. To that end, aga integrates with frontends: existing classroom software which allow submission of student code. Currently, only gradescope is supported.\n\nTo use aga:\n\n1. Write a golden solution to some programming problem.\n2. Decorate this solution with the `problem` decorator.\n3. Decorate this problem with any number of `test_case` decorators, which take arbitrary positional or keyword arguments and pass them verbatim to the golden and submitted functions.\n4. Generate the autograder using the CLI: `aga gen <function_name>`.\n\nThe `test_case` decorator may optionally take a special keyword argument called `aga_output`. This allows easy testing of the golden solution: aga will not successfully produce an autograder unless the golden solution\'s output matches the `aga_output`. You should use these as sanity checks to ensure your golden solution is implemented correctly.\n\nFor complete documentation, including configuring problem and test case metadata, see the [API reference](https://aga.readthedocs.io/en/stable/reference.html).\n\nFor CLI documentation, run `aga --help`, or access the docs [online](https://aga.readthedocs.io/en/stable/cli.html).\n\n## Contributing\n\nBug reports, feature requests, and pull requests are all welcome. For details on our test suite, development environment, and more, see the [developer documentation](https://aga.readthedocs.io/en/stable/development.html).\n\n<!-- vim:set tw=0: -->\n',
+    'author': 'Riley Shahar',
+    'author_email': 'riley.shahar@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/nihilistkitten/aga',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10.0,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
