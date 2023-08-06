138d13f8044fd9e838ab650d680232060fcdac30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:21 +0000

    Add package ms2query/0.3.3/ms2query-0.3.3-py3-none-any.whl

---
+++ packages/ms2query/ms2query-0.3.3-py3-none-any.whl/ms2query/run_ms2query.py
@@ -0,0 +1,111 @@
+import os
+from typing import Dict, List, Tuple, Union
+from urllib.request import urlretrieve
+from tqdm import tqdm
+from ms2query.ms2library import MS2Library
+from ms2query.utils import convert_files_to_matchms_spectrum_objects
+
+
+def default_library_file_base_names() -> Dict[str, str]:
+    """Returns a dictionary with the base names of default files for a MS2Library"""
+    return {"sqlite": "library_GNPS_15_12_2021.sqlite",
+            "s2v_trainables": "spec2vec_model_GNPS_15_12_2021.model.syn1neg.npy",
+            "s2v_vectors": "spec2vec_model_GNPS_15_12_2021.model.wv.vectors.npy",
+            "classifiers": "ALL_GNPS_210409_positive_processed_annotated_CF_NPC_classes.txt",
+            "s2v_model": "spec2vec_model_GNPS_15_12_2021.model",
+            "ms2ds_model": "ms2ds_model_GNPS_15_12_2021.hdf5",
+            "ms2query_model": "ms2query_random_forest_model.pickle",
+            "s2v_embeddings": "library_GNPS_15_12_2021_s2v_embeddings.pickle",
+            "ms2ds_embeddings": "library_GNPS_15_12_2021_ms2ds_embeddings.pickle"}
+
+
+def download_default_models(dir_to_store_files: str,
+                            file_name_dict: Dict[str, str]):
+    """Downloads files from Zenodo
+
+    Args:
+    ------
+    dir_to_store_files:
+        The path to the directory in which the downloaded files will be stored.
+        The directory does not have to exist yet.
+    file_name_dict:
+        A dictionary with as keys the type of file and as values the file names
+    """
+    if not os.path.exists(dir_to_store_files):
+        os.mkdir(dir_to_store_files)
+    zenodo_files_location = "https://zenodo.org/record/6997924/files/"
+    for file_name in tqdm(file_name_dict.values(),
+                          "Downloading library files"):
+        complete_url = zenodo_files_location + file_name + "?download=1"
+        file_location = os.path.join(dir_to_store_files, file_name)
+        if not os.path.exists(file_location):
+            urlretrieve(complete_url, file_location)
+        else:
+            print(f"file with the name {file_location} already exists, so was not downloaded")
+
+
+def run_complete_folder(ms2library: MS2Library,
+                        folder_with_spectra: str,
+                        results_folder: Union[str, None] = None,
+                        nr_of_analogs_to_store: int = 1,
+                        minimal_ms2query_score: Union[int, float] = 0.0,
+                        additional_metadata_columns: Tuple[str] = ("retention_time", "retention_index",),
+                        additional_ms2query_score_columns: List[str] = None
+                        ) -> None:
+    """Stores analog and library search results for all spectra files in folder
+
+    Args:
+    ------
+    ms2library:
+        MS2Library object
+    folder_with_spectra:
+        Path to folder containing spectra on which analog search should be run.
+    results_folder:
+        Path to folder in which the results are stored, folder does not have to exist yet.
+        In this folder the csv files with results are stored. When None results_folder is set to
+        folder_with_spectra/result.
+    nr_of_top_analogs_to_store:
+        The number of returned analogs that are stored.
+    minimal_ms2query_score:
+        The minimal ms2query metascore needed to be stored in the csv file.
+        Spectra for which no analog with this minimal metascore was found,
+        will not be stored in the csv file.
+    additional_metadata_columns:
+        Additional columns with query spectrum metadata that should be added. For instance "retention_time".
+    additional_ms2query_score_columns:
+        Additional columns with scores used for calculating the ms2query metascore
+        Options are: "mass_similarity", "s2v_score", "ms2ds_score", "average_ms2ds_score_for_inchikey14",
+        "nr_of_spectra_with_same_inchikey14*0.01", "chemical_neighbourhood_score",
+        "average_tanimoto_score_for_chemical_neighbourhood_score",
+        "nr_of_spectra_for_chemical_neighbourhood_score*0.01"
+    set_charge_to:
+        The charge of all spectra that have no charge is set to this value. This is important for precursor m/z
+        calculations. It is important that for positive mode charge is set to 1 and at negative mode charge is set to -1
+        for correct precursor m/z calculations.
+    change_all_charges:
+        If True the charge of all spectra is set to this value. If False only the spectra that do not have a specified
+        charge will be changed.
+    """
+    # pylint: disable=too-many-arguments
+
+    if results_folder is None:
+        results_folder = os.path.join(folder_with_spectra, "results")
+    # check if there is a results folder otherwise create one
+    if not os.path.exists(results_folder):
+        os.mkdir(results_folder)
+
+    # Go through spectra files in directory
+    for file_name in os.listdir(folder_with_spectra):
+        file_path = os.path.join(folder_with_spectra, file_name)
+        # skip folders
+        if os.path.isfile(file_path):
+            spectra = convert_files_to_matchms_spectrum_objects(os.path.join(folder_with_spectra, file_name))
+            if spectra is not None:
+                analogs_results_file_name = os.path.join(results_folder, os.path.splitext(file_name)[0] + ".csv")
+                ms2library.analog_search_store_in_csv(spectra,
+                                                      analogs_results_file_name,
+                                                      nr_of_top_analogs_to_save=nr_of_analogs_to_store,
+                                                      minimal_ms2query_metascore=minimal_ms2query_score,
+                                                      additional_metadata_columns=additional_metadata_columns,
+                                                      additional_ms2query_score_columns=additional_ms2query_score_columns)
+                print(f"Results stored in {analogs_results_file_name}")
---
+++ packages/ms2query/ms2query-0.3.3-py3-none-any.whl/tests/test_run_ms2query.py
@@ -0,0 +1,109 @@
+import os
+import sys
+from ms2query.run_ms2query import download_default_models, run_complete_folder
+from tests.test_ms2library import (MS2Library, create_test_classifier_csv_file,
+                                   file_names, test_spectra)
+
+
+if sys.version_info < (3, 8):
+    import pickle5 as pickle
+else:
+    import pickle
+
+
+def test_download_default_models(tmp_path):
+    """Tests downloading one of the files from zenodo
+
+    Only one file is downloaded to keep test running time acceptable"""
+
+    dir_to_store_files = os.path.join(tmp_path, "models")
+    download_default_models(dir_to_store_files,
+                            {"classifiers": "ALL_GNPS_210409_positive_processed_annotated_CF_NPC_classes.txt"})
+    assert os.path.exists(dir_to_store_files)
+    print(os.path.join(dir_to_store_files,
+                                       "ALL_GNPS_210409_positive_processed_annotated_CF_NPC_classes.txt"))
+    assert os.path.exists(os.path.join(dir_to_store_files,
+                                       "ALL_GNPS_210409_positive_processed_annotated_CF_NPC_classes.txt"))
+    with open(os.path.join(dir_to_store_files,
+                           "ALL_GNPS_210409_positive_processed_annotated_CF_NPC_classes.txt"), "r") as classifiers_file:
+        assert classifiers_file.readline() == "inchi_key\tsmiles\tcf_kingdom\tcf_superclass\tcf_class\t" \
+                                              "cf_subclass\tcf_direct_parent\tnpc_class_results\t" \
+                                              "npc_superclass_results\tnpc_pathway_results\tnpc_isglycoside\n"
+
+
+def create_test_folder_with_spectra_files(path, spectra):
+    """Creates a folder with two files containing two test spectra"""
+    spectra_files_folder = os.path.join(path, "spectra_files_folder")
+    os.mkdir(spectra_files_folder)
+
+    pickle.dump(spectra, open(os.path.join(spectra_files_folder, "spectra_file_1.pickle"), "wb"))
+    pickle.dump(spectra, open(os.path.join(spectra_files_folder, "spectra_file_2.pickle"), "wb"))
+    return spectra_files_folder
+
+
+def test_run_complete_folder(tmp_path, file_names, test_spectra):
+    sqlite_file_loc, spec2vec_model_file_loc, s2v_pickled_embeddings_file, \
+        ms2ds_model_file_name, ms2ds_embeddings_file_name, \
+        spectrum_id_column_name, ms2q_model_file_name = file_names
+
+    test_library = MS2Library(sqlite_file_loc, spec2vec_model_file_loc, ms2ds_model_file_name,
+                              s2v_pickled_embeddings_file, ms2ds_embeddings_file_name, ms2q_model_file_name,
+                              classifier_csv_file_name=None, spectrum_id_column_name=spectrum_id_column_name)
+
+    folder_with_spectra = create_test_folder_with_spectra_files(tmp_path, test_spectra)
+    results_directory = os.path.join(folder_with_spectra, "results")
+
+    run_complete_folder(ms2library=test_library,
+                        folder_with_spectra=folder_with_spectra,
+                        minimal_ms2query_score=0)
+    assert os.path.exists(results_directory), "Expected results directory to be created"
+
+    assert os.listdir(os.path.join(results_directory)).sort() == ['spectra_file_1.csv', 'spectra_file_2.csv'].sort()
+
+    with open(os.path.join(os.path.join(results_directory, 'spectra_file_1.csv')), "r") as file:
+        assert file.readlines() == \
+               ['query_spectrum_nr,ms2query_model_prediction,precursor_mz_difference,precursor_mz_query_spectrum,precursor_mz_analog,inchikey,spectrum_ids,analog_compound_name,retention_time,retention_index\n',
+                '1,0.5645,33.2500,907.0000,940.2500,KNGPFNUOXXLKCN,CCMSLIB00000001548,Hoiamide B,,\n',
+                '2,0.4090,61.3670,928.0000,866.6330,GRJSOZDXIUZXEW,CCMSLIB00000001570,Halovir A,,\n']
+
+    with open(os.path.join(os.path.join(results_directory, 'spectra_file_2.csv')), "r") as file:
+        assert file.readlines() == \
+               ['query_spectrum_nr,ms2query_model_prediction,precursor_mz_difference,precursor_mz_query_spectrum,precursor_mz_analog,inchikey,spectrum_ids,analog_compound_name,retention_time,retention_index\n',
+                '1,0.5645,33.2500,907.0000,940.2500,KNGPFNUOXXLKCN,CCMSLIB00000001548,Hoiamide B,,\n',
+                '2,0.4090,61.3670,928.0000,866.6330,GRJSOZDXIUZXEW,CCMSLIB00000001570,Halovir A,,\n']
+
+
+def test_run_complete_folder_with_classifiers(tmp_path, file_names, test_spectra):
+    sqlite_file_loc, spec2vec_model_file_loc, s2v_pickled_embeddings_file, \
+        ms2ds_model_file_name, ms2ds_embeddings_file_name, \
+        spectrum_id_column_name, ms2q_model_file_name = file_names
+    classifiers_file_location = create_test_classifier_csv_file(tmp_path)
+
+    test_library = MS2Library(sqlite_file_loc, spec2vec_model_file_loc, ms2ds_model_file_name,
+                              s2v_pickled_embeddings_file, ms2ds_embeddings_file_name, ms2q_model_file_name,
+                              classifier_csv_file_name=classifiers_file_location,
+                              spectrum_id_column_name=spectrum_id_column_name)
+
+    folder_with_spectra = create_test_folder_with_spectra_files(tmp_path, test_spectra)
+    results_directory = os.path.join(folder_with_spectra, "results")
+
+    run_complete_folder(ms2library=test_library,
+                        folder_with_spectra=folder_with_spectra,
+                        minimal_ms2query_score=0,
+                        additional_metadata_columns=["charge"],
+                        additional_ms2query_score_columns=["s2v_score", "ms2ds_score"]
+                        )
+    assert os.path.exists(results_directory), "Expected results directory to be created"
+
+    assert os.listdir(os.path.join(results_directory)).sort() == ['spectra_file_1.csv', 'spectra_file_2.csv'].sort()
+
+    with open(os.path.join(os.path.join(results_directory, 'spectra_file_1.csv')), "r") as file:
+        assert file.readlines() == \
+               ['query_spectrum_nr,ms2query_model_prediction,precursor_mz_difference,precursor_mz_query_spectrum,precursor_mz_analog,inchikey,spectrum_ids,analog_compound_name,charge,s2v_score,ms2ds_score,smiles,cf_kingdom,cf_superclass,cf_class,cf_subclass,cf_direct_parent,npc_class_results,npc_superclass_results,npc_pathway_results\n',
+                '1,0.5645,33.2500,907.0000,940.2500,KNGPFNUOXXLKCN,CCMSLIB00000001548,Hoiamide B,1,0.9996,0.9232,CCC[C@@H](C)[C@@H]([C@H](C)[C@@H]1[C@H]([C@H](Cc2nc(cs2)C3=N[C@](CS3)(C4=N[C@](CS4)(C(=O)N[C@H]([C@H]([C@H](C(=O)O[C@H](C(=O)N[C@H](C(=O)O1)[C@@H](C)O)[C@@H](C)CC)C)O)[C@@H](C)CC)C)C)OC)C)O,Organic compounds,Organic acids and derivatives,Peptidomimetics,Depsipeptides,Cyclic depsipeptides,Cyclic peptides,Oligopeptides,Amino acids and Peptides\n',
+                '2,0.4090,61.3670,928.0000,866.6330,GRJSOZDXIUZXEW,CCMSLIB00000001570,Halovir A,0,0.9621,0.4600,nan,nan,nan,nan,nan,nan,nan,nan,nan\n']
+    with open(os.path.join(os.path.join(results_directory, 'spectra_file_2.csv')), "r") as file:
+        assert file.readlines() == \
+               ['query_spectrum_nr,ms2query_model_prediction,precursor_mz_difference,precursor_mz_query_spectrum,precursor_mz_analog,inchikey,spectrum_ids,analog_compound_name,charge,s2v_score,ms2ds_score,smiles,cf_kingdom,cf_superclass,cf_class,cf_subclass,cf_direct_parent,npc_class_results,npc_superclass_results,npc_pathway_results\n',
+                '1,0.5645,33.2500,907.0000,940.2500,KNGPFNUOXXLKCN,CCMSLIB00000001548,Hoiamide B,1,0.9996,0.9232,CCC[C@@H](C)[C@@H]([C@H](C)[C@@H]1[C@H]([C@H](Cc2nc(cs2)C3=N[C@](CS3)(C4=N[C@](CS4)(C(=O)N[C@H]([C@H]([C@H](C(=O)O[C@H](C(=O)N[C@H](C(=O)O1)[C@@H](C)O)[C@@H](C)CC)C)O)[C@@H](C)CC)C)C)OC)C)O,Organic compounds,Organic acids and derivatives,Peptidomimetics,Depsipeptides,Cyclic depsipeptides,Cyclic peptides,Oligopeptides,Amino acids and Peptides\n',
+                '2,0.4090,61.3670,928.0000,866.6330,GRJSOZDXIUZXEW,CCMSLIB00000001570,Halovir A,0,0.9621,0.4600,nan,nan,nan,nan,nan,nan,nan,nan,nan\n']
