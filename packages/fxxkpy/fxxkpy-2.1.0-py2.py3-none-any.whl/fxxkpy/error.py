25b8ca04b3d595ca74196057f827f9ae553f023e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:43 +0000

    Add package fxxkpy/2.1.0/fxxkpy-2.1.0-py2.py3-none-any.whl

---
+++ packages/fxxkpy/fxxkpy-2.1.0-py2.py3-none-any.whl/fxxkpy/error.py
@@ -0,0 +1,7 @@
+# coding:utf-8
+def operation_error(variable1: any, variable2: any, methods: str):
+    class1 = str(variable1.__class__)[7:len(str(variable1.__class__)) - 1]
+    class2 = str(variable2.__class__)[7:len(str(variable2.__class__)) - 1]
+    raise TypeError(
+        f"unsupported operand type(s) for {methods}: {class1} and {class2}"
+    )
