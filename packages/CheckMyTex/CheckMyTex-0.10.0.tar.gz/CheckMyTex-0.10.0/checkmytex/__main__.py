47351b807c6ca52c29b46758975c7aad5f05c5ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:26 +0000

    Add package CheckMyTex/0.10.0/CheckMyTex-0.10.0.tar.gz

---
+++ packages/CheckMyTex/CheckMyTex-0.10.0.tar.gz/CheckMyTex-0.10.0/checkmytex/__main__.py
@@ -0,0 +1,64 @@
+"""
+A main file to execute CheckMyTex with CLI.
+"""
+import json
+
+from checkmytex.cli import InteractiveCli, log, parse_arguments
+from checkmytex.cli.rich_printer import RichPrinter
+from checkmytex.document_analyzer import DocumentAnalyzer
+from checkmytex.filtering import (
+    IgnoreIncludegraphics,
+    IgnoreLikelyAuthorNames,
+    IgnoreRefs,
+    IgnoreRepeatedWords,
+    IgnoreSpellingWithMath,
+    IgnoreWordsFromBibliography,
+    MathMode,
+    Whitelist,
+)
+from checkmytex.latex_document import LatexDocument
+from checkmytex.latex_document.parser import LatexParser
+
+
+def main():
+    """
+    A main function, such that it can also be called with different entry
+    points
+    :return: None
+    """
+    args = parse_arguments(log)
+    whitelist = Whitelist(args.whitelist)
+    log_ = log
+    log_("Parsing LaTeX project...")
+    try:
+        parser = LatexParser()
+        latex_document = parser.parse(args.path[0])
+        engine = DocumentAnalyzer(log=log_)
+        engine.log = log_
+        engine.setup_default()
+        # Add filter
+        engine.add_filter(whitelist)
+        engine.add_filter(IgnoreIncludegraphics())
+        engine.add_filter(IgnoreRefs())
+        engine.add_filter(IgnoreRepeatedWords(["problem", "problems"]))
+        engine.add_filter(IgnoreLikelyAuthorNames())
+        engine.add_filter(IgnoreWordsFromBibliography())
+        engine.add_filter(IgnoreSpellingWithMath())
+        engine.add_filter(
+            MathMode({"SPELLING": None, "languagetool": None, "Proselint": None})
+        )
+
+        analyzed_document = engine.analyze(latex_document)
+        if args.json:
+            with open(args.json, "w") as f:
+                json.dump(analyzed_document.serialize(), f)
+            return
+        if args.html:
+            RichPrinter(analyzed_document).save(args.html)
+        InteractiveCli(analyzed_document, whitelist, just_print=args.print)
+    except KeyError as key_error:
+        print("Error:", str(key_error))
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/CheckMyTex/CheckMyTex-0.10.0.tar.gz/CheckMyTex-0.10.0/checkmytex/cli/arguments.py
@@ -0,0 +1,34 @@
+"""
+Parsing the arguments from CLI.
+"""
+import argparse
+import os
+import sys
+
+
+def parse_arguments(log):
+    """
+    Parse CLI arguments.
+    :param log: A logging function.
+    :return: The parsed arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description="CheckMyTex: A simple tool for checking your LaTeX."
+    )
+
+    parser.add_argument("-w", help="Path to whitelist", type=str)
+    parser.add_argument("--json", type=str, help="Write result as json to file.")
+    parser.add_argument("--print", action="store_true", help="Just print the output")
+    parser.add_argument("--html", type=str, help="Create an HTML with the problems.")
+    parser.add_argument("path", nargs=1, help="Path to main.tex")
+    args = parser.parse_args()
+    if not args.path:
+        parser.print_help()
+        sys.exit(1)
+    if args.w:
+        args.whitelist = args.w
+    else:
+        path = os.path.dirname(args.path[0])
+        args.whitelist = os.path.join(path, ".whitelist.txt")
+        log(f"Saving whitelist to {args.whitelist}")
+    return args
