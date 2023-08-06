bf93e7b5067196851a3141b4fdfdb2079a684278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:18 +0000

    Add package express-integrations-monday/1.0/express_integrations_monday-1.0-py3-none-any.whl

---
+++ packages/express-integrations-monday/express_integrations_monday-1.0-py3-none-any.whl/monday/resources/boards.py
@@ -0,0 +1,33 @@
+from monday.query_joins import (
+    create_board_by_workspace_query,
+    get_board_items_query,
+    get_boards_by_id_query,
+    get_boards_query,
+    get_columns_by_board_query,
+)
+from monday.resources.base import BaseResource
+
+
+class BoardResource(BaseResource):
+    def __init__(self, token):
+        super().__init__(token)
+
+    def fetch_boards(self, **kwargs):
+        query = get_boards_query(**kwargs)
+        return self.client.execute(query)
+
+    def fetch_boards_by_id(self, board_ids):
+        query = get_boards_by_id_query(board_ids)
+        return self.client.execute(query)
+
+    def fetch_items_by_board_id(self, board_ids):
+        query = get_board_items_query(board_ids)
+        return self.client.execute(query)
+
+    def fetch_columns_by_board_id(self, board_ids):
+        query = get_columns_by_board_query(board_ids)
+        return self.client.execute(query)
+
+    def create_board(self, board_name, board_kind, workspace_id):
+        query = create_board_by_workspace_query(board_name, board_kind, workspace_id)
+        return self.client.execute(query)
