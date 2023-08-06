710675896eb787b340a69f4596e7b64a9d99f75a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:21 +0000

    Add package weepy/1.1.0/weepy-1.1.0.tar.gz

---
+++ packages/weepy/weepy-1.1.0.tar.gz/weepy-1.1.0/weepy/__init__.py
@@ -0,0 +1,6 @@
+from .request import Request, HTTPRequest
+from .response import Response, HTTPResponse
+from .route import Route, WSRoute, HTTPRoute
+from .asgi import ASGI
+
+__version__ = "1.1.0"
