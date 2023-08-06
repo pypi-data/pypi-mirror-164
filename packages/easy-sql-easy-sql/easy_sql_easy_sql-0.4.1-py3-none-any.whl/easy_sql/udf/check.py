5151c047d03fc81d977716d23829ec806246e66b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:48 +0000

    Add package easy-sql-easy-sql/0.4.1/easy_sql_easy_sql-0.4.1-py3-none-any.whl

---
+++ packages/easy-sql-easy-sql/easy_sql_easy_sql-0.4.1-py3-none-any.whl/easy_sql/udf/check.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import re
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections import Callable
+
+    from pyspark.sql.types import DataType
+
+
+class UDF:
+    def __init__(self, func: Callable, return_type: DataType):
+        self.func = func
+        self.return_type = return_type
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+
+def check_regex_func(pattern):
+    return lambda any_str: any_str if any_str and re.match(pattern, any_str) else None
