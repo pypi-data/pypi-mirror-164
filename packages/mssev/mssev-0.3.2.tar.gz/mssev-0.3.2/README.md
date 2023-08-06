f3bb62f2f4f13e170a53639c0416ae71a9d937ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:13 +0000

    Add package mssev/0.3.2/mssev-0.3.2.tar.gz

---
+++ packages/mssev/mssev-0.3.2.tar.gz/mssev-0.3.2/README.md
@@ -0,0 +1,5 @@
+# python-mssev
+
+Calculates ARMSS (age-related multiple sclerosis severity) and both local and global MSSS (multiple sclerosis severity score). This package is a Python port of the R package [ms.sev](https://rdrr.io/cran/ms.sev/).
+
+If you want more information, you can check [the original article](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5700773/).
---
+++ packages/mssev/mssev-0.3.2.tar.gz/mssev-0.3.2/mssev/global_msss.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from pandas.api.types import is_timedelta64_dtype
+
+
+datadir = Path(__file__).parent / 'data'
+
+GLOBAL_MSSS_TABLE_FILES = {
+    'original': datadir / 'Original-MSSS.tsv',
+    'updated': datadir / 'Updated-MSSS.tsv',
+}
+
+
+def _load_msss_table(path):
+    df = pd.read_csv(path, sep='\t')
+    df = df.rename(columns={'dd': 'Duration'})
+    df.Duration = df.Duration.str.replace('dd', '').astype(int)
+    df = df.rename(columns=lambda x: x.replace('EDSS', 'MSSS'))
+    df = pd.wide_to_long(df, 'MSSS', i='Duration', j='EDSS', sep='.', suffix=r'\d\.\d')
+    return df
+
+
+def global_msss(df, table='original', edss='edss', duration='dd'):
+    if isinstance(table, str) and table in GLOBAL_MSSS_TABLE_FILES:
+        table = _load_msss_table(GLOBAL_MSSS_TABLE_FILES.get(table))
+
+    df = df[[duration, edss]].copy()
+    if is_timedelta64_dtype(df[duration]):
+        df[duration] = df[duration] // np.timedelta64(1, 'Y')
+    df[duration] = np.floor(df[duration]).clip(upper=30)
+    results = df.merge(table, left_on=[duration, edss], right_index=True, how='left')
+    return results.MSSS
