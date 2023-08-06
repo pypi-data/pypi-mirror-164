4202c7837310e8f718fe112b6b430f8b686948f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:31 +0000

    Add package XICRA/1.4.3/XICRA-1.4.3.tar.gz

---
+++ packages/XICRA/XICRA-1.4.3.tar.gz/XICRA-1.4.3/XICRA/modules/__init__.py
@@ -0,0 +1,19 @@
+__all__ = [
+	'biotype',
+	'config',
+	'citation',
+	'database',
+	'help_XICRA',
+	'join',
+	'miRNA',
+	'prep',
+	'qc',
+	'trim',
+	'tRNA',
+	'piRNA',
+	'map'
+	
+]
+
+from XICRA.modules import *
+
