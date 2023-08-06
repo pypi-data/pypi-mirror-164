a59eac86ba901d10c7d098cc53f7b49610418cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:24 +0000

    Add package yb-wishes/0.0.4/yb_wishes-0.0.4-py3-none-any.whl

---
+++ packages/yb-wishes/yb_wishes-0.0.4-py3-none-any.whl/yb-wishes.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2022/8/26 13:19
+# @author   : yone
+# @FileName : yb-wishes.py
+
+def wishBirthDay(I,U):
+    say = f"{I} says: Happy Birthday! {U}"
+    print(say)
+
