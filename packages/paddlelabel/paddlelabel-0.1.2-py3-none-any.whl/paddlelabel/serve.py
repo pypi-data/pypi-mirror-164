a49600699e4cd41d77372305ee15684233d11a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:19 +0000

    Add package paddlelabel/0.1.2/paddlelabel-0.1.2-py3-none-any.whl

---
+++ packages/paddlelabel/paddlelabel-0.1.2-py3-none-any.whl/paddlelabel/serve.py
@@ -0,0 +1,40 @@
+import os.path as osp
+
+from flask_cors import CORS
+import flask
+
+import paddlelabel
+from paddlelabel.util import Resolver
+from paddlelabel.config import sqlite_url, db, connexion_app, db_path
+import paddlelabel.api
+import paddlelabel.task
+
+
+curr_dir = osp.abspath(osp.dirname(__file__))
+
+
+@connexion_app.app.route("/")
+def index():
+    return flask.redirect("/static/index.html")
+
+
+if not osp.exists(db_path):
+    print("Creating db")
+    db.create_all()
+
+    # TODO: move to base
+    from paddlelabel.config import basedir
+    from paddlelabel.api.controller.setting import init_site_settings
+
+    init_site_settings(osp.join(basedir, "default_setting.json"))
+
+
+connexion_app.add_api(
+    osp.join(curr_dir, "openapi.yml"),
+    resolver=Resolver("paddlelabel.api", collection_endpoint_name="get_all"),
+    # request with undefined param returns error, dont enforce body
+    strict_validation=True,
+    pythonic_params=True,
+)
+
+CORS(connexion_app.app)
---
+++ packages/paddlelabel/paddlelabel-0.1.2-py3-none-any.whl/paddlelabel/task/__init__.py
@@ -0,0 +1,4 @@
+from .base import BaseTask
+from .classification import Classification
+from .detection import Detection
+from .segmentation import InstanceSegmentation, SemanticSegmentation
