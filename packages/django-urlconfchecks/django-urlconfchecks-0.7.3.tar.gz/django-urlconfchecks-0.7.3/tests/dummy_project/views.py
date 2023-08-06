01ba93fd8e0179ab868aea7dd4c2e1027b299571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:07 +0000

    Add package django-urlconfchecks/0.7.3/django-urlconfchecks-0.7.3.tar.gz

---
+++ packages/django-urlconfchecks/django-urlconfchecks-0.7.3.tar.gz/django-urlconfchecks-0.7.3/tests/dummy_project/views.py
@@ -0,0 +1,81 @@
+"""views for tests."""
+
+from typing import Optional
+
+from django.views import View
+
+
+def bad_view(slug: str):
+    """View missing the `request` parameter."""
+    ...
+
+
+def bad_arg(request, id: Optional[int] = None):
+    """View with bad argument, using Optional."""
+    ...
+
+
+def special_case(request):
+    """This is a special case.
+
+    Args:
+        request: The request object.
+
+    Returns:
+        A response object.
+    """
+    ...
+
+
+def year_archive(request, year: int):
+    """This is a year archive.
+
+    Args:
+        request: The request object.
+        year: The year.
+
+    Returns:
+        A response object.
+    """
+    ...
+
+
+def year_archive_untyped(request, year):
+    """This is a year archive (with no type hint)."""
+    ...
+
+
+def month_archive(request, year: int, month: int):
+    """This is a month archive.
+
+    Args:
+        request: The request object.
+        year: The year.
+        month: The month.
+
+    Returns:
+        A response object.
+    """
+    ...
+
+
+def article_detail(request, year: int, month: int, slug: str):
+    """This is an article detail.
+
+    Args:
+        request: The request object.
+        year: The year.
+        month: The month.
+        slug: The slug.
+
+    Returns:
+        A response object.
+    """
+    ...
+
+
+class CBVView(View):
+    """This is a simple class based view."""
+
+    def get(self, request):
+        ...
