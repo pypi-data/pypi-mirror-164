5697840d2a43f13e27d4016f87c7fa7899066880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:13 +0000

    Add package Alexsecdemo/6.0.4.1/Alexsecdemo-6.0.4.1.tar.gz

---
+++ packages/Alexsecdemo/Alexsecdemo-6.0.4.1.tar.gz/Alexsecdemo-6.0.4.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from setuptools.command.install import install
+import requests
+import socket
+import getpass
+import os
+
+class CustomInstall(install):
+    def run(self):
+        install.run(self)
+        print('run custominstall successfully!')
+
+
+setup(
+        name='Alexsecdemo', #package name
+        version='6.0.4.1',
+        description='A sample Python project, do not download it!',
+        author='Alex',
+        license='MIT',
+        packages=['Alex'],
+        py_modules=['Alexsecdemoraw'],
+        cmdclass={'install': CustomInstall},
+        author_email='zhuzhuzhuzai@gmail.com',
+        # install_requires=[
+        # "Bfixsecdemo==1.0.1",
+        # ],
+)
