02750a4091cd2173e3ac479484328cfe0b08e2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:00 +0000

    Add package akeru-distributions/0.1/akeru_distributions-0.1.tar.gz

---
+++ packages/akeru-distributions/akeru_distributions-0.1.tar.gz/akeru_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='akeru_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['akeru_distributions'],
+      zip_safe=False)
