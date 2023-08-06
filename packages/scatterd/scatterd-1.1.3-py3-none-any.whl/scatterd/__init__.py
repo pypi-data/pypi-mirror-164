74de2e1aea40625eec2ec0753889216cd4e2c7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:08 +0000

    Add package scatterd/1.1.3/scatterd-1.1.3-py3-none-any.whl

---
+++ packages/scatterd/scatterd-1.1.3-py3-none-any.whl/scatterd/__init__.py
@@ -0,0 +1,40 @@
+from scatterd.scatterd import scatterd,import_example, set_colors, _preprocessing, gradient_on_density_color
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '1.1.3'
+
+# module level doc-string
+__doc__ = """
+scatterd
+=====================================================================
+
+Description
+-----------
+Scatterd is an easy and fast way of creating scatter plots.
+
+Examples
+--------
+>>> # Import library
+>>> from scatterd import scatterd, import_example
+>>> # Import example
+>>> df = import_example()
+>>> # plain scatter plot
+>>> ax = scatterd(df['tsneX'], df['tsneY'])
+>>> # plain scatter plot
+>>> ax = scatterd(df['tsneX'], df['tsneY'], labels=df['labx'])
+>>> # Gradient
+>>> ax = scatterd(df['tsneX'], df['tsneY'], labels=df['labx'], gradient='#FFFFFF')
+>>> # Change cmap
+>>> ax = scatterd(df['tsneX'], df['tsneY'], labels=df['labx'], gradient='#FFFFFF', cmap='Set2')
+>>> # Density
+>>> ax = scatterd(df['tsneX'], df['tsneY'], labels=df['labx'], density=True)
+>>> # Density with gradient
+>>> ax = scatterd(df['tsneX'], df['tsneY'], labels=df['labx'], density=True, gradient='#FFFFFF')
+>>>
+
+References
+----------
+https://github.com/erdogant/scatterd
+
+"""
