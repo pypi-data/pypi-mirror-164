ffd30431cfbd822f54a55c46c28f7cf47ef1af93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:44 +0000

    Add package thom01-distributions/0.1/thom01_distributions-0.1.tar.gz

---
+++ packages/thom01-distributions/thom01_distributions-0.1.tar.gz/thom01_distributions-0.1/setup.py
@@ -0,0 +1,13 @@
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
+from setuptools import setup
+
+setup(name='thom01_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['thom01_distributions'],
+      zip_safe=False)
