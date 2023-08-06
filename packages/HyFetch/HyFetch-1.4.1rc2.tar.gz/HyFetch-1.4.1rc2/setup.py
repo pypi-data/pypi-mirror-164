5c4643bc9dbc28e26f1ca92bebac0853bec584e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:25 +0000

    Add package HyFetch/1.4.1rc2/HyFetch-1.4.1rc2.tar.gz

---
+++ packages/HyFetch/HyFetch-1.4.1rc2.tar.gz/HyFetch-1.4.1rc2/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import pathlib
+
+from setuptools import setup
+
+import hyfetch
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="HyFetch",
+    version=hyfetch.__version__,
+    description="neofetch with flags <3",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hykilpikonna/HyFetch",
+    author="Azalea Gui",
+    author_email="me@hydev.org",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    packages=['hyfetch'],
+    package_data={'hyfetch': ['hyfetch/*']},
+    include_package_data=True,
+    install_requires=['setuptools', 'typing_extensions'],
+    entry_points={
+        "console_scripts": [
+            "hyfetch=hyfetch.main:run",
+        ]
+    },
+    scripts=['hyfetch/scripts/neowofetch']
+)
