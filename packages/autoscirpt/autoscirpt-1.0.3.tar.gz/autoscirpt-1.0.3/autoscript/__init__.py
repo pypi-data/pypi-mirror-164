3e830cd86a5d444ace30a6b451369c6efdda1533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:32 +0000

    Add package autoscirpt/1.0.3/autoscirpt-1.0.3.tar.gz

---
+++ packages/autoscirpt/autoscirpt-1.0.3.tar.gz/autoscirpt-1.0.3/autoscript/__init__.py
@@ -0,0 +1,13 @@
+'''
+Date         : 2022-08-24 11:18:38
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2022-08-24 12:36:04
+LastEditors  : BDFD
+Description  : 
+FilePath     : \autoscript\__init__.py
+Copyright (c) 2022 by BDFD, All Rights Reserved. 
+'''
+
+from autoscript.templateproj import add_one
+from autoscript.workflow_email import gmail, work_mail
