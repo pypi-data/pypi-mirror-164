93d04da73f632d1501ae380194dddb7d356b9752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:44 +0000

    Add package dascore/0.0.4/dascore-0.0.4-py3-none-any.whl

---
+++ packages/dascore/dascore-0.0.4-py3-none-any.whl/dascore/utils/progress.py
@@ -0,0 +1,29 @@
+"""
+Simple interface for progress markers.
+"""
+from rich.progress import Progress, SpinnerColumn, TimeElapsedColumn
+
+import dascore as dc
+
+
+def track(sequence, description):
+    """
+    A simple iterator for tracking updates.
+    """
+    # This is a dirty hack to allow debugging while running tests.
+    # Otherwise, pdb doesn't work in any tracking scope.
+    # See: https://github.com/Textualize/rich/issues/1053
+    if getattr(dc, "_debug") or not len(sequence):
+        yield from sequence
+        return
+    # Normal progress bar behavior
+    progress = Progress(
+        SpinnerColumn(),
+        *Progress.get_default_columns(),
+        TimeElapsedColumn(),
+    )
+    total = len(sequence)
+    with progress:
+        yield from progress.track(
+            sequence, total=total, description=description, update_period=1.0
+        )
