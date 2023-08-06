f30ffc6002c67338097caf20dd3502ed49e0ae28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:53 +0000

    Add package cryptodataset/0.3.1/cryptodataset-0.3.1-py3-none-any.whl

---
+++ packages/cryptodataset/cryptodataset-0.3.1-py3-none-any.whl/cryptodataset/cli.py
@@ -0,0 +1,56 @@
+from itertools import product
+from pathlib import Path
+from typing import List
+
+import click
+
+from . import CCXTData
+from . import MAXData
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('-e', '--exchange', type=click.STRING, default='binance', help='exchange name')
+@click.option('-s', '--symbol', type=click.STRING, default=['BTC/USDT'], multiple=True, help='symbol to download')
+@click.option('-t', '--timeframe', type=click.STRING, default=['1d'], multiple=True, help='timeframe')
+@click.option('-o', '--output-dir', type=click.STRING, default='data', help='output directory')
+@click.option('--all-symbols', is_flag=True, help='download all symbols')
+@click.option('--all-timeframes', is_flag=True, help='download all timeframes')
+def ccxt(exchange: str, symbol: List[str], timeframe: List[str], output_dir: str, all_symbols: bool,
+         all_timeframes: bool) -> None:
+    """Download OHLCV data from cryptocurrency exchange"""
+    ccxt_data = CCXTData(exchange)
+
+    if all_symbols:
+        symbol = ccxt_data.get_market_symbols()
+
+    if all_timeframes:
+        timeframe = list(ccxt_data.exchange.timeframes.keys())
+
+    for s, tf in product(symbol, timeframe):
+        ccxt_data.download_ohlcv(s, tf, output_dir)
+
+
+@cli.command()
+@click.option('-s', '--symbol', type=click.STRING, default=['BTC/USDT'], multiple=True, help='symbol to download')
+@click.option('-t', '--timeframe', type=click.STRING, default=['1d'], multiple=True, help='timeframe')
+@click.option('-o', '--output-dir', type=click.STRING, default='data', help='output directory')
+@click.option('--all-symbols', is_flag=True, help='download all symbols')
+def max(symbol: List[str], timeframe: List[str], output_dir: str, all_symbols: bool) -> None:
+    output_dir = Path(output_dir)
+
+    max_data = MAXData()
+
+    if all_symbols:
+        symbol = max_data.get_market_symbols()
+
+    for s, tf in product(symbol, timeframe):
+        max_data.download_ohlcv(s, tf, output_dir)
+
+
+if __name__ == '__main__':
+    cli()
