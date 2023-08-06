adfb79d31de309cc2dd3bcc4d3a93d23a68f8efc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:22 +0000

    Add package hdx-python-api/5.8.5/hdx-python-api-5.8.5.tar.gz

---
+++ packages/hdx-python-api/hdx-python-api-5.8.5.tar.gz/hdx-python-api-5.8.5/tests/hdx/facades/__init__.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+from hdx.api.configuration import Configuration
+
+
+class TestResult:
+    actual_result = None
+
+
+testresult = TestResult()
+
+
+def my_testfn():
+    testresult.actual_result = Configuration.read().get_hdx_site_url()
+
+
+def my_testkeyfn():
+    testresult.actual_result = Configuration.read().get_api_key()
+
+
+def my_testuafn():
+    testresult.actual_result = Configuration.read().user_agent
+
+
+def my_excfn():
+    testresult.actual_result = Configuration.read().get_hdx_site_url()
+    raise ValueError("Some failure!")
+
+
+def my_testfnkw(**kwargs):
+    fn = kwargs.get("fn")
+    if fn == "site":
+        testresult.actual_result = Configuration.read().get_hdx_site_url()
+    elif fn == "api":
+        testresult.actual_result = Configuration.read().get_api_key()
+    elif fn == "agent":
+        testresult.actual_result = Configuration.read().user_agent
+    elif fn == "exc":
+        testresult.actual_result = Configuration.read().get_hdx_site_url()
+        raise ValueError("Some failure!")
+
+
+def my_testfnia(mydata: Optional[str] = None) -> str:
+    testresult.actual_result = mydata
+    return mydata
