1489db9b6bb5e264a1c3a228354bb17b3b8e8409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:45 +0000

    Add package arclet-alconna-graia/0.4.7/arclet-alconna-graia-0.4.7.tar.gz

---
+++ packages/arclet-alconna-graia/arclet-alconna-graia-0.4.7.tar.gz/arclet-alconna-graia-0.4.7/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+setuptools.setup(
+    name="arclet-alconna-graia",
+    url="https://github.com/ArcletProject/Alconna-Graia",
+    version="0.4.7",
+    author="ArcletProject",
+    author_email="rf_tar_railt@qq.com",
+    description="Support Alconna to GraiaProject",
+    license='AGPL-3.0',
+    packages=['arclet.alconna.graia'],
+    install_requires=[
+        'arclet-alconna<1.3.0, >=1.2.0',
+        'graia-saya~=0.0.16',
+        'graia-ariadne<1.0.0, >=0.7.14',
+        'graia-amnesia>=0.5.0',
+        'graia-broadcast>=0.18.0',
+        'creart~=0.2.1',
+        'creart-graia~=0.1.5',
+        'nepattern>=0.1.2'
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'creart.creators': [
+            'alconna_behavior = arclet.alconna.graia.create:AlconnaBehaviorCreator'
+        ]
+    },
+    keywords=['alconna', 'graia', 'dispatcher', 'arclet'],
+    python_requires='>=3.8',
+    project_urls={
+        'Documentation': 'https://arcletproject.github.io/docs/alconna/tutorial',
+        'Bug Reports': 'https://github.com/ArcletProject/Alconna/issues',
+        'Source': 'https://github.com/ArcletProject/Alconna',
+    },
+)
