f9de886a7a59c94cba7ccb3be01f1e533e09f2ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:05 +0000

    Add package mpqlock/0.0.3/mpqlock-0.0.3.tar.gz

---
+++ packages/mpqlock/mpqlock-0.0.3.tar.gz/mpqlock-0.0.3/mpqlock/__init__.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append("D:\Programs\Python\Python39\Lib\site-packages\MPQ_code_Lock")
+from My_code_lock_2024 import *
+
+def codeLock():
+    code_Lock()
+if __name__ == '__main__':
+    codeLock()
+
