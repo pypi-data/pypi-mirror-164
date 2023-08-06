0afe893e8d593b0df74652b56b642465dae863f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:30 +0000

    Add package tvpy/0.0.5/tvpy-0.0.5.tar.gz

---
+++ packages/tvpy/tvpy-0.0.5.tar.gz/tvpy-0.0.5/README.md
@@ -0,0 +1,13 @@
+# 📺 TvPy 
+Generate html from folder names.
+
+## Installation
+```shell
+> pip install tvpy
+```
+
+## Usage
+```shell
+> mkdir Carnival.Row Resident.Alien Liar Under.the.Banner.of.Heaven
+> tv-json . && tv-html
+```
---
+++ packages/tvpy/tvpy-0.0.5.tar.gz/tvpy-0.0.5/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tvpy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fire>=0.4.0,<0.5.0', 'requests>=2.28.1,<3.0.0', 'tqdm>=4.64.0,<5.0.0']
+
+entry_points = \
+{'console_scripts': ['tv-html = tvpy.main:tv_html',
+                     'tv-json = tvpy.main:tv_json']}
+
+setup_kwargs = {
+    'name': 'tvpy',
+    'version': '0.0.5',
+    'description': '📺 TvPy',
+    'long_description': '# 📺 TvPy \nGenerate html from folder names.\n\n## Installation\n```shell\n> pip install tvpy\n```\n\n## Usage\n```shell\n> mkdir Carnival.Row Resident.Alien Liar Under.the.Banner.of.Heaven\n> tv-json . && tv-html\n```\n',
+    'author': 'Gilad Kutiel',
+    'author_email': 'gilad.kutiel@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/gkutiel/tvpy/tree/master',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
