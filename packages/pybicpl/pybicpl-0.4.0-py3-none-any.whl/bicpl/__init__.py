84e97ee80061efbaa9ef3e045ce7d6a70ea76e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:34 +0000

    Add package pybicpl/0.4.0/pybicpl-0.4.0-py3-none-any.whl

---
+++ packages/pybicpl/pybicpl-0.4.0-py3-none-any.whl/bicpl/__init__.py
@@ -0,0 +1,13 @@
+"""
+Library for reading and writing MNI `.obj` surface files.
+"""
+
+from bicpl.obj import PolygonObj
+
+__docformat__ = 'numpy'
+
+__all__ = [
+    'PolygonObj',
+    'types',
+    'math'
+]
