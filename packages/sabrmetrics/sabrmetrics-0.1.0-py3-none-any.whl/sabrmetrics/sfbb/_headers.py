c63ddafcca5848d8aae4e067ab6486fa7df2f084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:20 +0000

    Add package sabrmetrics/0.1.0/sabrmetrics-0.1.0-py3-none-any.whl

---
+++ packages/sabrmetrics/sabrmetrics-0.1.0-py3-none-any.whl/sabrmetrics/sfbb/_headers.py
@@ -0,0 +1,31 @@
+"""
+
+"""
+
+import json
+import pathlib
+
+
+class Headers:
+    """
+
+    """
+    @property
+    def path(self) -> pathlib.Path:
+        """
+
+        :return:
+        """
+        return pathlib.Path("sabrmetrics", "sfbb", "data", "headers.json")
+
+    @property
+    def content(self) -> dict[str, str]:
+        """
+
+        :return:
+        """
+        with open(self.path, "r", encoding="utf-8") as file:
+            return json.load(file)
+
+
+HEADERS = Headers().content
