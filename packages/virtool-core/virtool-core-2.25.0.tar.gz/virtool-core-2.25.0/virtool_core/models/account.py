f674d4dedb5ebe062d9a6f0ef91bc4ed82194c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:10 +0000

    Add package virtool-core/2.25.0/virtool-core-2.25.0.tar.gz

---
+++ packages/virtool-core/virtool-core-2.25.0.tar.gz/virtool-core-2.25.0/virtool_core/models/account.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from virtool_core.models.enums import QuickAnalyzeWorkflow
+from virtool_core.models.user import User
+from virtool_core.models.basemodel import BaseModel
+from pydantic import constr, validator, Field
+from email_validator import validate_email, EmailSyntaxError
+
+
+def check_email(email: Optional[str]) -> str:
+    """
+    Checks if the given email is valid.
+    """
+    try:
+        validate_email(email)
+    except EmailSyntaxError:
+        raise ValueError("The format of the email is invalid")
+
+    return email
+
+
+class AccountSettings(BaseModel):
+    quick_analyze_workflow: QuickAnalyzeWorkflow
+    show_ids: bool
+    show_versions: bool
+    skip_quick_analyze_dialog: bool
+
+
+class Account(User):
+    settings: AccountSettings
+    email: Optional[constr(strip_whitespace=True)] = Field(default="")
+
+    _email_validation = validator("email", allow_reuse=True)(check_email)
