ed1b472bfd752276f8d00148d5d0e71a8ad06b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:06 +0000

    Add package wincom/0.0.2/wincom-0.0.2.tar.gz

---
+++ packages/wincom/wincom-0.0.2.tar.gz/wincom-0.0.2/wincom/__init__.py
@@ -0,0 +1,12 @@
+"""
+:authors: two-it2022
+:license: Apache License, Version 0.2, see LICENSE file
+
+:copyright: (0) 2022 two-it2022
+"""
+
+from .main import *
+
+__author__ = 'two-it2022'
+__version__ = '0.0.2'
+__email__ = 'kodland.group@gmail.com'
