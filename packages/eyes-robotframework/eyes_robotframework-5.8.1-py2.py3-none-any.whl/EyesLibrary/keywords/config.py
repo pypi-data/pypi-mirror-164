ce98e84c4103b511eeadf60f52541e1ac83292ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:50 +0000

    Add package eyes-robotframework/5.8.1/eyes_robotframework-5.8.1-py2.py3-none-any.whl

---
+++ packages/eyes-robotframework/eyes_robotframework-5.8.1-py2.py3-none-any.whl/EyesLibrary/keywords/config.py
@@ -0,0 +1,79 @@
+from __future__ import absolute_import, unicode_literals
+
+from datetime import datetime
+from typing import Dict, Optional, Text, Union
+
+from robot.api.deco import keyword
+from six import string_types as basestring
+
+from applitools.common import deprecated
+from applitools.selenium import BatchInfo
+
+from ..base import LibraryComponent
+
+
+class ConfigurationKeywords(LibraryComponent):
+    @keyword(
+        "Create Batch Info",
+        types={
+            "name": str,
+            "started_at": (datetime, str, None),
+            "batch_sequence_name": (str, None),
+            "batch_id": (str, None),
+        },
+    )
+    def create_batch_info(
+        self,
+        name,  # type: Text
+        started_at=None,  # type: Optional[Union[datetime,Text]]
+        batch_sequence_name=None,  # type: Optional[Text]
+        batch_id=None,  # type: Optional[Text]
+    ):
+        # type: (...) -> Text
+        """
+        Returns a BatchInfo ID string that may be used as batch argument on `Eyes Open`.
+
+            | =Arguments=                  | =Description=                                                                              |
+            | Name                         | The name of the batch                                                                      |
+            | Started At                   | The date and time that will be displayed in the Test Manager as the batch start time *(*)* |
+            | Batch ID                     | This argument groups together tests ran in different executions                            |
+
+        The *Started At* argument may be passed as:
+        - String: YYYY-mm-dd HH:MM:SS
+        - Datetime variable: See [https://robotframework.org/robotframework/latest/libraries/DateTime.html|DateTime library]
+
+        *Example:*
+            | ${batch_id}= | Create Eyes Batch |
+        """
+
+        if started_at:
+            if isinstance(started_at, basestring):
+                started_at = datetime.strptime(started_at, "%Y-%m-%d %H:%M:%S")
+            elif not isinstance(started_at, datetime):
+                raise TypeError("BatchInfo started_at should be `str` or `datetime`")
+        batch = BatchInfo(
+            name, started_at=started_at, batch_sequence_name=batch_sequence_name
+        )
+        if batch_id:
+            batch = batch.with_batch_id(batch_id)
+        self.ctx.register_or_get_batch(batch)
+        return batch.id
+
+    @keyword("Get Eyes Configure Property", types=(str,))
+    def get_eyes_configure_property(self, name):
+        # type: (Text) -> Dict[Text,Text]
+        result = [p for p in self.ctx.configure.properties if p.get("name") == name]
+        if result:
+            return result[0]
+        return {}
+
+    @keyword("Set Eyes Configure Property", types=(str, str))
+    @deprecated.attribute("use `Eyes Configure Add Property` instead")
+    def set_eyes_configure_property(self, name, value):
+        # type: (Text, Text) -> None
+        self.eyes_configure_add_property(name, value)
+
+    @keyword("Eyes Configure Add Property", types=(str, str))
+    def eyes_configure_add_property(self, name, value):
+        # type: (Text, Text) -> None
+        self.ctx.configure.add_property(name, value)
