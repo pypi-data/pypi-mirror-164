6ffed3a000f6fa41809e20b845830b830489f8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:12 +0000

    Add package dtschema/2022.8.3/dtschema-2022.8.3.tar.gz

---
+++ packages/dtschema/dtschema-2022.8.3.tar.gz/dtschema-2022.8.3/dtschema/__init__.py
@@ -0,0 +1,18 @@
+from dtschema.lib import (
+    add_schema_path,
+    load_schema,
+    load,
+    set_schemas,
+    DTValidator,
+    format_error,
+    extract_compatibles,
+    extract_types,
+    get_undocumented_compatibles,
+    property_get_type,
+    property_get_type_dim,
+    sized_int,
+)
+
+from dtschema.version import (
+    __version__
+)
