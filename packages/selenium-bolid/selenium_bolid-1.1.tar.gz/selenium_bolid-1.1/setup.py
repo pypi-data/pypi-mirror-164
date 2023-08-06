3556ec4521b285d5f64909027b04b2241b19952f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:36 +0000

    Add package selenium-bolid/1.1/selenium_bolid-1.1.tar.gz

---
+++ packages/selenium-bolid/selenium_bolid-1.1.tar.gz/selenium_bolid-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='selenium_bolid',
+      version='1.1',
+      description='Mtehods distributions',
+      packages=['selenium_bolid'],
+      install_requires=["selenium", "pytest"],
+      author_email='gorelov2895@yandex.ru',
+      zip_safe=False)
