743c8625161f4167e8439b11cd5fec55a763b1e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:26 +0000

    Add package stepwise-process/2.5/stepwise_process-2.5.tar.gz

---
+++ packages/stepwise-process/stepwise_process-2.5.tar.gz/stepwise_process-2.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stepwise_process',
+    version='2.5',
+    license='MIT',
+    author="Helder Prado Santos",
+    author_email='helderprado@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/helderprado/stepwise-statsmodels',
+    keywords='stepwise process statsmodels',
+    install_requires=[
+        'pandas',
+        'statsmodels'
+    ],
+)
