f86831696b27fbd860104753e0623240b92a47a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:07 +0000

    Add package fabrica-de-bere/0.0.2/fabrica_de_bere-0.0.2.tar.gz

---
+++ packages/fabrica-de-bere/fabrica_de_bere-0.0.2.tar.gz/fabrica_de_bere-0.0.2/src/fabrica_de_bere/__init__.py
@@ -0,0 +1,5 @@
+from fabrica_de_bere.beer import Beer
+from fabrica_de_bere.cache import CallsCache
+from fabrica_de_bere.fabrica_de_bere import FabricaDeBere  # noqa: F401
+
+__all__ = ["FabriceDeBere", "Beer", "CallsCache"]
