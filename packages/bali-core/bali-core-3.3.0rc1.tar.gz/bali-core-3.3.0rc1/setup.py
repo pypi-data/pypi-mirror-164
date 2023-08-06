cad01db5ef016268b53bb93d0778662de8e41adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:33 +0000

    Add package bali-core/3.3.0rc1/bali-core-3.3.0rc1.tar.gz

---
+++ packages/bali-core/bali-core-3.3.0rc1.tar.gz/bali-core-3.3.0rc1/setup.py
@@ -0,0 +1,36 @@
+import bali
+from setuptools import find_packages, setup
+
+
+def read(f):
+    return open(f, 'r', encoding='utf-8').read()
+
+
+# https://pip.pypa.io/en/latest/user_guide/#fixing-conflicting-dependencies
+INSTALL_REQUIREMENTS = read('requirements.txt').splitlines()
+
+setup(
+    name='bali-core',
+    version=bali.__version__,
+    description='Simplify FastAPI integrate gRPC services development',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/bali-framework/bali',
+    author='Josh.Yu',
+    author_email='josh.yu_8@live.com',
+    license='MIT',
+    install_requires=INSTALL_REQUIREMENTS,
+    packages=find_packages(
+        exclude=[
+            'examples',
+            'examples.*',
+            'tests',
+            'tests.*',
+            'docker',
+            'docs',
+        ]
+    ),
+    package_data={'bali': ['db/*.pyi']},
+    include_package_data=True,
+    zip_safe=False,
+)
