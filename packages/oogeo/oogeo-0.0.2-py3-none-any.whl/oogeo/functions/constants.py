0c66395f9620070fbe8b552cbf26af8307025cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:08 +0000

    Add package oogeo/0.0.2/oogeo-0.0.2-py3-none-any.whl

---
+++ packages/oogeo/oogeo-0.0.2-py3-none-any.whl/oogeo/functions/constants.py
@@ -0,0 +1,20 @@
+BUFFER_END_STYLES = {
+    'ROUND': 1,
+    'FLAT': 2,
+    'SQUARE': 3
+}
+
+BUFFER_JOIN_STYLES = {
+    'ROUND': 1,
+    'MITRE': 2,
+    'BEVEL': 3
+}
+
+METADATA_TABLES = [
+    'spatial_ref_sys',
+    'pointcloud_formats',
+]
+
+SRIDS = {
+    '2263': 102718
+}
