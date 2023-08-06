8cc5504137904bd7fd38085cf60b182a28b1093d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:33 +0000

    Add package pyhmy/0.1.0/pyhmy-0.1.0-py3-none-any.whl

---
+++ packages/pyhmy/pyhmy-0.1.0-py3-none-any.whl/pyhmy/__init__.py
@@ -0,0 +1,23 @@
+"""
+`pyhmy` for interacting with the Harmony blockchain
+"""
+import sys
+import warnings
+
+__version__ = "0.1.0"
+
+if sys.version_info.major < 3:
+    warnings.simplefilter( "always", DeprecationWarning )
+    warnings.warn(
+        DeprecationWarning(
+            "`pyhmy` does not support Python 2. Please use Python 3."
+        )
+    )
+    warnings.resetwarnings()
+
+if sys.platform.startswith( "win32" ) or sys.platform.startswith( "cygwin" ):
+    warnings.simplefilter( "always", ImportWarning )
+    warnings.warn(
+        ImportWarning( "`pyhmy` does not work on Windows or Cygwin." )
+    )
+    warnings.resetwarnings()
