b4d6440da4d0819c43d0eb375a614cbf807b096f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:53 +0000

    Add package particle45/0.1.0/particle45-0.1.0.tar.gz

---
+++ packages/particle45/particle45-0.1.0.tar.gz/particle45-0.1.0/particle45.py
@@ -0,0 +1,12 @@
+"""A sample Python package that I want to put onto PyPi."""
+__version__ = "0.1.0"
+
+
+def language():
+    print("The language that I currently speak the best is called English.")
+
+
+if __name__ == "__main__":
+
+    # Test the things...
+    language()
