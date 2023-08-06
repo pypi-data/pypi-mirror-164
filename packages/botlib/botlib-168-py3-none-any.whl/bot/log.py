707928911aaafe33e21877efebc0e727852b5c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:10 +0000

    Add package botlib/168/botlib-168-py3-none-any.whl

---
+++ packages/botlib/botlib-168-py3-none-any.whl/bot/log.py
@@ -0,0 +1,36 @@
+# This file is placed in the Public Domain.
+# pylint: disable=E1101,C0116,C0413,C0411
+
+
+"log"
+
+
+import time
+
+
+from obj import Object, find, fntime, save
+from tmr import elapsed
+
+
+class Log(Object):
+
+    def __init__(self):
+        super().__init__()
+        self.txt = ""
+
+
+def log(event):
+    if not event.rest:
+        _nr = 0
+        for _fn, obj in find("log"):
+            event.reply("%s %s %s" % (
+                                      _nr,
+                                      obj.txt,
+                                      elapsed(time.time() - fntime(_fn)))
+                                     )
+            _nr += 1
+        return
+    obj = Log()
+    obj.txt = event.rest
+    save(obj)
+    event.reply("ok")
