fb55e4504fa7a034b0ce092399fb13832518fe52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:03 +0000

    Add package bioregistry/0.5.76/bioregistry-0.5.76-py3-none-any.whl

---
+++ packages/bioregistry/bioregistry-0.5.76-py3-none-any.whl/bioregistry/schema/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Data structures."""
+
+from .struct import (  # noqa:F401
+    Attributable,
+    Author,
+    Collection,
+    Context,
+    Registry,
+    Resource,
+    get_json_schema,
+)
+from .utils import sanitize_mapping, sanitize_model  # noqa:F401
