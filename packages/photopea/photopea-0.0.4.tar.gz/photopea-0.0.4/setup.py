a6164e1ea4e91465c1c96f90a71bcd278960453a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:17 +0000

    Add package photopea/0.0.4/photopea-0.0.4.tar.gz

---
+++ packages/photopea/photopea-0.0.4.tar.gz/photopea-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='photopea',
+  version='0.0.4',
+  description='Image and video converter module',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='iCoderNet',
+  author_email='iCoderNet@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='photopea', 
+  packages=find_packages(),
+  install_requires=['opencv-python','numpy','ffmpeg-python'] 
+)
