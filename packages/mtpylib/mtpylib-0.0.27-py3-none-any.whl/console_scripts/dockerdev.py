fd44f3f6b1ebd9d1025831e9658f3fb8a51eeea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:35 +0000

    Add package mtpylib/0.0.27/mtpylib-0.0.27-py3-none-any.whl

---
+++ packages/mtpylib/mtpylib-0.0.27-py3-none-any.whl/console_scripts/dockerdev.py
@@ -0,0 +1,12 @@
+import sys
+import os
+from mtlibs import process_helper
+
+from dotenv import load_dotenv, find_dotenv
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+
+def console_script_entry():
+    print("启动docker开发环境", flush=True)
