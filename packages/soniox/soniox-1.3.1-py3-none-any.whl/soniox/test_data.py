15a101cde537516bf035c72e4383c083442cf7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:06 +0000

    Add package soniox/1.3.1/soniox-1.3.1-py3-none-any.whl

---
+++ packages/soniox/soniox-1.3.1-py3-none-any.whl/soniox/test_data.py
@@ -0,0 +1,16 @@
+import os
+
+TEST_DATA_DIR = os.path.join(os.path.realpath(os.path.dirname(__file__)), "test_data_files")
+
+TEST_AUDIO_FLAC = os.path.join(TEST_DATA_DIR, "test_audio.flac")
+TEST_AUDIO_MP3 = os.path.join(TEST_DATA_DIR, "test_audio.mp3")
+TEST_AUDIO_RAW = os.path.join(TEST_DATA_DIR, "test_audio.raw")
+TEST_AUDIO_LONG_FLAC = os.path.join(TEST_DATA_DIR, "test_audio_long.flac")
+TEST_AUDIO_LONG_RAW = os.path.join(TEST_DATA_DIR, "test_audio_long.raw")
+TEST_AUDIO_SD_SPK1_FLAC = os.path.join(TEST_DATA_DIR, "test_audio_sd_spk1.flac")
+TEST_AUDIO_SD_SPK2_FLAC = os.path.join(TEST_DATA_DIR, "test_audio_sd_spk2.flac")
+TEST_AUDIO_SD_FLAC = os.path.join(TEST_DATA_DIR, "test_audio_sd.flac")
+TEST_AUDIO_SD_RAW = os.path.join(TEST_DATA_DIR, "test_audio_sd.raw")
+TEST_AUDIO_SPEECH_ADAPTATION_FLAC = os.path.join(TEST_DATA_DIR, "test_audio_speech_adaptation.flac")
+TEST_AUDIO_MULTI_CHANNEL_FLAC = os.path.join(TEST_DATA_DIR, "test_audio_multi_channel.flac")
+TEST_AUDIO_PROFANITY_MP3 = os.path.join(TEST_DATA_DIR, "test_audio_profanity.mp3")
