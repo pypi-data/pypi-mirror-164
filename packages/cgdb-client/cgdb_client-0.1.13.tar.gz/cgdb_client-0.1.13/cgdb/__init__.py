6fd7d5f55167a5f1dcd31150fc12f11ab48a3831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:37 +0000

    Add package cgdb-client/0.1.13/cgdb_client-0.1.13.tar.gz

---
+++ packages/cgdb-client/cgdb_client-0.1.13.tar.gz/cgdb_client-0.1.13/cgdb/__init__.py
@@ -0,0 +1,5 @@
+from cgdb.cgdb import CGDB
+from cgdb import exceptions
+from cgdb import recources
+from cgdb import managers
+from cgdb import utils
---
+++ packages/cgdb-client/cgdb_client-0.1.13.tar.gz/cgdb_client-0.1.13/cgdb/managers/aggregationsManager.py
@@ -0,0 +1,7 @@
+from cgdb.recources.aggregation import Aggregation
+
+
+class AggregationsManager:
+    def aggregations(self):
+        return [Aggregation("VAL"), Aggregation("AVG"), Aggregation("SUM"), Aggregation("MIN"), Aggregation("MAX"),
+                Aggregation("TM"), Aggregation("TN"), Aggregation("TE"), Aggregation("MED")]
