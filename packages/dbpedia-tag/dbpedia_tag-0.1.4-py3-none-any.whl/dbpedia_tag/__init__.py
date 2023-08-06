736a65b67ebc5968502a7c89da4118fa93db9dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:40 +0000

    Add package dbpedia-tag/0.1.4/dbpedia_tag-0.1.4-py3-none-any.whl

---
+++ packages/dbpedia-tag/dbpedia_tag-0.1.4-py3-none-any.whl/dbpedia_tag/__init__.py
@@ -0,0 +1,11 @@
+from .bp import *
+from .svc import *
+from .dmo import *
+
+from .bp.tagger import Tagger
+
+tagapi = Tagger()
+
+
+def dbpedia_tag(input_text) -> dict:
+    return tagapi.process(input_text)
---
+++ packages/dbpedia-tag/dbpedia_tag-0.1.4-py3-none-any.whl/dbpedia_tag/dmo/__init__.py
@@ -0,0 +1,3 @@
+from .sliding_window_extract import SlidingWindowExtract
+from .sliding_window_validate import SlidingWindowValidate
+from .candidate_fuzzy_match import CandidateFuzzyMatch
