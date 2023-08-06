f6906874bb803717c0909c769aefd24966228990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:22 +0000

    Add package leafs/0.0.1/leafs-0.0.1.tar.gz

---
+++ packages/leafs/leafs-0.0.1.tar.gz/leafs-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='leafs',
+    version='0.0.1',
+    author='qimoxue',
+    author_email='chenlong970208@163.com',
+    description='Used to read leafs format files',
+    long_description_content_type="""text/markdown""",
+    url='https://www.autotests.cn',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
