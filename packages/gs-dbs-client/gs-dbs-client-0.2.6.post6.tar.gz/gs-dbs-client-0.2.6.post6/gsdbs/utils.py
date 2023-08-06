6c2b768a1140b13a4f80027d9feed3c4292e04e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:38 +0000

    Add package gs-dbs-client/0.2.6.post6/gs-dbs-client-0.2.6.post6.tar.gz

---
+++ packages/gs-dbs-client/gs-dbs-client-0.2.6.post6.tar.gz/gs-dbs-client-0.2.6.post6/gsdbs/utils.py
@@ -0,0 +1,17 @@
+import base64
+import blosc
+import numpy as np
+
+
+def gsNpCompressBase64WithShape(frame):
+    bytes_array = frame.tostring()
+    zframe = blosc.compress(bytes_array)
+    b64frame = base64.b64encode((zframe))
+    return b64frame.decode('utf-8'), frame.shape[0], frame.shape[1], frame.shape[2]
+
+
+def gsNumpyDecompressBase64(b64frame, w, h, d):
+    zframe = base64.b64decode(b64frame.encode('utf-8'))
+    frame = np.frombuffer(blosc.decompress(zframe, True), dtype=np.uint8)
+    frame.shape = (int(w), int(h), int(d))
+    return frame
