59e3a45f5779dd6827cc5d9e5676fc4e730ccab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:18 +0000

    Add package lk-logger/5.2.1/lk-logger-5.2.1.tar.gz

---
+++ packages/lk-logger/lk-logger-5.2.1.tar.gz/lk-logger-5.2.1/lk_logger/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from .general import default_print
+from .global_control import disable
+from .global_control import enable
+from .global_control import setup
+from .global_control import setup_for_ipython
+from .global_control import unload
+from .global_control import update
+from .logger import lk
+
+__version__ = '5.2.1'
