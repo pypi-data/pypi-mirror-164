4b696fd653ab0f21a467f2acdeba7b7ad368e1ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:50 +0000

    Add package ansible-lint/6.5.0/ansible_lint-6.5.0-py3-none-any.whl

---
+++ packages/ansible-lint/ansible_lint-6.5.0-py3-none-any.whl/ansiblelint/generate_docs.py
@@ -0,0 +1,116 @@
+"""Utils to generate rules documentation."""
+import logging
+from typing import Iterable
+
+from rich import box
+
+# Remove this compatibility try-catch block once we drop support for rich < 10.7.0
+try:
+    from rich.console import group
+except ImportError:
+    from rich.console import render_group as group  # type: ignore
+
+from rich.markdown import Markdown
+from rich.table import Table
+
+from ansiblelint.config import PROFILES
+from ansiblelint.constants import DEFAULT_RULESDIR
+from ansiblelint.rules import RulesCollection
+
+DOC_HEADER = """
+# Default Rules
+
+(lint_default_rules)=
+
+Below you can see the list of default rules Ansible Lint use to evaluate playbooks and roles:
+
+"""
+
+_logger = logging.getLogger(__name__)
+
+
+def rules_as_str(rules: RulesCollection) -> str:
+    """Return rules as string."""
+    return "\n".join([str(rule) for rule in rules.alphabetical()])
+
+
+def rules_as_md(rules: RulesCollection) -> str:
+    """Return md documentation for a list of rules."""
+    result = DOC_HEADER
+
+    for rule in rules.alphabetical():
+
+        # because title == rule.id we get the desired labels for free
+        # and we do not have to insert `(target_header)=`
+        title = f"{rule.id}"
+
+        if rule.help:
+            if not rule.help.startswith(f"## {rule.id}"):
+                raise RuntimeError(
+                    f"Rule {rule.__class__} markdown help does not start with `## {rule.id}` header.\n{rule.help}"
+                )
+            result += f"\n\n{rule.help}"
+        else:
+            description = rule.description
+            if rule.link:
+                description += f" [more]({rule.link})"
+
+            result += f"\n\n## {title}\n\n**{rule.shortdesc}**\n\n{description}"
+
+    return result
+
+
+@group()
+def rules_as_rich(rules: RulesCollection) -> Iterable[Table]:
+    """Print documentation for a list of rules, returns empty string."""
+    width = max(16, *[len(rule.id) for rule in rules])
+    for rule in rules.alphabetical():
+        table = Table(show_header=True, header_style="title", box=box.MINIMAL)
+        table.add_column(rule.id, style="dim", width=width)
+        table.add_column(Markdown(rule.shortdesc))
+
+        description = rule.help or rule.description
+        if rule.link:
+            description += f" [(more)]({rule.link})"
+        table.add_row("description", Markdown(description))
+        if rule.version_added:
+            table.add_row("version_added", rule.version_added)
+        if rule.tags:
+            table.add_row("tags", ", ".join(rule.tags))
+        if rule.severity:
+            table.add_row("severity", rule.severity)
+        yield table
+
+
+def profiles_as_md() -> str:
+    """Return markdown representation of supported profiles."""
+    result = ""
+    default_rules = RulesCollection([DEFAULT_RULESDIR])
+
+    for name, profile in PROFILES.items():
+        extends = ""
+        if profile.get("extends", None):
+            extends = (
+                f" It extends [{profile['extends']}](#{profile['extends']}) profile."
+            )
+        result += f"## {name}\n\n{profile['description']}{extends}\n"
+        for rule, rule_data in profile["rules"].items():
+            for default_rule in default_rules.rules:
+                if default_rule.id == rule:
+                    result += f"- [{rule}](default_rules.md/#{rule})\n"
+                    break
+            else:
+                if not rule_data:
+                    raise RuntimeError(f"Rule {rule} has no data.")
+                url = rule_data.get("url", None)
+                if url:
+                    result += f"- [{rule}]({url})*\n"
+                else:
+                    raise RuntimeError("All planned rules must have an 'url' entry.")
+        result += "\n"
+    return result
+
+
+def profiles_as_rich() -> Markdown:
+    """Return rich representation of supported profiles."""
+    return Markdown(profiles_as_md())
---
+++ packages/ansible-lint/ansible_lint-6.5.0-py3-none-any.whl/ansiblelint/rules/name.py
@@ -0,0 +1,81 @@
+"""Implementation of NameRule."""
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING, Any, Dict, Union
+
+from ansiblelint.errors import MatchError
+from ansiblelint.rules import AnsibleLintRule
+from ansiblelint.utils import LINE_NUMBER_KEY
+
+if TYPE_CHECKING:
+    from typing import Optional
+
+    from ansiblelint.file_utils import Lintable
+
+
+class NameRule(AnsibleLintRule):
+    """Rule for checking task names and their content."""
+
+    id = "name"
+    description = (
+        "All tasks should have a distinct name for readability "
+        "and for ``--start-at-task`` to work"
+    )
+    severity = "MEDIUM"
+    tags = ["idiom"]
+    version_added = "historic"
+
+    def matchtask(
+        self, task: Dict[str, Any], file: Optional[Lintable] = None
+    ) -> Union[bool, str, MatchError]:
+        name = task.get("name")
+        if not name:
+            return self.create_matcherror(
+                message="All tasks should be named.",
+                linenumber=task[LINE_NUMBER_KEY],
+                tag="name[missing]",
+                filename=file,
+            )
+        if not name[0].isupper():
+            return self.create_matcherror(
+                message="All names should start with an uppercase letter.",
+                linenumber=task[LINE_NUMBER_KEY],
+                tag="name[casing]",
+                filename=file,
+            )
+        return False
+
+
+if "pytest" in sys.modules:  # noqa: C901
+
+    from ansiblelint.rules import RulesCollection
+    from ansiblelint.runner import Runner
+
+    def test_file_positive() -> None:
+        """Positive test for unnamed-task."""
+        collection = RulesCollection()
+        collection.register(NameRule())
+        success = "examples/playbooks/task-has-name-success.yml"
+        good_runner = Runner(success, rules=collection)
+        assert [] == good_runner.run()
+
+    def test_file_negative() -> None:
+        """Negative test for unnamed-task."""
+        collection = RulesCollection()
+        collection.register(NameRule())
+        failure = "examples/playbooks/task-has-name-failure.yml"
+        bad_runner = Runner(failure, rules=collection)
+        errs = bad_runner.run()
+        assert len(errs) == 4
+
+    def test_rule_name_lowercase() -> None:
+        """Negative test for a task that starts with lowercase."""
+        collection = RulesCollection()
+        collection.register(NameRule())
+        failure = "examples/playbooks/task-name-lowercase.yml"
+        bad_runner = Runner(failure, rules=collection)
+        errs = bad_runner.run()
+        assert len(errs) == 1
+        assert errs[0].tag == "name[casing]"
+        assert errs[0].rule.id == "name"
