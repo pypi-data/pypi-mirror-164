4800df8a8aa5a30f7a1423b29f82a240d76ac4cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:59:16 +0000

    Add package pard/0.2.0/pard-0.2.0.tar.gz

---
+++ packages/pard/pard-0.2.0.tar.gz/pard-0.2.0/pard/miyata.py
@@ -0,0 +1,12 @@
+import pard.raw_python_dictionaries
+from pard.handling_3_letter_code_decorator import handling_3_letter_code
+
+@handling_3_letter_code
+def miyata(amino_acid_1: str, amino_acid_2: str) -> float:
+    """
+    :param amino_acid_1: 1 letter code of the first amino acid
+    :param amino_acid_2: 1 letter code of the third amino acid
+
+    :return: An integer representing the Miyata distance between amino_acid_1 and amino_acid_2
+    """
+    return pard.raw_python_dictionaries.MIYATA_DICT[(amino_acid_1, amino_acid_2)]
