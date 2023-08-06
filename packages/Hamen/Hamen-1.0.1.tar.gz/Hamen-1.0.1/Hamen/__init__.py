686e484a0782265c54ffef74fef5bfda6c228d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:39 +0000

    Add package Hamen/1.0.1/Hamen-1.0.1.tar.gz

---
+++ packages/Hamen/Hamen-1.0.1.tar.gz/Hamen-1.0.1/Hamen/__init__.py
@@ -0,0 +1,5 @@
+from Hamen.Colors.__init__ import *
+from Hamen.Types.__init__ import *
+from Hamen.Math.__init__ import *
+from Hamen.Errors.__init__ import *
+from Hamen.Libraries.__init__ import *
---
+++ packages/Hamen/Hamen-1.0.1.tar.gz/Hamen-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name = 'Hamen',
+    packages = ['Hamen'],
+    version = '1.0.1',
+    license='GNU',
+    description = 'Hamen is an API',
+    author = 'Hamen',
+    author_email = 'hamen.connect@gmail.com',
+    url = 'https://hamen.tech/api',
+    download_url = 'https://github.com/danielhamen/Hamen-API/archive/refs/tags/v_1.0.1.tar.gz',
+    keywords = ['hamen', 'tools', 'tool', 'efficient', 'fast', 'faster', 'easy'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
