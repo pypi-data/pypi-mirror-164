1feaba28eb7524b6fb36d8f2e7721c1f5a03347a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:16 +0000

    Add package wthtest/0.1.1.2/wthtest-0.1.1.2.tar.gz

---
+++ packages/wthtest/wthtest-0.1.1.2.tar.gz/wthtest-0.1.1.2/setup.py
@@ -0,0 +1,16 @@
+# -*- coding = utf-8 -*-
+# @Time : 2022-08-01 15:02
+# @Author : 王铁翰
+# @File : setup.py
+# @Software : PyCharm
+from setuptools import setup
+
+setup(
+    name = 'wthtest',
+    version = '0.1.1.2',
+    description = 'This is a test lib',
+    packages = ['wthtest','sphinxcontrib'],
+    namespace_package = ['google'],
+    py_modules = ['tool'],
+    author = 'wth'
+)
