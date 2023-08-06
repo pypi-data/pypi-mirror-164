f24fa6180bce4ad3d1bf2dd6a0e1bc74dc43e104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:25 +0000

    Add package lava-logger/1.0.1/lava_logger-1.0.1.tar.gz

---
+++ packages/lava-logger/lava_logger-1.0.1.tar.gz/lava_logger-1.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='lava_logger',
+      version='1.0.1',
+      description='Custom logger by lava_frai',
+      packages=['lava_logger'],
+      author_email='lavafrai@yandex.ru',
+      zip_safe=False)
