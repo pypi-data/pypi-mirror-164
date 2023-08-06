6b51b237e8841d603816323c5c3fa97e193fa5f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:43 +0000

    Add package anybadge/1.14.0/anybadge-1.14.0.tar.gz

---
+++ packages/anybadge/anybadge-1.14.0.tar.gz/anybadge-1.14.0/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import os
+import re
+from setuptools import setup
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+# Attempt to get version number from TravisCI environment variable
+version = os.environ.get("TRAVIS_TAG", default="0.0.0")
+
+# Remove leading 'v'
+version = re.sub("^v", "", version)
+
+setup(
+    name="anybadge",
+    description="Simple, flexible badge generator for project badges.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=version,
+    author="Jon Grace-Cox",
+    author_email="jongracecox@gmail.com",
+    packages=["anybadge", "anybadge.templates", "anybadge.server"],
+    py_modules=["anybadge_server"],
+    setup_requires=["setuptools", "wheel"],
+    tests_require=[],
+    install_requires=["packaging"],
+    package_data={"anybadge": ["templates/*.svg"]},
+    options={"bdist_wheel": {"universal": False}},
+    python_requires=">=3.7",
+    url="https://github.com/jongracecox/anybadge",
+    entry_points={
+        "console_scripts": [
+            "anybadge=anybadge.cli:main",
+            "anybadge-server=anybadge.server.cli:main",
+        ],
+    },
+    classifiers=["License :: OSI Approved :: MIT License"],
+)
