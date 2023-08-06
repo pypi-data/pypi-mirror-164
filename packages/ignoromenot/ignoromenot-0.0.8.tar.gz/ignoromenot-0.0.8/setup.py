6c6131565f501420405f746e482f7e2820fd72f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:22 +0000

    Add package ignoromenot/0.0.8/ignoromenot-0.0.8.tar.gz

---
+++ packages/ignoromenot/ignoromenot-0.0.8.tar.gz/ignoromenot-0.0.8/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+VERSION = '0.0.8'
+DESCRIPTION = 'Ignorome finder'
+
+with open(path.join(here,"README.md"),encoding='utf-8') as fh:
+    long_description=fh.read()
+
+# Setting up
+setup(
+        name='ignoromenot',
+        version=VERSION,
+        author="An Phan",
+        author_email="<ahphan@iastate.edu>",
+        description=DESCRIPTION,
+        long_description=long_description,
+        long_description_content_type = 'text/markdown',
+        license='GPLv3',
+        packages=find_packages(),
+        install_requires=['networkx', 'matplotlib','numpy','Biopython','requests','pandas'],
+        include_package_data=True,
+        # package_dir={'': 'src'},
+        entry_points={
+            'console_scripts':[
+                'ignoromenot = ignoromenot.ignoromenot:main',
+            ]
+        },
+        url="https://github.com/anphan0828/IgnoroMeNot.git",
+        classifiers = [
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Operating System :: OS Independent",
+        ]
+)
+
+
