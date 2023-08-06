06dcd1c39e23ae9f7739314e910eab8d6eb2c4b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:08 +0000

    Add package OpihiExarata/2022.8.24/OpihiExarata-2022.8.24-py3-none-any.whl

---
+++ packages/OpihiExarata/OpihiExarata-2022.8.24-py3-none-any.whl/opihiexarata/opihi/__init__.py
@@ -0,0 +1,7 @@
+"""The class for the collection of Exarata solutions."""
+
+# The solution class itself for all other parts.
+from opihiexarata.opihi.solution import OpihiSolution
+
+# The solution for preprocessing.
+from opihiexarata.opihi.preprocess import OpihiPreprocessSolution
