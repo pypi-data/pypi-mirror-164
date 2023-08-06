6cbf9fc1c6717c439fd251280643d9a51ed4e8e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:37 +0000

    Add package internal-with-fin-tmp/0.1.5/internal_with_fin_tmp-0.1.5.tar.gz

---
+++ packages/internal-with-fin-tmp/internal_with_fin_tmp-0.1.5.tar.gz/internal_with_fin_tmp-0.1.5/sparknlp_jsl/extensions/finance/chunk_classification/__init__.py
@@ -0,0 +1,3 @@
+from sparknlp_jsl.extensions.finance.chunk_classification.resolution import *
+from sparknlp_jsl.extensions.finance.chunk_classification.deid import *
+from sparknlp_jsl.extensions.finance.chunk_classification.assertion import *
