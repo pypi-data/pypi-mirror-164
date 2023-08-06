20a8e3e9878b835678166bec9b00b4d249e1a2dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:19 +0000

    Add package torchversion/0.0.1/torchversion-0.0.1-py3-none-any.whl

---
+++ packages/torchversion/torchversion-0.0.1-py3-none-any.whl/telecomlib/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.2"
+
+from telecomlib import shell
+from telecomlib import getip
+from telecomlib import star
+
+__all__ = [
+    "__version__",
+    "shell"
+]
---
+++ packages/torchversion/torchversion-0.0.1-py3-none-any.whl/torchversion/utils/__init__.py
@@ -0,0 +1,5 @@
+from torchversion.utils.version_pytorch import last_version
+
+__all__ = [
+    'last_version'
+]
