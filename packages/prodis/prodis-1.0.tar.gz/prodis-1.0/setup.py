75a76eef8cbd5f211f3c19d4f0f9657a842d031e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:30 +0000

    Add package prodis/1.0/prodis-1.0.tar.gz

---
+++ packages/prodis/prodis-1.0.tar.gz/prodis-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='prodis',
+      
+      version='1.0',
+      
+      description='Gaussian and Binomial distributions',
+      
+      packages=['prodis'],
+      
+      author= 'Mizaid Ulises Ortega Ortega',
+      
+      author_email='uliortegao@gmail.com',
+      
+      zip_safe=False)
