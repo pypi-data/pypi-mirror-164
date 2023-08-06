fa2d695a785847c8137c89f342a30a49fe6c90dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:46 +0000

    Add package smalii/0.0.4/smalii-0.0.4.tar.gz

---
+++ packages/smalii/smalii-0.0.4.tar.gz/smalii-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+  name="smalii",
+  version="0.0.4",
+  author="malinkang",
+  author_email="linkang.ma@gmail.com",
+  description="",
+  long_description="",
+  url="https://github.com/malinkang/smalii",
+  packages=find_packages(),
+    entry_points={
+        "console_scripts": ["smalii = cli:main"],
+    },
+)
