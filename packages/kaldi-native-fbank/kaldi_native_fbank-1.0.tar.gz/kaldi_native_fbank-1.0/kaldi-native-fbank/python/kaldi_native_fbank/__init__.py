0d4104caf69f97d6c81c7b14e4866feface58250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:41 +0000

    Add package kaldi-native-fbank/1.0/kaldi_native_fbank-1.0.tar.gz

---
+++ packages/kaldi-native-fbank/kaldi_native_fbank-1.0.tar.gz/kaldi_native_fbank-1.0/kaldi-native-fbank/python/kaldi_native_fbank/__init__.py
@@ -0,0 +1,11 @@
+from _kaldi_native_fbank import (
+    FrameExtractionOptions,
+    MelBanksOptions,
+    OnlineFbank,
+    FbankOptions,
+)
+__version__ = '0.1'
+__version__ = '0.1'
+__version__ = '1.0'
+__version__ = '1.0'
+__version__ = '1.0'
