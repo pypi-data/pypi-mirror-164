23005d1074d225180ff70b6fffd148a01323755f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:08 +0000

    Add package indsl/6.1.0/indsl-6.1.0-py3-none-any.whl

---
+++ packages/indsl/indsl-6.1.0-py3-none-any.whl/indsl/__init__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Cognite AS
+from indsl import (
+    data_quality,
+    detect,
+    equipment,
+    exceptions,
+    filter,
+    fluid_dynamics,
+    forecast,
+    not_listed_operations,
+    oil_and_gas,
+    regression,
+    resample,
+    signals,
+    smooth,
+    statistics,
+    ts_utils,
+)
