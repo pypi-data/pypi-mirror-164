7acaa350534e50cefdd9c27b59739b03b29eaf34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:37 +0000

    Add package mage-ai/0.3.1/mage_ai-0.3.1-py3-none-any.whl

---
+++ packages/mage-ai/mage_ai-0.3.1-py3-none-any.whl/mage_ai/server/api/orchestration.py
@@ -0,0 +1,99 @@
+from .base import BaseHandler
+from mage_ai.data_preparation.models.pipeline import Pipeline
+from mage_ai.orchestration.db.models import BlockRun, PipelineRun, PipelineSchedule
+
+
+class ApiBlockRunListHandler(BaseHandler):
+    model_class = BlockRun
+
+    def get(self, pipeline_run_id):
+        block_runs = BlockRun.query.filter(
+            BlockRun.pipeline_run_id == int(pipeline_run_id),
+        ).all()
+        collection = [r.to_dict() for r in block_runs]
+
+        self.write(dict(block_runs=collection))
+        self.finish()
+
+
+class ApiBlockRunOutputHandler(BaseHandler):
+    def get(self, block_run_id):
+        block_run = BlockRun.query.get(int(block_run_id))
+        outputs = block_run.get_outputs()
+        self.write(dict(outputs=outputs))
+
+
+class ApiPipelineRunListHandler(BaseHandler):
+    model_class = PipelineRun
+
+    def get(self, pipeline_schedule_id):
+        pipeline_runs = PipelineRun.query.filter(
+            PipelineRun.pipeline_schedule_id == int(pipeline_schedule_id),
+        ).all()
+        collection = [r.to_dict() for r in pipeline_runs]
+
+        self.write(dict(pipeline_runs=collection))
+        self.finish()
+
+    def post(self, pipeline_schedule_id):
+        pipeline_schedule = PipelineSchedule.query.get(int(pipeline_schedule_id))
+
+        payload = self.get_payload()
+        payload['pipeline_schedule_id'] = pipeline_schedule.id
+        payload['pipeline_uuid'] = pipeline_schedule.pipeline_uuid
+        pipeline_run = PipelineRun.create(**payload)
+
+        from mage_ai.orchestration.pipeline_scheduler import PipelineScheduler
+        PipelineScheduler(pipeline_run).start()
+
+        self.write(dict(pipeline_run=pipeline_run.to_dict()))
+
+
+class ApiPipelineScheduleDetailHandler(BaseHandler):
+    datetime_keys = ['start_time']
+    model_class = PipelineSchedule
+
+    def get(self, pipeline_schedule_id):
+        pipeline_schedule = PipelineSchedule.query.get(int(pipeline_schedule_id))
+
+        self.write(dict(pipeline_schedule=pipeline_schedule.to_dict()))
+
+    def put(self, pipeline_schedule_id):
+        pipeline_schedule = PipelineSchedule.query.get(int(pipeline_schedule_id))
+        payload = self.get_payload()
+
+        pipeline_schedule.update(**payload)
+
+        self.write(dict(pipeline_schedule=pipeline_schedule.to_dict()))
+
+    def delete(self, pipeline_schedule_id):
+        pipeline_schedule = PipelineSchedule.query.get(int(pipeline_schedule_id))
+        pipeline_schedule.delete()
+        self.write(dict(pipeline_schedule=pipeline_schedule.to_dict()))
+
+
+class ApiPipelineScheduleListHandler(BaseHandler):
+    datetime_keys = ['start_time']
+    model_class = PipelineSchedule
+
+    def get(self, pipeline_uuid=None):
+        if pipeline_uuid is not None:
+            pipeline = Pipeline.get(pipeline_uuid)
+            schedules = PipelineSchedule.query.filter(
+                PipelineSchedule.pipeline_uuid == pipeline.uuid,
+            ).all()
+        else:
+            schedules = PipelineSchedule.query.all()
+        collection = [s.to_dict() for s in schedules]
+
+        self.write(dict(pipeline_schedules=collection))
+        self.finish()
+
+    def post(self, pipeline_uuid):
+        pipeline = Pipeline.get(pipeline_uuid)
+
+        payload = self.get_payload()
+        payload['pipeline_uuid'] = pipeline.uuid
+        pipeline_schedule = PipelineSchedule.create(**payload)
+
+        self.write(dict(pipeline_schedule=pipeline_schedule.to_dict()))
