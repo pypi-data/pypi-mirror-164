fbb8344808b1af283d62ed553beba39281a6cd0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:46 +0000

    Add package moneyonchain-prices-source/0.6.0/moneyonchain_prices_source-0.6.0.tar.gz

---
+++ packages/moneyonchain-prices-source/moneyonchain_prices_source-0.6.0.tar.gz/moneyonchain_prices_source-0.6.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from os.path    import dirname, abspath
+
+base_dir = dirname(abspath(__file__))
+
+with open(base_dir + "/README.md", "r") as file_:
+    long_description = file_.read()
+
+with open(base_dir + "/requirements.txt", "r") as file_:
+    requirements = file_.read().split()
+
+with open(base_dir + "/moc_prices_source/version.txt", "r") as file_:
+    version = file_.read().split()[0]
+
+setup(
+    name='moneyonchain_prices_source',
+    version=version,
+    packages=find_packages(),
+    author='Juan S. Bokser',
+    author_email='juan.bokser@moneyonchain.com',
+    description='Prices source for MoC projects',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'
+    ],
+    package_data={
+        "moc_prices_source": ["version.txt",
+                              "data/*.json"]
+    },
+    python_requires='>=3.6',
+    install_requires=requirements,
+    scripts=['moc_prices_source_check',
+             'moc_prices_source_to_db']
+)
