0ba3896d6c6a8225c98afcf82f43f49a3e622a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:05 +0000

    Add package thom02/0.2/thom02-0.2.tar.gz

---
+++ packages/thom02/thom02-0.2.tar.gz/thom02-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='thom02',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['thom02'],
+      author = 'Nguyen Huong Thom',
+      author_email = 'nguyenhuongthom@gmail.com',
+      zip_safe=False)
