3c31f285d9b7f385f3f562f0d208386f1de1d04c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:51 +0000

    Add package autoscirpt/1.0.1/autoscirpt-1.0.1-py3-none-any.whl

---
+++ packages/autoscirpt/autoscirpt-1.0.1-py3-none-any.whl/autoscript/__init__.py
@@ -0,0 +1,12 @@
+'''
+Date         : 2022-08-24 11:18:38
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2022-08-24 11:34:42
+LastEditors  : BDFD
+Description  : 
+FilePath     : \autoscript\__init__.py
+Copyright (c) 2022 by BDFD, All Rights Reserved. 
+'''
+
+from autoscript.templateproj import add_one
