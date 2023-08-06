092b70e91203d0c619161f6fb5e8001c66d97d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:22 +0000

    Add package BBT/0.0.7/BBT-0.0.7.tar.gz

---
+++ packages/BBT/BBT-0.0.7.tar.gz/BBT-0.0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 11',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3.7'
+]
+ 
+setup(
+  name='BBT',
+  version='0.0.7',
+  description='BBT GAME AND SHELDON JOKES',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Djsmanoj0000',
+  author_email='djsmanoj0000@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Game', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
