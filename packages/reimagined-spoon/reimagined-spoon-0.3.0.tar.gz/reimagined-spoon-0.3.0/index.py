6c106b64d1c15863f7ee3d3dce76603576b50312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:28 +0000

    Add package reimagined-spoon/0.3.0/reimagined-spoon-0.3.0.tar.gz

---
+++ packages/reimagined-spoon/reimagined-spoon-0.3.0.tar.gz/reimagined-spoon-0.3.0/index.py
@@ -0,0 +1,21 @@
+class Spoon(object):
+    """Main spoon class"""
+
+    def __init__(self):
+        super(Spoon, self).__init__()
+
+    def talk(self, message='¡Hola extraño!'):
+        print(message)
+
+    def bug(self):
+        if 1 > 2:
+            print("1 es mayor que 2")
+        elif 2 > 1:
+            print("2 es mayor que 1")
+
+    def spoon(self):
+        print("Spoon")
+
+
+spoon = Spoon()
+spoon.talk(message='¡Hola amigo!')
