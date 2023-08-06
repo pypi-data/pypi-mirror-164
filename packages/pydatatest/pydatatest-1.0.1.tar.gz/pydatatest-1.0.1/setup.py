9539f67d93f8ab4ba9c680daf88575d5c29c7ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:30 +0000

    Add package pydatatest/1.0.1/pydatatest-1.0.1.tar.gz

---
+++ packages/pydatatest/pydatatest-1.0.1.tar.gz/pydatatest-1.0.1/setup.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf8').read()
+
+setup(
+    name= 'pydatatest',
+    version= '1.0.1',
+    package_dir={"": "src"},
+    packages=find_packages('src'),
+    # py_modules =['api', 'data', 'common'],
+    author= 'aak1247',
+    author_email='aak1247@126.com',
+    url='https://github.com/aak1247/PyDataTest',
+    description= 'Data driven test framework',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        "pyyaml",
+        #"requests", # optional, only if you want to use session, it is needed
+    ],
+)
