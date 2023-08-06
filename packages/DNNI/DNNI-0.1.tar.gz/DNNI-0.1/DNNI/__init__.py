18c4fe1bdd138d77f9cc51f57cddae1e159bbe3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:31 +0000

    Add package DNNI/0.1/DNNI-0.1.tar.gz

---
+++ packages/DNNI/DNNI-0.1.tar.gz/DNNI-0.1/DNNI/__init__.py
@@ -0,0 +1,1 @@
+from .DNNI import DNNI
---
+++ packages/DNNI/DNNI-0.1.tar.gz/DNNI-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+ 
+setup(
+  name='DNNI',
+  version='0.1',
+  description='Deep Neural Network Integration(DNNI) - A package to obtain anti-derivatives using Deep Neural Networks',
+  author='Dibyajyoti Chakraborty',
+  author_email='dibyajyoti43@gmail.com',
+  packages=['DNNI'],
+  zip_safe=False 
+)
+
+
