61c6be67052e63ef936c8d83221f1abaf5a2e9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:04 +0000

    Add package pv-sizing/0.7/pv_sizing-0.7.tar.gz

---
+++ packages/pv-sizing/pv_sizing-0.7.tar.gz/pv_sizing-0.7/src/pv_sizing/utils/load_example.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+example_irr = pd.read_csv('example_data/example_irr.csv', header=6, skipfooter=12, engine='python', index_col='time')
+example_load = pd.read_csv('example_data/example_load.csv', sep=',')
