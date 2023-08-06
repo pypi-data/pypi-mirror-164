1abbfab3771d1fba241d33da0f0fda716344964f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:18 +0000

    Add package coolmessages/0.0.2/coolmessages-0.0.2.tar.gz

---
+++ packages/coolmessages/coolmessages-0.0.2.tar.gz/coolmessages-0.0.2/docs/conf.py
@@ -0,0 +1,16 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+project = 'coolmessages'
+copyright = '2022, anonymouscoolguy'
+author = 'anonymouscoolguy'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
