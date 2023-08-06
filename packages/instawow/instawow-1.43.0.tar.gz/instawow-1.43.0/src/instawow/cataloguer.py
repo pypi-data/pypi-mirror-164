8b303b39d719331d868bb0a768fb66c5cc6a5aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:26 +0000

    Add package instawow/1.43.0/instawow-1.43.0.tar.gz

---
+++ packages/instawow/instawow-1.43.0.tar.gz/instawow-1.43.0/src/instawow/cataloguer.py
@@ -0,0 +1,119 @@
+from __future__ import annotations
+
+from datetime import datetime
+import typing
+
+from attrs import asdict, frozen
+from cattrs import GenConverter
+from cattrs.preconf.json import configure_converter
+
+from . import manager
+from .config import Flavour
+from .utils import bucketise, cached_property, normalise_names
+
+BASE_CATALOGUE_VERSION = 6
+CATALOGUE_VERSION = 3
+
+
+catalogue_converter = GenConverter(
+    unstruct_collection_overrides={
+        # TODO: Replace with ``collections.abc.Set``
+        frozenset: sorted,
+    }
+)
+configure_converter(catalogue_converter)
+
+
+@frozen(kw_only=True)
+class CatalogueSameAs:
+    source: str
+    id: str
+
+
+@frozen(kw_only=True)
+class BaseCatalogueEntry:
+    source: str
+    id: str
+    slug: str = ''
+    name: str
+    url: str
+    game_flavours: typing.FrozenSet[Flavour]
+    download_count: int
+    last_updated: datetime
+    folders: typing.List[typing.FrozenSet[str]] = []
+    same_as: typing.List[CatalogueSameAs] = []
+
+
+@frozen(kw_only=True)
+class CatalogueEntry(BaseCatalogueEntry):
+    normalised_name: str
+    derived_download_score: float
+
+
+@frozen(kw_only=True)
+class BaseCatalogue:
+    version = BASE_CATALOGUE_VERSION
+    entries: typing.List[BaseCatalogueEntry]
+
+    @classmethod
+    async def collate(cls, start_date: datetime | None):
+        async with manager.init_web_client() as web_client:
+            entries = [e for r in manager.Manager.RESOLVERS async for e in r.catalogue(web_client)]
+            if start_date is not None:
+                entries = [e for e in entries if e.last_updated >= start_date]
+            return cls(entries=entries)
+
+
+@frozen(kw_only=True, slots=False)
+class Catalogue:
+    version = CATALOGUE_VERSION
+    entries: typing.List[CatalogueEntry]
+    curse_slugs: typing.Dict[str, str]
+
+    @classmethod
+    def from_base_catalogue(cls, unstructured_base_catalogue: object, start_date: datetime | None):
+        from ._sources.cfcore import CfCoreResolver
+        from ._sources.github import GithubResolver
+
+        normalise = normalise_names('')
+
+        base_entries = catalogue_converter.structure(
+            unstructured_base_catalogue, BaseCatalogue
+        ).entries
+        if start_date is not None:
+            base_entries = [e for e in base_entries if e.last_updated >= start_date]
+
+        most_downloads_per_source = {
+            s: max(e.download_count for e in i)
+            for s, i in bucketise(base_entries, key=lambda e: e.source).items()
+        }
+        same_as_from_github = {
+            (s.source, s.id): [
+                CatalogueSameAs(source=e.source, id=e.id),
+                *(i for i in e.same_as if i.source != s.source),
+            ]
+            for e in base_entries
+            if e.source == GithubResolver.metadata.id and e.same_as
+            for s in e.same_as
+        }
+        entries = [
+            CatalogueEntry(
+                **asdict(e, filter=lambda a, _: a.name != 'same_as'),
+                same_as=e.same_as
+                if e.source == GithubResolver.metadata.id
+                else (same_as_from_github.get((e.source, e.id)) or e.same_as),
+                normalised_name=normalise(e.name),
+                derived_download_score=0
+                if e.source == GithubResolver.metadata.id
+                else e.download_count / most_downloads_per_source[e.source],
+            )
+            for e in base_entries
+        ]
+        return cls(
+            entries=entries,
+            curse_slugs={e.slug: e.id for e in entries if e.source == CfCoreResolver.metadata.id},
+        )
+
+    @cached_property
+    def keyed_entries(self) -> dict[tuple[str, str], CatalogueEntry]:
+        return {(e.source, e.id): e for e in self.entries}
---
+++ packages/instawow/instawow-1.43.0.tar.gz/instawow-1.43.0/tests/test_config.py
@@ -0,0 +1,125 @@
+from __future__ import annotations
+
+from pathlib import Path
+import sys
+from typing import Any
+
+import pytest
+
+from instawow.common import Flavour, infer_flavour_from_path
+from instawow.config import Config, GlobalConfig
+
+
+def test_env_vars_have_prio(
+    monkeypatch: pytest.MonkeyPatch,
+    iw_global_config_values: dict[str, Any],
+    iw_config_values: dict[str, Any],
+):
+    monkeypatch.setenv('INSTAWOW_CONFIG_DIR', '/foo')
+    monkeypatch.setenv('INSTAWOW_GAME_FLAVOUR', 'classic')
+
+    config = Config.from_env(
+        global_config=GlobalConfig.from_env(**iw_global_config_values),
+        **iw_config_values,
+    )
+    assert config.global_config.config_dir == Path('/foo').resolve()
+    assert config.game_flavour is Flavour.burning_crusade_classic
+
+
+def test_config_dir_is_populated(
+    iw_global_config_values: dict[str, Any],
+    iw_config_values: dict[str, Any],
+):
+    global_config = GlobalConfig(**iw_global_config_values)
+    config = Config(global_config=global_config, **iw_config_values).write()
+    assert {i.name for i in config.profile_dir.iterdir()} == {'config.json', 'logs', 'plugins'}
+
+
+def test_read_profile_from_nonexistent_config_dir_raises(
+    iw_global_config_values: dict[str, Any],
+):
+    global_config = GlobalConfig(config_dir=iw_global_config_values['config_dir'])
+    with pytest.raises(FileNotFoundError):
+        Config.read(global_config, '__default__')
+
+
+def test_init_with_nonexistent_addon_dir_raises(
+    iw_global_config_values: dict[str, Any],
+    iw_config_values: dict[str, Any],
+):
+    global_config = GlobalConfig(**iw_global_config_values).write()
+    with pytest.raises(ValueError, match='not a writable directory'):
+        Config(global_config=global_config, **{**iw_config_values, 'addon_dir': '#@$foo'})
+
+
+@pytest.mark.skipif(
+    sys.platform == 'win32',
+    reason='requires absence of platform-specific env var',
+)
+def test_default_config_dir_is_platform_appropriate(
+    monkeypatch: pytest.MonkeyPatch,
+):
+    with monkeypatch.context() as patcher:
+        patcher.setattr(sys, 'platform', 'linux')
+        config_dir = GlobalConfig().config_dir
+        assert config_dir == Path.home() / '.config/instawow'
+
+        patcher.setenv('XDG_CONFIG_HOME', '/foo')
+        config_dir = GlobalConfig().config_dir
+        assert config_dir == Path('/foo/instawow')
+
+    with monkeypatch.context() as patcher:
+        patcher.setattr(sys, 'platform', 'darwin')
+        config_dir = GlobalConfig().config_dir
+        assert config_dir == Path.home() / 'Library/Application Support/instawow'
+
+
+@pytest.mark.skipif(
+    sys.platform != 'win32',
+    reason='requires presence of platform-specific env var',
+)
+def test_default_config_dir_is_unplatform_appropriate(
+    monkeypatch: pytest.MonkeyPatch,
+):
+    assert GlobalConfig().config_dir == Path.home() / 'AppData/Roaming/instawow'
+    monkeypatch.delenv('APPDATA')
+    assert GlobalConfig().config_dir == Path.home() / 'instawow'
+
+
+def test_can_infer_flavour_from_path():
+    # fmt: off
+    assert infer_flavour_from_path('wowzerz/_classic_/Interface/AddOns') is Flavour.burning_crusade_classic
+    assert infer_flavour_from_path('/foo/bar/_classic_beta_/Interface/AddOns') is Flavour.wrath_classic
+    assert infer_flavour_from_path('/foo/bar/_classic_ptr_/Interface/AddOns') is Flavour.wrath_classic
+    assert infer_flavour_from_path('_classic_era_/Interface/AddOns') is Flavour.vanilla_classic
+    assert infer_flavour_from_path('_classic_era_beta_/Interface/AddOns') is Flavour.vanilla_classic
+    assert infer_flavour_from_path('_classic_era_ptr_/Interface/AddOns') is Flavour.vanilla_classic
+    assert infer_flavour_from_path('wowzerz/_retail_/Interface/AddOns') is Flavour.retail
+    assert infer_flavour_from_path('anything goes') is Flavour.retail
+    # fmt: on
+
+
+def test_can_list_profiles(
+    monkeypatch: pytest.MonkeyPatch,
+    iw_global_config_values: dict[str, Any],
+    iw_config_values: dict[str, Any],
+):
+    monkeypatch.setenv('INSTAWOW_CONFIG_DIR', str(iw_global_config_values['config_dir']))
+
+    global_config = GlobalConfig.read()
+    assert global_config.list_profiles() == []
+
+    Config(global_config=global_config, **iw_config_values).write()
+    Config(global_config=global_config, **{**iw_config_values, 'profile': 'foo'}).write()
+    assert set(global_config.list_profiles()) == {'__default__', 'foo'}
+
+
+def test_can_delete_profile(
+    iw_global_config_values: dict[str, Any],
+    iw_config_values: dict[str, Any],
+):
+    global_config = GlobalConfig(**iw_global_config_values).write()
+    config = Config(global_config=global_config, **iw_config_values).write()
+    assert config.profile_dir.exists()
+    config.delete()
+    assert not config.profile_dir.exists()
