6530fe432468ad03adfb64000b3f860057dec0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:17 +0000

    Add package neoliqpay/3.0.0/neoliqpay-3.0.0.tar.gz

---
+++ packages/neoliqpay/neoliqpay-3.0.0.tar.gz/neoliqpay-3.0.0/neoliqpay/__init__.py
@@ -0,0 +1,11 @@
+from .async_liqpay import AsyncLiqPay
+from .sync_liqpay import LiqPay
+
+
+__title__ = 'asyncio LiqPay Python3 SDK'
+__version__ = '3.0.0'
+
+__all__ = [
+    'AsyncLiqPay',
+    'LiqPay',
+]
