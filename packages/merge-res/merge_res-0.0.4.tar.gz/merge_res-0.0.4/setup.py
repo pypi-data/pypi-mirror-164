6ad0e68c1f898fbdbd51baeded8fcb8d3ae2aa0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:41 +0000

    Add package merge-res/0.0.4/merge_res-0.0.4.tar.gz

---
+++ packages/merge-res/merge_res-0.0.4.tar.gz/merge_res-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+  name="merge_res",
+  version="0.0.4",
+  author="malinkang",
+  author_email="linkang.ma@gmail.com",
+  description="",
+  long_description="",
+  url="https://malinkang.com",
+  packages=find_packages(),
+    entry_points={
+        "console_scripts": ["merge_res = merge_res.cli:main"],
+    },
+)
