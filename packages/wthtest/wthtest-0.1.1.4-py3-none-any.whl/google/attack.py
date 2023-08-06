5c685dc976aa85e85aae4eced116be2b9265ea9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:36 +0000

    Add package wthtest/0.1.1.4/wthtest-0.1.1.4-py3-none-any.whl

---
+++ packages/wthtest/wthtest-0.1.1.4-py3-none-any.whl/google/attack.py
@@ -0,0 +1,6 @@
+# -*- coding = utf-8 -*-
+# @Time : 2022-08-16 18:38
+# @Author : 王铁翰
+# @File : attack.py
+# @Software : PyCharm
+print('be attacked')
---
+++ packages/wthtest/wthtest-0.1.1.4-py3-none-any.whl/tool.py
@@ -0,0 +1,5 @@
+import wthtest
+
+module = 'tool'
+
+print('wthtest module='+module)
