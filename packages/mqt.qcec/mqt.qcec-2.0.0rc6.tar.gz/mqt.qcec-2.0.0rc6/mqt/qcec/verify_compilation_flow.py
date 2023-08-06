7463d5dd90494a9cdba30995c8593a9605142db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:04 +0000

    Add package mqt.qcec/2.0.0rc6/mqt.qcec-2.0.0rc6.tar.gz

---
+++ packages/mqt.qcec/mqt.qcec-2.0.0rc6.tar.gz/mqt.qcec-2.0.0rc6/mqt/qcec/verify_compilation_flow.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING or sys.version_info < (3, 9, 0):
+    import importlib_resources as resources
+else:
+    from importlib import resources
+
+from mqt.qcec import Configuration, EquivalenceCheckingManager
+from mqt.qcec.compilation_flow_profiles import AncillaMode, generate_profile_name
+from qiskit import QuantumCircuit
+
+
+def verify_compilation(
+    original_circuit: QuantumCircuit | str,
+    compiled_circuit: QuantumCircuit | str,
+    optimization_level: int = 1,
+    ancilla_mode: AncillaMode = AncillaMode.NO_ANCILLA,
+    configuration: Configuration | None = None,
+) -> EquivalenceCheckingManager.Results:
+    """
+    Verify that the ``compiled_circuit`` (compiled with a certain ``optimization_level`` amd ``ancilla_mode``) is equivalent to the ``original_circuit``.
+    If ``configuration`` is not ``None``, it is used to configure the ``EquivalenceCheckingManager``.
+    """
+
+    if configuration is None:
+        configuration = Configuration()
+
+    # create the equivalence checker
+    ecm = EquivalenceCheckingManager(original_circuit, compiled_circuit, configuration)
+
+    # use the gate_cost scheme for the verification
+    ecm.set_application_scheme("gate_cost")
+
+    # get the pre-defined profile for the gate_cost scheme
+    profile_name = generate_profile_name(optimization_level=optimization_level, mode=ancilla_mode)
+    ref = resources.files("mqt.qcec") / "profiles" / profile_name
+    with resources.as_file(ref) as path:
+        ecm.set_gate_cost_profile(str(path))
+
+        # execute the check
+        ecm.run()
+
+    # obtain the result
+    return ecm.get_results()
