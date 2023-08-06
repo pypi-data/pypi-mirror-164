674d726976da17d6f3f432807377eb34b5d89535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:49 +0000

    Add package dstack/0.0.6/dstack-0.0.6-py3-none-any.whl

---
+++ packages/dstack/dstack-0.0.6-py3-none-any.whl/dstack/cli/schema.py
@@ -0,0 +1,27 @@
+workflows_schema_yaml = """type: object
+additionalProperties: false
+properties:
+  workflows:
+    type: array
+    items:
+      type: object
+      properties:
+        name:
+          type: string
+          minLength: 1
+        help:
+          type: string
+          minLength: 1
+        deps:
+          type: array
+          items:
+            type: string
+            minLength: 1
+          minItems: 1
+      additionalProperties: true
+      required:
+        - name
+        - provider
+required:
+  - workflows
+"""
