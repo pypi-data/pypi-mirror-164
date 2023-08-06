4bc8b2c9cec49bcabd10b12d1685cbf98307c951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:04 +0000

    Add package streamlit-extras/0.0.1/streamlit_extras-0.0.1.tar.gz

---
+++ packages/streamlit-extras/streamlit_extras-0.0.1.tar.gz/streamlit_extras-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="streamlit_extras",
+    packages=["streamlit_extras"],
+    version="0.0.1",
+    description="MVP is out!",
+    author="Arnaud Miribel",
+    author_email="arnaudmiribel@gmail.com",
+    url="https://github.com/arnaudmiribel/streamlit-extras",
+    download_url="https://github.com/arnaudmiribel/streamlit-extras/archive/refs/tags/v0.0.1.tar.gz",
+    keywords=["streamlit", "ui", "python", "data"],
+    classifiers=[],
+)
