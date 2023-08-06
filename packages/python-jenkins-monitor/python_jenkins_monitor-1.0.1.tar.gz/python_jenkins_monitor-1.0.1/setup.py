aa27a733a0b43bb8231751118e7d12d76418234a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:26 +0000

    Add package python-jenkins-monitor/1.0.1/python_jenkins_monitor-1.0.1.tar.gz

---
+++ packages/python-jenkins-monitor/python_jenkins_monitor-1.0.1.tar.gz/python_jenkins_monitor-1.0.1/setup.py
@@ -0,0 +1,21 @@
+#############################################
+# File Name: setup.py
+# Author: wqrf
+# Mail: 1074321997@qq.com
+# Created Time:  2022-8-24 21:06:00
+#############################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "python_jenkins_monitor",
+    version = "1.0.1",
+    keywords = ("monitor","jenkins","python","wqrf"),
+    description = "Simulate Jenkins scheduled task settings,for monitor.",
+    long_description = "If you need help, please email to 1074321997@qq.com",
+    license = "MIT",
+    url = "https://github.com/Woqurefan",
+    author = "wqrf",
+    author_email = "1074321997@qq.com",
+    packages = ['python_jenkins_monitor'],
+)
