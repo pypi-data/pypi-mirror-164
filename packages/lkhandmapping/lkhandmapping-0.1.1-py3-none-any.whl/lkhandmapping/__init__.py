690af269ba966746bed4dd9e014ea7855dacc8ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:45 +0000

    Add package lkhandmapping/0.1.1/lkhandmapping-0.1.1-py3-none-any.whl

---
+++ packages/lkhandmapping/lkhandmapping-0.1.1-py3-none-any.whl/lkhandmapping/__init__.py
@@ -0,0 +1,33 @@
+import cv2
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(model_complexity=0,min_detection_confidence=0.5,min_tracking_confidence=0.5,max_num_hands=1)
+
+def handTracker(image,draw=True):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            if draw:
+                mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS,
+                                          mp_drawing_styles.get_default_hand_landmarks_style(),
+                                          mp_drawing_styles.get_default_hand_connections_style())
+            handlms = []
+
+            c = 0
+            for i in hand_landmarks.landmark:
+                height, width, fc = image.shape
+                x = (i.x) * width
+                y = (i.y) * height
+                z = (i.z)
+                handlms.append([c, int(x), int(y), z])
+                c = c + 1
+            return [image, handlms]
+    else:
+        return [image,[0]]
+
