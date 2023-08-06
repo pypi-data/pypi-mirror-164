1a783f68fbfd0fc4dce9caade359bddf9f35c752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:54 +0000

    Add package regcensus/0.4.5/regcensus-0.4.5.tar.gz

---
+++ packages/regcensus/regcensus-0.4.5.tar.gz/regcensus-0.4.5/regcensus/__init__.py
@@ -0,0 +1,39 @@
+__all__ = [
+    'get_values',
+    'get_document_values',
+    'get_reading_time',
+    'get_series',
+    'get_periods',
+    'get_agencies',
+    'get_jurisdictions',
+    'get_industries',
+    'get_documents',
+    'get_versions',
+    'list_series',
+    'list_dates',
+    'list_document_types',
+    'list_agencies',
+    'list_clusters',
+    'list_jurisdictions',
+    'list_industries'
+]
+
+from . api import (
+    get_values,
+    get_document_values,
+    get_reading_time,
+    get_series,
+    get_periods,
+    get_agencies,
+    get_jurisdictions,
+    get_industries,
+    get_documents,
+    get_versions,
+    list_series,
+    list_dates,
+    list_document_types,
+    list_agencies,
+    list_clusters,
+    list_jurisdictions,
+    list_industries
+)
