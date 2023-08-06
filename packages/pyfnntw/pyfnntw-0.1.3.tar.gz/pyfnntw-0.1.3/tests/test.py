a72890920e65c89784b1fa330113ffb6a00606b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:34 +0000

    Add package pyfnntw/0.1.3/pyfnntw-0.1.3.tar.gz

---
+++ packages/pyfnntw/pyfnntw-0.1.3.tar.gz/pyfnntw-0.1.3/tests/test.py
@@ -0,0 +1,27 @@
+import pyfnntw;
+import numpy as np;
+from scipy.spatial import cKDTree as Tree
+from time import time
+
+ND = 10**5
+NQ = 10**6
+RUNS = 10
+TRIALS = 10
+WARMUP = 5
+
+# Get some data and queries
+data = np.random.uniform(size=(ND, 3))
+query = np.random.uniform(size=(NQ, 3))
+
+# Build and query the pynntw tree
+tree1 = pyfnntw.Tree(data, 32, 1)
+(_, ids1) = tree1.query(query)
+
+# Build and query the scipy tree
+tree2 = Tree(data, 32)
+(_, ids2) = tree2.query(query)
+
+if np.all(ids1 == ids2):
+    print("Success")
+else:
+    print("Failure")
