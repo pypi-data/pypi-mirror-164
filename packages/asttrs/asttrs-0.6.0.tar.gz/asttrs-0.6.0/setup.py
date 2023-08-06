3bd42f3189a5f2bcb3e700dad860514175f864ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:27 +0000

    Add package asttrs/0.6.0/asttrs-0.6.0.tar.gz

---
+++ packages/asttrs/asttrs-0.6.0.tar.gz/asttrs-0.6.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['asttrs']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['astor', 'attrs', 'black', 'cattrs', 'isort']
+
+setup_kwargs = {
+    'name': 'asttrs',
+    'version': '0.6.0',
+    'description': 'A attrs-style wrapper for python ast',
+    'long_description': '# asttrs\nA attrs-style wrapper for python ast\n',
+    'author': 'ryanchao2012',
+    'author_email': 'ryanchao2012@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/ryanchao2012/asttrs',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
