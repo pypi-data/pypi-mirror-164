e2185bc3932bd3d8ff8d380d263748b408c81d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:55 +0000

    Add package linuxforhealth-packaging/1.1b1/linuxforhealth_packaging-1.1b1-py3-none-any.whl

---
+++ packages/linuxforhealth-packaging/linuxforhealth_packaging-1.1b1-py3-none-any.whl/linuxforhealth/packaging/calculator.py
@@ -0,0 +1,15 @@
+"""
+calculator.py
+
+Provides basic arthimetic operations.
+"""
+
+
+def add(a, b):
+    """Adds two numbers, returning the sum"""
+    return a + b
+
+
+def subtract(a, b):
+    """Subtracts two numbers returning the difference."""
+    return a - b
