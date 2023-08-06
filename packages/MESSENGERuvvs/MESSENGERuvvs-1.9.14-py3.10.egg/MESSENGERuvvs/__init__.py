accb5785254ae3ac21381e74a3943a8e1e9ee5f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:37 +0000

    Add package MESSENGERuvvs/1.9.14/MESSENGERuvvs-1.9.14-py3.10.egg

---
+++ packages/MESSENGERuvvs/MESSENGERuvvs-1.9.14-py3.10.egg/MESSENGERuvvs/__init__.py
@@ -0,0 +1,9 @@
+from MESSENGERuvvs.MESSENGERdata import MESSENGERdata
+from .initialize_MESSENGERdata import initialize_MESSENGERdata
+
+
+name = 'MESSENGERuvvs'
+__author__ = 'Matthew Burger'
+__email__ = 'mburger@stsci.edu'
+__version__ = '1.9.14'
+__date__ = '2022-08-25'
