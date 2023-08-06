cd56e1683e1bcc6d6777adf04144eb7a75334bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:02 +0000

    Add package airflow-util-dv/2.7.2/airflow_util_dv-2.7.2.tar.gz

---
+++ packages/airflow-util-dv/airflow_util_dv-2.7.2.tar.gz/airflow_util_dv-2.7.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding:UTF-8 -*-
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="airflow_util_dv",
+    version="2.7.2",
+    author="boxue liu",
+    author_email="liu.boxue@detvista.com",
+    license="Apache License",
+    url="https://github.com/boxueliu/airflow_util",
+    packages=["airflow_util_dv"],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Indexing",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6"
+    ],
+)
