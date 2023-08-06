5fe5c069c46c676fd83439bd7160affed19be434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:41 +0000

    Add package sqlglot/5.0.0/sqlglot-5.0.0.tar.gz

---
+++ packages/sqlglot/sqlglot-5.0.0.tar.gz/sqlglot-5.0.0/sqlglot/dialects/mysql.py
@@ -0,0 +1,40 @@
+from sqlglot import exp
+from sqlglot.dialects.dialect import (
+    Dialect,
+    no_ilike_sql,
+    no_paren_current_date_sql,
+    no_tablesample_sql,
+    no_trycast_sql,
+)
+from sqlglot.generator import Generator
+from sqlglot.parser import Parser
+
+
+class MySQL(Dialect):
+    identifiers = ["`"]
+
+    # https://prestodb.io/docs/current/functions/datetime.html#mysql-date-functions
+    time_mapping = {
+        "%M": "%B",
+        "%c": "%-m",
+        "%e": "%-d",
+        "%h": "%I",
+        "%i": "%M",
+        "%s": "%S",
+        "%S": "%S",
+    }
+
+    class Parser(Parser):
+        STRICT_CAST = False
+
+    class Generator(Generator):
+        NULL_ORDERING_SUPPORTED = False
+
+        TRANSFORMS = {
+            **Generator.TRANSFORMS,
+            exp.CurrentDate: no_paren_current_date_sql,
+            exp.CurrentTimestamp: lambda *_: "CURRENT_TIMESTAMP",
+            exp.ILike: no_ilike_sql,
+            exp.TableSample: no_tablesample_sql,
+            exp.TryCast: no_trycast_sql,
+        }
