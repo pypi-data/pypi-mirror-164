4f00d658186d3b4d641ec01190dab0a81cb8bf3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:57 +0000

    Add package edc-adverse-event/0.3.31/edc_adverse_event-0.3.31-py3-none-any.whl

---
+++ packages/edc-adverse-event/edc_adverse_event-0.3.31-py3-none-any.whl/edc_adverse_event/auth_objects/__init__.py
@@ -0,0 +1,18 @@
+from .ae import (
+    AE,
+    AE_REVIEW,
+    AE_ROLE,
+    AE_SUPER,
+    ae_codenames,
+    ae_dashboard_tuples,
+    ae_navbar_tuples,
+)
+from .tmg import (
+    TMG,
+    TMG_REVIEW,
+    TMG_ROLE,
+    tmg_codenames,
+    tmg_dashboard_tuples,
+    tmg_navbar_tuples,
+    tmg_view_codenames,
+)
