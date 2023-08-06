db70b3b4f96a4b9d3cf58ea64e103af2a79b2b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:53 +0000

    Add package oelint-parser/2.6.0/oelint_parser-2.6.0.tar.gz

---
+++ packages/oelint-parser/oelint_parser-2.6.0.tar.gz/oelint_parser-2.6.0/setup.py
@@ -0,0 +1,50 @@
+import setuptools
+
+import subprocess
+_long_description = "See https://github.com/priv-kweihmann/oelint-parser for documentation"
+_long_description_content_type = "text/plain"
+try:
+    _long_description = subprocess.check_output(
+        ["pandoc", "--from", "markdown", "--to", "markdown", "README.md"]).decode("utf-8")
+    _long_description_content_type = "text/markdown"
+except (subprocess.CalledProcessError, FileNotFoundError):
+    pass
+
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="oelint_parser",
+    version="2.6.0",
+    author="Konrad Weihmann",
+    author_email="kweihmann@outlook.com",
+    description="Alternative parser for bitbake recipes",
+    long_description=_long_description,
+    long_description_content_type=_long_description_content_type,
+    url="https://github.com/priv-kweihmann/oelint-parser",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    extras_require={
+        'dev': [
+            'pydoc-markdown',
+            'pytest'
+        ]
+    },
+    package_data = {
+        'oelint_parser': ['data/*'],
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3",
+    ],
+)
