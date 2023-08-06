c50ca0945f7f9dd0d6d2fb1ec33dea34b1df9ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:18 +0000

    Add package automation_rest_server/3.2.0/automation_rest_server-3.2.0.tar.gz

---
+++ packages/automation_rest_server/automation_rest_server-3.2.0.tar.gz/automation_rest_server-3.2.0/automation_rest_server/rest_client/resource/test_resource.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+# pylint: disable=import-error,eval-used
+import json
+import time
+from utils import log
+from rest_client.resource.models.helper import rest_get_call, rest_post_json_call
+from test_framework.test_result import TestResult
+from test_framework.state import TestType, State
+
+
+class TestResource(object):
+
+    def __init__(self, host, port, session, time_out):
+        self.host = host
+        self.port = port
+        self.session = session
+        self.time_out = time_out
+        self.test_result = TestResult()
+
+    def update_test_result(self, test_key, status, result, test_type):
+        url_ = "http://{0}:{1}/api/result/{2}/{3}".format(self.host, self.port, TestType.url_map[test_type], test_key)
+        data = {"msg": self.get_test_log(result), "state": status, "data": result}
+        while True:
+            ret = rest_post_json_call(url_, self.session, json.dumps(data), self.time_out)
+            if ret["state"] == State.PASS:
+                break
+            else:
+                time.sleep(5)
+                print(ret)
+                log.INFO("Send result failed, try again")
+        return ret
+
+    def get_test_log(self, result):
+        logs = self.test_result.get_test_suite_test_msg(result)
+        return logs
