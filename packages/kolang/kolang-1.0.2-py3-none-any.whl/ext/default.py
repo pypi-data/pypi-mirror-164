13369ddcd39ab2549be5f9daba7b9b7d25099a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:17 +0000

    Add package kolang/1.0.2/kolang-1.0.2-py3-none-any.whl

---
+++ packages/kolang/kolang-1.0.2-py3-none-any.whl/ext/default.py
@@ -0,0 +1,6 @@
+import os
+def save(var):
+    return var
+
+def cls():
+    os.system('cls' if os.name in ('nt', 'dos') else 'clear')
---
+++ packages/kolang/kolang-1.0.2-py3-none-any.whl/ext/time.py
@@ -0,0 +1,4 @@
+import time
+
+def sleep(sec):
+    time.sleep(sec)
