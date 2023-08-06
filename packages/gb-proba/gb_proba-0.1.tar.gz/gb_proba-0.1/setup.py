e10774c1139d25dd74f0fd8f922f18540e91d8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:14 +0000

    Add package gb-proba/0.1/gb_proba-0.1.tar.gz

---
+++ packages/gb-proba/gb_proba-0.1.tar.gz/gb_proba-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gb_proba',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gb_proba'],
+      author = 'Ben Chan',
+      zip_safe=False)
