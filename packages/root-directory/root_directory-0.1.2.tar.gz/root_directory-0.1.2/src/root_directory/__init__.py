61acf38a09544ef9bece7ae981b2a184f3284066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:33 +0000

    Add package root-directory/0.1.2/root_directory-0.1.2.tar.gz

---
+++ packages/root-directory/root_directory-0.1.2.tar.gz/root_directory-0.1.2/src/root_directory/__init__.py
@@ -0,0 +1,31 @@
+
+import sys, os
+from pathlib import Path
+import __main__
+
+__all__ = ["get_root"]
+
+def get_root():
+    try:
+        file = Path(__main__.__file__)
+    except AttributeError:
+        return Path(os.getcwd())
+
+    for i in range(len(file.parts) - 1):
+        directory = file.parents[i]
+        if (directory/".git").is_dir() or (directory/".project-root").is_file():
+            return directory
+
+    assert False, "Project root not found. Please add an empty .project-root file to the root or turn it into a git repository."
+
+def _append_to_path(pth):
+    pth = str(pth)
+    if pth not in sys.path:
+        sys.path.insert(1, pth)
+
+_append_to_path(get_root())
+
+if __name__ == "__main__":
+    from src.root_directory import __init__ # Test if importing works
+    print("Project root: " + str(get_root()))
+    print("Test successful!")
