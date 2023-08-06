0b0ecd81a3200c37ed8f98f8826a2460849e8fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:36 +0000

    Add package techyshiv-evenodd/1.0.0/techyshiv-evenodd-1.0.0.tar.gz

---
+++ packages/techyshiv-evenodd/techyshiv-evenodd-1.0.0.tar.gz/techyshiv-evenodd-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="techyshiv-evenodd",
+    version="1.0.0",
+    description="Check Even and Odd for any given numbers or list and return True , False & list based on data type provided",
+    author="Shivang Saxena",
+    packages=["techyshiv_evenodd"],
+    zip_safe=False
+)
