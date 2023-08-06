8da6b0b9081ec78b104b6ab6f938e3e033459ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:59 +0000

    Add package MindsDB/22.8.4.0/MindsDB-22.8.4.0.tar.gz

---
+++ packages/MindsDB/MindsDB-22.8.4.0.tar.gz/MindsDB-22.8.4.0/mindsdb/interfaces/model/model_controller.py
@@ -0,0 +1,182 @@
+import os
+import sys
+import json
+import base64
+from copy import deepcopy
+from dateutil.parser import parse as parse_datetime
+
+import numpy as np
+
+import mindsdb.interfaces.storage.db as db
+from mindsdb.interfaces.storage.fs import FsStore
+from mindsdb.interfaces.database.integrations import IntegrationController
+from mindsdb.utilities.log import log
+from mindsdb.utilities.config import Config
+from mindsdb.utilities.json_encoder import json_serialiser
+from mindsdb.utilities.with_kwargs_wrapper import WithKWArgsWrapper
+from mindsdb.api.mysql.mysql_proxy.libs.constants.response_type import RESPONSE_TYPE
+
+IS_PY36 = sys.version_info[1] <= 6
+
+
+class ModelController():
+    config: Config
+    fs_store: FsStore
+
+    def __init__(self) -> None:
+        self.config = Config()
+        self.fs_store = FsStore()
+
+    def get_model_data(self, name, company_id: int):
+        predictor_record = db.session.query(db.Predictor).filter_by(company_id=company_id, name=name).first()
+        if predictor_record is None:
+            raise Exception(f"Model does not exists: {name}")
+
+        data = deepcopy(predictor_record.data)
+        data['dtype_dict'] = predictor_record.dtype_dict
+        data['created_at'] = str(parse_datetime(str(predictor_record.created_at).split('.')[0]))
+        data['updated_at'] = str(parse_datetime(str(predictor_record.updated_at).split('.')[0]))
+        data['predict'] = predictor_record.to_predict[0]
+        data['update'] = predictor_record.update_status
+        data['mindsdb_version'] = predictor_record.mindsdb_version
+        data['name'] = predictor_record.name
+        data['code'] = predictor_record.code
+        data['json_ai'] = predictor_record.json_ai
+        data['problem_definition'] = predictor_record.learn_args
+
+        # assume older models are complete, only temporary
+        if 'status' in predictor_record.data:
+            data['status'] = predictor_record.data['status']
+        elif 'error' in predictor_record.data:
+            data['status'] = 'error'
+        elif predictor_record.update_status == 'available':
+            data['status'] = 'complete'
+        elif predictor_record.json_ai is None and predictor_record.code is None:
+            data['status'] = 'generating'
+        elif predictor_record.data is None:
+            data['status'] = 'editable'
+        elif 'training_log' in predictor_record.data:
+            data['status'] = 'training'
+        elif 'error' not in predictor_record.data:
+            data['status'] = 'complete'
+        else:
+            data['status'] = 'error'
+
+        if data.get('accuracies', None) is not None:
+            if len(data['accuracies']) > 0:
+                data['accuracy'] = float(np.mean(list(data['accuracies'].values())))
+        return data
+
+    def get_model_description(self, name: str, company_id: int):
+        """
+        Similar to `get_model_data` but meant to be seen directly by the user, rather than parsed by something like the Studio predictor view.
+
+        Uses `get_model_data` to compose this, but in the future we might want to make this independent if we deprecated `get_model_data`
+
+        :returns: Dictionary of the analysis (meant to be foramtted by the APIs and displayed as json/yml/whatever)
+        """ # noqa
+        model_description = {}
+        model_data = self.get_model_data(name, company_id)
+
+        model_description['accuracies'] = model_data['accuracies']
+        model_description['column_importances'] = model_data['column_importances']
+        model_description['outputs'] = [model_data['predict']]
+        model_description['inputs'] = [col for col in model_data['dtype_dict'] if col not in model_description['outputs']]
+        model_description['datasource'] = model_data.get('data_source_name')
+        model_description['model'] = ' --> '.join(str(k) for k in model_data['json_ai'])
+
+        return model_description
+
+    def get_models(self, company_id: int):
+        models = []
+        for predictor_record in db.session.query(db.Predictor).filter_by(company_id=company_id):
+            model_data = self.get_model_data(predictor_record.name, company_id=company_id)
+            reduced_model_data = {}
+
+            for k in ['name', 'version', 'is_active', 'predict', 'status',
+                      'current_phase', 'accuracy', 'data_source', 'update',
+                      'data_source_name', 'mindsdb_version', 'error']:
+                reduced_model_data[k] = model_data.get(k, None)
+
+            for k in ['train_end_at', 'updated_at', 'created_at']:
+                reduced_model_data[k] = model_data.get(k, None)
+                if reduced_model_data[k] is not None:
+                    try:
+                        reduced_model_data[k] = parse_datetime(str(reduced_model_data[k]).split('.')[0])
+                    except Exception as e:
+                        # @TODO Does this ever happen
+                        log.error(f'Date parsing exception while parsing: {k} in get_models: ', e)
+                        reduced_model_data[k] = parse_datetime(str(reduced_model_data[k]))
+
+            models.append(reduced_model_data)
+        return models
+
+    def delete_model(self, model_name: str, company_id: int, integration_name: str = 'lightwood'):
+        integration_controller = WithKWArgsWrapper(IntegrationController(), company_id=company_id)
+        lw_handler = integration_controller.get_handler(integration_name)
+        response = lw_handler.native_query(f'drop predictor {model_name}')
+        if response.type == RESPONSE_TYPE.ERROR:
+            raise Exception(response.error_message)
+
+    def rename_model(self, old_name, new_name, company_id: int):
+        db_p = db.session.query(db.Predictor).filter_by(company_id=company_id, name=old_name).first()
+        db_p.name = new_name
+        db.session.commit()
+
+    def export_predictor(self, name: str, company_id: int) -> json:
+        predictor_record = db.session.query(db.Predictor).filter_by(company_id=company_id, name=name).first()
+        assert predictor_record is not None
+
+        fs_name = f'predictor_{company_id}_{predictor_record.id}'
+        self.fs_store.get(fs_name, base_dir=self.config['paths']['predictors'])
+        local_predictor_savefile = os.path.join(self.config['paths']['predictors'], fs_name)
+        predictor_binary = open(local_predictor_savefile, 'rb').read()
+
+        # Serialize a predictor record into a dictionary 
+        # move into the Predictor db class itself if we use it again somewhere
+        predictor_record_serialized = {
+            'name': predictor_record.name,
+            'data': predictor_record.data,
+            'to_predict': predictor_record.to_predict,
+            'mindsdb_version': predictor_record.mindsdb_version,
+            'native_version': predictor_record.native_version,
+            'is_custom': predictor_record.is_custom,
+            'learn_args': predictor_record.learn_args,
+            'update_status': predictor_record.update_status,
+            'json_ai': predictor_record.json_ai,
+            'code': predictor_record.code,
+            'lightwood_version': predictor_record.lightwood_version,
+            'dtype_dict': predictor_record.dtype_dict,
+            'predictor_binary': predictor_binary
+        }
+
+        return json.dumps(predictor_record_serialized, default=json_serialiser)
+
+    def import_predictor(self, name: str, payload: json, company_id: int) -> None:
+        prs = json.loads(payload)
+
+        predictor_record = db.Predictor(
+            name=name,
+            data=prs['data'],
+            to_predict=prs['to_predict'],
+            company_id=company_id,
+            mindsdb_version=prs['mindsdb_version'],
+            native_version=prs['native_version'],
+            is_custom=prs['is_custom'],
+            learn_args=prs['learn_args'],
+            update_status=prs['update_status'],
+            json_ai=prs['json_ai'],
+            code=prs['code'],
+            lightwood_version=prs['lightwood_version'],
+            dtype_dict=prs['dtype_dict']
+        )
+
+        db.session.add(predictor_record)
+        db.session.commit()
+
+        predictor_binary = base64.b64decode(prs['predictor_binary'])
+        fs_name = f'predictor_{company_id}_{predictor_record.id}'
+        with open(os.path.join(self.config['paths']['predictors'], fs_name), 'wb') as fp:
+            fp.write(predictor_binary)
+
+        self.fs_store.put(fs_name, base_dir=self.config['paths']['predictors'])
