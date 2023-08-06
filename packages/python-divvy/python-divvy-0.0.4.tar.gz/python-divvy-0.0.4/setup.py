40bfe5f9408c82d983a02a8a36353fb78e5593a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:52 +0000

    Add package python-divvy/0.0.4/python-divvy-0.0.4.tar.gz

---
+++ packages/python-divvy/python-divvy-0.0.4.tar.gz/python-divvy-0.0.4/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+from pathlib import Path
+
+HERE = Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="python-divvy",
+    version="0.0.4",
+    author="William Dean",
+    author_email="wd60622@gmail.com",
+    url="https://github.com/wd60622/divvy",
+    description="Python Client for Chicago Ridesharing Data.",
+    license="MIT",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    packages=find_packages(),
+    install_requires=["requests", "pandas"],
+    extras_require={"test": ["pytest", "pytest-cov"]},
+)
