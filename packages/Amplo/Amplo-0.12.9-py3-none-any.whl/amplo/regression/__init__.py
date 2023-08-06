3ca9059e091947e0415bcece206f950f9a4a14fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:23 +0000

    Add package Amplo/0.12.9/Amplo-0.12.9-py3-none-any.whl

---
+++ packages/Amplo/Amplo-0.12.9-py3-none-any.whl/amplo/regression/__init__.py
@@ -0,0 +1,7 @@
+#  Copyright (c) 2022 by Amplo.
+
+from amplo.regression.catboost import CatBoostRegressor
+from amplo.regression.lgbm import LGBMRegressor
+from amplo.regression.partial_boosting import PartialBoostingRegressor
+from amplo.regression.stacking import StackingRegressor
+from amplo.regression.xgb import XGBRegressor
