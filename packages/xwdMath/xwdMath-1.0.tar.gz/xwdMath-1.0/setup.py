d7ca0d9176479e7fbd40f0ee29af2492de11e6fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:50 +0000

    Add package xwdMath/1.0/xwdMath-1.0.tar.gz

---
+++ packages/xwdMath/xwdMath-1.0.tar.gz/xwdMath-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='xwdMath',
+    version='1.0',
+    description='对外发布的模块',
+    author='xwd',
+    author_email='993@qq.com',
+    py_modules=['xwdMath.demo1','xwdMath.demo2']
+)
