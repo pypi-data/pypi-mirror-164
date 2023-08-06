f7d34eb586559582209c10a4156f9598380fb3c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:39 +0000

    Add package ptextpad/0.1.0a4/ptextpad-0.1.0a4-py3-none-any.whl

---
+++ packages/ptextpad/ptextpad-0.1.0a4-py3-none-any.whl/ptextpad/detect_lang.py
@@ -0,0 +1,42 @@
+"""Detect language using fastlid.
+
+Detect language using longid.classify.
+detct as Chinese if chinese_char_ratio>= threthold else dectect_language()
+"""
+# import logging
+from typing import Optional
+
+from fastlid import fastlid
+from logzero import logger
+
+
+def detect_lang(text1: str, checklen: Optional[int] = None) -> str:
+    """Detect Chinese and other languages.
+
+    Args:
+        text1: to detect which lang
+        checklen: number of chars to process, default 3000
+    """
+    if checklen is None:
+        checklen = 3000
+    else:
+        try:
+            checklen = int(checklen)
+        except Exception:
+            checklen = 3000
+    if checklen <= 1:
+        checklen = 3000
+
+    text1 = str(text1)
+
+    # langid.set_languages(langs)
+    text0 = text1[:checklen]
+
+    detected = "en"
+    try:
+        # detected = langid.classify(text0)[0]
+        detected = fastlid(text0)[0]
+    except Exception as exc:
+        logger.warning(" fastlid failed: %s, set to en", exc)
+
+    return detected
