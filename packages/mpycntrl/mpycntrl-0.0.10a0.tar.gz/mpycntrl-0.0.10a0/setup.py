a9f05175698c67907ab9e4fac818b8b9e7aa03ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:16 +0000

    Add package mpycntrl/0.0.10a0/mpycntrl-0.0.10a0.tar.gz

---
+++ packages/mpycntrl/mpycntrl-0.0.10a0.tar.gz/mpycntrl-0.0.10a0/setup.py
@@ -0,0 +1,66 @@
+import setuptools
+import os
+import re
+
+with open("README.MD", "r") as fh:
+    long_description = fh.read()
+
+
+def find_version(fnam, version="VERSION"):
+    with open(fnam) as f:
+        cont = f.read()
+    regex = f'{version}\s*=\s*["]([^"]+)["]'
+    match = re.search(regex, cont)
+    if match is None:
+        raise Exception(
+            f"version with spec={version} not found, use double quotes for version string"
+        )
+    return match.group(1)
+
+
+def find_projectname():
+    cwd = os.getcwd()
+    name = os.path.basename(cwd)
+    return name
+
+
+projectname = find_projectname()
+file = os.path.join(projectname, "mpycntrl.py")
+version = find_version(file)
+
+setuptools.setup(
+    name=projectname,
+    version=version,
+    author="k. goger",
+    author_email=f"k.r.goger+{projectname}@gmail.com",
+    description="Control MicroPython with your own code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/kr-g/{projectname}",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    keywords="micropython utility shell",
+    install_requires=["pyserial", "pttydev"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Embedded Systems",
+        "Topic :: Terminals :: Serial",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.8",
+)
+
+# python3 -m setup sdist build bdist_wheel
+
+# test.pypi
+# twine upload --repository testpypi dist/*
+# python3 -m pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/ **proj**
+
+# pypi
+# twine upload dist/*
+
