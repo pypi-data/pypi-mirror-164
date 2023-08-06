4b5429eb99168fb051017f4ab4fe498602d3793a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:33 +0000

    Add package pygow-poker/0.0.1/pygow-poker-0.0.1.tar.gz

---
+++ packages/pygow-poker/pygow-poker-0.0.1.tar.gz/pygow-poker-0.0.1/README.md
@@ -0,0 +1,38 @@
+# pygow-poker
+
+Simulate or play your favorite pai gow poker variant with python!
+
+## Installation
+
+Run the following to install
+
+```
+pip install pygow-poker
+```
+
+On init (Windows)
+
+```
+python -m virtualenv venv
+
+source venv/Scripts/activate
+
+pip install -r dev-requirements.txt
+```
+
+On init (Mac)
+
+```
+virtualenv venv
+
+source venv/bin/activate
+
+pip install -r dev-requirements.txt
+```
+
+Build
+
+```
+pip install --upgrade build
+python -m build
+```
---
+++ packages/pygow-poker/pygow-poker-0.0.1.tar.gz/pygow-poker-0.0.1/src/pygow-poker/betting_strat.py
@@ -0,0 +1,44 @@
+def betting_strat(player_bank:int, last_rank:str, last_outcome:str, last_bet:dict, win_streak:int, loss_streak:int):
+	ante = 15 ## ante unit
+
+	## Modified Martingale System
+	ladder_top = 20
+	second_bonus = 15
+	if last_outcome == 'Player Wins':
+		ante = ante
+	elif last_outcome == 'Dealer Wins' and last_bet:
+		if last_bet['ante'] == ante: ## second ladder step
+			ante = ante + second_bonus
+		else:
+			if last_bet['ante'] * 2 < (ante + second_bonus) * 2**(ladder_top-2):
+				ante = last_bet['ante'] * 2
+			else:
+				ante = ante
+	elif last_bet:
+		ante = last_bet['ante']
+
+	# ## 1-3-2-6 System
+	# if last_outcome == 'Player Wins':
+	# 	if win_streak%4 == 0:
+	# 		ante = ante
+	# 	elif win_streak%4 == 1:
+	# 		ante = ante * 3
+	# 	elif win_streak%4 == 2:
+	# 		ante = ante * 2
+	# 	elif win_streak%4 == 3:
+	# 		ante = ante * 6
+
+	ace_high = 0
+	fortune = 0
+	progressive = 0 ## max of 1
+	envy = 0 ## don't have multiple seat games setup yet
+
+	bet = {
+		'ante': ante,
+		'ace_high': ace_high,
+		'fortune': fortune,
+		'progressive': progressive,
+		'envy': envy
+	}
+
+	return bet
