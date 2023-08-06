0b4aa0ea75dfb12f66a49fd35860d1b67165592b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:40 +0000

    Add package hero-test/0.0.6/hero-test-0.0.6.tar.gz

---
+++ packages/hero-test/hero-test-0.0.6.tar.gz/hero-test-0.0.6/src/hero/hero.py
@@ -0,0 +1,18 @@
+from jsonschema import validate
+import json
+import pkgutil
+
+
+def test_validate(instance):
+    resources = pkgutil.get_data('src.hero.inner', 'test_schema.json')
+    schema = json.loads(resources)
+    validate(instance, schema)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    instance = {
+        "id": "sfad"
+    }
+    test_validate(instance)
+
