d65e8dd51dc1422b2990c265b5920f923c8b88c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:15 +0000

    Add package distributions-caluclator/1.0/distributions_caluclator-1.0.tar.gz

---
+++ packages/distributions-caluclator/distributions_caluclator-1.0.tar.gz/distributions_caluclator-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_caluclator',
+      version='1.0',
+      description='Gaussian & Binomial distributions',
+      packages=['distributions_caluclator'],
+      author='Farouk EL',
+      author_email='farouk.sallak@outlook.com',
+      zip_safe=False)
