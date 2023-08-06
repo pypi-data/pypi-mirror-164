d84fe71c201735cf4bfb4070f02bd114b99117a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:32 +0000

    Add package suhteita/2022.8.24/suhteita-2022.8.24-py2.py3-none-any.whl

---
+++ packages/suhteita/suhteita-2022.8.24-py2.py3-none-any.whl/suhteita/cli.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long
+"""CLI operations for relationships (Finnish: suhteita) maintained across distances as load test core."""
+import sys
+from typing import List, Union
+
+import suhteita.suhteita as api
+from suhteita import __version__ as version
+
+
+# pylint: disable=expression-not-assigned
+def main(argv: Union[List[str], None] = None) -> int:
+    """Delegate processing to functional module."""
+    argv = sys.argv[1:] if argv is None else argv
+    return api.main(argv, version=version)
