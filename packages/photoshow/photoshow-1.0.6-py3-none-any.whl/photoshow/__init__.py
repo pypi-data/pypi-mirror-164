af3178c3ddf063b11b465ec559c59c527e42527f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:19 +0000

    Add package photoshow/1.0.6/photoshow-1.0.6-py3-none-any.whl

---
+++ packages/photoshow/photoshow-1.0.6-py3-none-any.whl/photoshow/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -----------------------------------------------
+"""
+    DESCRIPTION:
+        Slideshow for photos
+
+    ASSUMPTIONS:
+        No assumptions to note
+
+    ACCURACY:
+        No accuracy issues to note
+"""
+# -----------------------------------------------
+
+from ._photoshow import present
+
+__version__ = '1.0.6'
+
+# -----------------------------------------------
+# End.
