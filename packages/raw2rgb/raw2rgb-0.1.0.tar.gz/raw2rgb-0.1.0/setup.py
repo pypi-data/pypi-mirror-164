7bb92dae3f8aaba744d3d9ce8d55b8f6ba9b923c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:01 +0000

    Add package raw2rgb/0.1.0/raw2rgb-0.1.0.tar.gz

---
+++ packages/raw2rgb/raw2rgb-0.1.0.tar.gz/raw2rgb-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name='raw2rgb',
+    version='0.1.0',
+    description='A python tool to convert .tiff image file to .png file',
+    author='Haochen Yu',
+    zip_safe=False
+)
