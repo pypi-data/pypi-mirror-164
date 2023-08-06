78143304749e0ccc4f0c1b07e015cd966dd00f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:18 +0000

    Add package mne-icalabel/0.4/mne-icalabel-0.4.tar.gz

---
+++ packages/mne-icalabel/mne-icalabel-0.4.tar.gz/mne-icalabel-0.4/mne_icalabel/__init__.py
@@ -0,0 +1,7 @@
+"""Automatic ICA labeling for MEG, EEG and iEEG data."""
+
+from . import annotation  # noqa: F401
+from . import datasets  # noqa: F401
+from . import features  # noqa: F401
+from ._version import __version__  # noqa: F401
+from .label_components import label_components  # noqa: F401
