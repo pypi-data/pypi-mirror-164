648fecf6da81abea23814846d45eaec4cc34df06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:22 +0000

    Add package pgZhSearch/0.0.4/pgZhSearch-0.0.4-py3-none-any.whl

---
+++ packages/pgZhSearch/pgZhSearch-0.0.4-py3-none-any.whl/pgZhSearch/__init__.py
@@ -0,0 +1,17 @@
+try:
+    from .base_search import *
+except Exception as e:
+    print(e)
+
+
+def version():
+    """
+    # 更新说明
+
+    - 初始化       # 0.0.1
+    - ReadMe出现bug       # 0.0.2
+    - 拉取更新. search方法从此支持queryset    # 0.0.3
+    - 修复pypi没有上传txt文件的问题    # 0.0.4
+    """
+    return '0.0.4'
+
