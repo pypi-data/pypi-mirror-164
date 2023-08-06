6bf2ea2dbfb28dd1e0e4b6dcf78d6e08dc704a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:07 +0000

    Add package MambuPy/2.0.0b10/MambuPy-2.0.0b10.tar.gz

---
+++ packages/MambuPy/MambuPy-2.0.0b10.tar.gz/MambuPy-2.0.0b10/MambuPy/api/mambuloan.py
@@ -0,0 +1,119 @@
+"""MambuLoan entity: a MambuEntity struct for credit Loans.
+
+.. autosummary::
+   :nosignatures:
+   :toctree: _autosummary
+"""
+
+import copy
+import json
+
+from .entities import (MambuEntity, MambuEntityWritable,
+                       MambuEntityAttachable,
+                       MambuEntitySearchable,
+                       MambuEntityCommentable,
+                       MambuEntityOwnable,
+                       MambuInstallment)
+from MambuPy.mambuutil import MambuPyError
+
+
+class MambuLoan(
+    MambuEntity,
+    MambuEntityWritable,
+    MambuEntityAttachable,
+    MambuEntityCommentable,
+    MambuEntityOwnable,
+    MambuEntitySearchable
+):
+    """MambuLoan entity"""
+
+    _prefix = "loans"
+    """prefix constant for connections to Mambu"""
+
+    _filter_keys = [
+        "branchId",
+        "centreId",
+        "accountState",
+        "accountHolderType",
+        "accountHolderId",
+        "creditOfficerUsername",
+    ]
+    """allowed filters for get_all filtering"""
+
+    _sortBy_fields = [
+        "creationDate",
+        "lastModifiedDate",
+        "id",
+        "loanName",
+    ]
+    """allowed fields for get_all sorting"""
+
+    _ownerType = "LOAN_ACCOUNT"
+    """owner type of this entity"""
+
+    _vos = [("disbursementDetails", "MambuDisbursementDetails")]
+    """2-tuples of elements and Value Objects"""
+
+    _entities = [
+        ("assignedUserKey", "mambuuser.MambuUser", "assignedUser"),
+        ("assignedBranchKey", "mambubranch.MambuBranch", "assignedBranch"),
+        ("assignedCentreKey", "mambucentre.MambuCentre", "assignedCentre"),
+        ("productTypeKey", "mambuproduct.MambuProduct", "productType"),
+        ("originalAccountKey", "mambuloan.MambuLoan", "originalAccount"),
+        ("accountHolderKey", "", "accountHolder")]
+    """3-tuples of elements and Mambu Entities"""
+
+    _accepted_actions = [
+        "REQUEST_APPROVAL",
+        "SET_INCOMPLETE",
+        "APPROVE",
+        "UNDO_APPROVE",
+        "REJECT",
+        "WITHDRAW",
+        "CLOSE",
+        "UNDO_REJECT",
+        "UNDO_WITHDRAW",
+        "UNDO_CLOSE",
+    ]
+    """accepted actions to change"""
+
+    def __init__(self, **kwargs):
+        self._entities = copy.deepcopy(MambuLoan._entities)
+        super().__init__(**kwargs)
+        self._attachments = {}
+
+    def get_schedule(self):
+        """Retrieves the installments schedule."""
+        resp = self._connector.mambu_loanaccount_getSchedule(self.id)
+
+        installments = json.loads(resp.decode())["installments"]
+
+        self.schedule = []
+        for installment in installments:
+            installment_entity = MambuInstallment(**installment)
+            installment_entity._tzattrs = copy.deepcopy(installment)
+            installment_entity._convertDict2Attrs()
+            self.schedule.append(installment_entity)
+
+    def set_state(self, action, notes):
+        """Change status of mambu loan
+
+        Args:
+            action (str): specify the action state
+            notes (str): notes to associate to the change of status
+
+        Raises:
+          `MambuPyError`: if action not in _accepted_actions
+        """
+        if action in self._accepted_actions:
+            resp = self._connector.mambu_change_state(
+                entid=self.id, prefix=self._prefix, action=action, notes=notes
+            )
+            resp = json.loads(resp)
+            self.accountState = resp["accountState"]
+        else:
+            raise MambuPyError(
+                "field {} not in allowed _accepted_actions: {}".format(
+                    action, self._accepted_actions
+                )
+            )
