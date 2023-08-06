112a16f5d67a8ddff197117f015a25536ab6dd68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:17 +0000

    Add package python-tss-sdk/1.2.0/python-tss-sdk-1.2.0.tar.gz

---
+++ packages/python-tss-sdk/python-tss-sdk-1.2.0.tar.gz/python-tss-sdk-1.2.0/conftest.py
@@ -0,0 +1,31 @@
+import os
+import pytest
+from dotenv import load_dotenv
+from delinea.secrets.server import PasswordGrantAuthorizer, SecretServerCloud
+
+load_dotenv()
+
+
+@pytest.fixture
+def env_vars():
+    return {
+        "username": os.getenv("TSS_USERNAME"),
+        "password": os.getenv("TSS_PASSWORD"),
+        "tenant": os.getenv("TSS_TENANT"),
+        "secret_id": os.getenv("SECRET_ID"),
+        "secret_path": os.getenv("SECRET_PATH"),
+    }
+
+
+@pytest.fixture
+def authorizer(env_vars):
+    return PasswordGrantAuthorizer(
+        f"https://{env_vars['tenant']}.secretservercloud.com",
+        env_vars["username"],
+        env_vars["password"],
+    )
+
+
+@pytest.fixture
+def secret_server(env_vars, authorizer):
+    return SecretServerCloud(env_vars["tenant"], authorizer)
---
+++ packages/python-tss-sdk/python-tss-sdk-1.2.0.tar.gz/python-tss-sdk-1.2.0/example.py
@@ -0,0 +1,26 @@
+import os
+
+from delinea.secrets.server import (
+    SecretServerCloud,
+    SecretServerError,
+)
+
+if __name__ == "__main__":
+
+    creds = {
+        "username": os.getenv("TSS_USERNAME"),
+        "password": os.getenv("TSS_PASSWORD"),
+        "tenant": os.getenv("TSS_TENTANT"),
+    }
+
+    secret_server = SecretServerCloud(**creds)
+
+    try:
+        secret = secret_server.get_secret(1)
+        print(
+            f"""username: {secret.fields['username'].value}
+                password: {secret.fields['password'].value}
+                template: {secret.secret_template_name}"""
+        )
+    except SecretServerError as error:
+        print(error.response.text)
