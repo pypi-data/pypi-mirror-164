bbb1a6dd85002d578f25894d2aba98a36080fa6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:56 +0000

    Add package numofdicerolls/2.68/numofdicerolls-2.68.tar.gz

---
+++ packages/numofdicerolls/numofdicerolls-2.68.tar.gz/numofdicerolls-2.68/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '2.68'
+DESCRIPTION = 'Python script from pipline'
+LONG_DESCRIPTION = 'This pipeline was automated by Jenkins'
+
+# Setting up
+setup(
+    name="numofdicerolls",
+    version=VERSION,
+    author="Dean",
+    author_email="<dneugebauer@live.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
