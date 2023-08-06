be3517cb43038b3a9449b6fdb3c71f2193773ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:59:31 +0000

    Add package tailslide-sdk/0.1.6/tailslide-sdk-0.1.6.tar.gz

---
+++ packages/tailslide-sdk/tailslide-sdk-0.1.6.tar.gz/tailslide-sdk-0.1.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='tailslide-sdk',
+    version='0.1.6',
+    description='A example Python package',
+    url='https://github.com/shuds13/pyexample',
+    author='Steven Liou, Trent Cooper, Elaine Vuong, Jordan Eggers',
+    author_email='tailslide.io.2205@gmail.com ',
+    license='MIT',
+    packages=['tailslide'],
+    install_requires=[
+        "nats-py >= 2.1.4",
+        "redis >= 4.3.4"
+    ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
