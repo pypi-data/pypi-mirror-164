ec9d7ff4adebd75849ef4d4f3935ac4c05f33e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:21 +0000

    Add package get-wow-data-async/0.1.6.2/get_wow_data_async-0.1.6.2-py3-none-any.whl

---
+++ packages/get-wow-data-async/get_wow_data_async-0.1.6.2-py3-none-any.whl/getwowdataasync/urls.py
@@ -0,0 +1,22 @@
+"""Contians all urls used in get-wow-data-async"""
+urls = {
+    "access_token": "https://{region}.battle.net/oauth/token",
+    "connected_realm_index": "https://{region}.api.blizzard.com/data/wow/connected-realm/index",
+    "realm": "https://{region}.api.blizzard.com/data/wow/connected-realm/{connected_realm_id}",
+    "auction": "https://{region}.api.blizzard.com/data/wow/connected-realm/{connected_realm_id}/auctions",
+    "profession_index": "https://{region}.api.blizzard.com/data/wow/profession/index",
+    "profession_skill_tier": "https://{region}.api.blizzard.com/data/wow/profession/{profession_id}",
+    "profession_tier_detail": "https://{region}.api.blizzard.com/data/wow/profession/{profession_id}/skill-tier/{skill_tier_id}",
+    "profession_icon": "https://{region}.api.blizzard.com/data/wow/media/profession/{profession_id}",
+    "recipe_detail": "https://{region}.api.blizzard.com/data/wow/recipe/{recipe_id}",
+    "repice_icon": "https://{region}.api.blizzard.com/data/wow/media/recipe/{recipe_id}",
+    "item_classes": "https://{region}.api.blizzard.com/data/wow/item-class/index",
+    "item_subclass": "https://{region}.api.blizzard.com/data/wow/item-class/{item_class_id}",
+    "item_set_index": "https://{region}.api.blizzard.com/data/wow/item-set/index?",
+    "item_icon": "https://{region}.api.blizzard.com/data/wow/media/item/{item_id}",
+    "wow_token": "https://{region}.api.blizzard.com/data/wow/token/index",
+    "search_realm": "https://{region}.api.blizzard.com/data/wow/search/connected-realm",
+    "search_item": "https://{region}.api.blizzard.com/data/wow/search/item",
+    "search_media": "https://{region}.api.blizzard.com/data/wow/search/media",
+    "item_bonuses": "https://www.raidbots.com/static/data/live/bonuses.json",
+}
