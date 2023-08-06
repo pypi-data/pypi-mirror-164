9b159d92aaa8dd74974ccea5deb07f56ce43fef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:43 +0000

    Add package voicevox-py/1.0.1.2/voicevox.py-1.0.1.2.tar.gz

---
+++ packages/voicevox-py/voicevox.py-1.0.1.2.tar.gz/voicevox.py-1.0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="voicevox.py",
+    version="1.0.1.2",
+    author="sonyakun",
+    packages=find_packages(),
+    install_requires=["requests","asyncio"],
+    include_package_data=True,
+)
