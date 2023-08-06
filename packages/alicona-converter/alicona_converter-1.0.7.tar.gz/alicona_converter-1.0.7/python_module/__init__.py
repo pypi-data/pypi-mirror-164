825d5acb9392fe1e102559c0d5891bc38495fd8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:14 +0000

    Add package alicona-converter/1.0.7/alicona_converter-1.0.7.tar.gz

---
+++ packages/alicona-converter/alicona_converter-1.0.7.tar.gz/alicona_converter-1.0.7/python_module/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""This is the python-module. It contains functions and methods, which are
+useful across several different projects and repositories. It also contains
+stub-files with several data-type annotations for scipy and matplot-lib
+functions."""
