35e38a9a42ac00120c36c409abdb4c5b52f7316c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:39 +0000

    Add package jelastic-client/1.3.28/jelastic-client-1.3.28.tar.gz

---
+++ packages/jelastic-client/jelastic-client-1.3.28.tar.gz/jelastic-client-1.3.28/setup.py
@@ -0,0 +1,40 @@
+import os
+
+import setuptools
+
+install_requires = [
+    "httpx[http2]>=0.18",
+    "simplejson",
+]
+
+test_requires = [
+    "pytest",
+    "pytest-xdist",
+    "requests"
+]
+
+package_version = os.environ.get("PACKAGE_VERSION", "0.0.0")
+package_name = os.environ.get("PACKAGE_NAME")
+server_version = os.environ.get("JELASTIC_VERSION", "0.0.0")
+
+setuptools.setup(
+    name=package_name,
+    version=package_version,
+    author="Laurent Michel",
+    author_email="softozor@gmail.com",
+    description=f"A client library for Jelastic v{server_version}",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://gitlab.hidora.com/softozor/packages",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    extras_require={
+        "test": test_requires,
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
