10dd9c1ef4e3632087e4f79fe9305505d8e7e9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:59:58 +0000

    Add package asciirt/0.0.3/asciirt-0.0.3-py3-none-any.whl

---
+++ packages/asciirt/asciirt-0.0.3-py3-none-any.whl/library/__init__.py
@@ -0,0 +1,6 @@
+from library.asciirt import macintosh
+from library.asciirt import babymogus
+from library.asciirt import amogus
+from library.asciirt import pacman
+from library.asciirt import mouse
+from library.asciirt import atari
