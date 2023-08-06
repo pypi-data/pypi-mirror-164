00036591ea848feb5863dfa903a898cf2b18d4f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:09 +0000

    Add package wujiantao/0.0.1/wujiantao-0.0.1.tar.gz

---
+++ packages/wujiantao/wujiantao-0.0.1.tar.gz/wujiantao-0.0.1/setup.py
@@ -0,0 +1,13 @@
+"""发布pypi包"""
+from setuptools import setup
+
+setup(
+    name="wujiantao",
+    version="0.0.1",
+    author="wujiantao",
+    author_email="wujiantao1005@gmail.com",
+    description=u"微小助",
+    packages=["wxz_decrpty"],
+    install_requires=[],
+    entry_points={},
+)
