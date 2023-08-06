0399c89b5264aba5437f0a60738931b108d56696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:08 +0000

    Add package comun-log/0.4/comun_log-0.4.tar.gz

---
+++ packages/comun-log/comun_log-0.4.tar.gz/comun_log-0.4/comun_log.py
@@ -0,0 +1,37 @@
+import datetime
+import os
+
+import requests
+from datetime import datetime
+
+
+def insert_log_ok(id_project, fecha_inicio, observations=''):
+    try:
+        fecha_fin = datetime.now()
+        obj = {
+            "project_id": id_project,
+            "start_date": str(fecha_inicio),
+            "end_date": str(fecha_fin),
+            "successful": True,
+            "observations": observations
+        }
+        r = requests.post(f'{os.getenv("PARAMS_API_URL")}/execution_log', json=obj, headers={'token': os.getenv('API_TOKEN')})
+        r.raise_for_status()
+    except Exception as e:
+        print(str(e))
+
+
+def insert_log_ko(id_project, fecha_inicio, observations=''):
+    try:
+        fecha_fin = datetime.now()
+        obj = {
+            "project_id": id_project,
+            "start_date": str(fecha_inicio),
+            "end_date": str(fecha_fin),
+            "successful": False,
+            "observations": observations
+        }
+        r = requests.post(f'{os.getenv("PARAMS_API_URL")}/execution_log', json=obj, headers={'token': os.getenv('API_TOKEN')})
+        r.raise_for_status()
+    except Exception as e:
+        print(str(e))
