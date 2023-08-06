3b22260f2f1195bf5b887d558392c68c22f4b61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:14 +0000

    Add package streamlit-image-select/0.1.1/streamlit_image_select-0.1.1-py3-none-any.whl

---
+++ packages/streamlit-image-select/streamlit_image_select-0.1.1-py3-none-any.whl/streamlit_image_select/frontend/build/precache-manifest.9da124838d8d8b3746bcc4bc3a46ea0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c64f18b4c09415b12028ee239a3c3e5f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f5e4f1852c06a5c76c42",
+    "url": "./static/js/2.e8544d91.chunk.js"
+  },
+  {
+    "revision": "b5321db7731dbb9a09b1fc4c60b61213",
+    "url": "./static/js/2.e8544d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73d3fbd9c5d850de82f8",
+    "url": "./static/js/main.90cea14b.chunk.js"
+  },
+  {
+    "revision": "08bfe9a8f9db97a558b1",
+    "url": "./static/js/runtime-main.fce4c018.js"
+  }
+]);
