9e67010134651ee7e41f1b4a9ac4ca6fb33caf8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:40 +0000

    Add package TeleAuto/0.0.2/TeleAuto-0.0.2.tar.gz

---
+++ packages/TeleAuto/TeleAuto-0.0.2.tar.gz/TeleAuto-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_packages
+
+setup(
+	name = "TeleAuto",
+	version = "0.0.2",
+	keywords = [],
+	description = "tele's auto",
+	long_description = "tele's auto",
+	license = "MIT",
+	url = "https://github.com/hikkki/py-try",
+	author = "yxq",
+	author_email = "yunyxq@163.com",
+	#packages = find_packages(),
+	packages = ['cases'],
+	include_package_data = True,
+	platforms = "any",
+	install_requires = []
+)
