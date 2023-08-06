9c69a66919105a3ee5ccf5a6bab20bf7f270780e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:07 +0000

    Add package greenplum-python-fork/1.0.0b3/greenplum-python-fork-1.0.0b3.tar.gz

---
+++ packages/greenplum-python-fork/greenplum-python-fork-1.0.0b3.tar.gz/greenplum-python-fork-1.0.0b3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+setuptools.setup(
+    name="greenplum-python-fork",
+    version="1.0.0-beta3",
+    install_requires=["psycopg2"],
+    author="Greenplum Python",
+    author_email="greenplum-python@vmware.com",
+    description="Python interface for Greenplum",
+    long_description="Python interface for Greenplum and Postgres",
+    long_description_content_type="text/markdown",
+    url="https://github.com/greenplum-db/GreenplumPython",
+    packages=setuptools.find_packages(include=["greenplumpython", "greenplumpython.*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
+
