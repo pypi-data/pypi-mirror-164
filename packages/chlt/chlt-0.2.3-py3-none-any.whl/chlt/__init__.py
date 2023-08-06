1880fcf8681361329c584a3f431600c7ef27a8bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:27 +0000

    Add package chlt/0.2.3/chlt-0.2.3-py3-none-any.whl

---
+++ packages/chlt/chlt-0.2.3-py3-none-any.whl/chlt/__init__.py
@@ -0,0 +1,7 @@
+print('import chlt.Chinese_learn')
+from chlt.Chinese_learn import *
+print('import chlt.gif_play')
+from chlt.gif_play import *
+print('import chlt.playsound')
+from chlt.playsound import playsound
+import tkinter as tk
