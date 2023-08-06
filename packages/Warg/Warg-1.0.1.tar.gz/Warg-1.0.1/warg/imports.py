12026c78db5d887b5c98d5d5afd2f14903f16a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:13 +0000

    Add package Warg/1.0.1/Warg-1.0.1.tar.gz

---
+++ packages/Warg/Warg-1.0.1.tar.gz/Warg-1.0.1/warg/imports.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Christian Heider Nielsen"
+__doc__ = r"""
+
+           Created on 28/06/2020
+           """
+
+__all__ = ["is_module_available", "import_warning", "reimported_warning"]
+
+from importlib.util import find_spec
+from warnings import warn
+
+
+def is_module_available(module: str) -> bool:
+    """**Return True if module is available.**
+
+    Parameters
+    ----------
+    module: str
+          Name of the module to be checked.
+
+    Returns
+    -------
+    bool
+          True if installed.
+    """
+    return find_spec(module) is not None
+
+
+def import_warning(module_name: str) -> None:
+    """
+    Inform the user that module has been imported,
+    useful when repeated imports are heavy in the contexts of multiprocessing.
+    Lets the user identify which file is reporting heavy loading and restructure code to avoid repeated importing
+
+    :param module_name:
+    :return:
+    """
+    from sys import modules
+
+    if module_name in modules:
+        warn(
+            f"You already {module_name} had imported, consider restructuring your code to avoid repeated imports"
+        )
+
+
+def reimported_warning(module_name: str) -> None:
+    """
+    Just an idea
+
+    :return:
+    """
+    raise NotImplemented
+    # TODO: touch .lock file to system for module_name for a multiprocess warning if already exists,
+    # delete it again once process is done?
+    # context_wrapper maybe useful
+
+
+if __name__ == "__main__":
+
+    def main() -> None:
+        """
+        :rtype: None
+        """
+        mod = "matplotlib"
+        import_warning(mod)
+        from matplotlib import pyplot
+
+        import_warning(mod)
+        pyplot.figure()
+
+    main()
