58218a6d8445e1aa5d577bb2bd18e3177a56fa7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:59:21 +0000

    Add package dfshell/1.2/dfshell-1.2-py3-none-any.whl

---
+++ packages/dfshell/dfshell-1.2-py3-none-any.whl/test2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='sdomdiscover',
+    version="1.2",
+    scripts=['SDomDiscover.py'],
+    author='D3Ext',
+    author_email="d3ext@proton.me",
+    description='An easy-to-use python tool to perform dns recon and more',
+    url='https://github.com/D3Ext/SDomDiscover',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
