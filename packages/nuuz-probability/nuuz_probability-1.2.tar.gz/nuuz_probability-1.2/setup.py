5f8e1bbbad87c90b3c028cdb0233fb85d80f8673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:40 +0000

    Add package nuuz-probability/1.2/nuuz_probability-1.2.tar.gz

---
+++ packages/nuuz-probability/nuuz_probability-1.2.tar.gz/nuuz_probability-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nuuz_probability',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['nuuz_probability'],
+      author = 'Doston Kholdarov',
+      author_email = 'roostamovic@yahoo.com',
+      zip_safe=False)
