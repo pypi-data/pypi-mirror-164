a23376be6cca754a021dd8e031f8cdbdd280a278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:54 +0000

    Add package packperry0/0.2.0/packperry0-0.2.0.tar.gz

---
+++ packages/packperry0/packperry0-0.2.0.tar.gz/packperry0-0.2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='packperry0',
+   version='0.2.0',
+   author='Perihane',
+   author_email='perihane.youssef@vodafone.com',
+   packages=['packperry0']
+)
