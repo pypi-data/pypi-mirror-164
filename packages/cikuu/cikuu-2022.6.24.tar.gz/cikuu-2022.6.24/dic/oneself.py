0d89f73dd662bb3b5542bfe2f7d81c0dde53d388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:17 +0000

    Add package cikuu/2022.6.24/cikuu-2022.6.24.tar.gz

---
+++ packages/cikuu/cikuu-2022.6.24.tar.gz/cikuu-2022.6.24/dic/oneself.py
@@ -0,0 +1,14 @@
+# 2022.8.26, used in ES postag indexing 
+oneself = {
+"himself": "_oneself", 
+"herself":"_oneself", 
+"myself":"_oneself", 
+"yourself":"_oneself", 
+"itself":"_oneself", 
+"ourselves":"_oneself", 
+"yourselves":"_oneself", 
+"themselves":"_oneself", 
+}
+
+if __name__	== '__main__':
+	print(oneself)
