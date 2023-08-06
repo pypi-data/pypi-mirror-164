bc425a1b17e4d129628c6786f72f8b506b93047f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:34 +0000

    Add package edulint/0.0.11/edulint-0.0.11.tar.gz

---
+++ packages/edulint/edulint-0.0.11.tar.gz/edulint-0.0.11/edulint/config/config.py
@@ -0,0 +1,134 @@
+from edulint.config.arg import ProcessedArg, UnprocessedArg, ImmutableArg
+from edulint.options import UnionT, ImmutableT, Option, TakesVal, OptionParse, get_option_parses, get_name_to_option
+from edulint.config.config_translations import get_config_translations, Translation
+from typing import Dict, List, Optional, Tuple, Iterator
+from dataclasses import dataclass
+from argparse import Namespace
+import re
+import sys
+import shlex
+
+
+@dataclass(frozen=True)
+class Config:
+
+    config: Tuple[ImmutableArg]
+
+    @staticmethod
+    def to_immutable(v: UnionT) -> ImmutableT:
+        return v if not isinstance(v, list) else tuple(v)
+
+    def __init__(self, config: Optional[List[ProcessedArg]] = None,
+                 option_parses: Dict[Option, OptionParse] = get_option_parses()) -> None:
+        config = config if config is not None else []
+        wip_config: List[Optional[ImmutableArg]] = [None for _ in Option]
+
+        for arg in config:
+            assert wip_config[int(arg.option)] is None
+            wip_config[int(arg.option)] = ImmutableArg(arg.option, self.to_immutable(arg.val))
+
+        object.__setattr__(self, "config", tuple([arg if arg is not None else ImmutableArg(
+            o, self.to_immutable(option_parses[o].default)) for o, arg in zip(Option, wip_config)]))
+
+    def __str__(self) -> str:
+        return f"Config({', '.join(arg.option.name + '=' + str(arg.val) for arg in self.config)})"
+
+    def __getitem__(self, option: Option) -> ImmutableArg:
+        return self.config[int(option)]
+
+    def __contains__(self, option: Option) -> bool:
+        return self[option] is not None
+
+    def __iter__(self) -> Iterator[ImmutableArg]:
+        return filter(lambda x: x is not None, self.config.__iter__())
+
+
+def extract_args(filename: str) -> List[str]:
+    edulint_re = re.compile(r"\s*#[\s#]*edulint:\s*", re.IGNORECASE)
+
+    result: List[str] = []
+    with open(filename) as f:
+        for i, line in enumerate(f):
+            line = line.strip()
+            edmatch = edulint_re.match(line)
+            if not edmatch:
+                continue
+
+            raw_args = line[edmatch.end():]
+            result.extend(shlex.split(raw_args))
+    return result
+
+
+def parse_args(args: List[str], option_parses: Dict[Option, OptionParse]) -> List[UnprocessedArg]:
+    name_to_option = get_name_to_option(option_parses)
+
+    def get_name_val(arg: str) -> Tuple[str, Optional[str]]:
+        if "=" in arg:
+            name, val = arg.split("=", 1)
+            return name, val
+        return arg, None
+
+    result: List[UnprocessedArg] = []
+    for arg in args:
+        name, val = get_name_val(arg)
+        option = name_to_option.get(name)
+
+        if option is None:
+            print(f"edulint: unrecognized option {name}", file=sys.stderr)
+        else:
+            option_parse = option_parses[option]
+            if option_parse.takes_val == TakesVal.YES and val is None:
+                print(f"edulint: option {name} takes an argument but none was supplied", file=sys.stderr)
+            elif option_parse.takes_val == TakesVal.NO and val is not None:
+                print(f"edulint: option {name} takes no argument but {val} was supplied", file=sys.stderr)
+            else:
+                result.append(UnprocessedArg(option, val))
+
+    return result
+
+
+def fill_in_val(arg: UnprocessedArg, translation: List[str]) -> List[str]:
+    result = []
+    for t in translation:
+        if "<val>" in t:
+            assert isinstance(arg.val, str)
+            result.append(t.replace("<val>", arg.val))
+        else:
+            result.append(t)
+    return result
+
+
+def combine_and_translate(
+        args: List[UnprocessedArg],
+        option_parses: Dict[Option, OptionParse],
+        config_translations: Dict[Option, Translation]) -> Config:
+    def combine(option_vals: List[UnionT], option: Option, val: Optional[str]) -> None:
+        parse = option_parses[option]
+        old_val = option_vals[int(option)]
+        option_vals[int(option)] = parse.combine(old_val, parse.convert(val))
+
+    option_vals = [option_parses[o].default for o in Option]
+
+    for arg in args:
+        combine(option_vals, arg.option, arg.val)
+
+        translated = config_translations.get(arg.option)
+        if translated is not None:
+            translated_option = translated.for_linter.to_option()
+            for val in translated.vals:
+                combine(option_vals, translated_option, val)
+
+    return Config([ProcessedArg(o, v) for o, v in zip(Option, option_vals) if v is not None])
+
+
+def get_config(
+        filename: str, cmd_args: List[str],
+        option_parses: Dict[Option, OptionParse] = get_option_parses(),
+        config_translations: Dict[Option, Translation] = get_config_translations()) -> Config:
+    extracted = extract_args(filename) + cmd_args
+    parsed = parse_args(extracted, option_parses)
+    return combine_and_translate(parsed, option_parses, config_translations)
+
+
+def get_cmd_args(args: Namespace) -> List[str]:
+    return [s for arg in args.options for s in shlex.split(arg)]
---
+++ packages/edulint/edulint-0.0.11.tar.gz/edulint-0.0.11/edulint/config/config_translations.py
@@ -0,0 +1,41 @@
+from edulint.linters import Linter
+from edulint.options import Option
+from typing import Dict, List
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Translation:
+    for_linter: Linter
+    vals: List[str]
+
+
+CONFIG_TRANSLATIONS: Dict[Option, Translation] = {
+    Option.ENHANCEMENT: Translation(
+        Linter.PYLINT,
+        ["--enable=no-self-use,superfluous-parens,consider-using-min-builtin,"
+         "consider-using-max-builtin,consider-using-with,unspecified-encoding,"
+         "use-augmenting-assignment"
+         ]
+    ),
+    Option.PYTHON_SPEC: Translation(
+        Linter.PYLINT,
+        ["--enable=unidiomatic-typecheck,misplaced-format-function,"
+         "unnecessary-lambda,protected-access,multiple-imports,"
+         "wrong-import-position,consider-using-from-import,wildcard-import,"
+         "reimported,improve-for-loop,consider-iterating-dictionary,"
+         "consider-using-dict-items,consider-using-f-string,"
+         "inconsistent-return-statements,consider-swap-variables,"
+         "consider-using-join,consider-using-set-comprehension,"
+         "unnecessary-comprehension,use-a-generator,use-list-literal,"
+         "use-dict-literal,invalid-name,consider-using-in"]
+    ),
+    Option.ALLOWED_ONECHAR_NAMES: Translation(
+        Linter.PYLINT,
+        ["--bad-names-rgxs=^[a-z]$"]
+    )
+}
+
+
+def get_config_translations() -> Dict[Option, Translation]:
+    return CONFIG_TRANSLATIONS
