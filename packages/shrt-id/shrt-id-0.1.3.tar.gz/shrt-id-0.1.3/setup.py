8dce8303766b0badde3a0a9560b969ab139a66b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:20 +0000

    Add package shrt-id/0.1.3/shrt-id-0.1.3.tar.gz

---
+++ packages/shrt-id/shrt-id-0.1.3.tar.gz/shrt-id-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shrt-id',
+    version='0.1.3',
+    license='GNU',
+    author="keocoin",
+    author_email='keocoin@gmail.com',
+    packages=["shrt-id"],
+    url='https://github.com/shrt-id/py',
+    keywords='Short id, uuid, unique id, short',
+    install_requires=[]
+)
