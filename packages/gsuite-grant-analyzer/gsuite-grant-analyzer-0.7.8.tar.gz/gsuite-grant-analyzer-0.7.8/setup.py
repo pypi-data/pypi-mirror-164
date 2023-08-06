755a363bb7832eea1688a1182123f172276863e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:56 +0000

    Add package gsuite-grant-analyzer/0.7.8/gsuite-grant-analyzer-0.7.8.tar.gz

---
+++ packages/gsuite-grant-analyzer/gsuite-grant-analyzer-0.7.8.tar.gz/gsuite-grant-analyzer-0.7.8/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="gsuite-grant-analyzer",
+    version="0.7.8",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="gsuite-grant-analyzer of gsuite-grant-analyzer of gsuite-grant-analyzer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
