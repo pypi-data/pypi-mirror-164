6afdc3d3fc5d5e3a332a4a69fa4d5f48463a92b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:19 +0000

    Add package lxparse/1.0.4/lxparse-1.0.4-py3-none-any.whl

---
+++ packages/lxparse/lxparse-1.0.4-py3-none-any.whl/lxparse/parsepack/parse_content.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from lxparse.readability import Document
+from lxml import etree
+from html import unescape
+
+class ContentExtractor:
+    def extractor(self, html, content_xpath):
+        element = etree.HTML(html)
+        if content_xpath:
+            body = element.xpath(content_xpath)
+            if body:
+                content = unescape(etree.tostring(body[0], encoding='utf-8').decode())
+                return content
+        doc = Document(html)
+        return doc.summary()
