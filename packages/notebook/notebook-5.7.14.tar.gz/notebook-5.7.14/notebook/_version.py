3922976fc66218c6a89092e9838873a645e8599d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:33 +0000

    Add package notebook/5.7.14/notebook-5.7.14.tar.gz

---
+++ packages/notebook/notebook-5.7.14.tar.gz/notebook-5.7.14/notebook/_version.py
@@ -0,0 +1,16 @@
+"""
+store the current version info of the notebook.
+
+"""
+import re
+
+# Next beta (b)/ alpha (a)/ release candidate (rc) release: The version number for alpha is X.Y.ZaN
+__version__ = '5.7.14'
+
+# Build up version_info tuple for backwards compatibility
+pattern = r'(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)'
+match = re.match(pattern, __version__)
+parts = [int(match[part]) for part in ['major', 'minor', 'patch']]
+if match['rest']:
+    parts.append(match['rest'])
+version_info = tuple(parts)
