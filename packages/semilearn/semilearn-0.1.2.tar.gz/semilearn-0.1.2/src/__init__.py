b2466a70e334b5259a8249d32bdfeaedbf390206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:16 +0000

    Add package semilearn/0.1.2/semilearn-0.1.2.tar.gz

---
+++ packages/semilearn/semilearn-0.1.2.tar.gz/semilearn-0.1.2/src/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+import os
+import sys
+sys.path.append(os.path.dirname(__file__))
+
+from lighting import Trainer, get_config
+from utils import get_dataset, net_builder
+from algorithms import get_algorithm
+from datasets.utils import get_data_loader, split_ssl_data
+
+# TODO: replace this with Dataset and Custom dataset in lighting
+from datasets.cv_datasets.datasetbase import BasicDataset
