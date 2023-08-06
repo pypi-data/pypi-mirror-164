f8576a36fd5fd2e8f4e6c50fcf920019b7156e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:55 +0000

    Add package P201-Functions/0.1/P201_Functions-0.1.tar.gz

---
+++ packages/P201-Functions/P201_Functions-0.1.tar.gz/P201_Functions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='P201_Functions',
+	version='0.1',
+	description='Fitting and Weighted Average Functions for Physics 201/202',
+	author='Edward Brash',
+	packages=['P201_Functions'],
+	zip_safe=False)
