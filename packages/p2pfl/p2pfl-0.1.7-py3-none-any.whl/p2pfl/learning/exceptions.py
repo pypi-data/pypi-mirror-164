30eb6f60eed6ab3ddb902406ed4be4bb430159a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:32 +0000

    Add package p2pfl/0.1.7/p2pfl-0.1.7-py3-none-any.whl

---
+++ packages/p2pfl/p2pfl-0.1.7-py3-none-any.whl/p2pfl/learning/exceptions.py
@@ -0,0 +1,11 @@
+class DecodingParamsError(Exception):  
+    """
+    An exception raised when decoding parameters fails.
+    """
+    pass
+
+class ModelNotMatchingError(Exception):  
+    """
+    An exception raised when parameters not match with the model.
+    """
+    pass
