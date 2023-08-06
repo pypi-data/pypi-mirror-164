613825eee8a44cbf0dd7a3d40919724c47f900c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:50 +0000

    Add package yaqd-wright/2022.8.0/yaqd-wright-2022.8.0.tar.gz

---
+++ packages/yaqd-wright/yaqd-wright-2022.8.0.tar.gz/yaqd-wright-2022.8.0/CHANGELOG.md
@@ -0,0 +1,72 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/).
+
+## [Unreleased]
+
+## [2022.8.0]
+
+### Changed
+- Unified FuYu linear and filter wheel into one daemon, wright-stepper-box
+- Fixed problems with multiple setting of stepper box daemons
+
+## [2022.3.0]
+
+### Changed
+- A factor of -1 was incorporated into the `_set_position` method to correct
+
+### Added
+- NDInterp daemon to do N-dimensional interpolation of complicated offsets
+
+## [2022.2.0]
+
+## Added
+- wright-fuyu-linear daemon for the customized FUYU linear stage
+- 'Y' home feature for arduino INO for homing towards upper limits
+
+
+## [2021.4.0]
+
+## Added
+- test script for viewing wright-ingaas spectra is now included in repo
+- wright-aerotech daemon for the customized aerotech translation stage
+
+## Changed
+- `wright-ingaas` now uses `has-mapping` trait to map spectral axis
+- `wright-ingaas` daemon can query spectrometer for position
+- update is-sensor trait
+
+## [2021.1.0]
+
+### Added
+- conda-forge as installation source
+
+### Fixed
+- rerendered avprs based on recent traits change
+- avoid byte order problems in the InGaAs array
+
+### Changed
+- use new parent classes from upstream core
+
+## [2020.10.1]
+
+### Added
+- CI upload to PyPI
+
+### Fixed
+- Metadata URLs
+
+## [2020.10.0]
+
+### Added
+- initial release
+
+[Unreleased]: https://github.com/wright-group/yaqd-wright/compare/v2022.8.0...HEAD
+[2022.8.0]: https://github.com/wright-group/yaqd-wright/compare/v2022.3.0...v2022.8.0
+[2022.3.0]: https://github.com/wright-group/yaqd-wright/compare/v2022.2.0...v2022.3.0
+[2022.2.0]: https://github.com/wright-group/yaqd-wright/compare/v2021.4.0...v2022.2.0
+[2021.4.0]: https://github.com/wright-group/yaqd-wright/compare/v2021.1.0...v2021.4.0
+[2021.1.0]: https://github.com/wright-group/yaqd-wright/compare/v2020.10.1...v2021.1.0
+[2020.10.1]: https://github.com/wright-group/yaqd-wright/compare/v2020.10.0...v2020.10.1
+[2020.10.0]: https://github.com/wright-group/yaqd-wright/releases/tag/v2020.10.0
---
+++ packages/yaqd-wright/yaqd-wright-2022.8.0.tar.gz/yaqd-wright-2022.8.0/yaqd_wright/_wright_stepper_box.py
@@ -0,0 +1,76 @@
+__all__ = ["WrightStepperBox"]
+
+import asyncio
+from typing import Dict
+import time
+
+from yaqd_core import IsHomeable, HasLimits, IsDiscrete, HasPosition, UsesUart, UsesSerial, aserial
+
+
+class WrightStepperBox(IsHomeable, HasLimits, IsDiscrete, HasPosition, UsesUart, UsesSerial):
+    _kind = "wright-stepper-box"
+    serial_dispatchers: Dict[str, aserial.ASerial] = {}
+
+    def __init__(self, name, config, config_filepath):
+        super().__init__(name, config, config_filepath)
+        self._reset_on_not_busy = False
+        self._motornum = config["motor"]
+        self._units = config["units"]
+        self._microstep = config["microstep"]
+        self._steps_per_unit = config["steps_per_unit"]
+        if config["serial_port"] in WrightStepperBox.serial_dispatchers:
+            self._serial_port = WrightStepperBox.serial_dispatchers[config["serial_port"]]
+        else:
+            self._serial_port = aserial.ASerial(config["serial_port"], config["baud_rate"])
+            WrightStepperBox.serial_dispatchers[config["serial_port"]] = self._serial_port
+        self._set_microstep(self._microstep)
+
+    def _set_position(self, position):
+        self._reset_on_not_busy = True
+
+    def direct_serial_write(self, message):
+        self._busy = True
+        self._serial_port.write(message)
+
+    def home(self):
+        loop = asyncio.get_event_loop()
+        loop.create_task(self._home())
+
+    async def _home(self):
+        self._busy = True
+        self._serial_port.write(f"H {self._motornum}\n".encode())
+        self._reset_on_not_busy = True
+        self._state["position"] = 0.0
+
+    def _set_microstep(self, microint):
+        self._busy = True
+        if microint in [2**i for i in range(0, 6)]:
+            self._serial_port.write(f"U {microint}\n".encode())
+            self._microstep = microint
+        else:
+            raise ValueError("microint must be a power of 2 (e.g. 1, 2,... 32)")
+
+    async def update_state(self):
+        while True:
+            line = await self._serial_port.awrite_then_readline(f"Q {self._motornum}\n".encode())
+            self._busy = line[0:1] != b"R"
+            if self._reset_on_not_busy and not self._busy:
+                self._busy = True
+                self._reset_on_not_busy = False
+                step_position = round(
+                    self._microstep
+                    * (self._state["destination"] - self._state["position"])
+                    * self._steps_per_unit
+                )
+                self._serial_port.write(f"M {self._motornum} {step_position}\n".encode())
+                self._state["position"] += step_position / (self._steps_per_unit * self._microstep)
+            self.logger.debug(f"{self._busy=}")
+            await asyncio.sleep(0.2)
+            if self._busy:
+                self._state["position_identifier"] = None
+            else:
+                k1 = None
+                for k, v in self._position_identifiers.items():
+                    if round(self._state["position"]) == round(v):
+                        k1 = k
+                self._state["position_identifier"] = k1
