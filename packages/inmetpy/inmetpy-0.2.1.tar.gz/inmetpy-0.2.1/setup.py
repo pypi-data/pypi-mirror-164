b83b29a054de3df82985cdbdf024d91b94330601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:40 +0000

    Add package inmetpy/0.2.1/inmetpy-0.2.1.tar.gz

---
+++ packages/inmetpy/inmetpy-0.2.1.tar.gz/inmetpy-0.2.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(
+    name="inmetpy",
+    version="0.2.1",
+    description="A Python API Client to consume the API of the Brazilian National Institute of Meteorology - INMET",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    author="Felippe Galdino, Tobias Ferreira",
+    author_email="ocfgaldino@gmail.com",
+    license="GPLv3+",
+    url="https://github.com/ocfgaldino/inmetpy",
+    install_requires=requirements,
+    test_suite="tests",
+    include_package_data=True,
+    scripts=["scripts/inmetpy"],
+    zip_safe=False,
+)
