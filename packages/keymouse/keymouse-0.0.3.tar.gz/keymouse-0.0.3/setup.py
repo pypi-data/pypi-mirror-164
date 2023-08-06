6da4804dd256182c148704d0aabab8cf5939390f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:05 +0000

    Add package keymouse/0.0.3/keymouse-0.0.3.tar.gz

---
+++ packages/keymouse/keymouse-0.0.3.tar.gz/keymouse-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="keymouse",
+    version="0.0.3",
+    license="MIT",
+    author="Roman Smolnyk",
+    author_email="poma23324@gmail.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="https://gitlab.com/roman-smolnyk/keymouse",
+    keywords="Key Mouse",
+    install_requires=[
+        "pynput",
+        "pyautogui",
+        "pyperclip",
+        "pylayout",
+    ],
+    description="Python module to work with keyboard and mouse"
+)
