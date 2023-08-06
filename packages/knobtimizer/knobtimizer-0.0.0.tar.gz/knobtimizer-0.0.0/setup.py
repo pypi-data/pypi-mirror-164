72f9da8d2c86db3cccf4a53973131a38aa7d9d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:38 +0000

    Add package knobtimizer/0.0.0/knobtimizer-0.0.0.tar.gz

---
+++ packages/knobtimizer/knobtimizer-0.0.0.tar.gz/knobtimizer-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='knobtimizer',
+    version='0.0.0',
+    description='Framework for Optimization with different accelerator codes',
+    long_description='',
+    url='https://github.com/pylhc/knobtimizer',
+    author='pylhc',
+    author_email='pylhc@github.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
