7c50378e4eae1506e1208e308ee7b034702845f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:11 +0000

    Add package kisaBob8gook/5/kisaBob8gook-5.tar.gz

---
+++ packages/kisaBob8gook/kisaBob8gook-5.tar.gz/kisaBob8gook-5/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="kisaBob8gook",
+    version="5",
+    license='MIT',
+    author="bob8gook",
+    author_email="hyeok.jang@lotte.net",
+    description="kisa",
+    long_description=open('README.md').read(),
+    url="https://github.com/woounnan",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
