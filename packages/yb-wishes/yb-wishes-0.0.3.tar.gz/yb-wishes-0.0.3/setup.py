9dcf492a9f7cdf9afbe7247858583ce835a05ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:11 +0000

    Add package yb-wishes/0.0.3/yb-wishes-0.0.3.tar.gz

---
+++ packages/yb-wishes/yb-wishes-0.0.3.tar.gz/yb-wishes-0.0.3/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2022/8/26 13:19
+# @author   : yone
+# @FileName : setup.py
+
+import setuptools
+
+setuptools.setup(
+    name="yb-wishes",
+    version="0.0.3",
+    author="yone",
+    author_email="1242925780@qq.com",
+    description="wishes",
+    packages=setuptools.find_packages(where='.', exclude=(), include=('*',)),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+
+)
---
+++ packages/yb-wishes/yb-wishes-0.0.3.tar.gz/yb-wishes-0.0.3/yb-wishes/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2022/8/26 13:18
+# @author   : yone
+# @FileName : __init__.py.py
