319be7c9f07a0a8e47ea6a6bca1bb973029be7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:05 +0000

    Add package vmklib/1.6.7/vmklib-1.6.7.tar.gz

---
+++ packages/vmklib/vmklib-1.6.7.tar.gz/vmklib-1.6.7/setup.py
@@ -0,0 +1,40 @@
+# =====================================
+# generator=datazen
+# version=2.1.0
+# hash=9380b3085406ef55b40edf585b26a53f
+# =====================================
+
+"""
+vmklib - Package definition for distribution.
+"""
+
+# third-party
+try:
+    from setuptools_wrapper.setup import setup
+except (ImportError, ModuleNotFoundError):
+    from vmklib_bootstrap.setup import setup  # type: ignore
+
+# internal
+from vmklib import DESCRIPTION, PKG_NAME, VERSION
+
+author_info = {
+    "name": "Vaughn Kottler",
+    "email": "vaughnkottler@gmail.com",
+    "username": "vkottler",
+}
+pkg_info = {
+    "name": PKG_NAME,
+    "slug": PKG_NAME.replace("-", "_"),
+    "version": VERSION,
+    "description": DESCRIPTION,
+    "versions": [
+        "3.7",
+        "3.8",
+        "3.9",
+        "3.10",
+    ],
+}
+setup(
+    pkg_info,
+    author_info,
+)
