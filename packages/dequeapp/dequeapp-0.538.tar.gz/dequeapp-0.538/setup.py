7663c9786fb8cbc4e4c7f74e748f7618598f902e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:35 +0000

    Add package dequeapp/0.538/dequeapp-0.538.tar.gz

---
+++ packages/dequeapp/dequeapp-0.538.tar.gz/dequeapp-0.538/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension
+
+
+setup(
+    name='dequeapp',
+    version='0.00538',
+    description='Python Package for Deque tools',
+    author="The Deque Team",
+    author_email='team@deque.app',
+    packages=["deque"],
+    url='https://github.com/rijupahwa/deque',
+    keywords='deque client for experiment tracking, sweep and other deep learning tooling',
+    install_requires=[
+          'redis','redis-server',"coolname","requests","pillow","numpy","psutil","GPUtil"
+      ],
+)
