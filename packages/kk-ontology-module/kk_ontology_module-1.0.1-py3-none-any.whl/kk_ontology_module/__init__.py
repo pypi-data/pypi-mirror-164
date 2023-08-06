a859de7dd1b1580a8c7e3051046ac0a3d1c7e468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:02 +0000

    Add package kk-ontology-module/1.0.1/kk_ontology_module-1.0.1-py3-none-any.whl

---
+++ packages/kk-ontology-module/kk_ontology_module-1.0.1-py3-none-any.whl/kk_ontology_module/__init__.py
@@ -0,0 +1,5 @@
+from kk_ontology_module.load_data import load_data
+from kk_ontology_module.load_onto import load_onto
+from kk_ontology_module.map_data import map_data
+from kk_ontology_module.datamanip import to_pandas, has_icd10_code, has_morphology_code
+from kk_ontology_module.reason import CancerOntology, test_data
