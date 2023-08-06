4cc0a43b705675d902b19da221a692723b32f238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:44 +0000

    Add package mqttwarn/0.29.0/mqttwarn-0.29.0-py3-none-any.whl

---
+++ packages/mqttwarn/mqttwarn-0.29.0-py3-none-any.whl/mqttwarn/model.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# (c) 2021 The mqttwarn developers
+import dataclasses
+from dataclasses import dataclass, field
+from typing import Dict, List, Union
+
+
+@dataclass
+class ProcessorItem:
+    """
+    A processor item for feeding information into service handlers.
+    """
+
+    service: str = None
+    target: str = None
+    config: Dict = field(default_factory=dict)
+    addrs: List[str] = field(default_factory=list)
+    priority: int = None
+    topic: str = None
+    title: str = None
+    message: Union[str, bytes] = None
+    data: Dict = None
+
+    def asdict(self):
+        return dataclasses.asdict(self)
