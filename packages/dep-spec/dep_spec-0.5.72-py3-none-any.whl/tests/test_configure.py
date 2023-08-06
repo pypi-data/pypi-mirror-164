55f38f855cf09decb8becc47a1cdeab2b4a762e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:44 +0000

    Add package dep-spec/0.5.72/dep_spec-0.5.72-py3-none-any.whl

---
+++ packages/dep-spec/dep_spec-0.5.72-py3-none-any.whl/tests/test_configure.py
@@ -0,0 +1,25 @@
+"""Test configure app."""
+
+from spec.service.configure import App, UserSettings, create
+
+
+def test_default():
+    """Test create default."""
+
+    assert isinstance(create(), App)
+
+
+def test_with_proxy_kw():
+    """Test create with proxy kwargs for super class."""
+
+    kw = {'debug': True}
+    app = create(kw=kw)
+    assert app.debug
+
+
+def test_with_user_settings():
+    """Test create with user settings."""
+
+    class ThirdPartySettings(UserSettings): setting = 'yeap'  # noqa
+    app = create(settings=ThirdPartySettings)
+    assert app.settings == ThirdPartySettings()
