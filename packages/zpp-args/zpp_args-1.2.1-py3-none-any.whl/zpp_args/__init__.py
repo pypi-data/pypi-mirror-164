44eb4fdec93749cc1bcf9c325937f979165765bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:33 +0000

    Add package zpp-args/1.2.1/zpp_args-1.2.1-py3-none-any.whl

---
+++ packages/zpp-args/zpp_args-1.2.1-py3-none-any.whl/zpp_args/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '1.2.1'
+__author__ = 'ZephyrOff  (Alexandre Pajak)'
+__descriptor__ = "Module pour le traitement des arguments d'une ligne de commande"
+__lastupdate__ = '26/08/2022'
+__releasenote__ = 'Correct error with digit default value'
+
+from .args import *
