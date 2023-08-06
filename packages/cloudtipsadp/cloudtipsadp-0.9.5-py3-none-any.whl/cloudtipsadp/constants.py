8cac546bfb2b2b6d5c2d11bc2cba4df9dff04bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:52 +0000

    Add package cloudtipsadp/0.9.5/cloudtipsadp-0.9.5-py3-none-any.whl

---
+++ packages/cloudtipsadp/cloudtipsadp-0.9.5-py3-none-any.whl/cloudtipsadp/constants.py
@@ -0,0 +1,29 @@
+__all__ = [
+    'BASE_URL',
+    'BASE_URL_API',
+    'BASE_URL_SANDBOX',
+    'BASE_URL_API_SANDBOX',
+    'M_BAD_CONNECT',
+    'M_BASE_IMPLEMENTED',
+    'API_VERSION',
+    'HEADERS_REQUEST',
+    'SITE_RETURNING_URL',
+    'FILE_PATH_BAD',
+    'LOGS_NOT_LINK',
+    'CTA',
+    'LOGS_NOT_VALUES',
+]
+CTA = 'cloudtipsadp'
+BASE_URL = 'https://identity.cloudtips.ru'
+BASE_URL_SANDBOX = 'https://identity-sandbox.cloudtips.ru'
+BASE_URL_API = 'https://api.cloudtips.ru/api'
+BASE_URL_API_SANDBOX = 'https://api-sandbox.cloudtips.ru'
+M_BAD_CONNECT = 'No service connection.'
+M_BASE_IMPLEMENTED = 'Base class method not implemented.'
+# URL сайта для возврата плательщика после аутентификации карты
+SITE_RETURNING_URL = 'http://127.0.0.1:8000'
+API_VERSION = 'api'
+FILE_PATH_BAD = 'File path missing:'
+HEADERS_REQUEST = {"Content-Type": "application/x-www-form-urlencoded"}
+LOGS_NOT_LINK = 'cloudtipsadp: путь вывода логов не указан'
+LOGS_NOT_VALUES = 'не установлены переменные окружения для логирования'
