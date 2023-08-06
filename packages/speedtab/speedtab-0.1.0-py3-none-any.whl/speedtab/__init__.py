aa85250336b5edd8190653af17fec796bd1c5ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:54 +0000

    Add package speedtab/0.1.0/speedtab-0.1.0-py3-none-any.whl

---
+++ packages/speedtab/speedtab-0.1.0-py3-none-any.whl/speedtab/__init__.py
@@ -0,0 +1,3 @@
+from client import Client
+from enums import MergeType, BorderStyle, HorizontalAlignment, VerticalAlignment, WrapStrategy, ShareRole, ChartType, StackedType, LegendPosition, AxisPosition, DefaultColors
+from formats import Color, Number, Dollar, Percent, Date, Time, DateTime, Scientific, Border
