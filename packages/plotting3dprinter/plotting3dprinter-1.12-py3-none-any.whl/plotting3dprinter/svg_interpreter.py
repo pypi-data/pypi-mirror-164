9c953ee2e3c4148371a4a80f95962f143e27debf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:27 +0000

    Add package plotting3dprinter/1.12/plotting3dprinter-1.12-py3-none-any.whl

---
+++ packages/plotting3dprinter/plotting3dprinter-1.12-py3-none-any.whl/plotting3dprinter/svg_interpreter.py
@@ -0,0 +1,331 @@
+from more_itertools import windowed
+import matplotlib.pyplot as plt
+import numpy as np
+import xml.etree.ElementTree as et
+
+def interpolateBezier( points, steps=10, t=None):
+    points = tuple(points)
+    if len(points)==3:
+        mapper = lambda t,p: (1-t)**2 * p[0] + 2*(1-t)*t*p[1] + t**2*p[2]
+    elif len(points)==4:
+        mapper = lambda t,p: (np.power( (1-t),3)*p[0] +\
+         3* np.power((1-t),2) *t *p[1] +\
+         3*(1-t)*np.power(t,2)*p[2] +\
+         np.power(t,3)*p[3])
+    else:
+        raise Exception('Can only interpolate cubic and quadratic splines (3 or 4 parameters, got: %s'  % str(points))
+
+    if t is not None:
+        return   mapper(t, [q[0] for q in points]), mapper(t, [q[1] for q in points])
+    xGen = ( mapper(t, [q[0] for q in points]) for t in np.linspace(0, 1, steps) )
+    yGen = ( mapper(t, [q[1] for q in points]) for t in np.linspace(0, 1, steps) )
+
+    return zip(xGen, yGen)
+
+
+def parse_coord(inp):
+    return np.array([ float(next(inp)),  float(next(inp))])
+
+def svg_to_coordinate_chomper( inp, yield_control=False,PRECISION=5,verbose=False):
+    prev = None
+    try:
+        while True:
+            chunk = next(inp)
+            if chunk == 'M':
+                #print('Got new start coordinate')
+
+                start = parse_coord(inp)
+                prev = start
+                yield [np.nan,np.nan], 'M'
+                yield list(start), 'M'
+
+                if verbose:
+                    print(f'M {start}')
+                    #plt.scatter([start[0]],[start[1]])
+                continue
+
+            if chunk == 'm':
+                #print('Got new start coordinate')
+
+                #m = next(inp)
+                if prev is None:
+                    start = parse_coord(inp)
+                else:
+                    start = parse_coord(inp) +prev
+                prev = start
+                yield [np.nan,np.nan], 'm'
+                yield list(start), 'm'
+                #yield [np.nan,np.nan]
+                #print(f'm {start}')
+                continue
+
+
+
+            #print(chunk)
+            if chunk in 'zZ':
+                # Go to start:
+                #print("Returning to start coordinate")
+                prev= start
+                yield start, 'z'
+
+                #print("Done")
+                continue
+
+
+            if chunk.strip()=='l':
+                # Line to command:
+                yield prev, 'l'
+                cur = parse_coord(inp)+prev
+                yield cur, 'l'
+                prev = cur
+
+                continue
+
+            if chunk.strip()=='L':
+                # Line to command:
+                yield prev, 'L'
+                cur = parse_coord(inp)
+                yield cur, 'L'
+                prev = cur
+                if verbose:
+                    print(f'L {prev} > {cur}')
+                continue
+
+            if chunk[0]=='c': # bezier mode
+                 #c dx1,dy1 dx2,dy2 dx,dy
+
+                dxdy1 = parse_coord(inp) + prev
+                dxdy2 = parse_coord(inp)  + prev
+                dxdy = parse_coord(inp)  + prev
+
+                #print('C Bezier',prev,dxdy1,dxdy2,dxdy)
+
+                if yield_control:
+                    yield dxdy1
+                    yield dxdy2
+                    yield dxdy
+
+                else:
+                    # Resample the bezier curve
+                    for x,y in interpolateBezier(
+                        [
+                            prev,
+                            dxdy1   ,
+                            dxdy2   ,
+                            dxdy
+                        ], steps=PRECISION
+
+                    ):
+                        yield np.array([x,y]), 'c'
+
+
+                prev = dxdy
+                #yield prev
+                continue
+
+            if chunk[0]=='C': # bezier mode
+                 #c dx1,dy1 dx2,dy2 dx,dy
+
+                dxdy1 = parse_coord(inp)
+                dxdy2 = parse_coord(inp)
+                dxdy = parse_coord(inp)
+
+                #print('C Bezier',prev,dxdy1,dxdy2,dxdy)
+
+                if yield_control:
+                    yield dxdy1
+                    yield dxdy2
+                    yield dxdy
+
+                else:
+                    # Resample the bezier curve
+                    for x,y in interpolateBezier(
+                        [
+                            prev,
+                            dxdy1   ,
+                            dxdy2   ,
+                            dxdy
+                        ], steps=PRECISION
+
+                    ):
+                        yield np.array([x,y]), 'C'
+
+                prev = dxdy
+                #yield prev
+                continue
+
+            if chunk[0]=='A': # ARC mode:
+                #print('Got arc')
+                rx,ry = parse_coord(inp)
+
+                x_ax_rot = float( next(inp) )
+                large_arc = int( next(inp) )
+                sweep = int( next(inp) )
+                cx,cy = parse_coord(inp)
+
+                raise NotImplementedError()
+                #yield from arc_sampler( rx,ry,x_ax_rot,large_arc,sweep, cx,cy,n_segs = 30 )
+                continue
+
+
+            if chunk[0]=='q': # quadratic bezier mode
+                 #c dx1,dy1 dx2,dy2 dx,dy
+
+                dxdy1 = parse_coord(inp) + prev
+                dxdy = parse_coord(inp)  + prev
+
+                #print('Q Bezier',prev,dxdy1, dxdy)
+
+                if yield_control:
+                    yield dxdy1
+                    yield dxdy
+
+                else:
+                    # Resample the bezier curve
+                    for x,y in interpolateBezier(
+                        [
+                            prev,
+                            dxdy1   ,
+                            dxdy
+                        ], steps=PRECISION
+
+                    ):
+                        yield np.array([x,y]), 'q'
+
+                prev = dxdy
+                #yield prev
+                continue
+
+
+            if chunk[0]=='Q': # quadratic bezier , absolute
+                 #c dx1,dy1 dx2,dy2 dx,dy
+                # C x1 y1, x2 y2, x y
+
+                dxdy1 = parse_coord(inp)
+                dxdy = parse_coord(inp)
+
+                #print('Q Bezier',dxdy1, dxdy)
+
+                if yield_control:
+                    yield dxdy1
+                    yield dxdy
+
+                else:
+                    # Resample the bezier curve
+                    for x,y in interpolateBezier(
+                        [
+                            prev,
+                            dxdy1   ,
+                            dxdy
+                        ], steps=PRECISION
+
+                    ):
+                        yield np.array([x,y]), 'Q'
+
+                prev = dxdy
+                #yield prev
+                continue
+
+            if chunk not in 'HhVv':
+                raise ValueError(f'Unknown command {chunk}')
+                #print("MISS:",chunk)
+                prev +=  parse_coord(chunk)
+                #print(prev)
+                yield list(prev) #prev #+start
+            elif chunk=='h':
+                # Parse the next chunk: (single horizontal coordinate)
+
+                yield list(prev), 'h'
+
+                chunk2 = next(inp)
+                x = float(chunk2)
+                prev[0] += x
+
+                yield list(prev), 'h' #+start
+            elif chunk=='H':
+                # Parse the next chunk: (single horizontal coordinate)
+
+                yield list(prev), 'H'
+
+                chunk2 = next(inp)
+                x = float(chunk2)
+                prev[0] = x
+
+                if verbose:
+                    print(f'H {x} ({prev})')
+
+                yield list(prev), 'H' #+start
+
+            elif chunk=='v':
+                # Parse the next chunk: (single horizontal coordinate)
+                yield list(prev), 'v'
+
+                chunk2 = next(inp)
+                y = float(chunk2)
+                prev[1] += y
+
+                yield list(prev), 'v' #+start
+
+            elif chunk=='V':
+                # Parse the next chunk: (single horizontal coordinate)
+                yield list(prev), 'V'
+
+                chunk2 = next(inp)
+                y = float(chunk2)
+                prev[1] = y
+                if verbose:
+                    print(f'H {y} ({prev})')
+                yield list(prev), 'V' #+start
+    except StopIteration:
+        pass
+
+def repart(inp):
+    for p in inp:
+        if len(p)>1 and p[0].upper() in 'HZMCQ':
+            yield p[0]
+            yield p[1:]
+        else:
+            yield p
+
+
+
+def svg_to_segment_blocks(svg_path,precision=5):
+    tree = et.parse(svg_path)
+    ns = {'sn': 'http://www.w3.org/2000/svg'}
+    root = tree.getroot()
+    for i,path in enumerate(root.findall('.//sn:path', ns)):
+        # Parse the path in d:
+        d  = path.attrib['d'].replace(',', ' ')
+        parts = d.split()
+        coordinates = []
+        for (x,y),c in svg_to_coordinate_chomper(
+            inp=repart(parts), PRECISION=precision):
+            coordinates.append([x,y])
+
+        if len(coordinates)>0:
+            yield np.array(list(coordinates_to_segments( coordinates )))
+
+
+def coordinates_to_segments(coordinates):
+    for (x,y) in coordinates:
+
+        if np.isnan(x):
+            current = x,y
+            is_down=False
+            continue
+        else:
+            if not is_down:
+                # Move the head to the target location, while still being up
+                is_down=True
+                prev=None
+
+            if (x,y) != prev:
+                if  not np.isnan(current[0]):
+
+                    yield [[current[0],current[1]], [x, y]]
+
+                current = x,y
+            else:
+                # Dont write duplicate coordinates. Waste of space
+                pass
+            prev = current
---
+++ packages/plotting3dprinter/plotting3dprinter-1.12-py3-none-any.whl/plotting3dprinter/svg_to_gcode.py
@@ -0,0 +1,216 @@
+import xml.etree.ElementTree as et
+from .svg_interpreter import svg_to_coordinate_chomper, repart, svg_to_segment_blocks
+from .raycaster import cast_rays
+import matplotlib.pyplot as plt
+import matplotlib
+from copy import copy
+import numpy as np
+
+
+
+def get_optimal_ordering(blockset):
+    remaining_blocks = copy(blockset)
+    i = 0
+
+    current_block = remaining_blocks.pop()
+    yield current_block[-1]
+
+    while len(remaining_blocks)>0:
+        # find nearest block
+        if len(remaining_blocks)==0:
+            return
+        end = current_block[-1][-1][1]
+
+        best = None
+        best_dist = None
+        best_idx = None
+
+        for j,rblock in enumerate(remaining_blocks):
+
+            (xstart,ystart) = rblock[-1][0][0]
+
+            dist = np.sqrt( np.power(xstart-end[0],2) +
+                           np.power(ystart-end[1],2))
+            #print(dist)
+            if best is None or dist<best_dist:
+                best = rblock
+                best_dist=dist
+                best_idx=j
+
+        print('next best block is ',best_idx, best_dist )
+        current_block = best
+
+        yield best[-1]
+        remaining_blocks.pop(best_idx)
+
+
+def svg_to_gcode(svg_path,
+                gcode_path,
+                precision=10,
+                speed = 4600,
+                x_offset = 50,
+                y_offset = 50,
+                z_draw = 5,
+                z_up = 7,
+                create_outline=True,
+                create_fill=False,
+                min_fill_segment_size=2, # Don't fill with lines shorter than this value
+                longest_edge = None, # Rescale longest edge to this value
+                bed_size_x=250,
+                bed_size_y=200,
+ ):
+
+    cmdcolor = {
+        'L':'r',
+        'l':'r',
+        'M':'k',
+        'V':'b',
+        'v':'b',
+        'H':'b',
+        'h':'b'
+
+    }
+
+    v_z=speed
+    tree = et.parse(svg_path)
+    # Add namespace for svg
+    ns = {'sn': 'http://www.w3.org/2000/svg'}
+    # get root element
+    root = tree.getroot()
+
+    # Find coordinate extends, for rescaling
+    max_x = None
+    min_x = None
+    max_y = None
+    min_y = None
+
+    for i,path in enumerate(root.findall('.//sn:path', ns)):
+        # Parse thew path in d:
+        d  = path.attrib['d'].replace(',', ' ')
+        parts = d.split()
+
+        #coordinates = np.array(list(
+    #        map(list, list( svg_to_coordinate_chomper(
+    #        inp=repart(parts), PRECISION=precision) ))
+#        ))
+
+        coordinates = []
+        commands = []
+        for t in svg_to_coordinate_chomper(
+            inp=repart(parts), PRECISION=precision):
+            #print(t)
+            (x,y),c = t
+
+            coordinates.append([x,y])
+            commands.append(c)
+
+        #coordinates = [ [x,y]  for (x,y),c in  svg_to_coordinate_chomper(
+    #        inp=repart(parts), PRECISION=precision)]
+        coordinates = np.array(coordinates)
+
+        bot = np.nanmin( coordinates[:,0])
+        if min_x is None or bot<min_x:
+            min_x = bot
+
+        top = np.nanmax( coordinates[:,0])
+        if max_x is None or top>max_x:
+            max_x = top
+
+
+        bot = np.nanmin( coordinates[:,1])
+        if min_y is None or bot<min_y:
+            min_y = bot
+
+        top = np.nanmax( coordinates[:,1])
+        if max_y is None or top>max_y:
+            max_y = top
+
+    # perform scaling such that the longest edge < longest_edge mm
+    if longest_edge is not None:
+        scale_x = longest_edge/(max_x - min_x)
+        scale_y = longest_edge/(max_y - min_y)
+        scaler = min(scale_x,scale_y)
+
+    fig, ax = plt.subplots()
+
+
+    pathcolors = plt.get_cmap('tab10')
+    with open(gcode_path,'w') as o:
+
+        # Move pen up:
+        o.write(f'G1 Z{z_up+20} F{v_z}\n') # Perform up
+        o.write('G28 X Y\n') # perform home
+        o.write(f'G1 Z{z_up} F{v_z}\n') # Perform up
+
+
+        segment_blocks = list( svg_to_segment_blocks(svg_path) )
+        prev=None
+        # Determine block, start and ends
+        blockset = []
+        for block in segment_blocks:
+            blockset.append([block[0][0], block[-1][1], block])
+
+        if create_outline:
+            prev = None
+            for block in get_optimal_ordering(blockset):
+                for ii,( (x1,y1),(x2,y2) ) in enumerate( block ):
+
+                    if longest_edge is not None:
+                        x1-=min_x
+                        x2-=min_x
+                        y1-=min_y
+                        y2-=min_y
+                        # Convert video coordinates to xy coordinates (flip y)
+                        y1 = (max_y-min_y)-(y1)
+                        y2 = (max_y-min_y)-(y2)
+                        # Scale
+                        x1*=scaler
+                        x2*=scaler
+                        y1*=scaler
+                        y2*=scaler
+
+                    if x1>bed_size_x or y1>bed_size_y or x2>bed_size_x or y2>bed_size_y:
+                        raise ValueError(f'Coordinates generated which fall outside of supplied printer bed size, adjust printer bed size or add "-longest_edge {min(bed_size_x,bed_size_y)}" to the command to scale the coordinates')
+
+                    if prev is None or prev!=(x1,y1):
+                        # Perform travel move:
+                        print('traveling ...', (x1,y1))
+                        if prev is not None:
+                            # go up first
+                            o.write(f'G1 X{x_offset+prev[0]:.2f} Y{y_offset+prev[1]:.2f} Z{z_up} F{speed}\n')
+                            plt.plot([prev[0],x1],[prev[1],y2],c='grey')
+                        o.write(f'G1 X{x_offset+x1:.2f} Y{y_offset+y1:.2f} Z{z_up} F{speed}\n')
+                        # Drop pen down at current position
+                        o.write(f'G1 X{x_offset+x1:.2f} Y{y_offset+y1:.2f} Z{z_draw} F{v_z}\n')
+
+                    #if prev!=(x1,y1):
+                    #    print(x1,y1)
+
+                    o.write(f'G1 X{x_offset+x1:.2f} Y{y_offset+y1:.2f} Z{z_draw} F{speed}\n')
+                    #print(x2,y2)
+                    o.write(f'G1 X{x_offset+x2:.2f} Y{y_offset+y2:.2f} Z{z_draw} F{speed}\n')
+                    plt.plot([x1,x2],[y1,y2],c='r')
+                    prev = (x2,y2)
+
+            #o.write(f'#NEXT\n')
+
+            # block ended.. travel
+            #o.write(f'G1 X{x_offset+x2:.2f} Y{y_offset+y:.2f} Z{z_up} F{speed}\n')
+
+        o.write(f'G1 Z{z_up+50} F{speed}\n')
+
+        #plt.plot( coordinates[:,0],  coordinates[:,1])
+
+        print('All done')
+        plt.xlim(-10,bed_size_x+10)
+
+        plt.axvline(0,c='r',ls=':')
+        plt.axvline(bed_size_x,c='r',ls=':')
+        plt.axvline(bed_size_x-x_offset,c='r',ls=':')
+
+        plt.axhline(0,c='r',ls=':')
+        plt.axhline(bed_size_y,c='r',ls=':')
+        plt.axhline(bed_size_y-y_offset,c='r',ls=':')
+
+
+        plt.savefig(f'{gcode_path.replace(".gcode","")}.png', dpi=300)
