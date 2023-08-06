2fd5e8cd157c4b7e4e49d530ee6026cb6983b906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:15 +0000

    Add package crudhex/0.9.0/crudhex-0.9.0.tar.gz

---
+++ packages/crudhex/crudhex-0.9.0.tar.gz/crudhex-0.9.0/crudhex/adapters/application/cli/main.py
@@ -0,0 +1,100 @@
+from pathlib import Path
+from typing import Optional
+
+import typer
+from rich.console import Console
+from rich.theme import Theme
+from rich.progress import Progress
+
+from crudhex.domain.services import dsl_parser, config_context, db_adapter_generator, domain_generator, rest_generator
+
+
+_CONF_HELP = f'Project config file to know packages and code paths. defaults to {config_context.DEFAULT_CONFIG}'
+_SPEC_HELP = 'Spec file path to process'
+
+app = typer.Typer()
+out_console: Optional[Console] = None
+err_console: Optional[Console] = None
+
+
+def main():
+    _setup()
+    app()
+
+
+@app.command()
+def generate(
+        project_config: str = typer.Option(None, '--config', '-c', help=_CONF_HELP),
+        spec_file: str = typer.Argument(..., help=_SPEC_HELP)
+):
+    if not project_config:
+        out_console.print('Loading from default config path...', style='info')
+
+    config_context.load_config(Path(project_config) if project_config else None)
+
+    spec_path = Path(spec_file)
+    if not spec_path.exists():
+        err_console.print('Unable to find spec file: {}'.format(spec_path.resolve()))
+        raise typer.Exit(code=1)
+
+    out_console.print('Processing spec...', end='\n\n', style='notify')
+    with Progress(transient=True) as progress:
+        parse_task = progress.add_task('Parsing config', total=100)
+        entities = dsl_parser.parse_spec_file(spec_path)
+        entities_map = {e.name: e for e in entities}
+        progress.update(parse_task, advance=100)
+
+        generate_task = progress.add_task('Generate classes', total=100)
+        for entity in entities:
+            progress.console.rule(entity.name)
+            out_path = domain_generator.create_model_class(entity)
+            progress.console.print(f'Domain model: {out_path}', style='bright_blue')
+
+            out_path = domain_generator.create_command_class(entity, entities_map)
+            progress.console.print(f'Domain command: {out_path}', style='bright_blue')
+
+            out_path = domain_generator.create_db_port_class(entity)
+            progress.console.print(f'DB port: {out_path}', style='bright_blue')
+
+            out_path = domain_generator.create_use_case_port_class(entity)
+            progress.console.print(f'Use case port: {out_path}', style='bright_blue')
+
+            out_path = domain_generator.create_use_case_class(entity)
+            progress.console.print(f'Use case: {out_path}', style='bright_blue')
+
+            out_path = db_adapter_generator.create_entity_class(entity)
+            progress.console.print(f'Entity: {out_path}', style='bright_cyan')
+
+            out_path = db_adapter_generator.create_repository_class(entity)
+            progress.console.print(f'Repository: {out_path}', style='bright_cyan')
+
+            out_path = db_adapter_generator.create_adapter_class(entity, entities_map)
+            progress.console.print(f'DB adapter: {out_path}', style='bright_cyan')
+
+            out_path = rest_generator.create_model_class(entity)
+            progress.console.print(f'Controller: {out_path}', style='bright_yellow')
+
+            progress.update(generate_task, advance=100/len(entities))
+            progress.console.print('')
+
+        progress.update(generate_task, advance=100)
+
+    out_console.print('\n-- All classes generated --\n', style='finished')
+
+
+def _setup():
+    theme = Theme({
+        "notify": "cyan",
+        "info": "dim cyan",
+        "success": "green",
+        "finished": "black on green",
+        "warning": "yellow",
+        "error": "red",
+        "critical": "bold red"
+    })
+
+    global out_console
+    out_console = Console(theme=theme)
+
+    global err_console
+    err_console = Console(stderr=True, style="red")
---
+++ packages/crudhex/crudhex-0.9.0.tar.gz/crudhex-0.9.0/crudhex/domain/models/project_config.py
@@ -0,0 +1,124 @@
+from pathlib import Path
+from typing import Dict, Optional
+
+from crudhex.domain.utils.package_utils import pkg_to_path
+
+
+class ProjectConfig:
+    src: Optional[str]
+    _domain_src: Optional[str]
+    _db_adapter_src: Optional[str]
+    _rest_adapter_src: Optional[str]
+
+    domain_models_pkg: str
+    domain_commands_pkg: str
+    domain_in_ports_pkg: str
+    domain_out_ports_pkg: str
+
+    db_models_pkg: str
+    db_repositories_pkg: str
+    db_adapters_pkg: str
+    db_mapper_class: Optional[str]
+
+    rest_models_pkg: str
+    rest_controllers_pkg: str
+
+    def __init__(self):
+        self.src = None
+        self._domain_src = None
+        self._db_adapter_src = None
+        self._rest_adapter_src = None
+
+        self.domain_models_pkg = ''
+        self.domain_commands_pkg = ''
+        self.domain_in_ports_pkg = ''
+        self.domain_out_ports_pkg = ''
+        self.domain_use_cases_pkg = ''
+
+        self.db_models_pkg = ''
+        self.db_repositories_pkg = ''
+        self.db_adapters_pkg = ''
+        self.db_mapper_class = None
+
+        self.rest_models_pkg = ''
+        self.rest_controllers_pkg = ''
+
+    @classmethod
+    def from_dict(cls, data: Dict[str, str]) -> 'ProjectConfig':
+        project_config = ProjectConfig()
+
+        data = ProjectConfig._process_config(data)
+        project_config.__dict__.update(**data)
+
+        return project_config
+
+    def get_domain_models_path(self) -> Path:
+        return pkg_to_path(self.domain_models_pkg, self.domain_src)
+
+    def get_domain_commands_path(self) -> Path:
+        return pkg_to_path(self.domain_commands_pkg, self.domain_src)
+
+    def get_domain_in_ports_path(self) -> Path:
+        return pkg_to_path(self.domain_in_ports_pkg, self.domain_src)
+
+    def get_domain_out_ports_path(self) -> Path:
+        return pkg_to_path(self.domain_out_ports_pkg, self.domain_src)
+
+    def get_domain_use_cases_path(self) -> Path:
+        return pkg_to_path(self.domain_use_cases_pkg, self.domain_src)
+
+    def get_db_models_path(self) -> Path:
+        return pkg_to_path(self.db_models_pkg, self.db_adapter_src)
+
+    def get_db_repositories_path(self) -> Path:
+        return pkg_to_path(self.db_repositories_pkg, self.db_adapter_src)
+
+    def get_db_adapters_path(self) -> Path:
+        return pkg_to_path(self.db_adapters_pkg, self.db_adapter_src)
+
+    def get_rest_models_path(self) -> Path:
+        return pkg_to_path(self.rest_models_pkg, self.rest_adapter_src)
+
+    def get_rest_controllers_path(self) -> Path:
+        return pkg_to_path(self.rest_controllers_pkg, self.rest_adapter_src)
+
+    @property
+    def domain_src(self):
+        if self._domain_src is None:
+            return self.src
+
+        return self._domain_src
+
+    @domain_src.setter
+    def domain_src(self, src: str):
+        self._domain_src = src
+
+    @property
+    def db_adapter_src(self):
+        if self._db_adapter_src is None:
+            return self.src
+
+        return self._db_adapter_src
+
+    @db_adapter_src.setter
+    def db_adapter_src(self, src: str):
+        self._db_adapter_src = src
+
+    @property
+    def rest_adapter_src(self):
+        if self._rest_adapter_src is None:
+            return self.src
+
+        return self._rest_adapter_src
+
+    @rest_adapter_src.setter
+    def rest_adapter_src(self, src: str):
+        self._rest_adapter_src = src
+
+    @classmethod
+    def _process_config(cls, config_data: Dict[str, str]) -> Dict[str, str]:
+        p_config = {}
+        for key, val in config_data.items():
+            p_config[key.replace('-', '_')] = val
+
+        return p_config
