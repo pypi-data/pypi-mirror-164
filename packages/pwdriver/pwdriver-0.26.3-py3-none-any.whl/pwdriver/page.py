9cab571a208dbb4915ba07c5ce5aef35936a64c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:05 +0000

    Add package pwdriver/0.26.3/pwdriver-0.26.3-py3-none-any.whl

---
+++ packages/pwdriver/pwdriver-0.26.3-py3-none-any.whl/pwdriver/page.py
@@ -0,0 +1,42 @@
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class BaseElement(object):
+    def __init__(self, driver):
+        self.__driver = driver
+        self.__element = None
+
+    @property
+    def element(self) -> WebElement:
+        return self.__element
+
+    @element.setter
+    def element(self, locator) -> None:
+        self.__element = WebDriverWait(driver=self.__driver, timeout=3).until(
+            lambda d: d.find_element(*locator))
+
+
+class BasePage(object):
+    def __init__(self, driver):
+        self._driver = driver
+        self._target = BaseElement(driver)
+        self._url = None
+        self._locator = None
+
+    def _is_loaded(self) -> None:
+        assert self._url in self._driver.current_url
+
+    def _load(self) -> None:
+        self._driver.get(self._url)
+
+    def get(self) -> None:
+        try:
+            self._is_loaded()
+        except AssertionError:
+            self._load()
+        self._is_loaded()
+
+    def _by(self, key) -> WebElement:
+        self._target.element = self._locator[key]
+        return self._target.element
