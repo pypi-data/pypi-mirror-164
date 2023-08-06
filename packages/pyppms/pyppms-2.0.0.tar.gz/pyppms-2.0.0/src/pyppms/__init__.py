4a7dff7cce2497caafc7714a9442475e746b01b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:31 +0000

    Add package pyppms/2.0.0/pyppms-2.0.0.tar.gz

---
+++ packages/pyppms/pyppms-2.0.0.tar.gz/pyppms-2.0.0/src/pyppms/__init__.py
@@ -0,0 +1,10 @@
+"""The `pyppms` package provides a Python interface to Stratocore's PUMAPI.
+
+.. include:: ../../README.md
+
+.. include:: ../../TESTING.md
+"""
+
+__version__ = "2.0.0"
+
+from .ppms import PpmsConnection
