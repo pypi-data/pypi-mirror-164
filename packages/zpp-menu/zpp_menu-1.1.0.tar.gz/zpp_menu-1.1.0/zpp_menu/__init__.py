8f68a9ae75fb90f211f90eb2c156cec7d273a9c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:58 +0000

    Add package zpp-menu/1.1.0/zpp_menu-1.1.0.tar.gz

---
+++ packages/zpp-menu/zpp_menu-1.1.0.tar.gz/zpp_menu-1.1.0/zpp_menu/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '1.1.0'
+__author__ = 'ZephyrOff  (Alexandre Pajak)'
+__descriptor__ = 'Générateur de menu à touches fléchées'
+__lastupdate__ = '24/08/2022'
+__releasenote__ = 'Patch terminal_mode'
+
+from .menu import *
