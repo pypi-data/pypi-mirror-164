554024a5cffb6dd550968670f8e7d426edebe197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:01 +0000

    Add package slipcover/0.2.0/slipcover-0.2.0.tar.gz

---
+++ packages/slipcover/slipcover-0.2.0.tar.gz/slipcover-0.2.0/setup.py
@@ -0,0 +1,107 @@
+import setuptools
+from setuptools.command.build_ext import build_ext
+import sys
+from pathlib import Path
+
+def get_version():
+    import re
+    v = re.findall(r"\nVERSION *= *\"([^\"]+)\"", Path("slipcover/slipcover.py").read_text())[0]
+    return v
+
+VERSION = get_version()
+REPO_URL = "https://github.com/plasma-umass/slipcover"
+
+def get_description():
+#    from pathlib import Path
+    import re
+    readme_md = Path("README.md")
+    text = readme_md.read_text(encoding="utf-8")
+
+    # rewrite any relative paths to version-specific absolute paths
+    sub = r'\1' + REPO_URL + "/blob/v" + VERSION + r'/\2'
+    text = re.sub(r'(src=")((?!https?://))', sub, text)
+    text = re.sub(r'(\[.*?\]\()((?!https?://))', sub, text)
+
+    return text
+
+# If we're testing packaging, build using a ".devN" suffix in the version number,
+# so that we can upload new files (as testpypi/pypi don't allow re-uploading files with
+# the same name as previously uploaded).
+# Numbering scheme: https://www.python.org/dev/peps/pep-0440
+
+dev_build = '.dev' + Path('dev-build.txt').read_text().strip() if Path('dev-build.txt').exists() else ''
+
+def cxx_version(v):
+    return [f"-std={v}" if sys.platform != "win32" else f"/std:{v}"]
+
+def platform_compile_args():
+    if sys.platform == 'darwin':
+        return "-arch x86_64 -arch arm64 -arch arm64e".split()
+    if sys.platform == 'win32':
+        return ['/MT']  # avoids creating Visual Studio dependencies
+    return []
+
+def platform_link_args():
+    if sys.platform != 'win32':
+        return platform_compile_args() # clang/gcc is used
+    return []
+
+def limited_api_args():
+    # We would like to use METH_FASTCALL, but that's only available in the
+    # Python 3.10+ stable ABI, and we'd like to support Python 3.8+
+    #
+    # To re-enable, we also need setup.cfg with
+    #
+    # [bdist_wheel]
+    # py-limited-api=cp310
+    #
+#    return ['-DPy_LIMITED_API=0x030a0000']
+    return []
+
+class CppExtension(build_ext):
+    def build_extensions(self):
+        if sys.platform == "linux":
+            self.compiler.compiler_so[0] = "g++"
+            self.compiler.compiler_cxx[0] = "g++"
+            self.compiler.linker_so[0] = "g++"
+        build_ext.build_extensions(self)
+
+tracker = setuptools.extension.Extension(
+            'slipcover.tracker',
+            sources=['tracker.cxx'],
+            extra_compile_args=cxx_version('c++17') + platform_compile_args() + limited_api_args(),
+            extra_link_args=platform_link_args(),
+            py_limited_api=bool(limited_api_args()),
+            language='C++'
+)
+
+
+setuptools.setup(
+    name="slipcover",
+    version=VERSION + dev_build,
+    description="Near Zero-Overhead Python Code Coverage",
+    keywords="coverage testing",
+    long_description=get_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/plasma-umass/slipcover",
+    author="Juan Altmayer Pizzorno, Emery Berger",
+    author_email="juan@altmayer.com, emery@cs.umass.edu",
+    license="Apache License 2.0",
+    packages=['slipcover'],
+    ext_modules=([tracker]),
+    python_requires=">=3.8,<3.12",
+    install_requires=[
+        "tabulate"
+    ],
+    cmdclass={"build_ext": CppExtension},
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows :: Windows 10"
+    ]
+)
---
+++ packages/slipcover/slipcover-0.2.0.tar.gz/slipcover-0.2.0/tracker.cxx
@@ -0,0 +1,169 @@
+#define PY_SSIZE_T_CLEAN    // programmers love obscure statements
+#include <Python.h>
+#include <algorithm>
+#include "pyptr.h"
+
+
+/**
+ * Tracks code coverage.
+ */
+class Tracker {
+    PyPtr<> _sci;
+    PyPtr<> _filename;
+    PyPtr<> _lineno_or_branch;
+    bool _signalled;
+    bool _instrumented;
+    int _d_miss_count;
+    int _u_miss_count;
+    int _hit_count;
+    int _d_miss_threshold;
+
+public:
+    Tracker(PyObject* sci, PyObject* filename, PyObject* lineno_or_branch, PyObject* d_miss_threshold):
+        _sci(PyPtr<>::borrowed(sci)), _filename(PyPtr<>::borrowed(filename)),
+        _lineno_or_branch(PyPtr<>::borrowed(lineno_or_branch)),
+        _signalled(false), _instrumented(true),
+        _d_miss_count(-1), _u_miss_count(0), _hit_count(0),
+        _d_miss_threshold(PyLong_AsLong(d_miss_threshold)) {}
+
+
+    static PyObject*
+    newCapsule(Tracker* t) {
+        return PyCapsule_New(t, NULL,
+                             [](PyObject* cap) {
+                                 delete (Tracker*)PyCapsule_GetPointer(cap, NULL);
+                             });
+    }
+
+
+    PyObject* signal() {
+        if (!_signalled) {
+            _signalled = true;
+
+            PyPtr<> newly_seen = PyObject_GetAttrString(_sci, "newly_seen");
+            if (!newly_seen) {
+                PyErr_SetString(PyExc_Exception, "newly_seen missing");
+                return NULL;
+            }
+
+            PyPtr<> newly_seen_for_file = PyObject_GetItem(newly_seen, _filename);
+            if (!newly_seen_for_file) {
+                PyErr_SetString(PyExc_Exception, "newly_seen_for_file missing");
+                return NULL;
+            }
+
+            if (PySet_Add(newly_seen_for_file, _lineno_or_branch) < 0) {
+                PyErr_SetString(PyExc_Exception, "Unable to add to set");
+                return NULL;
+            }
+        }
+
+        if (_instrumented) {
+            // Limit D misses by deinstrumenting once we see several for a line
+            // Any other lines getting D misses get deinstrumented at the same time,
+            // so this needn't be a large threshold.
+            if (++_d_miss_count == _d_miss_threshold) {
+                PyPtr<> deinstrument_seen = PyUnicode_FromString("deinstrument_seen");
+                PyPtr<> result = PyObject_CallMethodObjArgs(_sci, deinstrument_seen, NULL);
+            }
+        }
+        else {
+            ++_u_miss_count;
+        }
+
+        Py_RETURN_NONE;
+    }
+
+
+    PyObject* hit() {
+        ++_hit_count;
+        Py_RETURN_NONE;
+    }
+
+
+    PyObject* deinstrument() {
+        _instrumented = false;
+        Py_RETURN_NONE;
+    }
+
+
+    PyObject* get_stats() {
+        PyPtr<> d_miss_count = PyLong_FromLong(std::max(_d_miss_count, 0));
+        PyPtr<> u_miss_count = PyLong_FromLong(_u_miss_count);
+        PyPtr<> total_count = PyLong_FromLong(1 + _d_miss_count + _u_miss_count + _hit_count);
+        return PyTuple_Pack(5, (PyObject*)_filename, (PyObject*)_lineno_or_branch,
+                            (PyObject*)d_miss_count, (PyObject*)u_miss_count,
+                            (PyObject*)total_count);
+    }
+
+    PyObject* is_instrumented() {
+        if (_instrumented) {
+            Py_RETURN_TRUE;
+        }
+        Py_RETURN_FALSE;
+    }
+};
+
+
+PyObject*
+tracker_register(PyObject* self, PyObject* const* args, Py_ssize_t nargs) {
+    if (nargs < 4) {
+        PyErr_SetString(PyExc_Exception, "Missing argument(s)");
+        return NULL;
+    }
+
+    return Tracker::newCapsule(new Tracker(args[0], args[1], args[2], args[3]));
+}
+
+#define METHOD_WRAPPER(method) \
+    static PyObject*\
+    tracker_##method(PyObject* self, PyObject* const* args, Py_ssize_t nargs) {\
+        if (nargs < 1) {\
+            PyErr_SetString(PyExc_Exception, "Missing argument");\
+            return NULL;\
+        }\
+    \
+        return static_cast<Tracker*>(PyCapsule_GetPointer(args[0], NULL))->method();\
+    }
+
+METHOD_WRAPPER(signal);
+METHOD_WRAPPER(hit);
+METHOD_WRAPPER(deinstrument);
+METHOD_WRAPPER(get_stats);
+METHOD_WRAPPER(is_instrumented);
+
+
+static PyMethodDef methods[] = {
+    {"register",     (PyCFunction)tracker_register, METH_FASTCALL, "registers a new tracker"},
+    {"signal",       (PyCFunction)tracker_signal, METH_FASTCALL, "signals the line was reached"},
+    {"hit",          (PyCFunction)tracker_hit, METH_FASTCALL, "signals the line was reached after full deinstrumentation"},
+    {"deinstrument", (PyCFunction)tracker_deinstrument, METH_FASTCALL, "marks a tracker deinstrumented"},
+    {"get_stats",    (PyCFunction)tracker_get_stats, METH_FASTCALL, "returns tracker stats"},
+    {"is_instrumented", (PyCFunction)tracker_is_instrumented, METH_FASTCALL, "returns whether tracker is instrumented"},
+    {NULL, NULL, 0, NULL}
+};
+
+
+static struct PyModuleDef tracker_module = {
+    PyModuleDef_HEAD_INIT,
+    "tracker",
+    NULL, // no documentation
+    -1,
+    methods,
+    NULL,
+    NULL,
+    NULL,
+    NULL
+};
+
+
+PyMODINIT_FUNC
+PyInit_tracker() {
+    PyObject* m = PyModule_Create(&tracker_module);
+    if (m == nullptr) {
+        return nullptr;
+    }
+
+    return m;
+}
+
