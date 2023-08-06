a1acf688a992f27d63a99da4e7a77d27348907d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:45 +0000

    Add package greenplum-python-fork/1.0.0b1/greenplum-python-fork-1.0.0b1.tar.gz

---
+++ packages/greenplum-python-fork/greenplum-python-fork-1.0.0b1.tar.gz/greenplum-python-fork-1.0.0b1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+setuptools.setup(
+    name="greenplum-python-fork",
+    version="1.0.0-beta1",
+    install_requires=requirements,
+    author="Greenplum Python",
+    author_email="greenplum-python@vmware.com",
+    description="Python interface for Greenplum",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/greenplum-db/GreenplumPython",
+    packages=setuptools.find_packages(include=["greenplumpython", "greenplumpython.*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
+
