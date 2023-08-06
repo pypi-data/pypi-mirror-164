c7c9d33cc912990cd5d6079370757cf019721678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:35 +0000

    Add package alert360/0.0.8/alert360-0.0.8.tar.gz

---
+++ packages/alert360/alert360-0.0.8.tar.gz/alert360-0.0.8/alert360/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'alert360.apps.Alert360Config'
---
+++ packages/alert360/alert360-0.0.8.tar.gz/alert360-0.0.8/alert360/apps.py
@@ -0,0 +1,24 @@
+from django.apps import AppConfig
+import threading
+import os
+
+
+class Alert360Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'alert360'
+    verbose_name = 'The ultimate observability solution'
+
+    def ready(self):
+        # WARNING: This way the Thread wouldn't run if
+        # Statereloader is off
+        print("Alert360 is ready!")
+        if os.environ.get('RUN_MAIN') == 'true':
+            from .models import StateWatcher
+            threading.Thread(
+                name="StateWatcherLoop",
+                target=StateWatcher.start,
+                daemon=True
+            ).start()
+            print("Alert360 Thread started")
+        else:
+            print("Processes dismissed")
