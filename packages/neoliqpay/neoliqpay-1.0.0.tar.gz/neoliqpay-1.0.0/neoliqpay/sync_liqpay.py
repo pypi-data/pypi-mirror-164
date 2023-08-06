6638b9af23325433959d8a0b1a080efc975bb756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:11 +0000

    Add package neoliqpay/1.0.0/neoliqpay-1.0.0.tar.gz

---
+++ packages/neoliqpay/neoliqpay-1.0.0.tar.gz/neoliqpay-1.0.0/neoliqpay/sync_liqpay.py
@@ -0,0 +1,35 @@
+import json
+from typing import Optional
+from urllib.parse import urljoin
+
+try:
+    import requests
+except ImportError:
+    requests = None
+
+from .core import LiqPayBase
+
+
+class LiqPay(LiqPayBase):
+    def __init__(self, *args, **kwargs):
+        if not requests:
+            raise NotImplementedError('requests module not found')
+
+        super(LiqPay, self).__init__(*args, **kwargs)
+
+    def api(
+        self, 
+        url: str,
+        params: Optional[dict] = None
+    ):
+        params = self._prepare_params(params)
+
+        json_encoded_params = json.dumps(params)
+        private_key = self._private_key
+        signature = self._make_signature(private_key, json_encoded_params, private_key)
+
+        request_url = urljoin(self._host, url)
+        request_data = {"data": json_encoded_params, "signature": signature}
+        with requests.sessions.Session() as session:
+            with session.post(request_url, data=request_data) as response:
+                return response.json()
---
+++ packages/neoliqpay/neoliqpay-1.0.0.tar.gz/neoliqpay-1.0.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='neoliqpay',
+    version='2.0.0',
+    description='Asyncio LiqPay Python3 SDK',
+    packages=['neoliqpay'],
+    author_email='mistickusya.2012@gmail.com'
+)
