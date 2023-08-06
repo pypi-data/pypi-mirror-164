3d2ffe99c7eae29bbbd0b44c6ae2020926f77ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:37 +0000

    Add package herripdf2/2.0/herripdf2-2.0-py3-none-any.whl

---
+++ packages/herripdf2/herripdf2-2.0-py3-none-any.whl/herripdf2/pdf2text.py
@@ -0,0 +1,17 @@
+""" This module provides functions to convert a PDF to text. """
+
+
+class Converter:
+    """ A simple converter for converting PDFs to text. """
+
+    def convert(self, path):
+        """
+        Convert the given path to text.
+
+        Parameters:
+        path (str): The path to a pdf file.
+
+        Returns:
+        str: The content of the PDF file as text.
+        """
+        print("pdf2test")
