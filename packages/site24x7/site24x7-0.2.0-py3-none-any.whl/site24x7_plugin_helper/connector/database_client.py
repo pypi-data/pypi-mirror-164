14f1be393f02879fe5b3a36df187e64b18f9594d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:30 +0000

    Add package site24x7/0.2.0/site24x7-0.2.0-py3-none-any.whl

---
+++ packages/site24x7/site24x7-0.2.0-py3-none-any.whl/site24x7_plugin_helper/connector/database_client.py
@@ -0,0 +1,55 @@
+from abc import abstractmethod
+import pyodbc
+from .models.model import ClientModel
+
+class DatabaseClient(ClientModel):
+    def __init__(self, server, database, username, password, driverLocation, *args, **kwargs):
+        self.server=server
+        self.database=database
+        self.username=username
+        self.password=password
+        self.driverLocation=driverLocation
+        if self.databaseType().lower() == "mssql":
+            self.cnxn = pyodbc.connect(
+                "DRIVER="+str(driverLocation)+";"+
+                "SERVER="+str(server)+";"+
+                "DATABASE="+str(database)+";"+
+                "UID="+str(username)+";"+
+                "PWD="+str(password)+";"
+            , timeout=5)
+            self.cursor = self.cnxn.cursor()
+        else:
+            self.cursor=self.getCursorFromDatabase(
+                self.server,
+                self.database,
+                self.username,
+                self.password,
+                self.driverLocation
+            )
+        super().__init__(*args, **kwargs)
+
+    def getCursor(self):
+        return self.cursor
+
+    @abstractmethod
+    def databaseType(self):
+        return "MSSQL"
+
+    def getCursorFromDatabase(self,
+        server:str,
+        database:str,
+        username:str,
+        password:str,
+        driverLocation:str
+    ):
+        raise NotImplementedError("getCursorFromDatabase is not implemented")
+
+    def fetchall(self, query:str):
+        self.cursor.execute(query)
+        rowAll = self.cursor.fetchall()
+        return rowAll
+
+    def fetchone(self, query:str):
+        self.cursor.execute(query)
+        row = self.cursor.fetchone()
+        return row
