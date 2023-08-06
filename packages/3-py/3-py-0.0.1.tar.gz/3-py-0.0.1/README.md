b5434106a6ac2318f1d4a969b25b84bf6a8e13aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:08 +0000

    Add package 3-py/0.0.1/3-py-0.0.1.tar.gz

---
+++ packages/3-py/3-py-0.0.1.tar.gz/3-py-0.0.1/README.md
@@ -0,0 +1,17 @@
+Tree tool written in Python.
+
+Example:
+
+```
+3
+```
+
+Output:
+
+```
+.gitignore
+3
+LICENSE.md
+README.md
+setup.py
+```
---
+++ packages/3-py/3-py-0.0.1.tar.gz/3-py-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+    name='3-py',
+    version='0.0.1',
+    author='Vladimir Chebotarev',
+    description='Tree tool written in Python',
+    packages=[],
+    scripts=['3'],
+    install_requires=[]
+)
