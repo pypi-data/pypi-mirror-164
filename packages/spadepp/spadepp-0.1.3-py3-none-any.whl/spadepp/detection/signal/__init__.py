9136c81391670948628abf0758b14ae0e204950a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:08 +0000

    Add package spadepp/0.1.3/spadepp-0.1.3-py3-none-any.whl

---
+++ packages/spadepp/spadepp-0.1.3-py3-none-any.whl/spadepp/detection/signal/__init__.py
@@ -0,0 +1,3 @@
+from .format import CharFormatSignal, WordFormatSignal, PunctFormatSignal, CharValueSignal, MinCharFrequencySignal
+from .value import FastTextSignal, DictTypoSignal, WebTableBoolSignal, WebTableSignal
+from .missing_value import MissingValueSignal
