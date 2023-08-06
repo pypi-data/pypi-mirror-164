6859cbf48305403f3aee70a16d46aea6595a6b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:39 +0000

    Add package querier/0.4.1/querier-0.4.1.tar.gz

---
+++ packages/querier/querier-0.4.1.tar.gz/querier-0.4.1/querier/queries/drop.py
@@ -0,0 +1,29 @@
+# Authors: Thierry Moudiki
+#
+# License: BSD 3
+
+
+from ..utils import memoize
+
+# creates a copy
+@memoize
+def drop(df, req=None):
+    """ Drop columns.
+       
+    Args: 
+       
+        req: str
+            comma-separated list of columns to be dropped
+                     
+    Examples:     
+       
+        https://github.com/thierrymoudiki/querier/tree/master/querier/demo
+       
+    """
+
+    try:
+        return df.drop(req.replace(" ", "").split(","), axis=1)
+    except:
+        raise ValueError(
+            "request must contain df" "s column names (comma-separated)"
+        )
