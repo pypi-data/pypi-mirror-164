5dbf736f51b2172d9acd3e48efaabee21a1aca20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:24 +0000

    Add package bing-hashing-image-downloader/22.8.22.1623/bing_hashing_image_downloader-22.8.22.1623.tar.gz

---
+++ packages/bing-hashing-image-downloader/bing_hashing_image_downloader-22.8.22.1623.tar.gz/bing_hashing_image_downloader-22.8.22.1623/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bing_hashing_image_downloader",
+    version="22.8.22.1623",
+    author="Jeremiah Korreck (fork), Guru Prasad Singh (original)",
+    author_email="korreckj328@gmail.com",
+    description="Python library to download bulk images from Bing.com without repeated images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/korreckj328/bing_hashing_image_downloader",
+    keywords=['bing', 'images', 'scraping', 'image download', 'bulk image downloader','sha1'],
+    packages=['bing_hashing_image_downloader'],
+    classifiers=[
+	"Programming Language :: Python :: 3",
+	"License :: OSI Approved :: MIT License",
+	"Operating System :: OS Independent",
+        ]
+)
