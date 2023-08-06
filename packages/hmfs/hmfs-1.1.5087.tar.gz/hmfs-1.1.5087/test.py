aa538e18c5ac6ed1c5516abdd016fa9d5e50dd3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:49 +0000

    Add package hmfs/1.1.5087/hmfs-1.1.5087.tar.gz

---
+++ packages/hmfs/hmfs-1.1.5087.tar.gz/hmfs-1.1.5087/test.py
@@ -0,0 +1,33 @@
+# from hamunafs.utils.redisutil import XRedisAsync
+
+# import asyncio
+
+# redis = XRedisAsync('cache.ai.hamuna.club', '1987yang', 6379)
+
+# asyncio.run(redis.zadd('1', '2', 3))
+import asyncio
+from hamunafs.backends.bk_wy import WYSF
+from hamunafs.backends.bk_minio import MinioBackend
+
+async def main():
+    wysf_client = WYSF({
+        'key': '46b5e0508a9641a485c6d3f62078e7ab',
+        'secret': 'fd187963fb8943febc2624e1241081ed',
+        'domain': 'hamuna-images.nos-eastchina1.126.net',
+        'default_bucket': 'hamuna-images'
+    })
+
+    # ret, e = wysf_client.put('/home/superpigy/文档/hamuna-backend/html/index.html', 'tmp', 'hi.html')
+    # print(ret)
+
+    minio_client = MinioBackend({
+        'key': 'hmcz',
+        'secret': 'hmcz1234',
+        'domain': 'stream.ai.hamuna.club:9001',
+        'default_bucket': 'tmps'
+    })
+    ret, e = wysf_client.put('/home/superpigy/文档/hamuna-backend/html/index.html', 'tmp', 'hi.html')
+    print(ret)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
