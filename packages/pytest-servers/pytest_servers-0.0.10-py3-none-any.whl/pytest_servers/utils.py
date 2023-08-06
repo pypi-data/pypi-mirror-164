029222f12eaf710006cd8f1d72cc68573f3f7549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:31 +0000

    Add package pytest-servers/0.0.10/pytest_servers-0.0.10-py3-none-any.whl

---
+++ packages/pytest-servers/pytest_servers-0.0.10-py3-none-any.whl/pytest_servers/utils.py
@@ -0,0 +1,59 @@
+import logging
+import random
+import socket
+import string
+import time
+
+import pytest
+
+logger = logging.getLogger(__name__)
+
+
+def wait_until(pred, timeout: float, pause: float = 0.1):
+    start = time.perf_counter()
+    while (time.perf_counter() - start) < timeout:
+        value = pred()
+        if value:
+            return value
+        time.sleep(pause)
+    raise TimeoutError("timed out waiting")
+
+
+def random_string(n: int = 6):
+    return "".join(random.choices(string.ascii_lowercase, k=n))
+
+
+@pytest.fixture(scope="session")
+def monkeypatch_session():
+    """Session-scoped monkeypatch"""
+    from _pytest.monkeypatch import MonkeyPatch
+
+    m = MonkeyPatch()
+    yield m
+    m.undo()
+
+
+@pytest.fixture(scope="session")
+def docker_client():
+    """Run docker commands using the python API"""
+    import docker
+
+    client = docker.from_env()
+
+    yield client
+
+    client.close()
+
+
+def get_free_port() -> None:
+    retries = 3
+    while retries >= 0:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.listen()
+                free_port = s.getsockname()[1]
+            return free_port
+        except OSError as exc:
+            retries -= 1
+            if retries <= 0:
+                raise SystemError("Could not get a free port") from exc
