ed84e7f83257d0855ce021fa932e5a24b3996df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:14 +0000

    Add package laniakea-utils/0.0.1/laniakea-utils-0.0.1.tar.gz

---
+++ packages/laniakea-utils/laniakea-utils-0.0.1.tar.gz/laniakea-utils-0.0.1/setup.py
@@ -0,0 +1,30 @@
+'''
+laniakea-utils
+Utilities for Laniakea applications
+'''
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name='laniakea-utils',
+  version="0.0.1",
+  author="Marco Antonio Tangaro",
+  description='Utilities for Laniakea applications',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url='https://github.com/Laniakea-elixir-it/laniakea-utils',
+  project_urls={
+    "Bug Tracker": "https://github.com/Laniakea-elixir-it/laniakea-utils/issues",
+  },
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: POSIX :: Linux",
+  ],
+  package_dir={"": "src"},
+  packages=setuptools.find_packages(where="src"),
+  python_requires=">=3.6",
+  data_files=[('etc/laniakea',['config/laniakea-utils.ini'])],
+)
