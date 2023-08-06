7eb206bc498e97a7ae3f6893303bf96d3d587afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:04 +0000

    Add package hrthy.core/1.1.25/hrthy.core-1.1.25-py3-none-any.whl

---
+++ packages/hrthy.core/hrthy.core-1.1.25-py3-none-any.whl/hrthy_core/events/events/user_event.py
@@ -0,0 +1,102 @@
+from hrthy_core.events.events.base_event import BaseEvent, CompanyBasePayload
+from hrthy_core.security.security import Requester
+
+
+class UserCreatedPayload(CompanyBasePayload):
+    first_name: str = None
+    last_name: str = None
+    email: str
+    active: bool
+
+
+class UserInvitedPayload(UserCreatedPayload):
+    invitation_id: str
+
+
+class UserInvitedResendPayload(UserInvitedPayload):
+    pass
+
+
+class UserRestoredPayload(UserCreatedPayload):
+    invitation_id: str = None
+
+
+class UserAcceptedInvitationPayload(UserCreatedPayload):
+    pass
+
+
+class UserLoggedInPayload(CompanyBasePayload):
+    session_duration: int
+    pass
+
+
+class UserLoggedOutPayload(CompanyBasePayload):
+    pass
+
+
+class UserLoginRefreshedPayload(CompanyBasePayload):
+    session_duration: int
+    pass
+
+
+class UserForgotPasswordPayload(CompanyBasePayload):
+    pass
+
+
+class UserUpdatedPayload(UserCreatedPayload):
+    pass
+
+
+class UserDeletedPayload(CompanyBasePayload):
+    pass
+
+
+class UserAcceptedInvitationEvent(BaseEvent):
+    type = 'UserAcceptedInvitationEvent'
+    payload: UserAcceptedInvitationPayload
+
+
+class UserLoggedInEvent(BaseEvent):
+    type = 'UserLoggedInEvent'
+    payload: UserLoggedInPayload
+
+
+class UserLoggedOutEvent(BaseEvent):
+    type = 'UserLoggedOutEvent'
+    payload: UserLoggedOutPayload
+
+
+class UserLoginRefreshedEvent(BaseEvent):
+    type = 'UserLoginRefreshedEvent'
+    payload: UserLoginRefreshedPayload
+
+
+class UserForgotPasswordEvent(BaseEvent):
+    type = 'UserForgotPasswordEvent'
+    payload: UserForgotPasswordPayload
+
+
+class UserInvitedEvent(BaseEvent):
+    requester: Requester = None
+    type = 'UserInvitedEvent'
+    payload: UserInvitedPayload
+
+
+class UserInvitedResendEvent(BaseEvent):
+    type = 'UserInvitedResendEvent'
+    payload: UserInvitedResendPayload
+
+
+class UserUpdatedEvent(BaseEvent):
+    type = 'UserUpdatedEvent'
+    payload: UserUpdatedPayload
+
+
+class UserDeletedEvent(BaseEvent):
+    type = 'UserDeletedEvent'
+    payload: UserDeletedPayload
+
+
+class UserRestoredEvent(BaseEvent):
+    type = 'UserRestoredEvent'
+    payload: UserRestoredPayload
