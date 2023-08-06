e98a8a8b0ffd52c0fd6ba3ba7ca47d2322edf7d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:59 +0000

    Add package open-dev/0.1.2/open_dev-0.1.2.tar.gz

---
+++ packages/open-dev/open_dev-0.1.2.tar.gz/open_dev-0.1.2/tests/app/test_entities.py
@@ -0,0 +1,24 @@
+"""Test entities."""
+import pytest
+
+from open_dev.src.open_dev_repo import OpenDevRepo, RepoStatus
+
+
+@pytest.mark.skip()
+def test_inits_repo():
+    """Creates the repo."""
+    OpenDevRepo()
+
+
+@pytest.mark.skip()
+def test_gets_repo():
+    """Repo object retrieves the current head."""
+    repo = OpenDevRepo()
+    assert isinstance(repo.current_head, str)
+
+
+@pytest.mark.skip()
+def test_status():
+    """Repo object retrieves the current head."""
+    repo = OpenDevRepo()
+    assert isinstance(repo.status, RepoStatus)
