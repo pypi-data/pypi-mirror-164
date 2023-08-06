a2248b9b672f61de68c7ab4ea25b452fb060686a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:18 +0000

    Add package neural-pipeline-search/0.5.0/neural-pipeline-search-0.5.0.tar.gz

---
+++ packages/neural-pipeline-search/neural-pipeline-search-0.5.0.tar.gz/neural-pipeline-search-0.5.0/neps/optimizers/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from typing import Callable
+
+from .base_optimizer import BaseOptimizer
+from .bayesian_optimization.cost_cooling import CostCooling
+from .bayesian_optimization.multi_fidelity import BayesianOptimizationMultiFidelity
+from .bayesian_optimization.optimizer import BayesianOptimization
+from .grid_search.optimizer import GridSearch
+from .random_search.optimizer import RandomSearch
+from .regularized_evolution.optimizer import RegularizedEvolution
+
+SearcherMapping: dict[str, Callable] = {
+    "bayesian_optimization": BayesianOptimization,
+    "mf_bayesian_optimization": BayesianOptimizationMultiFidelity,
+    "cost_cooling_bayesian_optimization": CostCooling,
+    "random_search": RandomSearch,
+    "cost_cooling": CostCooling,
+    "regularized_evolution": RegularizedEvolution,
+    "grid_search": GridSearch,
+}
