bdc087fe9f59ff7af0102b80b603de21f6b9c071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:56 +0000

    Add package easydata/0.2.0/easydata-0.2.0.tar.gz

---
+++ packages/easydata/easydata-0.2.0.tar.gz/easydata-0.2.0/easydata/queries/__init__.py
@@ -0,0 +1,19 @@
+from easydata.queries.jp import JMESPathSearch, JMESPathStrictSearch
+from easydata.queries.key import KeySearch, KeyStrictSearch
+from easydata.queries.pq import PyQuerySearch, PyQueryStrictSearch
+from easydata.queries.qs import Queries
+from easydata.queries.re import ReSearch, ReStrictSearch
+
+jp = JMESPathSearch
+jp_strict = JMESPathStrictSearch
+
+key = KeySearch
+key_strict = KeyStrictSearch
+
+pq = PyQuerySearch
+pq_strict = PyQueryStrictSearch
+
+qs = Queries
+
+re = ReSearch
+re_strict = ReStrictSearch
