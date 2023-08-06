9da085905372357ff5cbd4dd3ae34e7dd5f08be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:36 +0000

    Add package wthtest/0.1.1.3/wthtest-0.1.1.3.tar.gz

---
+++ packages/wthtest/wthtest-0.1.1.3.tar.gz/wthtest-0.1.1.3/wthtest/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding = utf-8 -*-
+# @Time : 2022-08-01 14:59
+# @Author : 王铁翰
+# @File : __init__.py.py
+# @Software : PyCharm
+
+from . import testmodule1
---
+++ packages/wthtest/wthtest-0.1.1.3.tar.gz/wthtest-0.1.1.3/wthtest/testmodule1.py
@@ -0,0 +1,7 @@
+# -*- coding = utf-8 -*-
+# @Time : 2022-08-01 16:12
+# @Author : 王铁翰
+# @File : test-module1.py
+# @Software : PyCharm
+module = 'testmodule1'
+print('wthtest module='+module)
