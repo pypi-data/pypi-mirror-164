7c96c302f55a14821e127bd182f2579fe0223384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:18 +0000

    Add package internal-with-fin/0.1.6/internal_with_fin-0.1.6-py3-none-any.whl

---
+++ packages/internal-with-fin/internal_with_fin-0.1.6-py3-none-any.whl/sparknlp_jsl/extensions/finance/__init__.py
@@ -0,0 +1,4 @@
+from sparknlp_jsl.extensions.finance.chunk_classification import *
+from sparknlp_jsl.extensions.finance.graph import *
+from sparknlp_jsl.extensions.finance.token_classification import *
+from sparknlp_jsl.extensions.finance.sequence_classification import *
