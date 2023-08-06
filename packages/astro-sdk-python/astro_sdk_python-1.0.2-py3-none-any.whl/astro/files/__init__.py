26ca87aa434ba62620de34d005b8989bf7726f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:04 +0000

    Add package astro-sdk-python/1.0.2/astro_sdk_python-1.0.2-py3-none-any.whl

---
+++ packages/astro-sdk-python/astro_sdk_python-1.0.2-py3-none-any.whl/astro/files/__init__.py
@@ -0,0 +1,16 @@
+from airflow.hooks.base import BaseHook
+
+from astro.files.base import File  # noqa: F401 # skipcq: PY-W2000
+from astro.files.base import resolve_file_path_pattern  # noqa: F401 # skipcq: PY-W2000
+
+
+def check_if_connection_exists(conn_id: str) -> bool:
+    """
+    Given an Airflow connection ID, identify if it exists.
+    Return True if it does or raise an AirflowNotFoundException exception if it does not.
+
+    :param conn_id: Airflow connection ID
+    :return bool: If the connection exists, return True
+    """
+    BaseHook.get_connection(conn_id)
+    return True
