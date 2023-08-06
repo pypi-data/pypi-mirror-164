e51378d63e8b3c6ae56cc08cc77333972687f08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:07 +0000

    Add package rookieutils/2.0.1/rookieutils-2.0.1.tar.gz

---
+++ packages/rookieutils/rookieutils-2.0.1.tar.gz/rookieutils-2.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='rookieutils',
+    version='2.0.1',
+    py_modules=['rookieutils'],
+    author='qinexpire',
+    author_email='qinexpire@gmail.com',
+    url='https://blog.csdn.net/andrew_wf',
+    description='universal function'
+)
