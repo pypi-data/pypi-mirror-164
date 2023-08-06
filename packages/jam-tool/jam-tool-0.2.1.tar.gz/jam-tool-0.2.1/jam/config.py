2fb76e21c03f6f3b9632c6eee0f2e045340bb89f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:15 +0000

    Add package jam-tool/0.2.1/jam-tool-0.2.1.tar.gz

---
+++ packages/jam-tool/jam-tool-0.2.1.tar.gz/jam-tool-0.2.1/jam/config.py
@@ -0,0 +1,45 @@
+from collections import namedtuple
+
+from .constant import LOG_LEVEL_MAP, YAML_EXT, JSON_EXT
+from .log import log
+
+JamConfig = namedtuple(
+    "JamConfig",
+    ["loglvl", "input_paths", "output_path", "output_format"],
+)
+
+
+def get_loglvl(verbosity):
+    loglvl = LOG_LEVEL_MAP.get(verbosity, None)
+    if loglvl is None:
+        log.warn(f"Invalid verbosity level {verbosity}. Defaulting to debug.")
+        return "debug"
+    return loglvl.upper()
+
+def get_output_format(args):
+    output_format = "json"
+    # Explicit flags takes precedence
+    if args.json:
+        return 'json'
+    if args.yaml:
+        return 'yaml'
+    # if no flags, base it on the output suffix
+    if args.output.suffix in JSON_EXT:
+        return 'json'
+    if args.output.suffix in YAML_EXT:
+        return 'yaml'
+    # If outputting to stdout use the first input file
+    if args.input[0].suffix in JSON_EXT:
+        return 'json'
+    if args.input[0].suffix in YAML_EXT:
+        return 'yaml'
+    # Default to json
+    return 'json'
+
+def load_config(args):
+    return JamConfig(
+        loglvl=get_loglvl(args.verbose),
+        input_paths=args.input,
+        output_path=args.output,
+        output_format=get_output_format(args),
+    )
---
+++ packages/jam-tool/jam-tool-0.2.1.tar.gz/jam-tool-0.2.1/jam/entry.py
@@ -0,0 +1,109 @@
+import argparse
+import configparser
+import logging
+import os
+import pathlib
+import sys
+from functools import partial
+
+from . import __version__ as jam_version
+from .config import load_config
+from .constant import MAX_LOG_LEVEL, STDIN, STDOUT
+from .error import JamError
+from .log import log
+from .merge import recurse_update
+from .reference import resolve_refs
+from .util import has_yaml, path_type, safe_dump_json, safe_dump_yaml, safe_read
+
+path_type_w_stdin = partial(path_type, allow_stdin=True)
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        prog=pathlib.Path(sys.argv[0]).name,
+        description="A cli for merging JSON and YAML files.",
+    )
+
+    parser.add_argument(
+        "--version", action="store_true", help="Print program version information."
+    )
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Increase log verbosity. Can be passed multiple times.",
+    )
+
+    parser.add_argument(
+        "-j",
+        "--json",
+        action="store_true",
+        help="Force output in json format",
+    )
+
+    parser.add_argument(
+        "-y",
+        "--yaml",
+        action="store_true",
+        help="Force output in yaml format",
+    )
+
+    parser.add_argument(
+        'input',
+        nargs="+",
+        type=path_type_w_stdin,
+        default=[],
+        help="Read input from one or more paths.",
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output",
+        action="store",
+        default='-',
+        type=path_type_w_stdin,
+        help="Write merged output to this path.",
+    )
+
+    args = parser.parse_args()
+
+    if args.version:
+        print(f"jam v{jam_version}")
+        sys.exit(1)
+
+    if args.verbose > MAX_LOG_LEVEL:
+        args.verbose = MAX_LOG_LEVEL
+
+    if not args.input:
+        log.error("Must specify at least one input or pass --stdin.")
+        return parser.parse_args(["--help"])
+
+    return args
+
+def cli():
+    args = get_args()
+    config = load_config(args)
+    log.setLevel(config.loglvl)
+
+    if config.output_format == "yaml" and not has_yaml:
+        log.error("Output format set to yaml but PyYaml not installed.")
+        sys.exit(1)
+
+    merged = None
+    for path in config.input_paths:
+        log.info(f"Merging document: {path.name}")
+        doc = safe_read(path)
+        if doc is None:
+            log.error(f"Failed to read input file {path}")
+            sys.exit(1)
+        try:
+            merged = recurse_update(merged, resolve_refs(path, doc))
+        except JamError as e:
+            log.exception("Unhandled Error")
+            sys.exit(1)
+
+    if config.output_format == "yaml":
+        safe_dump_yaml(output_file, merged)
+    else:
+        safe_dump_json(output_file, merged)
