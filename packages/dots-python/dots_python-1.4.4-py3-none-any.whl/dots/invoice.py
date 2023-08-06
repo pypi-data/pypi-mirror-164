bc41196713aeaab447468eb45aa15e45e6a77696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:35 +0000

    Add package dots-python/1.4.4/dots_python-1.4.4-py3-none-any.whl

---
+++ packages/dots-python/dots_python-1.4.4-py3-none-any.whl/dots/invoice.py
@@ -0,0 +1,58 @@
+import dots
+from dots import token
+
+
+class Invoice():
+
+    @classmethod
+    def create(cls, amount, expires_in=None, items=None, breakdown=None, requested_information=None, metadata=None):
+
+        json = {
+            'amount': amount,
+        }
+
+        if expires_in is not None:
+            json['expires_in'] = expires_in
+
+        if items is not None:
+            json['items'] = items
+
+        if breakdown is not None:
+            json['breakdown'] = breakdown
+
+        if requested_information is not None:
+            json['requested_information'] = requested_information
+
+        if metadata is not None:
+            json['metadata'] = metadata
+
+        headers = {
+            'Authorization': 'Basic ' + token.get_auth_token()
+        }
+
+        response = dots._session.post(dots.api_base + '/invoice/create', json=json, headers=headers)
+        response.raise_for_status()
+
+        data = response.json()
+
+        if data['success']:
+            return data['invoice']
+        else:
+            response.raise_for_status()
+
+    @classmethod
+    def get(cls, invoice_id):
+
+        headers = {
+            'Authorization': 'Basic ' + token.get_auth_token()
+        }
+
+        response = dots._session.get(dots.api_base + '/invoice/get/' + invoice_id, headers=headers)
+        response.raise_for_status()
+
+        data = response.json()
+
+        if data['success']:
+            return data['invoice']
+        else:
+            response.raise_for_status()
---
+++ packages/dots-python/dots_python-1.4.4-py3-none-any.whl/dots/user.py
@@ -0,0 +1,169 @@
+import dots
+from dots import token
+
+
+class User():
+
+    @classmethod
+    def create(cls, email, country_code, phone_number, first_name, last_name, username=None):
+
+        json = {
+            'email': email,
+            'country_code': country_code,
+            'phone_number': phone_number,
+            'first_name': first_name,
+            'last_name': last_name,
+        }
+
+        if username is not None:
+            json['username'] = username
+
+        headers = {
+            'Authorization': 'Basic ' + token.get_auth_token()
+        }
+
+        response = dots._session.post(
+            dots.api_base + '/users/create', json=json, headers=headers)
+        response.raise_for_status()
+
+        data = response.json()
+
+        if (data['success']):
+            return data['verification_id']
+        else:
+            response.raise_for_status()
+
+    @classmethod
+    def get(cls, user_id):
+
+        headers = {
+            'Authorization': 'Basic ' + token.get_auth_token()
+        }
+
+        response = dots._session.get(
+            dots.api_base + '/users/get/' + user_id, headers=headers)
+        response.raise_for_status()
+
+        data = response.json()
+
+        if data['success']:
+            return data['user']
+        else:
+            response.raise_for_status()
+
+    @classmethod
+    def send_verification_token(cls, verification_id):
+
+        headers = {
+            'Authorization': 'Basic ' + token.get_auth_token()
+        }
+
+        response = dots._session.post(
+            dots.api_base + '/users/send_verification_token', json={'verification_id': verification_id}, headers=headers)
+        response.raise_for_status()
+
+        data = response.json()
+
+        if data['success']:
+            return True
+        else:
+            print(data.message)
+            response.raise_for_status()
+
+    @classmethod
+    def verify_user(cls, verification_id, verification_token):
+
+        headers = {
+            'Authorization': 'Basic ' + token.get_auth_token()
+        }
+
+        response = dots._session.post(
+            dots.api_base + '/users/verify_user', json={'verification_id': verification_id, 'verification_token': verification_token}, headers=headers)
+        response.raise_for_status()
+
+        data = response.json()
+
+        if data['success']:
+            return data['user']['id']
+        else:
+            return False
+
+    @classmethod
+    def get_wallet(cls, user_id):
+
+        headers = {
+            'Authorization': 'Basic ' + token.get_auth_token()
+        }
+
+        response = dots._session.get(
+            dots.api_base + '/users/wallet/get/' + user_id, headers=headers)
+        response.raise_for_status()
+
+        data = response.json()
+
+        if data['success']:
+            return data['wallet']
+        else:
+            response.raise_for_status()
+
+    @classmethod
+    def generate_refill_link(cls, user_id):
+
+        headers = {
+            'Authorization': 'Basic ' + token.get_auth_token()
+        }
+
+        response = dots._session.post(
+            dots.api_base + '/users/wallet/refill', json={'user_id': user_id}, headers=headers)
+        response.raise_for_status()
+
+        data = response.json()
+
+        if data['success'] == True:
+            return data['link']
+        elif data['success'] == False:
+            raise Exception(data.message)
+        else:
+            response.raise_for_status()
+
+    @classmethod
+    def generate_payout_link(cls, user_id):
+
+        headers = {
+            'Authorization': 'Basic ' + token.get_auth_token()
+        }
+
+        response = dots._session.post(
+            dots.api_base + '/users/wallet/payout', json={'user_id': user_id}, headers=headers)
+        response.raise_for_status()
+
+        data = response.json()
+
+        if data['success'] == True:
+            return data['link']
+        elif data['success'] == False:
+            raise Exception(data.message)
+        else:
+            response.raise_for_status()
+
+    @classmethod
+    def get_transactions(cls, user_id, page=None):
+
+        headers = {
+            'Authorization': 'Basic ' + token.get_auth_token()
+        }
+
+        query = ''
+        if page is not None:
+            query = '?page=' + str(page)
+
+        response = dots._session.get(
+            dots.api_base + '/transactions/get/user/' + user_id + query, headers=headers)
+        response.raise_for_status()
+
+        data = response.json()
+
+        if data['success']:
+            return data['transactions']
+        else:
+            response.raise_for_status()
