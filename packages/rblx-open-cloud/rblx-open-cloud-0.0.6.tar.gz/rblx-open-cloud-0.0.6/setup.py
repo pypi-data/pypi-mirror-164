a81db589ccaf7fde32350211c1e8a0cd5b836d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:23 +0000

    Add package rblx-open-cloud/0.0.6/rblx-open-cloud-0.0.6.tar.gz

---
+++ packages/rblx-open-cloud/rblx-open-cloud-0.0.6.tar.gz/rblx-open-cloud-0.0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name='rblx-open-cloud',
+    version='0.0.6',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    author="TreeBen77",
+    packages=find_packages('src'),
+    url='https://github.com/TreeBen77/rblx-open-cloud',
+    keywords='roblox, datastores, opencloud',
+    install_requires=[
+        'python-dateutil',
+        'requests'
+    ]
+)
