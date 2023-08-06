98fc238798bb33b76b1c046fbb62fb67f312084b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:27 +0000

    Add package local-ssh-config/0.4.4/local_ssh_config-0.4.4-py3-none-any.whl

---
+++ packages/local-ssh-config/local_ssh_config-0.4.4-py3-none-any.whl/local_ssh_config/ssh/_constants.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+SSH_DIR = Path.home() / ".ssh/"
+SSH_CONFIG_DIR = Path.home() / ".ssh/config.d/"
+SSH_CONFIG_FILE = Path.home() / ".ssh/config"
+
+SSH_CONFIG_INCLUDE_DIRECTIVE = "Include config.d/*"
+
+WINDOWS_MULTIPASS_DEFAULT_ID_RSA = (
+    "C:/Windows/ProgramData/Multipass/data/ssh-keys/id_rsa"
+)
