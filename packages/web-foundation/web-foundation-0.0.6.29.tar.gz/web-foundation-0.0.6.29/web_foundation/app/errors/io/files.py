350a0eff0dbb95ba88b96e6a11514dcbaa82079a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:13 +0000

    Add package web-foundation/0.0.6.29/web-foundation-0.0.6.29.tar.gz

---
+++ packages/web-foundation/web-foundation-0.0.6.29.tar.gz/web-foundation-0.0.6.29/web_foundation/app/errors/io/files.py
@@ -0,0 +1,17 @@
+from web_foundation.app.errors.application.application import ApplicationError
+
+
+class SectionNotFound(ApplicationError):
+    pass
+
+
+class FileNotExist(ApplicationError):
+    pass
+
+
+class NothingToWrite(ApplicationError):
+    pass
+
+
+class OsIOError(ApplicationError):
+    pass
