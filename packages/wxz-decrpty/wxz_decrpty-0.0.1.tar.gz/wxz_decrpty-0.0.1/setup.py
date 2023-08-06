8eca4df76b384a76663abe39852bb756121915c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:01 +0000

    Add package wxz-decrpty/0.0.1/wxz_decrpty-0.0.1.tar.gz

---
+++ packages/wxz-decrpty/wxz_decrpty-0.0.1.tar.gz/wxz_decrpty-0.0.1/setup.py
@@ -0,0 +1,13 @@
+"""发布pypi包"""
+from setuptools import setup
+
+setup(
+    name="wxz_decrpty",
+    version="0.0.1",
+    author="wujiantao",
+    author_email="wujiantao@yunke.ai",
+    description=u"微小助",
+    packages=["wxz_decrpty"],
+    install_requires=[],
+    entry_points={},
+)
