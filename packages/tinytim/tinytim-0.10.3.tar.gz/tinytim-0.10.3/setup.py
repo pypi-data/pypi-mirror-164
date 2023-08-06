c364d5dd101e4ff90c880efc8978de9a23d9dbfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:04 +0000

    Add package tinytim/0.10.3/tinytim-0.10.3.tar.gz

---
+++ packages/tinytim/tinytim-0.10.3.tar.gz/tinytim-0.10.3/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="tinytim",
+    version="0.10.3",
+    description="Pure Python data table functions.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eddiethedean/tinytim",
+    author="Odos Matthews",
+    author_email="odosmatthews@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    python_requires='>=3.6',
+    install_requires=[]
+)
