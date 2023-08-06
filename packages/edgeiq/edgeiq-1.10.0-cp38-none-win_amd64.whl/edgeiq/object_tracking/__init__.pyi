cf82cf2f67b5a30a4392a9979824a27fea3a0d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:38 +0000

    Add package edgeiq/1.10.0/edgeiq-1.10.0-cp38-none-win_amd64.whl

---
+++ packages/edgeiq/edgeiq-1.10.0-cp38-none-win_amd64.whl/edgeiq/object_tracking/__init__.pyi
@@ -0,0 +1,4 @@
+from .centroid import CentroidTracker as CentroidTracker
+from .correlation import CorrelationTracker as CorrelationTracker
+from .kalman import KalmanTracker as KalmanTracker
+from .object_tracking import RESULT_TYPE as RESULT_TYPE, TrackablePrediction as TrackablePrediction, TrackerAlgorithm as TrackerAlgorithm, TrackingResults as TrackingResults, match_greedy as match_greedy, match_optimal as match_optimal
