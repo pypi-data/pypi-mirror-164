0281d4f435a3eee2ed93db3b4cfb5bf793381c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:37 +0000

    Add package mtxcli/0.0.85/mtxcli-0.0.85-py3-none-any.whl

---
+++ packages/mtxcli/mtxcli-0.0.85-py3-none-any.whl/mtxp/services/__init__.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import threading
+import logging
+from .clash import ClashService
+from .tor import TorService
+from .pf import PfService
+from .nginx import NginxService
+from .phpfpm import PhpFpmService
+from .wordpress import WordPressService
+logger = logging.getLogger(__name__)
+
+def start_all_services():
+    logger.info(f"start_all_services() {os.getpid()}")
+    ClashService().start()
+    TorService().start()
+    PfService().start()
+    NginxService().start()
+    PhpFpmService().start()
+    WordPressService().start()
