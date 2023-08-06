a199c0285911db5fd8efbde574950bc814ba0c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:59:53 +0000

    Add package asciirt/0.0.2/asciirt-0.0.2.tar.gz

---
+++ packages/asciirt/asciirt-0.0.2.tar.gz/asciirt-0.0.2/asciirt/__init__.py
@@ -0,0 +1,6 @@
+from asciirt import macintosh
+from asciirt import babymogus
+from asciirt import amogus
+from asciirt import pacman
+from asciirt import mouse
+from asciirt import atari
---
+++ packages/asciirt/asciirt-0.0.2.tar.gz/asciirt-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Ascii Art easier than ever.'
+
+# Setting up
+setup(
+    name="asciirt",
+    version=VERSION,
+    author="Arxify (Daniel Rodriguez)",
+    author_email="<arxifybusiness@proton.me>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['colorama'],
+    keywords=['python', 'ascii', 'art', 'colours', 'colors', 'text', 'unicode'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
