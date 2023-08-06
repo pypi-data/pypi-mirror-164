57f546a04d0fdf6b6789100ecb76c35d24a43f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:56 +0000

    Add package chargebee/2.18.0/chargebee-2.18.0.tar.gz

---
+++ packages/chargebee/chargebee-2.18.0.tar.gz/chargebee-2.18.0/chargebee/models/purchase.py
@@ -0,0 +1,21 @@
+import json
+from chargebee.model import Model
+from chargebee import request
+from chargebee import APIError
+
+class Purchase(Model):
+
+    fields = ["id", "customer_id", "created_at", "modified_at", "subscription_ids", "invoice_ids"]
+
+
+    @staticmethod
+    def create(params, env=None, headers=None):
+        return request.send('post', request.uri_path("purchases"), params, env, headers)
+
+    @staticmethod
+    def retrieve(id, env=None, headers=None):
+        return request.send('get', request.uri_path("purchases",id), None, env, headers)
+
+    @staticmethod
+    def estimate(params, env=None, headers=None):
+        return request.send('post', request.uri_path("purchases","estimate"), params, env, headers)
