49775ae8ae96ca30300bf3f05c98d3637b80b91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:23 +0000

    Add package fxxkpy/2.1.1/fxxkpy-2.1.1-py2.py3-none-any.whl

---
+++ packages/fxxkpy/fxxkpy-2.1.1-py2.py3-none-any.whl/fxxkpy/__init__.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+from .start import *
+from .math import *
+from .vector import *
+from .remove import *
+from .languages import C as c
+from .languages import Cpp as cpp
+from .languages import Java as java
+from .error import *
+from .wallpaper import Dynamic as DynamicWallpaper
+from .game import *
