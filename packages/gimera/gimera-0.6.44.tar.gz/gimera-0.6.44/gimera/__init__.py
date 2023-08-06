d0b6f78af1aa5d83464a2dba2dfa8b5c8ff7f707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:23 +0000

    Add package gimera/0.6.44/gimera-0.6.44.tar.gz

---
+++ packages/gimera/gimera-0.6.44.tar.gz/gimera-0.6.44/gimera/__init__.py
@@ -0,0 +1,12 @@
+import click
+
+from . import gimera
+from . import repo
+from . import gitcommands
+from . import tools
+try:
+    from . import tests
+except ModuleNotFoundError:
+    pass
+except ImportError:
+    pass
