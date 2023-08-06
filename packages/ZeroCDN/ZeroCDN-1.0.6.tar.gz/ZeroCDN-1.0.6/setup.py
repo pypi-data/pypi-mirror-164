fcbbc110975468ea1f73fa90f6f3983e6608e22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:01 +0000

    Add package ZeroCDN/1.0.6/ZeroCDN-1.0.6.tar.gz

---
+++ packages/ZeroCDN/ZeroCDN-1.0.6.tar.gz/ZeroCDN-1.0.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('README.md', encoding='UTF-8') as readme:
+    long_description = readme.read()
+with open('requirements.txt', encoding='UTF-8') as requirements:
+    install_requires = requirements.read()
+setup(
+    name='ZeroCDN',
+    version='1.0.6',
+    description='ZeroCDN.com API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='dimnissv',
+    python_requires='>=3.8',
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Typing :: Typed',
+    ],
+    install_requires=install_requires,
+    packages=['zerocdn']
+)
