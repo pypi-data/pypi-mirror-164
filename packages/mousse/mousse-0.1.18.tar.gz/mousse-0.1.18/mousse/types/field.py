a70178d4720b64efe8c6c44a9e0658c02f99bd1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:40 +0000

    Add package mousse/0.1.18/mousse-0.1.18.tar.gz

---
+++ packages/mousse/mousse-0.1.18.tar.gz/mousse-0.1.18/mousse/types/field.py
@@ -0,0 +1,31 @@
+from functools import lru_cache
+from typing import *
+
+
+__all__ = ["Field", "get_fields_info"]
+
+
+class Field:
+    def __init__(
+        self,
+        default: Any = Ellipsis,
+        alias: str = None,
+        freeze: bool = False,
+        exclude: bool = None,
+        # validator: Callable = None,
+    ) -> None:
+        self.default = default
+        self.alias = alias
+        self.freeze = freeze
+        self.annotation = None
+        self.exclude = exclude
+        # self.validator = validator
+
+
+def get_fields_info(cls: Any) -> Dict[str, Field]:
+    return _get_fields_info(cls)
+
+
+@lru_cache(typed=True)
+def _get_fields_info(cls: Any) -> Dict[str, Field]:
+    return {}
