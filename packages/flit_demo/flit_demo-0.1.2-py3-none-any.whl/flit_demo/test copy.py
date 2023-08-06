4deb9fac0b11a72da9b0ab5d611ed229d668f628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:47 +0000

    Add package flit_demo/0.1.2/flit_demo-0.1.2-py3-none-any.whl

---
+++ packages/flit_demo/flit_demo-0.1.2-py3-none-any.whl/flit_demo/test copy.py
@@ -0,0 +1,19 @@
+def multiply_2_numbers(a, b):
+    """Multiply two numbers
+
+    This function only returns the multiplication of two input numbers, which
+    can be either `int` or `float` types.
+
+    Parameters
+    ----------
+    a : int or float
+        First number
+    b : int or float
+        Second number
+
+    Returns
+    -------
+    int or float
+        `a` times `b`
+    """
+    return a * b
