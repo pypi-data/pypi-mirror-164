3ca4958e6ec65d6711822ba8f63dd2671128f4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:24 +0000

    Add package voicevox-py/0.0.2/voicevox.py-0.0.2.tar.gz

---
+++ packages/voicevox-py/voicevox.py-0.0.2.tar.gz/voicevox.py-0.0.2/setup.py
@@ -0,0 +1,69 @@
+# Author: Kenta Nakamura <c60evaporator@gmail.com>
+# Copyright (c) 2020-2021 Kenta Nakamura
+# License: BSD 3 clause
+
+from setuptools import setup
+import voicevox
+
+DESCRIPTION = "Python上から簡単にVORCEVOXの音声合成を利用できるモジュール。"
+NAME = 'voicevox.py'
+AUTHOR = 'sonyakun'
+AUTHOR_EMAIL = 'sonyaumg@gmail.com'
+URL = 'https://github.com/sonyakun/voicevox.py'
+LICENSE = 'MIT'
+VERSION = "0.0.2"
+PYTHON_REQUIRES = ">=3.6"
+
+INSTALL_REQUIRES = [
+    'asyncio',
+    'requests',
+    'urllib3',
+]
+
+EXTRAS_REQUIRE = {
+    'tutorial': [
+        'mlxtend>=0.18.0',
+        'xgboost>=1.4.2',
+    ]
+}
+
+PACKAGES = [
+    'voicevox'
+]
+
+CLASSIFIERS = [
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3 :: Only',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Visualization',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Multimedia :: Graphics',
+    'Framework :: Matplotlib',
+]
+
+
+readme = "README.md"
+long_description = readme
+
+setup(name=NAME,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      maintainer=AUTHOR,
+      maintainer_email=AUTHOR_EMAIL,
+      description=DESCRIPTION,
+      long_description=long_description,
+      license=LICENSE,
+      url=URL,
+      version=VERSION,
+      python_requires=PYTHON_REQUIRES,
+      install_requires=INSTALL_REQUIRES,
+      extras_require=EXTRAS_REQUIRE,
+      packages=PACKAGES,
+      classifiers=CLASSIFIERS
+    )
