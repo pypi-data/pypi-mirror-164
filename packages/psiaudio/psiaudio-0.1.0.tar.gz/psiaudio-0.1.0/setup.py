53f315020881971b7ebc3362c6ada59e9ee66023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:08 +0000

    Add package psiaudio/0.1.0/psiaudio-0.1.0.tar.gz

---
+++ packages/psiaudio/psiaudio-0.1.0.tar.gz/psiaudio-0.1.0/setup.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from setuptools import find_packages, setup
+
+
+requirements = [
+    'numpy',
+    'scipy',
+    'pandas',
+    'matplotlib',
+]
+
+
+extras_require = {
+    'docs': ['sphinx', 'sphinx_rtd_theme', 'pygments-enaml'],
+    'test': ['pytest', 'pytest-benchmark'],
+}
+
+
+# Get version number
+version_file = Path(__file__).parent / 'psiaudio' / '__init__.py'
+for line in version_file.open():
+    if line.strip().startswith('__version__'):
+        version = line.split('=')[1].strip().strip('\'')
+        break
+else:
+    raise RuntimeError('Could not determine version')
+
+
+setup(
+    name='psiaudio',
+    author='psiaudio development team',
+    install_requires=requirements,
+    extras_require=extras_require,
+    packages=find_packages(),
+    include_package_data=True,
+    license='LICENSE.txt',
+    description='Audio tools supporting psiexperiment',
+    entry_points={
+        'console_scripts': [],
+    },
+    version=version,
+)
