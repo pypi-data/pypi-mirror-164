00482b042aed8b59b8a7ed91766bb41be795a264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:40 +0000

    Add package bunnycdn/0.0.5/bunnycdn-0.0.5.tar.gz

---
+++ packages/bunnycdn/bunnycdn-0.0.5.tar.gz/bunnycdn-0.0.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bunnycdn']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.28.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'bunnycdn',
+    'version': '0.0.5',
+    'description': 'An Unofficial SDK for BunnyCDN',
+    'long_description': None,
+    'author': 'Dan Sikes',
+    'author_email': 'dansikes7@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3,<4',
+}
+
+
+setup(**setup_kwargs)
