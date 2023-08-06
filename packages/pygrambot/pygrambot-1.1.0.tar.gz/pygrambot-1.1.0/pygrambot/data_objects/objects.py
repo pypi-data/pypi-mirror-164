3fe8581ccc5cec62ed21e49bdf74b581b8c74910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:06 +0000

    Add package pygrambot/1.1.0/pygrambot-1.1.0.tar.gz

---
+++ packages/pygrambot/pygrambot-1.1.0.tar.gz/pygrambot-1.1.0/pygrambot/data_objects/objects.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+
+
+class Message:
+    pass
+    #date
+    #text
+    #message_id
+    #id
+    #is_bot
+    #first_name
+    #username
+    #language_cod
+
+
+class Chat:
+    pass
+    #id
+    #first_name
+    #username
+    #type
+
+
+class UpdateDt:
+    """
+    One message.
+    """
+
+    update_id: int = None
+    message: Message = None
+    chat: Chat = None
+    data: dict = {}
+
+    def new_message(self):
+        self.message = Message()
+
+    def new_chat(self):
+        self.chat = Chat()
+
+
+@dataclass
+class BotCommandDt:
+    command: str
+    description: str
+
+
+@dataclass
+class CatchMultipleMessageDt:
+    user_id: str
+    messages = []
