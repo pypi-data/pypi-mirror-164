87328d49b6c343ba4962e7dc8f17eca99b09aabc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:59:40 +0000

    Add package kilroy-module-server-py-sdk/0.7.1/kilroy-module-server-py-sdk-0.7.1.tar.gz

---
+++ packages/kilroy-module-server-py-sdk/kilroy-module-server-py-sdk-0.7.1.tar.gz/kilroy-module-server-py-sdk-0.7.1/src/kilroy_module_server_py_sdk/module.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+from typing import Any, AsyncIterable, Dict, Generic, List, Set, Tuple, TypeVar
+from uuid import UUID
+
+from kilroy_module_py_shared import Metadata
+from kilroy_server_py_utils import Configurable, classproperty
+from kilroy_server_py_utils.schema import JSONSchema
+
+from kilroy_module_server_py_sdk.metrics import Metric
+
+StateType = TypeVar("StateType")
+
+
+class Module(Configurable[StateType], Generic[StateType], ABC):
+    @classproperty
+    @abstractmethod
+    def metadata(cls) -> Metadata:
+        pass
+
+    @classproperty
+    @abstractmethod
+    def post_schema(cls) -> JSONSchema:
+        pass
+
+    @abstractmethod
+    async def get_metrics(self) -> Set[Metric]:
+        pass
+
+    @abstractmethod
+    def generate(self, n: int) -> AsyncIterable[Tuple[UUID, Dict[str, Any]]]:
+        pass
+
+    @abstractmethod
+    async def fit_posts(self, posts: AsyncIterable[Dict[str, Any]]) -> None:
+        pass
+
+    @abstractmethod
+    async def fit_scores(self, scores: List[Tuple[UUID, float]]) -> None:
+        pass
+
+    @abstractmethod
+    async def step(self) -> None:
+        pass
