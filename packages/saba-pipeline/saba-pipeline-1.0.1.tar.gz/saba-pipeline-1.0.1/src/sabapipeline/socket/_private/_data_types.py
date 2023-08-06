1f074b41375761307fc64d289612b2af68f7352e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:31 +0000

    Add package saba-pipeline/1.0.1/saba-pipeline-1.0.1.tar.gz

---
+++ packages/saba-pipeline/saba-pipeline-1.0.1.tar.gz/saba-pipeline-1.0.1/src/sabapipeline/socket/_private/_data_types.py
@@ -0,0 +1,16 @@
+from ... import Server
+
+
+class SocketConfig:
+    def __init__(self,
+                 server: Server,
+                 max_reconnect_timeout: float = 5,
+                 initial_reconnect_timeout: float = 0.1,
+                 **kwargs):
+        """
+        :kwargs args for https://websockets.readthedocs.io/en/7.0/api.html#websockets.client.connect
+        """
+        self.server = server
+        self.max_reconnect_timeout = max_reconnect_timeout
+        self.initial_reconnect_timeout = initial_reconnect_timeout
+        self.connection_kwargs = kwargs
