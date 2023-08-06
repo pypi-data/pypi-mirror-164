151f723b92fa8a091ed474cecbec3729f5f318da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:19 +0000

    Add package libraryArsein/3.7.1/libraryArsein-3.7.1.tar.gz

---
+++ packages/libraryArsein/libraryArsein-3.7.1.tar.gz/libraryArsein-3.7.1/libraryArsein/Cliob.py
@@ -0,0 +1,8 @@
+#Copyright (C) 2022 ArianBOT
+
+version= "3.7.1"
+copyright = " Arsein Copyright (C) 2022 arianabasi Team ArianBOT \n\n"
+Arian = "ArianBOT"
+
+class chup:
+	pchap = print(f"Arsein library version {version}\n{copyright}\n☞ library Arsein: https://github.com/Arseinlibrary/Arsein__library.git\n\nدر حال فعال شدن کمی صیور باشید...\n")
