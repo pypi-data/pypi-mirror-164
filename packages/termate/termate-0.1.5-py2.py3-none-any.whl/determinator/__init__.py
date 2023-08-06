dc7f0d2d874ccc74d581f221bf8e97fc340639d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:45 +0000

    Add package termate/0.1.5/termate-0.1.5-py2.py3-none-any.whl

---
+++ packages/termate/termate-0.1.5-py2.py3-none-any.whl/determinator/__init__.py
@@ -0,0 +1,13 @@
+"""Top-level package for terminator."""
+
+__author__ = """Willem Jan Willemse"""
+__email__ = "w.j.willemse@dnb.nl"
+__version__ = "0.1.2"
+
+from .determinator import *
+from .importskos import *
+from .convert2skos import *
+from .convert2rdf import *
+from .utils import *
+from .vocab import *
+from .types import *
