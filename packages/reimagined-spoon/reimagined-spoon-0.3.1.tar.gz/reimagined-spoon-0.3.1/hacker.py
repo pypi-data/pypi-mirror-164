6b8897c372438a90eefa76dcb21c767468008565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:39 +0000

    Add package reimagined-spoon/0.3.1/reimagined-spoon-0.3.1.tar.gz

---
+++ packages/reimagined-spoon/reimagined-spoon-0.3.1.tar.gz/reimagined-spoon-0.3.1/hacker.py
@@ -0,0 +1,14 @@
+class Hacker(object):
+    """This will help you to hack anything!"""
+
+    def __init__(self, hacker_name, target):
+        super(Hacker, self).__init__()
+        self.hacker_name = hacker_name
+        self.target = target
+
+    def lets_hack(self):
+        print(self.hacker_name, " is the ", self.target, " hacker")
+
+
+h = Hacker('Jalkhov', 'Pentagon')
+h.lets_hack()
