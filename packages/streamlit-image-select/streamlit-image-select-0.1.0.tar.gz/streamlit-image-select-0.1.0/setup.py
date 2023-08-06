6ec54c8c5dc6406d867f42aa13859426ba32bdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:31 +0000

    Add package streamlit-image-select/0.1.0/streamlit-image-select-0.1.0.tar.gz

---
+++ packages/streamlit-image-select/streamlit-image-select-0.1.0.tar.gz/streamlit-image-select-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['streamlit_image_select']
+
+package_data = \
+{'': ['*'],
+ 'streamlit_image_select': ['frontend/*',
+                            'frontend/public/*',
+                            'frontend/src/*']}
+
+install_requires = \
+['streamlit>=1.12.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'streamlit-image-select',
+    'version': '0.1.0',
+    'description': '🖼️ An image select component for Streamlit',
+    'long_description': None,
+    'author': 'Johannes Rieke',
+    'author_email': 'johannes.rieke@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
