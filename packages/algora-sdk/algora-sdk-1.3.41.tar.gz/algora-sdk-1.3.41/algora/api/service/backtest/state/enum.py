de6c28fb00f4a5f4abab9caff223d4352babaedc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:55 +0000

    Add package algora-sdk/1.3.41/algora-sdk-1.3.41.tar.gz

---
+++ packages/algora-sdk/algora-sdk-1.3.41.tar.gz/algora-sdk-1.3.41/algora/api/service/backtest/state/enum.py
@@ -0,0 +1,6 @@
+from algora.common.base import BaseEnum
+
+
+class BacktestEventType(BaseEnum):
+    PORTFOLIO = 'PORTFOLIO'
+    CASH_PAYMENT = 'CASH_PAYMENT'
---
+++ packages/algora-sdk/algora-sdk-1.3.41.tar.gz/algora-sdk-1.3.41/algora/api/service/data_engine/enum.py
@@ -0,0 +1,8 @@
+from algora.common.base import BaseEnum
+
+
+class FieldFill(BaseEnum):
+    NULL = 'NULL'
+    ZERO = 'ZERO'
+    PREVIOUS = 'PREVIOUS'
+    NEXT = 'NEXT'
