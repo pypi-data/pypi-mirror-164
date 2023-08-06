631e315faa1fb3130ba37c5a735920b056ad0e5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:09 +0000

    Add package aioaquarea/0.1.0/aioaquarea-0.1.0.tar.gz

---
+++ packages/aioaquarea/aioaquarea-0.1.0.tar.gz/aioaquarea-0.1.0/aioaquarea/const.py
@@ -0,0 +1,8 @@
+"""Constants definition"""
+
+AQUAREA_SERVICE_BASE = "https://aquarea-smart.panasonic.com/"
+AQUAREA_SERVICE_LOGIN = "remote/v1/api/auth/login"
+AQUAREA_SERVICE_DEVICES = "remote/v1/api/devices"
+AQUAREA_SERVICE_CONTRACT = "remote/contract"
+
+PANASONIC = "Panasonic"
