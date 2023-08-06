5d46120ab1db7e07ae7f97736c4fb2dc00e325a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:33 +0000

    Add package igeBullet/0.5.0/igeBullet-0.5.0.tar.gz

---
+++ packages/igeBullet/igeBullet-0.5.0.tar.gz/igeBullet-0.5.0/igeBullet.h
@@ -0,0 +1,10 @@
+#include <Python.h>
+
+#include "pyBulletUtils.h"
+#include "pyConstraint.h"
+#include "pyRigidBody.h"
+#include "pyShape.h"
+#include "pySoftbody.h"
+#include "pyVehicle.h"
+#include "pyWorld.h"
+using namespace ige::bullet;
---
+++ packages/igeBullet/igeBullet-0.5.0.tar.gz/igeBullet-0.5.0/igeBullet_doc_en.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+#include <Python.h>
+
+PyDoc_STRVAR(igeBullet_doc,
+			 "Python Bullet extension for Indi Game Engine\n");
