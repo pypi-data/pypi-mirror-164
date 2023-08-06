eda9db60a7cffbcf5ea2a92e3c6979e32a1c5f59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:09 +0000

    Add package libreria-uno-prueba/0.2/libreria_uno_prueba-0.2.tar.gz

---
+++ packages/libreria-uno-prueba/libreria_uno_prueba-0.2.tar.gz/libreria_uno_prueba-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_namespace_packages, setup
+
+setup(
+name='libreria_uno_prueba',
+version='0.2',
+packages=find_namespace_packages(),
+py_modules=['libreria_uno_prueba']
+)
