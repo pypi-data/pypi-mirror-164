e666885c8346fa298008d97593b040fa09893baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:28 +0000

    Add package connector-def-runner/1.2.0/connector_def_runner-1.2.0.tar.gz

---
+++ packages/connector-def-runner/connector_def_runner-1.2.0.tar.gz/connector_def_runner-1.2.0/connector_definition_runner/authenticate.py
@@ -0,0 +1,60 @@
+import curlify
+class Authenticate:
+    def __init__(self, session, asset, asset_name, security):
+        self.session = session
+        self.security = security
+        self.asset = asset
+        self.asset_name = asset_name
+        self.params = {}
+        self.curl = []
+
+        self.auth_handler()
+
+    def auth_handler(self):
+        # The asset name is the name of the function which was set by the get_asset_name function.
+        auth = getattr(self, self.asset_name)
+        auth()
+
+    def http_basic(self):
+        self.session.auth = (self.asset['username'], self.asset['password'])
+        return self.session
+
+    def http_bearer(self):
+        self.session.headers.update({
+            'Authorization': f'Bearer {self.asset["token"]}'
+        })
+        return self.session
+
+    def apikey(self):
+        if self.security['in'] == 'header':
+            self.session.headers.update({
+                self.security['name']: self.asset[self.security['name']]
+            })
+        elif self.security['in'] == 'cookie':
+            self.session.cookies.update({
+                self.security['name']: self.asset[self.security['name']]
+            })
+        elif self.security['in'] == 'query':
+            self.params.update({
+                self.security['name']: self.asset[self.security['name']]
+            })
+        return self.session
+
+    def oauth2_client_credentials(self):
+        scopes = self.asset.get('scopes')
+        if scopes:
+            scopes = scopes.join(' ')
+        data = {
+            'client_id': self.asset['client_id'],
+            'client_secret': self.asset['client_secret'],
+            'scope': scopes,
+            'grant_type': 'client_credentials'
+        }
+
+        token_url = self.asset['token_url']
+        response = self.session.request("POST", token_url, data=data)
+        access_token = response.json()['access_token']
+        self.curl.append(curlify.to_curl(response.request))
+
+        self.session.headers.update({"Authorization": "Bearer {}".format(access_token)})
+        return self.session
