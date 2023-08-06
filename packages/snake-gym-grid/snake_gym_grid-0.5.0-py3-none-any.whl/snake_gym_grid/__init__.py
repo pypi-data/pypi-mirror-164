bcba6ec24fef8424b084ec49a5f6cc90d412a15d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:38 +0000

    Add package snake-gym-grid/0.5.0/snake_gym_grid-0.5.0-py3-none-any.whl

---
+++ packages/snake-gym-grid/snake_gym_grid-0.5.0-py3-none-any.whl/snake_gym_grid/__init__.py
@@ -0,0 +1,22 @@
+from gym.envs.registration import register
+
+register(
+    id="snake-gym-grid-10x20-tiny-v0",
+    entry_point="snake_gym_grid.envs.snake_gym_grid:SnakeGymGrid10x20Tiny",
+    max_episode_steps=1000000,
+    reward_threshold=198
+)
+
+register(
+    id="snake-gym-grid-10x20-normal-v0",
+    entry_point="snake_gym_grid.envs.snake_gym_grid:SnakeGymGrid10x20Normal",
+    max_episode_steps=1000000,
+    reward_threshold=198
+)
+
+register(
+    id="snake-gym-grid-10x20-1d-v0",
+    entry_point="snake_gym_grid.envs.snake_gym_grid:SnakeGymGrid1D",
+    max_episode_steps=1000000,
+    reward_threshold=198
+)
