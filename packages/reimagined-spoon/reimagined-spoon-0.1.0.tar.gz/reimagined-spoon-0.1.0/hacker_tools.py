8c1edd3d08ea6f030ef92103cc6a595135f15cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:06 +0000

    Add package reimagined-spoon/0.1.0/reimagined-spoon-0.1.0.tar.gz

---
+++ packages/reimagined-spoon/reimagined-spoon-0.1.0.tar.gz/reimagined-spoon-0.1.0/hacker_tools.py
@@ -0,0 +1,14 @@
+def get_ip(host):
+    return host.ip_address()
+
+
+def get_emails():
+    return 'all users emails'
+
+
+def get_gmail_password():
+    return 'email password hacked!'
+
+
+def sabotage():
+    return 'can sabotage your friends internet connection'
