55da2799195fbd9961f9f9bbe94df92ad645f7f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:27 +0000

    Add package keys-school/0.1/keys_school-0.1.tar.gz

---
+++ packages/keys-school/keys_school-0.1.tar.gz/keys_school-0.1/src/keys/keynames.py
@@ -0,0 +1,142 @@
+all_keys = {
+    '\x0e': 'shift in',
+    '\x0f': 'shift out',
+
+    '\x1b': 'esc',
+    '\x1b[H': 'home',
+    '\x1b[F': 'end',
+    '\x1b[5': 'page_up',
+    '\x1b[6': 'page_down',
+    '\n': 'enter',
+    '\r': 'enter',
+    '\x7f': 'backspace',
+    '\x08': 'backspace',
+    '\x1b[2~': 'insert',
+    '\x1b[3~': 'delete',
+    '\x0b': 'tab',
+    '\t': 'tab',
+
+    ' ': 'space',          # 0x20
+    '!': 'exclamation',    # 0x21
+    '"': 'double quote',   # 0x22
+    '#': 'hash',           # 0x23
+    '$': 'dollar',         # 0x24
+    '%': 'percent',        # 0x25
+    '&': 'and',            # 0x26
+   #'\'': 'single quote',  # 0x27
+    '(': 'open paren',     # 0x28
+    ')': 'close paren',    # 0x29
+    '*': 'asterisk',       # 0x2a
+    '+': 'plus',           # 0x2b
+    ',': 'comma',          # 0x2c
+    '-': 'minus',          # 0x2d
+    '.': 'period',         # 0x2e
+    '/': 'slash',          # 0x2f
+
+    ':': 'colon',          # 0x3a
+    ';': 'semicolon',      # 0x3b
+    '<': 'less than',      # 0x3c
+    '=': 'equals',         # 0x3d
+    '>': 'greater than',   # 0x3e
+    '?': 'question',       # 0x3f
+    '@': 'at',             # 0x40
+
+    '[': 'left bracket',   # 0x5b
+    '\\': 'backslash',     # 0x5c
+    ']': 'right bracket',  # 0x5d
+    '^': 'caret',          # 0x5e
+    '_': 'underscore',     # 0x5f
+    '`': 'backtick',       # 0x60
+
+    '{': 'left brace',     # 0x7b
+    '|': 'pipe',           # 0x7c
+    '}': 'right brace',    # 0x7d
+    '~': 'tilde',          # 0x7e
+    '\x1bOP': 'f1',
+    '\x1bOQ': 'f2',
+    '\x1bOR': 'f3',
+    '\x1bOS': 'f4',
+
+    '\x1b[11~': 'f1',
+    '\x1b[12~': 'f2',
+    '\x1b[13~': 'f3',
+    '\x1b[14~': 'f4',
+    '\x1b[15~': 'f5',
+    '\x1b[17~': 'f6',
+    '\x1b[18~': 'f7',
+    '\x1b[19~': 'f8',
+    '\x1b[20~': 'f9',
+    '\x1b[21~': 'f10',
+    '\x1b[23~': 'f11',
+    '\x1b[24~': 'f12',
+
+    '\x1bO11~': 'f1',
+    '\x1bO12~': 'f2',
+    '\x1bO13~': 'f3',
+    '\x1bO14~': 'f4',
+    '\x1bO15~': 'f5',
+    '\x1bO17~': 'f6',
+    '\x1bO18~': 'f7',
+    '\x1bO19~': 'f8',
+    '\x1bO20~': 'f9',
+    '\x1bO21~': 'f10',
+    '\x1bO23~': 'f11',
+    '\x1bO24~': 'f12',
+
+    '\x1b[A': 'up',
+    '\x1b[B': 'down',
+    '\x1b[C': 'right',
+    '\x1b[D': 'left',
+
+    '\x1bOA': 'up',
+    '\x1bOB': 'down',
+    '\x1bOC': 'right',
+    '\x1bOD': 'left',
+
+    '\xe0H': 'up',
+    '\xe0P': 'down',
+    '\xe0M': 'left',
+    '\xe0K': 'right',
+
+    '\x1bOp': 'keypad_0',
+    '\x1bOq': 'keypad_1',
+    '\x1bOr': 'keypad_2',
+    '\x1bOs': 'keypad_3',
+    '\x1bOt': 'keypad_4',
+    '\x1bOu': 'keypad_5',
+    '\x1bOv': 'keypad_6',
+    '\x1bOw': 'keypad_7',
+    '\x1bOx': 'keypad_8',
+    '\x1bOy': 'keypad_9',
+    '\x1bOm': 'keypad_minus',
+    '\x1bOl': 'keypad_coma',
+    '\x1bOn': 'keypad_period',
+    '\x1bOM': 'keypad_enter',
+
+    '\x00': 'something',
+    '\x01': 'something',
+    '\x02': 'something',
+    '\x03': 'something',
+    '\x04': 'ctrl w',
+    '\x05': 'something',
+    '\x06': 'something',
+    '\x07': 'something',
+    '\x0c': 'something',
+    '\x10': 'something',
+    '\x11': 'something',
+    '\x12': 'something',
+    '\x13': 'something',
+    '\x14': 'something',
+    '\x15': 'something',
+    '\x16': 'something',
+    '\x17': 'something',
+    '\x18': 'ctrl x',
+    '\x19': 'something',
+    '\x1a': 'something',
+    '\x1c': 'something',
+    '\x1d': 'something',
+    '\x1e': 'something',
+    '\x1f': 'something',
+
+    '\x1bx': 'ctrl x'
+}
