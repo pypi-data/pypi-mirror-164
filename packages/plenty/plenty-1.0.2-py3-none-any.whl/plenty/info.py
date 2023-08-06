1cf6569a4e3fcee44fd28e1cb154265b145ff7f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:24 +0000

    Add package plenty/1.0.2/plenty-1.0.2-py3-none-any.whl

---
+++ packages/plenty/plenty-1.0.2-py3-none-any.whl/plenty/info.py
@@ -0,0 +1,19 @@
+#
+# ==============================
+# Save some package information.
+# ==============================
+#
+
+VERSION = "1.0.2"
+PROJECT = "plenty"
+
+URL = "https://github.com/zlj-zz/plenty"
+URI = URL
+
+AUTHOR = "Zachary Zhang"
+EMAIL = "zlj19971222@outlook.com"
+
+LICENSE = "MIT"
+COPYRIGHT = "Copyright (c) 2022 Zachary"
+
+DESC = "Terminal diversification class library."
