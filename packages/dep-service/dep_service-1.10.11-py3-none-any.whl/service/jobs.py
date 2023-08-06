f1b6459dc7e33d77086f9b69a13f7567dfac74aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:31 +0000

    Add package dep-service/1.10.11/dep_service-1.10.11-py3-none-any.whl

---
+++ packages/dep-service/dep_service-1.10.11-py3-none-any.whl/service/jobs.py
@@ -0,0 +1,80 @@
+"""Cron jobs."""
+
+from aiocronjob import Manager
+from aiocronjob.main import add_routes
+
+from dataclasses import dataclass
+from logging import getLogger
+from typing import Coroutine, Callable
+
+from pathlib import Path
+from starlette.middleware.cors import CORSMiddleware
+from starlette.staticfiles import StaticFiles
+
+from spec.types import App
+
+log = getLogger(__name__)
+
+
+@dataclass(frozen=True)
+class CronJob:
+    """Cron job."""
+
+    name: str
+    crontab: str
+    job: Callable[[], Coroutine]
+
+
+class CronManager(Manager):
+    """Cron manager."""
+
+    async def on_job_started(self, job_name: str):
+        """On cron job start."""
+        log.debug(f'Job {job_name} started')
+
+    async def on_job_exception(self, job_name: str, exception: BaseException):
+        """On cron job on job exception."""
+        log.error(f'Job {job_name} exc: {exception}', exc_info=True)
+
+    async def on_job_cancelled(self, job_name: str):
+        """On cron job cancelled."""
+        log.error(f'Job {job_name} cancelled')
+
+    async def on_job_finished(self, job_name: str):
+        """On cron job execution."""
+        log.error(f'Job {job_name} finished')
+
+    async def on_startup(self):
+        """Cron manager prepare."""
+        log.error('Cron manager started')
+
+    async def on_shutdown(self):
+        """Cron manager release."""
+        log.error('Cron manager finished')
+
+
+def init_cron(app: App, manager: Manager):
+    """Init cron."""
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+    )
+
+    add_routes(
+        app=app,
+        path='/service/cron',
+        manager=manager,
+    )
+
+    cron_dir = Path(app.spec.path.assets / 'crontab')
+    cron_dir.mkdir(parents=True, exist_ok=True)
+
+    app.mount(
+        '/',
+        StaticFiles(directory=cron_dir.as_posix(), html=True),
+        name='crontab',
+    )
