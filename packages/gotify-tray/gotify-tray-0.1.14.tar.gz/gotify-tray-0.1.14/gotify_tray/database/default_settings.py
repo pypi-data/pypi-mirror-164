27bde98d5192f3d609603148d76b285077c9f65b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:21 +0000

    Add package gotify-tray/0.1.14/gotify-tray-0.1.14.tar.gz

---
+++ packages/gotify-tray/gotify-tray-0.1.14.tar.gz/gotify-tray-0.1.14/gotify_tray/database/default_settings.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+from ..__version__ import __title__
+
+
+DEFAULT_SETTINGS = {
+    "message/check_missed/notify": True,
+    "logging/level": "Disabled",
+    "export/path": os.path.join(
+        Path.home(), f"{__title__.replace(' ', '-').lower()}-settings.bytes"
+    ),
+    "shortcuts/quit": "Ctrl+Q",
+    "tray/notifications/priority": 5,
+    "tray/notifications/duration_ms": 5000,
+    "tray/notifications/icon/show": True,
+    "watchdog/interval/s": 60,
+    "MessageWidget/image/size": 33,
+    "MainWindow/label/size": 25,
+    "MainWindow/button/size": 33,
+    "MainWindow/application/icon/size": 40,
+}
