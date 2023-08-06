9c119187f96190b7c85ddbf352ac713bdd21a219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:56 +0000

    Add package moonlightpy/0.0.12/moonlightpy-0.0.12-py3-none-any.whl

---
+++ packages/moonlightpy/moonlightpy-0.0.12-py3-none-any.whl/moonlightpy/msg.py
@@ -0,0 +1,41 @@
+from typing import Tuple
+
+from .coding import Coding, EC
+
+
+class MsgType:
+    OPEN_CONN = 1
+    CLOSE_CONN = 2
+    DATA = 3
+    REG = 4
+
+
+class Msg:
+    def __init__(self, type: int = MsgType.DATA, id: str = '', data: bytes = b''):
+        self.type = type
+        self.id = id
+        self.data = data
+
+    def encode(self) -> bytearray:
+        res = bytearray()
+        res += Coding.encode_int(self.type, 1)
+        res += Coding.encode_string(self.id)
+        res += Coding.encode_bytes(self.data)
+        return res
+
+    def decode(self, buf: bytearray) -> Tuple[int, int]:
+        pos = 0
+        self.type, size, ec = Coding.decode_int(buf[pos:], 1)
+        if ec != EC.NONE:
+            return 0, ec
+        pos += size
+
+        self.id, size, ec = Coding.decode_string(buf[pos:])
+        if ec != EC.NONE:
+            return 0, ec
+        pos += size
+
+        self.data, size, ec = Coding.decode_bytes(buf[pos:])
+        pos += size
+
+        return pos, ec
