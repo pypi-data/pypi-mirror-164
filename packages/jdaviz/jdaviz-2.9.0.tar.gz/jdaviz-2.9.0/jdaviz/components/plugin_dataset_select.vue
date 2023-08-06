c3a50a324d67f399b71aba060a34591c989adaa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:50 +0000

    Add package jdaviz/2.9.0/jdaviz-2.9.0.tar.gz

---
+++ packages/jdaviz/jdaviz-2.9.0.tar.gz/jdaviz-2.9.0/jdaviz/components/plugin_dataset_select.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+  <v-row v-if="items.length > 1 || show_if_single_entry">
+    <v-select
+      :items="items"
+      v-model="selected"
+      @change="$emit('update:selected', $event)"
+      :label="label ? label : 'Data'"
+      :hint="hint ? hint : 'Select data.'"
+      :rules="rules ? rules : []"
+      item-text="label"
+      item-value="label"
+      persistent-hint
+    >
+    <template slot="selection" slot-scope="data">
+      <div class="single-line">
+        <span>
+          <v-icon v-if="data.item.icon" style='margin-right: 2px'>{{ data.item.icon }}</v-icon>
+          {{ data.item.label }}
+        </span>
+      </div>
+    </template>
+    <template slot="item" slot-scope="data">
+      <div class="single-line">
+        <span>
+          <v-icon v-if="data.item.icon" style='margin-right: 2px'>{{ data.item.icon }}</v-icon>
+          {{ data.item.label }}
+        </span>
+      </div>
+    </template>
+   </v-select>
+  </v-row>
+ </div>
+</template>
+<script>
+module.exports = {
+  props: ['items', 'selected', 'label', 'hint', 'rules', 'show_if_single_entry']
+};
+</script>
+
+<style>
+  .v-select__selections {
+    flex-wrap: nowrap !important;
+  }
+  .single-line {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+  }
+</style>
