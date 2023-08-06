28790fd57c4be99cd5dfa167ac2f305b841024e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:17 +0000

    Add package tinytim/0.5/tinytim-0.5-py3-none-any.whl

---
+++ packages/tinytim/tinytim-0.5-py3-none-any.whl/tinytim/__init__.py
@@ -0,0 +1,9 @@
+from tinytim.columns import itercolumns
+from tinytim.copy import copy_table
+from tinytim.edit import drop_row
+from tinytim.features import head, tail, size, shape
+from tinytim.filter import sample
+from tinytim.group import groupby
+from tinytim.rows import iterrows
+
+
