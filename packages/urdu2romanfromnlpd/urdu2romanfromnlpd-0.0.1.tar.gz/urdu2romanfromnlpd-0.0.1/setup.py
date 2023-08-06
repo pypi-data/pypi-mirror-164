63b39f4e3a187de497e80ab1268ae8a1a496bdf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:09 +0000

    Add package urdu2romanfromnlpd/0.0.1/urdu2romanfromnlpd-0.0.1.tar.gz

---
+++ packages/urdu2romanfromnlpd/urdu2romanfromnlpd-0.0.1.tar.gz/urdu2romanfromnlpd-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+    ]
+
+setup(
+    name='urdu2romanfromnlpd',
+    version='0.0.1',
+    description='A roman to urdu converter',
+    long_description=open('readme.txt').read() + '\n\n' +open('CHANGELOG.txt').read(),
+    url='',
+    author='Mohammad Ayyaz Azeem',
+    author_email='ayyaz.maju@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords = 'urdu2roman,roman',
+    packages=find_packages(),
+    install_requires=['']
+)
