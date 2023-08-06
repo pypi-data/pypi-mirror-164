3c9eb2cfca6dce15b2377c0909d4ca896f466a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:36 +0000

    Add package nomenclature-iamc/0.5/nomenclature_iamc-0.5-py3-none-any.whl

---
+++ packages/nomenclature-iamc/nomenclature_iamc-0.5-py3-none-any.whl/nomenclature/codes.py
@@ -0,0 +1,134 @@
+from pathlib import Path
+import yaml
+from typing import Union, Dict, Optional
+from pydantic import BaseModel
+from jsonschema import validate
+
+from nomenclature.code import Code, Tag, replace_tags
+
+
+here = Path(__file__).parent.absolute()
+
+
+def read_validation_schema(i):
+    with open(here / "validation_schemas" / f"{i}_schema.yaml", "r") as f:
+        schema = yaml.safe_load(f)
+    return schema
+
+
+SCHEMA_TYPES = ("variable", "tag", "region", "generic")
+SCHEMA_MAPPING = dict([(i, read_validation_schema(i)) for i in SCHEMA_TYPES])
+
+
+class CodeList(BaseModel):
+    """A class for nomenclature codelists & attributes"""
+
+    name: str
+    mapping: Optional[Dict[str, Dict[str, Union[str, float, int, None]]]] = {}
+
+    def __setitem__(self, key, value):
+        if key in self.mapping:
+            raise ValueError(f"Duplicate {self.name} key: {key}")
+        self.mapping[key] = value
+
+    def __getitem__(self, k):
+        return self.mapping[k]
+
+    def __iter__(self):
+        return iter(self.mapping)
+
+    def __len__(self):
+        return len(self.mapping)
+
+    def __repr__(self):
+        return self.mapping.__repr__()
+
+    def items(self):
+        return self.mapping.items()
+
+    def keys(self):
+        return self.mapping.keys()
+
+    def values(self):
+        return self.mapping.values()
+
+    @classmethod
+    def from_directory(
+        cls,
+        name: str,
+        path: Path,
+        file: str = None,
+        ext: str = ".yaml",
+    ):
+        """Initialize a CodeList from a directory with codelist files
+
+        Parameters
+        ----------
+        name : str
+            Name of the CodeList
+        path : :class:`pathlib.Path` or path-like
+            Directory with the codelist files
+        file : str, optional
+            Pattern to downselect codelist files by name
+        ext : str, optional
+            Extension of the codelist files
+        top_level_attr : str, optional
+            A top-level hierarchy for codelist files with a nested structure
+
+        Returns
+        -------
+        CodeList
+
+        """
+        code_list, tag_dict = [], CodeList(name="tag")
+
+        # parse all files in path if file is None
+        file = file or "**/*"
+
+        # parse all files
+        for f in path.glob(f"{file}{ext}"):
+            with open(f, "r", encoding="utf-8") as stream:
+                _code_list = yaml.safe_load(stream)
+
+            # check if this file contains a dictionary with <tag>-style keys
+            if f.name.startswith("tag_"):
+                # validate against the tag schema
+                validate(_code_list, SCHEMA_MAPPING["tag"])
+
+                # cast _codes to `Tag`
+                for item in _code_list:
+                    tag = Tag.from_dict(mapping=item)
+                    tag_dict[tag.name] = [Code.from_dict(a) for a in tag.attributes]
+                continue
+
+            # validate against the schema of this codelist domain (default `generic`)
+            validate(_code_list, SCHEMA_MAPPING.get(name, SCHEMA_MAPPING["generic"]))
+
+            # a "region" codelist assumes a top-level key to be used as attribute
+            if name == "region":
+                _region_code_list = []  # save refactored list as new (temporary) object
+                for top_level_cat in _code_list:
+                    for top_key, _codes in top_level_cat.items():
+                        for item in _codes:
+                            item = Code.from_dict(item)
+                            item.set_attribute("hierarchy", top_key)
+                            _region_code_list.append(item)
+                _code_list = _region_code_list
+            else:
+                _code_list = [Code.from_dict(_dict) for _dict in _code_list]
+
+            # add `file` attribute to each element and add to main list
+            for item in _code_list:
+                item.set_attribute("file", str(f.relative_to(path.parent)))
+            code_list.extend(_code_list)
+
+        # replace tags by the items of the tag-dictionary
+        for tag, tag_attrs in tag_dict.items():
+            code_list = replace_tags(code_list, tag, tag_attrs)
+
+        # iterate over the list to guard against silent replacement of duplicates
+        cl = CodeList(name=name)
+        for item in code_list:
+            cl[item.name] = item.attributes
+
+        return cl
---
+++ packages/nomenclature-iamc/nomenclature_iamc-0.5-py3-none-any.whl/nomenclature/region_mapping_models.py
@@ -0,0 +1,270 @@
+import logging
+from collections import Counter
+from pathlib import Path
+from typing import Dict, List, Optional, Set, Tuple, Union
+
+import pyam
+import yaml
+from jsonschema import ValidationError, validate
+from pyam import IamDataFrame
+from pydantic import BaseModel, root_validator, validate_arguments, validator
+from pydantic.errors import PydanticValueError
+from pydantic.types import DirectoryPath, FilePath
+
+from nomenclature.definition import DataStructureDefinition
+
+logger = logging.getLogger(__name__)
+
+here = Path(__file__).parent.absolute()
+
+Sequence = Union[List[str], Tuple[str], Set[str]]
+
+
+class RegionNameCollisionError(PydanticValueError):
+    code = "region_name_collision"
+    msg_template = "Name collision in {location} for {duplicates} in {file}"
+
+    def __init__(self, location: str, duplicates: Sequence, file: Path) -> None:
+        super().__init__(
+            location=location,
+            duplicates=duplicates,
+            file=file.relative_to(Path.cwd()),
+        )
+
+
+class NativeRegion(BaseModel):
+    name: str
+    rename: Optional[str]
+
+    @property
+    def target_native_region(self):
+        return self.rename if self.rename is not None else self.name
+
+
+class CommonRegion(BaseModel):
+    name: str
+    constituent_regions: List[str]
+
+
+class RegionAggregationMapping(BaseModel):
+    model: str
+    file: FilePath
+    native_regions: Optional[List[NativeRegion]]
+    common_regions: Optional[List[CommonRegion]]
+
+    @validator("native_regions")
+    def validate_native_regions(cls, v, values):
+        target_names = [nr.target_native_region for nr in v]
+        duplicates = [
+            item for item, count in Counter(target_names).items() if count > 1
+        ]
+        if duplicates:
+            # Raise the custom RegionNameCollisionError and give the parameters
+            # duplicates and file.
+            raise RegionNameCollisionError("native regions", duplicates, values["file"])
+        return v
+
+    @validator("common_regions")
+    def validate_common_regions(cls, v, values):
+        names = [cr.name for cr in v]
+        duplicates = [item for item, count in Counter(names).items() if count > 1]
+        if duplicates:
+            raise RegionNameCollisionError("common regions", duplicates, values["file"])
+        return v
+
+    @root_validator()
+    def check_native_or_common_regions(cls, values):
+        # Check that we have at least one of the two: native and common regions
+        if (
+            values.get("native_regions") is None
+            and values.get("common_regions") is None
+        ):
+            raise ValueError(
+                "At least one of the two: 'native_regions', 'common_regions' must be"
+                f"given in {values['file']}"
+            )
+        return values
+
+    @root_validator()
+    def check_illegal_renaming(cls, values):
+        """Check if any renaming overlaps with common regions"""
+        # Skip if only either native-regions or common-regions are specified
+        if values.get("native_regions") is None or values.get("common_regions") is None:
+            return values
+        native_region_names = {
+            nr.target_native_region for nr in values["native_regions"]
+        }
+        common_region_names = {cr.name for cr in values["common_regions"]}
+        overlap = list(native_region_names & common_region_names)
+        if overlap:
+            raise RegionNameCollisionError(
+                "native and common regions", overlap, values["file"]
+            )
+        return values
+
+    @classmethod
+    def create_from_region_mapping(cls, file: Union[Path, str]):
+        SCHEMA_FILE = here / "validation_schemas" / "region_mapping_schema.yaml"
+        with open(file, "r") as f:
+            mapping_input = yaml.safe_load(f)
+        with open(SCHEMA_FILE, "r") as f:
+            schema = yaml.safe_load(f)
+
+        # Validate the input data using jsonschema
+        try:
+            validate(mapping_input, schema)
+        except ValidationError as e:
+            # Add file information in case of error
+            raise ValidationError(f"{e.message} in {file}")
+
+        # Add the file name to mapping_input
+        mapping_input["file"] = file
+
+        # Reformat the "native_regions"
+        if "native_regions" in mapping_input:
+            native_region_list: List[Dict] = []
+            for nr in mapping_input["native_regions"]:
+                if isinstance(nr, str):
+                    native_region_list.append({"name": nr})
+                elif isinstance(nr, dict):
+                    native_region_list.append(
+                        {"name": list(nr)[0], "rename": list(nr.values())[0]}
+                    )
+            mapping_input["native_regions"] = native_region_list
+
+        # Reformat the "common_regions"
+        if "common_regions" in mapping_input:
+            common_region_list: List[Dict[str, List[Dict[str, str]]]] = []
+            for cr in mapping_input["common_regions"]:
+                cr_name = list(cr)[0]
+                common_region_list.append(
+                    {
+                        "name": cr_name,
+                        "constituent_regions": cr[cr_name],
+                    }
+                )
+            mapping_input["common_regions"] = common_region_list
+        return cls(**mapping_input)
+
+    @property
+    def all_regions(self) -> List[str]:
+        # For the native regions we take the **renamed** (if given) names
+        nr_list = [x.target_native_region for x in self.native_regions or []]
+        cr_list = [x.name for x in self.common_regions or []]
+        return nr_list + cr_list
+
+    @property
+    def model_native_region_names(self) -> List[str]:
+        # List of the **original** model native region names
+        return [x.name for x in self.native_regions]
+
+    @property
+    def rename_mapping(self) -> Dict[str, str]:
+        return {r.name: r.target_native_region for r in self.native_regions or []}
+
+
+class ModelMappingCollisionError(PydanticValueError):
+    code = "model_mapping_collision"
+    msg_template = "Multiple region aggregation mappings for model {model} in {files}"
+
+    def __init__(
+        self, mapping1: RegionAggregationMapping, mapping2: RegionAggregationMapping
+    ) -> None:
+        files = (
+            mapping1.file.relative_to(Path.cwd()),
+            mapping2.file.relative_to(Path.cwd()),
+        )
+        super().__init__(model=mapping1.model, files=files)
+
+
+class RegionNotDefinedError(PydanticValueError):
+    code = "region_not_defined"
+    msg_template = (
+        "Region(s) {region} in {file} not defined in the DataStructureDefinition"
+    )
+
+    def __init__(self, region, file) -> None:
+        self.file = file.relative_to(Path.cwd())
+        super().__init__(region=region, file=file)
+
+
+class RegionProcessor(BaseModel):
+    """Region aggregation mappings for scenario processing"""
+
+    definition: DataStructureDefinition
+    mappings: Dict[str, RegionAggregationMapping]
+
+    class Config:
+        # necessary since DataStructureDefinition is not a pydantic class
+        arbitrary_types_allowed = True
+
+    @classmethod
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def from_directory(cls, path: DirectoryPath, definition: DataStructureDefinition):
+        mapping_dict: Dict[str, RegionAggregationMapping] = {}
+        for file in path.glob("**/*.yaml"):
+            mapping = RegionAggregationMapping.create_from_region_mapping(file)
+            if mapping.model not in mapping_dict:
+                mapping_dict[mapping.model] = mapping
+            else:
+                raise ModelMappingCollisionError(mapping, mapping_dict[mapping.model])
+        return cls(definition=definition, mappings=mapping_dict)
+
+    @validator("mappings", each_item=True)
+    def validate_mapping(cls, v, values):
+        invalid = [c for c in v.all_regions if c not in values["definition"].region]
+        if invalid:
+            raise RegionNotDefinedError(invalid, v.file)
+        return v
+
+    def apply(self, df: IamDataFrame) -> IamDataFrame:
+        processed_dfs: List[IamDataFrame] = []
+        for model in df.model:
+            model_df = df.filter(model=model)
+
+            # If no mapping is defined the data frame is returned unchanged
+            if model not in self.mappings:
+                logging.info(f"No region aggregation mapping found for model {model}")
+                processed_dfs.append(model_df)
+            # Otherwise we first rename, then aggregate
+            else:
+                logging.info(
+                    f"Applying region aggregation mapping for model {model} from file "
+                    f"{self.mappings[model].file}"
+                )
+                # Rename
+                if self.mappings[model].native_regions is not None:
+                    processed_dfs.append(
+                        model_df.filter(
+                            region=self.mappings[model].model_native_region_names
+                        ).rename(region=self.mappings[model].rename_mapping)
+                    )
+
+                # Aggregate
+                if self.mappings[model].common_regions is not None:
+                    for cr in self.mappings[model].common_regions:
+                        PYAM_AGG_KWARGS = {
+                            "components",
+                            "method",
+                            "weight",
+                            "drop_negative_weights",
+                        }
+                        vars = {
+                            var: {
+                                key: value
+                                for key, value in kwargs.items()
+                                if key in PYAM_AGG_KWARGS
+                            }
+                            for var, kwargs in self.definition.variable.items()
+                            if var in model_df.variable
+                            and not kwargs.get("skip-region-aggregation", False)
+                        }
+                        for var, kwargs in vars.items():
+                            agg_df = model_df.aggregate_region(
+                                var,
+                                cr.name,
+                                cr.constituent_regions,
+                                **kwargs,
+                            )
+                            processed_dfs.append(agg_df)
+        return pyam.concat(processed_dfs)
