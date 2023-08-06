876654af3dfbb1c60585a756115631e04c54bc87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:43 +0000

    Add package libxbydragon/0.0.1/libxbydragon-0.0.1.tar.gz

---
+++ packages/libxbydragon/libxbydragon-0.0.1.tar.gz/libxbydragon-0.0.1/libxbydragon/__init__.py
@@ -0,0 +1,11 @@
+def plural(num1 , num2):
+    return num1 + num2
+
+def subtract(num1 , num2):
+    return num1 - num2
+
+def multipliedby(num1 , num2):
+    return num1 * num2
+
+def dividedby(num1 , num2):
+    return num1 /  num2
