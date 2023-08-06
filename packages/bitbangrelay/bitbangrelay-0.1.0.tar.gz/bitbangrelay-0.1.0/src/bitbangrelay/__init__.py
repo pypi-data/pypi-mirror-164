d5ae598ab268ffbf3e10d8c5f338d3becbd289e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:57 +0000

    Add package bitbangrelay/0.1.0/bitbangrelay-0.1.0.tar.gz

---
+++ packages/bitbangrelay/bitbangrelay-0.1.0.tar.gz/bitbangrelay-0.1.0/src/bitbangrelay/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021-2022 SanCloud Ltd
+# SPDX-License-Identifier: Apache-2.0
+
+__progname__ = "bitbangrelay"
+__description__ = (
+    "Control USB relay boards based on FTDI FT232R/FT245R chips in bitbang mode"
+)
+__version__ = "0.1.0"
