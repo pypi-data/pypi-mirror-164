dabc705e6da07ff1acb6d77d170bc4c0f6b6e2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:18 +0000

    Add package steemhd/1.0.1/steemhd-1.0.1.tar.gz

---
+++ packages/steemhd/steemhd-1.0.1.tar.gz/steemhd-1.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'steemhd',
+    packages = find_packages(exclude=['test']),
+    install_requires = ['requests >= 2.23.0', 'pycryptodome >= 3.9.8','ecdsa >= 0.17.0','crcmod >= 1.7','eth_utils >= 0.9.0'],
+    version = '1.0.1',
+    description = 'Bitcoin/Ethereum/STEEM key manipulation',
+    author = 'maiyude',
+    author_email = 'steem@steem.vip',
+    url = 'https://github.com/maiyude2018/steem_to_hdwallet',
+    keywords = ['steem','security', 'cryptography', 'cryptocurrency', 'bitcoin'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries',
+    ],
+    python_requires='>=3',
+    include_package_data=True
+)
+
+
+
+
+
+
