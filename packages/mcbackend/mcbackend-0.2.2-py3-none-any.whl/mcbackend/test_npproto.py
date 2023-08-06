258e82a44bc6519ce085bbb3443eedfd4c8b8747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:04 +0000

    Add package mcbackend/0.2.2/mcbackend-0.2.2-py3-none-any.whl

---
+++ packages/mcbackend/mcbackend-0.2.2-py3-none-any.whl/mcbackend/test_npproto.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+import numpy
+import pytest
+
+from mcbackend import npproto
+from mcbackend.npproto import utils
+
+
+class TestUtils:
+    @pytest.mark.parametrize(
+        "arr",
+        [
+            numpy.arange(5),
+            numpy.random.uniform(size=(2, 3)),
+            numpy.array(5),
+            numpy.array(["hello", "world"]),
+            numpy.array([datetime(2020, 3, 4, 5, 6, 7, 8), datetime(2020, 3, 4, 5, 6, 7, 9)]),
+            numpy.array(
+                [datetime(2020, 3, 4, 5, 6, 7, 8), datetime(2020, 3, 4, 5, 6, 7, 9)],
+                dtype="datetime64",
+            ),
+            numpy.array([(1, 2), (3, 2, 1)], dtype=object),
+        ],
+    )
+    def test_conversion(self, arr: numpy.ndarray):
+        nda = utils.ndarray_from_numpy(arr)
+        enc = bytes(nda)
+        dec = npproto.Ndarray().parse(enc)
+        assert isinstance(dec.data, bytes)
+        result = utils.ndarray_to_numpy(dec)
+        numpy.testing.assert_array_equal(result, arr)
+        pass
