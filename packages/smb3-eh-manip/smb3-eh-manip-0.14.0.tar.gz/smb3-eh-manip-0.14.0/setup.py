f2964a9b12bb18dd820bd610278d15193830b3f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:57 +0000

    Add package smb3-eh-manip/0.14.0/smb3-eh-manip-0.14.0.tar.gz

---
+++ packages/smb3-eh-manip/smb3-eh-manip-0.14.0.tar.gz/smb3-eh-manip-0.14.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="smb3-eh-manip",
+    version="0.14.0",
+    description=("Ingest video data to render smb3 eh manip stimuli"),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="smb3-eh-manip",
+    author="Jon Robison",
+    author_email="narfman0@blastedstudios.com",
+    license="LICENSE",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=["opencv-python", "python-vlc", "pygame"],
+    test_suite="tests",
+)
