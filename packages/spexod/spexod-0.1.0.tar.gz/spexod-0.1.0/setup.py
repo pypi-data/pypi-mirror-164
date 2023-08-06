fc4608a8e5b2bb9c27a12d730021399e521c2ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:37 +0000

    Add package spexod/0.1.0/spexod-0.1.0.tar.gz

---
+++ packages/spexod/spexod-0.1.0.tar.gz/spexod-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(name='spexod',
+      version='0.1.0',
+      description='Community tools for SpExoDisks (spexodisks.com)',
+      author='Caleb Wheeler',
+      author_email='chw3k5@gmail.com',
+      packages=find_packages(),
+      url="https://github.com/spexod/spexod",
+      install_requires=['numpy', 'astropy']
+      )
