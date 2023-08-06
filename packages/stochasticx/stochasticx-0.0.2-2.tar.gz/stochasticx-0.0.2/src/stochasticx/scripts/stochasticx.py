db2d0d72320200669aedb2c4d21b02604611cebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:11 +0000

    Add package stochasticx/0.0.2/stochasticx-0.0.2-2.tar.gz

---
+++ packages/stochasticx/stochasticx-0.0.2-2.tar.gz/stochasticx-0.0.2/src/stochasticx/scripts/stochasticx.py
@@ -0,0 +1,22 @@
+import click
+from stochasticx.client.models import models
+from stochasticx.client.datasets import datasets
+from stochasticx.client.jobs import jobs
+from stochasticx.client.auth import login, me
+from stochasticx.client.deployments import instances, deployments
+from stochasticx.client.inferences import inference
+
+
+@click.group()
+def cli():
+    pass
+    
+ 
+cli.add_command(models)
+cli.add_command(datasets)
+cli.add_command(jobs)
+cli.add_command(login)
+cli.add_command(me)
+cli.add_command(instances)
+cli.add_command(deployments)
+cli.add_command(inference)
