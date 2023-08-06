4ec85a7b01af8cb97f72478b99c18b12d914d016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:48 +0000

    Add package mage-ai/0.3.3/mage-ai-0.3.3.tar.gz

---
+++ packages/mage-ai/mage-ai-0.3.3.tar.gz/mage-ai-0.3.3/mage_ai/data_preparation/executors/block_executor.py
@@ -0,0 +1,31 @@
+from typing import Callable, Dict
+
+
+class BlockExecutor:
+    def __init__(self, pipeline, block_uuid):
+        self.pipeline = pipeline
+        self.block_uuid = block_uuid
+        self.block = self.pipeline.get_block(block_uuid)
+
+    def execute(
+        self,
+        analyze_outputs: bool = False,
+        execution_partition: str = None,
+        global_vars: Dict = None,
+        update_status: bool = False,
+        on_complete: Callable[[str], None] = None,
+        on_failure: Callable[[str], None] = None,
+    ) -> None:
+        try:
+            self.block.execute_sync(
+                analyze_outputs=analyze_outputs,
+                execution_partition=execution_partition,
+                global_vars=global_vars,
+                update_status=update_status,
+            )
+        except Exception as e:
+            if on_failure is not None:
+                on_failure(self.block_uuid)
+            raise e
+        if on_complete is not None:
+            on_complete(self.block_uuid)
