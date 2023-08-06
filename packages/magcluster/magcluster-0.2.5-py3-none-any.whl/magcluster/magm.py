ae2bd226d6e7958f3a00bc8e32cd5d80e31e2026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:24 +0000

    Add package magcluster/0.2.5/magcluster-0.2.5-py3-none-any.whl

---
+++ packages/magcluster/magcluster-0.2.5-py3-none-any.whl/magcluster/magm.py
@@ -0,0 +1,24 @@
+from .capture_args import capture_args
+from subprocess import run
+
+def get_clinker_cmd(args):
+
+    args_dic = args.__dict__
+
+    usr_args = capture_args()
+    del usr_args[0:2]
+    usr_args.insert(0, 'clinker')
+
+    if args_dic['plot']:
+        if args_dic['plot'] == True: #user have -p but no file.html
+            if '-p' in usr_args:
+                usr_args.insert(usr_args.index('-p')+1, 'MGC_align.html')
+            if '--plot' in usr_args:
+                usr_args.insert(usr_args.index('--plot')+1, 'MGC_align.html')
+    else:
+        usr_args.extend(['-p', 'MGC_align.html'])
+    return usr_args
+
+def magm(usr_args):
+    run(usr_args)
+
