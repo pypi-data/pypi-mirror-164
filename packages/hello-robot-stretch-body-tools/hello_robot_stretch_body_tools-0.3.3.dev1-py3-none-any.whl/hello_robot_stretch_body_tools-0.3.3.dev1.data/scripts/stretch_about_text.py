b2ffb2423299105c0b9897f8cd69939ecbbd2b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:00:02 +0000

    Add package hello-robot-stretch-body-tools/0.3.3.dev1/hello_robot_stretch_body_tools-0.3.3.dev1-py3-none-any.whl

---
+++ packages/hello-robot-stretch-body-tools/hello_robot_stretch_body_tools-0.3.3.dev1-py3-none-any.whl/hello_robot_stretch_body_tools-0.3.3.dev1.data/scripts/stretch_about_text.py
@@ -0,0 +1,20 @@
+#!python
+import os
+import yaml
+
+params_filename = os.environ['HELLO_FLEET_PATH']+'/'+os.environ['HELLO_FLEET_ID']+'/stretch_configuration_params.yaml'
+
+with open(params_filename, 'r') as fid:
+    params = yaml.safe_load(fid)
+    robot_info = params['robot']
+    batch_name = robot_info['batch_name']
+    serial_number = robot_info['serial_no']
+    model=robot_info['model_name']
+
+batch_name_string = 'Stretch Batch Name: {0}'.format(batch_name)
+serial_number_string = 'Stretch Serial Number: {0}'.format(serial_number)
+model_string = 'Stretch Model: {0}'.format(model)
+
+print(batch_name_string)
+print(serial_number_string)
+print(model_string)
