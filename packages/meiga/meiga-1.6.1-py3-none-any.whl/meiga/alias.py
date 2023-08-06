c254a2173b221ab4a0f69fc9a6778690f82f0dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:44 +0000

    Add package meiga/1.6.1/meiga-1.6.1-py3-none-any.whl

---
+++ packages/meiga/meiga-1.6.1-py3-none-any.whl/meiga/alias.py
@@ -0,0 +1,22 @@
+from typing import Any, Generic, cast
+
+from meiga.error import Error
+from meiga.result import TF, TS, Result
+
+
+class Success(Result, Generic[TS]):
+    def __init__(self, value: TS = cast(TS, True)) -> None:
+        Result.__init__(self, success=value)
+
+
+class Failure(Result, Generic[TF]):
+    def __init__(self, error: TF = cast(TF, Error())) -> None:
+        Result.__init__(self, failure=error)
+
+
+isSuccess: Success = Success()
+isFailure: Failure = Failure()
+NotImplementedMethodError = isFailure
+
+BoolResult = Result[bool, Error]
+AnyResult = Result[Any, Error]
