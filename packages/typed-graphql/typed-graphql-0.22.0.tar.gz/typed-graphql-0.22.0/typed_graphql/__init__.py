52f7c2d14f3cd729cdb4ac881f5aeb5961ba4d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:42 +0000

    Add package typed-graphql/0.22.0/typed-graphql-0.22.0.tar.gz

---
+++ packages/typed-graphql/typed-graphql-0.22.0.tar.gz/typed-graphql-0.22.0/typed_graphql/__init__.py
@@ -0,0 +1,9 @@
+from .core import (
+    ReturnTypeMissing,
+    TypedGraphqlMiddlewareManager,
+    graphql_input_type,
+    graphql_type,
+    resolver,
+    resolverclass,
+    staticresolver,
+)
