a95cd9ac2e7fe1aa593ba30c51f61e69b57e7d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:16 +0000

    Add package aio-clients/0.1.0b6/aio_clients-0.1.0b6-py3-none-any.whl

---
+++ packages/aio-clients/aio_clients-0.1.0b6-py3-none-any.whl/aio_clients/struct.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+from aiohttp import ClientTimeout
+from multidict import CIMultiDictProxy
+
+
+@dataclass
+class Options:
+    is_json: bool = True
+    is_raw: bool = False
+
+    # use only one reqeust
+    is_close_session: bool = False
+
+    timeout: ClientTimeout = None
+
+
+@dataclass
+class Response:
+    code: int
+
+    headers: CIMultiDictProxy
+
+    option: Options
+
+    json: Optional[Dict[Any, Any]] = None
+    raw_body: Optional[Any] = None
