dc33f6472d9b04b6de86127d6739d1de29fa856f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:34 +0000

    Add package remi-portal-gun/0.4.0/remi_portal_gun-0.4.0-py3-none-any.whl

---
+++ packages/remi-portal-gun/remi_portal_gun-0.4.0-py3-none-any.whl/remi_portal_gun/server.py
@@ -0,0 +1,24 @@
+import uvicorn
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+
+async def catch_all(request, exc):
+    return FileResponse("dist/index.html", status_code=200)
+
+
+exceptions = {
+    404: catch_all,
+}
+
+def run_server():
+
+    app = FastAPI(exception_handlers=exceptions)
+
+
+    app.mount("/", StaticFiles(directory="dist", html=True), name="static")
+
+
+    uvicorn.run(app, port=5000, log_level="info")
