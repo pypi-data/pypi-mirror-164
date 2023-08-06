338e0ba8c15c091b8b88f732206c9cc08efe688d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:31 +0000

    Add package smalin/0.0.1/smalin-0.0.1.tar.gz

---
+++ packages/smalin/smalin-0.0.1.tar.gz/smalin-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+  name="smalin",
+  version="0.0.1",
+  author="malinkang",
+  author_email="linkang.ma@gmail.com",
+  description="",
+  long_description="",
+  url="https://github.com/malinkang/mali",
+  packages=find_packages(),
+)
