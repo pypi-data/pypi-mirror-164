1924acda7e4fd1206b4b3f8525c9c8239e9c96dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:59 +0000

    Add package bernmix/1.11.39/bernmix-1.11.39.tar.gz

---
+++ packages/bernmix/bernmix-1.11.39.tar.gz/bernmix-1.11.39/bernmix/bernmix_fancy/__init__.py
@@ -0,0 +1,6 @@
+from .bm_dynam import dp_int, joint_distr_cov, dyn_tree_cov, get_v_max
+from .bm_joint import pmf_joint
+# from .bm_dynam_fast import dp_int_fast
+
+__all__ = ['dp_int', 'joint_distr_cov', 'dyn_tree_cov', 'get_v_max', 'pmf_joint']
+# __all__ += ['dp_int_fast']
---
+++ packages/bernmix/bernmix-1.11.39.tar.gz/bernmix-1.11.39/bernmix/bernmix_fancy/bm_joint.py
@@ -0,0 +1,59 @@
+"""
+This module aims to
+"""
+
+import numpy as np
+from bm_dynam import *
+from bernmix.bernmix_int import pmf as bmi
+
+def pmf_joint(p, w_vec, v = None, tol = 1e-5):
+    """
+    Function to predict joint distribution of
+    :param p: pribabilities of Bernoulli RVs
+    :param w_vec: vector of vectors of weights for RVs
+    :param tol: tolerance
+    :return:
+    """
+    # Checks
+    # TODO
+
+    # -----------------------------------
+    # Preparation
+    # -----------------------------------
+
+    # Algorithm
+
+
+    w_max = [sum(w) + 1 for w in w_vec]
+    m_max = np.cumprod(w_max)
+
+    w_new = w_vec[0]
+    for i in range(1, len(w_vec)):
+        w_new = [w1 + w2 * m_max[i-1] for w1, w2 in zip(w_new, w_vec[i])]
+
+    if v is None:
+        s = bmi(p, w_new)
+    else:
+        s = dyn_tree_cov(p, w_new, v)
+
+    res = [[i, val] for i, val in enumerate(s) if val > tol]
+    prob_sum = sum([val for _, val in res])
+    res = [[i, val / prob_sum] for i, val in res]
+
+    i = 7598
+
+
+    res_brv = []
+    for i, val in res:
+        y = []
+        for j in range(len(w_max)):
+            y_tmp = i % w_max[j]
+            y += [y_tmp]
+            i -= y_tmp
+            i /= w_max[j]
+            i = round(i)
+        res_brv += [y + [val]]
+
+
+
+
