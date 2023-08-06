a66e18a25ae9a05d7846773952e2dbd0f12e95b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:19 +0000

    Add package torchversion/0.0.1/torchversion-0.0.1.tar.gz

---
+++ packages/torchversion/torchversion-0.0.1.tar.gz/torchversion-0.0.1/torchversion/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.1"
+
+from torchversion import shell
+from torchversion import utils
+
+__all__ = [
+    "__version__",
+    "shell",
+    "utils"
+]
---
+++ packages/torchversion/torchversion-0.0.1.tar.gz/torchversion-0.0.1/torchversion/utils/version_pytorch.py
@@ -0,0 +1,2 @@
+def last_version():
+    return "1.12.0"
