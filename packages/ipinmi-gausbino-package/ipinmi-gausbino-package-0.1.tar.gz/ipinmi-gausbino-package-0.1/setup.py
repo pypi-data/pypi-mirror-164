3ce013ddc73ea33ee2f29d3c527e96c5dd2092a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:34 +0000

    Add package ipinmi-gausbino-package/0.1/ipinmi-gausbino-package-0.1.tar.gz

---
+++ packages/ipinmi-gausbino-package/ipinmi-gausbino-package-0.1.tar.gz/ipinmi-gausbino-package-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ipinmi-gausbino-package',
+      version='0.1',
+      description=' Binomial and Gaussian distributions',
+      packages=['ipinmi-gausbino-package'],
+      author="Chibundum Adebayo",
+      author_email= "chibundum.adebayo@stu.cu.edu.ng",
+      zip_safe=False)
