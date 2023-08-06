167d8cbb84587c4c8bd40641a59c28f23b40d169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:28 +0000

    Add package gcloud-config-helper/0.3.1/gcloud_config_helper-0.3.1-py2.py3-none-any.whl

---
+++ packages/gcloud-config-helper/gcloud_config_helper-0.3.1-py2.py3-none-any.whl/gcloud_config_helper/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for GCloud config helper."""
+
+__author__ = """Mark van Holsteijn"""
+__email__ = "markvanholsteijn@binx.io"
+__version__ = '0.2.2'
+
+from gcloud_config_helper.gcloud_config_helper import (
+    on_path,
+    default,
+    GCloudCredentials,
+)
+
