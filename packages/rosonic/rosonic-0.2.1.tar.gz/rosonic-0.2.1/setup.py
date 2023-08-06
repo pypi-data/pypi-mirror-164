5291862c7d2982be172b59dc6c903efd39eb0c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:37 +0000

    Add package rosonic/0.2.1/rosonic-0.2.1.tar.gz

---
+++ packages/rosonic/rosonic-0.2.1.tar.gz/rosonic-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='rosonic',
+    version='0.2.1',
+    description='Write pythonic ROS nodes, fast and comfortably',
+    author='Kaj Munhoz Arfvidsson',
+    author_email='kajarf@kth.se',
+    url='https://kaarmu.github.io/rosonic/',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=2.7',
+    install_requires=['rospy>=1.14'],
+    extras_require={
+        'docs': ['mkdocs', 'mkdocs-with-pdf', ],
+    },
+    package_dir={'': 'src'},
+    packages=['rosonic'],
+    provides=['rosonic'],
+)
