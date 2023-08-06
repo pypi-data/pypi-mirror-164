76f5218b04e06ae956d831b08617358f90636e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:44 +0000

    Add package ptrmetagen/1.1.13/ptrmetagen-1.1.13.tar.gz

---
+++ packages/ptrmetagen/ptrmetagen-1.1.13.tar.gz/ptrmetagen-1.1.13/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup
+from metagen import __version__
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="ptrmetagen",
+    version=__version__,
+    description="Package for generation of metastructures for Panter project",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gisat-panther/ptr-metagen",
+    author="Michal Opetal",
+    author_email="michal.opletal@gisat.cz",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9"],
+    packages=["metagen", "metagen.components", "metagen.utils", "metagen.config"],
+    package_data = {'': ['metagen/config/config.yaml']},
+    include_package_data=True,
+    entry_points='''
+       [console_scripts]
+       metagen=metagen.config.cli:main
+   ''',
+    )
