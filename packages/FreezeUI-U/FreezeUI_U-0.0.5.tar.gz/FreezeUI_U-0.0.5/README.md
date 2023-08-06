0a4ec370bc2bf94de274b27d10cc28b5afe32eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:59 +0000

    Add package FreezeUI-U/0.0.5/FreezeUI_U-0.0.5.tar.gz

---
+++ packages/FreezeUI-U/FreezeUI_U-0.0.5.tar.gz/FreezeUI_U-0.0.5/README.md
@@ -0,0 +1,55 @@
+<img src="https://raw.githubusercontent.com/AkshatChauhan18/FreezeUI/master/readme_assets/icon.png" width="100">
+
+# **FreezeUI**
+
+FreezeUI is a python package use to create [cx_Freeze](https://pypi.org/project/cx-Freeze/) setup files and run them to create applications and msi from python scripts (converts .py to .exe or .msi) . FreezeUI uses GUI.
+
+Currently it can only make **Windows MSI and EXE** .
+
+---
+
+# Profit💰 of using FreezeUI
+
+In order to convert .py to .exe using cx_freeze you need to write extra setup python files which is time taking process. FreezeUI creates those setup files for you using GUI which is a easy process and later you can edit those files using FreezeUI editor like in inno setup.
+
+___
+
+# Installation⏬
+
+```
+pip install FreezeUI
+```
+
+Check its <a href="https://akshatchauhan18.github.io/FreezeUI" target="_blank"><img src="https://raw.githubusercontent.com/AkshatChauhan18/FreezeUI/master/readme_assets/doc_button.svg" alt="doc" width="100" height="25"></a> for more
+information.
+
+___
+
+## Usage🧾
+
+For creating **EXE** the command is
+
+``` 
+freezeui-exe
+```
+
+For creating **MSI** the command is
+
+```
+freezeui-msi
+```
+
+___
+
+
+# Gallery🖼️
+
+### **Check demo [here](https://akshatchauhan18.github.io/FreezeUI/demo.html) .**
+
+|Exe Window |Msi Window | 
+| ----------- | ----------- | 
+|![exewin](https://raw.githubusercontent.com/AkshatChauhan18/FreezeUI/master/docs/assets/exe_win.png)|![msiwin](https://raw.githubusercontent.com/AkshatChauhan18/FreezeUI/master/docs/assets/msi_win.png)|
+
+|Editor Window|
+| ----------- |
+|![editorwin](https://raw.githubusercontent.com/AkshatChauhan18/FreezeUI/master/docs/assets/editor_window.png)|
---
+++ packages/FreezeUI-U/FreezeUI_U-0.0.5.tar.gz/FreezeUI_U-0.0.5/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+from FreezeUI_U import __version__ as ver
+
+DESCRIPTION = 'This a GUI for converting python scripts to applications using cx_Freeze.'
+LONG_DESCRIPTION = open("README.md", encoding="utf-8").read()
+
+setup(
+    name="FreezeUI_U",
+    version=ver,
+    author="Akshat Chauhan",
+    author_email="coder.akshatch@gmail.com",
+    license='GNU GPL v3',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    install_requires=["PyQt6", "cx_Freeze", "python_version <=3.6",
+                      "QtPy", "qtconsole", "pyqt6-qscintilla ", "plyer"],
+    packages=['FreezeUI_U'],
+    data_files=[('bitmaps', ['FreezeUI_U/assets/icon.gif','FreezeUI_U/assets/icon.ico','FreezeUI_U/assets/msi_text.png','FreezeUI_U/assets/exe_text.png','FreezeUI_U/assets/pyicon.svg']),("text files",["FreezeUI_U/assets/msi_template.txt","FreezeUI_U/assets/exe_template.txt"])],
+    include_package_data=True,
+    url="https://akshatchauhan18.github.io/FreezeUI",
+    keywords=['python', 'gui', 'cx_Freeze', 'FreezeUI'],
+    extras_require = {"dev":["pytest == 6.2.4"]},
+    classifiers=[
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'Operating System :: Microsoft :: Windows',
+    ],
+    entry_points={
+        "console_scripts": [
+            "freezeui-exe = FreezeUI_U.__main__:create_exe",
+            "freezeui-msi = FreezeUI_U.__main__:create_msi"
+        ],
+    }
+)
