c83f012ab5330a35253c12259e9b8671ba27c414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:15 +0000

    Add package nerdlerxml/1.0.1/nerdlerxml-1.0.1.tar.gz

---
+++ packages/nerdlerxml/nerdlerxml-1.0.1.tar.gz/nerdlerxml-1.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="nerdlerxml",
+    version="1.0.1",
+    author="Hendrik Koester",
+    description="Converts XML to a dictionary. Can take inputs from URL, a file and binary data",
+    long_description="Converts XML to a dictionary. Can take inputs from URL, a file and binary data",
+    url="https://github.com/nerdlertech/nerdlerxml",
+    packages=["nerdlerxml"]
+)
