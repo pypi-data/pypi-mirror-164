8690b5b82127a2d097058e096fc98ba032ab456e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:08 +0000

    Add package sgr-demo-v0.0.1/0.0.1/sgr_demo_v0.0.1-0.0.1.tar.gz

---
+++ packages/sgr-demo-v0.0.1/sgr_demo_v0.0.1-0.0.1.tar.gz/sgr_demo_v0.0.1-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'SGr demo'
+
+# Setting up
+setup(
+    name="sgr_demo_v0.0.1",
+    version=VERSION,
+    author="Zupeuc (CLEMAP)",
+    author_email="<daniel@clemap.ch>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'SGr'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
