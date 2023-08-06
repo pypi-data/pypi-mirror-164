0788ded3dd8ae69729cc6f94a25b1ca3150ef1d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:40 +0000

    Add package neoliqpay/2.1.0/neoliqpay-2.1.0.tar.gz

---
+++ packages/neoliqpay/neoliqpay-2.1.0.tar.gz/neoliqpay-2.1.0/neoliqpay/sync_liqpay.py
@@ -0,0 +1,41 @@
+import json
+from typing import Optional
+from urllib.parse import urljoin
+
+try:
+    import requests
+except ImportError:
+    requests = None
+
+from .core import LiqPayBase
+
+
+class LiqPay(LiqPayBase):
+    def __init__(
+        self,
+        public_key: str,
+        private_key: str,
+        host: Optional[str] = None,
+        sandbox: Optional[bool] = False
+    ):
+        if not requests:
+            raise NotImplementedError('requests module not found')
+
+        super(LiqPay, self).__init__(public_key, private_key, host, sandbox)
+
+    def api(
+        self, 
+        url: str,
+        params: Optional[dict] = None
+    ):
+        params = self._prepare_params(params or {})
+
+        json_encoded_params = json.dumps(params)
+        private_key = self._private_key
+        signature = self._make_signature(private_key, json_encoded_params, private_key)
+
+        request_url = urljoin(self._host, url)
+        request_data = {"data": json_encoded_params, "signature": signature}
+        with requests.sessions.Session() as session:
+            with session.post(request_url, data=request_data) as response:
+                return response.json()
