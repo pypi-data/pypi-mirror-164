a3361ffa7e3001e417cb0b670e1f6607886776ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:15 +0000

    Add package moleculekit/1.3.3/moleculekit-1.3.3.tar.gz

---
+++ packages/moleculekit/moleculekit-1.3.3.tar.gz/moleculekit-1.3.3/moleculekit/home.py
@@ -0,0 +1,72 @@
+# (c) 2015-2022 Acellera Ltd http://www.acellera.com
+# All Rights Reserved
+# Distributed under HTMD Software License Agreement
+# No redistribution in whole or part
+#
+import moleculekit
+import os
+import sys
+import inspect
+import platform
+
+
+def home(dataDir=None, libDir=False, shareDir=None):
+    """Return the pathname of the moleculekit root directory (or a data subdirectory).
+
+    Parameters
+    ----------
+    dataDir : str
+        If not None, return the path to a specific data directory
+    libDir : bool
+        If True, return path to the lib directory
+
+    Returns
+    -------
+    dir : str
+        The directory
+
+    Example
+    -------
+    >>> from moleculekit.home import home
+    >>> home()                                 # doctest: +ELLIPSIS
+    '.../moleculekit'
+    >>> home(dataDir="dhfr")                   # doctest: +ELLIPSIS
+    '.../data/dhfr'
+    >>> os.path.join(home(dataDir="dhfr"),"dhfr.pdb")  # doctest: +ELLIPSIS
+    '.../data/dhfr/dhfr.pdb'
+    """
+
+    homeDir = os.path.dirname(inspect.getfile(moleculekit))
+    try:
+        if sys._MEIPASS:
+            homeDir = sys._MEIPASS
+    except Exception:
+        pass
+
+    if dataDir:
+        return os.path.join(homeDir, "test-data", dataDir)
+    elif libDir:
+        if platform.machine() == "ppc64le":
+            libdir = os.path.join(homeDir, "lib", "ppc64le")
+        else:
+            libdir = os.path.join(homeDir, "lib", platform.system())
+
+        if not os.path.exists(libdir):
+            raise FileNotFoundError("Could not find libs.")
+        return libdir
+    elif shareDir:
+        sharedir = os.path.join(homeDir, "share")
+        if not os.path.exists(sharedir):
+            raise FileNotFoundError("Could not find moleculekit share directory.")
+        return os.path.join(sharedir, shareDir)
+    else:
+        return homeDir
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+
+    h = home()
+    print(h)
---
+++ packages/moleculekit/moleculekit-1.3.3.tar.gz/moleculekit-1.3.3/setup.py
@@ -0,0 +1,93 @@
+import setuptools
+import unittest
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import numpy
+import os
+import subprocess
+
+
+def my_test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover(start_dir="moleculekit", pattern="*.py")
+    return test_suite
+
+
+try:
+    version = (
+        subprocess.check_output(["git", "describe", "--abbrev=0", "--tags"])
+        .strip()
+        .decode("utf-8")
+    )
+except Exception:
+    print("Could not get version tag. Defaulting to version 0")
+    version = "0"
+
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+extentions = [
+    "moleculekit/interactions/hbonds/hbonds.pyx",
+    "moleculekit/interactions/pipi/pipi.pyx",
+    "moleculekit/interactions/cationpi/cationpi.pyx",
+    "moleculekit/interactions/sigmahole/sigmahole.pyx",
+    "moleculekit/wrapping/wrapping.pyx",
+    "moleculekit/bondguesser_utils/bondguesser_utils.pyx",
+    "moleculekit/atomselect_utils/atomselect_utils.pyx",
+]
+extentions = [
+    Extension(
+        name=os.path.dirname(ext).replace("/", "."),
+        sources=[ext],
+        include_dirs=[numpy.get_include()],
+        language="c++",
+        # extra_compile_args=["-fopenmp"],
+        # extra_link_args=["-fopenmp"],
+    )
+    for ext in extentions
+]
+# extra_compile_args = ["-O3",]
+# extra_compile_args=extra_compile_args
+# libraries=["examples"],
+# library_dirs=["lib"],
+
+if __name__ == "__main__":
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+    setuptools.setup(
+        name="moleculekit",
+        version=version,
+        author="Acellera",
+        author_email="info@acellera.com",
+        description="A molecule reading/writing and manipulation package.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/acellera/moleculekit/",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: POSIX :: Linux",
+        ],
+        packages=setuptools.find_packages(
+            include=["moleculekit*"],
+            exclude=[
+                "test-data",
+                "*test*",
+            ],
+        ),
+        package_data={
+            "moleculekit": [
+                "lib/*/*",
+                "vmd_wrapper",
+                "logging.ini",
+                "share/*/*/*",
+                "share/*/*",
+                "share/*",
+            ],
+        },
+        zip_safe=False,
+        test_suite="setup.my_test_suite",
+        install_requires=requirements,
+        ext_modules=cythonize(extentions, language_level="3"),
+    )
