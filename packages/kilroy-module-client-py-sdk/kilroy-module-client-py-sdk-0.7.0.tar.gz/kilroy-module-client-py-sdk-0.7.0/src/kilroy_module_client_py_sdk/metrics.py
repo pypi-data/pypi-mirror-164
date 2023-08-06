1e3537806a4200f42eff19a9812ab774a8233f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:19 +0000

    Add package kilroy-module-client-py-sdk/0.7.0/kilroy-module-client-py-sdk-0.7.0.tar.gz

---
+++ packages/kilroy-module-client-py-sdk/kilroy-module-client-py-sdk-0.7.0.tar.gz/kilroy-module-client-py-sdk-0.7.0/src/kilroy_module_client_py_sdk/metrics.py
@@ -0,0 +1,15 @@
+from typing import Any, Dict
+
+from kilroy_module_py_shared import SerializableModel
+
+
+class MetricConfig(SerializableModel):
+    id: str
+    label: str
+    config: Dict[str, Any]
+
+
+class MetricData(SerializableModel):
+    metric_id: str
+    dataset_id: int
+    data: Dict[str, Any]
