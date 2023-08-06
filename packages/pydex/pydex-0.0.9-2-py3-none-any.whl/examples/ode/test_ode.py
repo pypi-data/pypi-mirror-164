d27aecb7ce99a1bfd109de70cde78188a51c36ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:14 +0000

    Add package pydex/0.0.9/pydex-0.0.9-2-py3-none-any.whl

---
+++ packages/pydex/pydex-0.0.9-2-py3-none-any.whl/examples/ode/test_ode.py
@@ -0,0 +1,7 @@
+from examples.ode import case_1
+from examples.ode import case_2
+from examples.ode import case_3
+from examples.ode import case_4
+from examples.ode import case_5
+
+print("ODE tests are successful!")
