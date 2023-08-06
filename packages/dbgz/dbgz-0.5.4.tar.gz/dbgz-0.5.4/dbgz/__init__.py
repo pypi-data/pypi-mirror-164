fe6f50647f7a9bdaae5b3ae0cd6abc43f40873db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:02 +0000

    Add package dbgz/0.5.4/dbgz-0.5.4.tar.gz

---
+++ packages/dbgz/dbgz-0.5.4.tar.gz/dbgz-0.5.4/dbgz/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: <utf-8> -*-
+from .dbgz import DBGZWriter
+from .dbgz import DBGZReader
+from .dbgz import readIndicesDictionary
+
+__version__ = "0.5.4"
+
---
+++ packages/dbgz/dbgz-0.5.4.tar.gz/dbgz-0.5.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+   requirements = fh.readlines()
+
+setuptools.setup(
+    name="dbgz",
+    version="0.5.4",
+    author="Filipi N. Silva",
+    author_email="filipi@iu.edu",
+    description="Python library to load DBGZ files",
+    install_requires=[req for req in requirements if req[:2] != "# "],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/filipinascimento/dbgz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
