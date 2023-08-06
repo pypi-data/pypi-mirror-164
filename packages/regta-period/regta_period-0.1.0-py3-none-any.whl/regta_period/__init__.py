adf0722a5724a001da4135ce583850b0f97f2772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:23 +0000

    Add package regta-period/0.1.0/regta_period-0.1.0-py3-none-any.whl

---
+++ packages/regta-period/regta_period-0.1.0-py3-none-any.whl/regta_period/__init__.py
@@ -0,0 +1,10 @@
+from .periods import AbstractPeriod, Period, PeriodAggregation
+from .enums import Weekdays
+
+__version__ = '0.1.0'
+__all__ = [
+    "AbstractPeriod",
+    "Period",
+    "PeriodAggregation",
+    "Weekdays",
+]
