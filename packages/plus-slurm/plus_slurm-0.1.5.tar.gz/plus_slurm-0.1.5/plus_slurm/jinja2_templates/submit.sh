bf51b9392a7be747c77e49363a7c5840d0ce95df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:35 +0000

    Add package plus-slurm/0.1.5/plus_slurm-0.1.5.tar.gz

---
+++ packages/plus-slurm/plus_slurm-0.1.5.tar.gz/plus_slurm-0.1.5/plus_slurm/jinja2_templates/submit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+{% set log_dir = jobs_dir / "log" %}
+#SBATCH --cpus-per-task={{ request_cpus }}
+#SBATCH --mem={{ required_mem }}
+#SBATCH --array=1-{{ n_jobs }}
+#SBATCH --time={{ request_time }}
+#SBATCH --output={{ log_dir }}/out_%a.log
+
+{{ executable }} {{ arguments }}
