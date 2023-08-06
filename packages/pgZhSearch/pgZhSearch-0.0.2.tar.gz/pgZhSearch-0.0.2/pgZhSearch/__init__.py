873cb6f016288d68daa6f06e929324498aec80b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:41 +0000

    Add package pgZhSearch/0.0.2/pgZhSearch-0.0.2.tar.gz

---
+++ packages/pgZhSearch/pgZhSearch-0.0.2.tar.gz/pgZhSearch-0.0.2/pgZhSearch/__init__.py
@@ -0,0 +1,15 @@
+try:
+    from .base_search import *
+except Exception as e:
+    print(e)
+
+
+def version():
+    """
+    # 更新说明
+
+    - 初始化       # 0.0.1
+    - ReadMe出现bug       # 0.0.2
+    """
+    return '0.0.2'
+
